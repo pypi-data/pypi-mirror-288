274cb8fc9a8782254f756d2f71dd9e9458e6c83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:05 +0000

    Add package pydeepskylog/1.0.1/pydeepskylog-1.0.1.tar.gz

---
+++ packages/pydeepskylog/pydeepskylog-1.0.1.tar.gz/pydeepskylog-1.0.1/pydeepskylog/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__
+from .contrast_reserve import contrast_reserve, optimal_detection_magnification, surface_brightness
+
+__all__ = ["__version__", "contrast_reserve", "optimal_detection_magnification", "surface_brightness"]
