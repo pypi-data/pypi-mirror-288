9680599ce01d1a03a25ad96b6ad12085cdf41c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:01 +0000

    Add package wata/0.1.3.1/wata-0.1.3.1.tar.gz

---
+++ packages/wata/wata-0.1.3.1.tar.gz/wata-0.1.3.1/wata/display/display.py
@@ -0,0 +1,16 @@
+from wata.display.utils import utils
+
+class Display:
+    
+    @staticmethod
+    def print(content,type="r"):
+        '''
+
+        Args:
+            content: 内容
+            type: 类型支持 字母r g b y p 代表颜色,rr表示粗体,r_表示红色下划线,rx表示红色斜体 
+
+        Returns:
+            无
+        '''
+        return utils.wataprint(content,type)
