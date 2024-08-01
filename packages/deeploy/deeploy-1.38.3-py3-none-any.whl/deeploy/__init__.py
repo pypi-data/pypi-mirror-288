70fdfebbf5e0a94351d89345f5b4cd512b52a925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:09 +0000

    Add package deeploy/1.38.3/deeploy-1.38.3-py3-none-any.whl

---
+++ packages/deeploy/deeploy-1.38.3-py3-none-any.whl/deeploy/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+from ._version import __version__
+from .client import Client
+from .models import (
+    CreateActuals,
+    CreateAzureMLDeployment,
+    CreateDeployment,
+    CreateSageMakerDeployment,
+    CreateEvaluation,
+    CreateExplainerReference,
+    CreateMetadata,
+    CreateModelReference,
+    CreateTransformerReference,
+    UpdateSageMakerDeployment,
+    UpdateDeployment,
+    UpdateAzureMLDeployment,
+    UpdateDeploymentDescription,
+    BlobReference,
+    DockerReference,
+    MLFlowReference,
+    AzureMLReference,
+)
