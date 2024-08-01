8d5d8138581065150cfd98e888c696e5f4230ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:16 +0000

    Add package smartpasslib/0.5.0/smartpasslib-0.5.0-py3-none-any.whl

---
+++ packages/smartpasslib/smartpasslib-0.5.0-py3-none-any.whl/smartpasslib/__init__.py
@@ -0,0 +1,19 @@
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright © 2018-2024, A.A Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# https://github.com/smartlegionlab/
+# --------------------------------------------------------
+"""Smart Password Library - Cross-platform library for generating smart passwords."""
+from .generators import (
+    SmartPasswordMaster,
+    HashGenerator,
+    UrandomGenerator,
+    SmartKeyGenerator,
+    BasePasswordGenerator,
+    StrongPasswordGenerator,
+    SmartPasswordGenerator
+)
+__version__ = '0.5.0'
