fdc1b3ce5b19321882ff1c8fa61e2febfe7fd04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:33 +0000

    Add package junifer/0.0.6.dev3/junifer-0.0.6.dev3.tar.gz

---
+++ packages/junifer/junifer-0.0.6.dev3.tar.gz/junifer-0.0.6.dev3/junifer/onthefly/__init__.py
@@ -0,0 +1,9 @@
+"""Utilities for on-the-fly analyses."""
+
+# Authors: Synchon Mandal <s.mandal@fz-juelich.de>
+# License: AGPL
+
+from .read_transform import read_transform
+
+
+__all__ = ["read_transform"]
