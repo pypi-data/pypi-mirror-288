e245d2cb6b5db82b2ddafa6717e7f30d1ef604aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:10 +0000

    Add package toldwords/0.8.4/toldwords-0.8.4-py3-none-any.whl

---
+++ packages/toldwords/toldwords-0.8.4-py3-none-any.whl/toldwords/__init__.py
@@ -0,0 +1,24 @@
+''' __init__ '''
+__version__ = '0.8.4'
+from .openai import (Choice, Message, OpenAIAPI, RespCompletions, Role,
+                     TokenUsage)
+from .pretalx import Pretalx, PretalxResponse, Room, Speaker, Submission, Talk
+from .utils import DATA2022, DATA2023
+
+__all__ = [
+    "__version__",
+    "Choice",
+    "DATA2022",
+    "DATA2023",
+    "Message",
+    "OpenAIAPI",
+    "Pretalx",
+    "PretalxResponse",
+    "RespCompletions",
+    "Role",
+    "Room",
+    "Speaker",
+    "Submission",
+    "Talk",
+    "TokenUsage",
+]
