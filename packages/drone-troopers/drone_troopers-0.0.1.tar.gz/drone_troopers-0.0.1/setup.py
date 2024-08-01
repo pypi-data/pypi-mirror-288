a78a8d94ef2b69f43a935690658ea64a324ec69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:07 +0000

    Add package drone-troopers/0.0.1/drone_troopers-0.0.1.tar.gz

---
+++ packages/drone-troopers/drone_troopers-0.0.1.tar.gz/drone_troopers-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drone_troopers',
+    version='0.0.1',
+    author='Tech Troopers',
+    author_email='dhruvyadav2905@gmail.com',
+    description='A module for drone data processing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    # url='https://github.com/yourusername/drone_troopers',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scikit-learn',
+        'xgboost',
+        # Example: 'numpy>=1.18.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
