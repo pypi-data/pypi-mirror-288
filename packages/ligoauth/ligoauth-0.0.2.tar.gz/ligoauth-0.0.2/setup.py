de35be33df172e91c8802f36d6e9c2ea0f4cb632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:35 +0000

    Add package ligoauth/0.0.2/ligoauth-0.0.2.tar.gz

---
+++ packages/ligoauth/ligoauth-0.0.2.tar.gz/ligoauth-0.0.2/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='ligoauth',
+    version='0.0.2',
+    description='ligo upload mac sdk',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
