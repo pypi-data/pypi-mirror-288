036f5b133e5f64316b03afebb9d193f65525ee09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:07 +0000

    Add package djangorestframework-ext/0.27/djangorestframework_ext-0.27-py3-none-any.whl

---
+++ packages/djangorestframework-ext/djangorestframework_ext-0.27-py3-none-any.whl/rest_framework_ext/mixins.py
@@ -0,0 +1,13 @@
+from django.shortcuts import get_object_or_404
+
+
+class MultiFieldLookupMixin:
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        filter_kwargs = {}
+        for field in self.lookup_fields:
+            if self.kwargs.get(field, None):
+                filter_kwargs[field] = self.kwargs[field]
+        obj = get_object_or_404(queryset, **filter_kwargs)
+        self.check_object_permissions(self.request, obj)
+        return obj
---
+++ packages/djangorestframework-ext/djangorestframework_ext-0.27-py3-none-any.whl/rest_framework_ext/permissions.py
@@ -0,0 +1,53 @@
+from rest_framework import permissions
+
+
+class DjangoModelPermissions(permissions.DjangoModelPermissions):
+    perms_map = {
+        **permissions.DjangoModelPermissions.perms_map,
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': ['%(app_label)s.view_%(model_name)s'],
+        'HEAD': ['%(app_label)s.view_%(model_name)s']
+    }
+
+
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsCurrentUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+
+class IsSuperuser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
+
+
+class HasPermission(permissions.BasePermission):
+    permission_codename = ''
+
+    def __init__(self, permission_codename):
+        super().__init__()
+        self.permission_codename = permission_codename
+
+    def __call__(self):
+        return self
+
+    def has_permission(self, request, view):
+        return request.user.has_perm(self.permission_codename)
+
+
+class ExportPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'export':
+            model = view.get_queryset().model
+            kwargs = {
+                'app_label': model._meta.app_label,
+                'model_name': model._meta.model_name
+            }
+
+            return request.user.has_perm('%(app_label)s.export_%(model_name)s' % kwargs)
+        else:
+            return False
