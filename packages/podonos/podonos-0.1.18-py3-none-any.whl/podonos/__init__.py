1d9f754f19c7c527400ae7dca9d71d0cb733417f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:11 +0000

    Add package podonos/0.1.18/podonos-0.1.18-py3-none-any.whl

---
+++ packages/podonos/podonos-0.1.18-py3-none-any.whl/podonos/__init__.py
@@ -0,0 +1,6 @@
+from .core.file import File
+from .sdk import Podonos, Client
+
+__version__ = "0.1.18"
+
+init = Podonos.init
---
+++ packages/podonos/podonos-0.1.18-py3-none-any.whl/podonos/common/constant.py
@@ -0,0 +1,30 @@
+"""
+Default constant values across whole SDK
+"""
+
+
+# Text colors on terminal
+class TerminalColor:
+    BOLD = '\033[1m'
+    ENDC = '\033[0m'
+    FAIL = '\033[91m'
+    OK = '\033[92m'
+    WARN = '\033[93m'
+
+# Podonos homepage
+PODONOS_HOME = 'https://www.podonos.com/'
+
+# Podonos docs API key
+PODONOS_DOCS_API_KEY = 'https://docs.podonos.com/apikey'
+
+# API key environment variable
+PODONOS_API_KEY = 'PODONOS_API_KEY'
+
+# Podonos Workspace
+PODONOS_WORKSPACE = "https://workspace.podonos.com"
+
+# Podonos API base URL
+PODONOS_API_BASE_URL = "https://prod.podonosapi.com"
+
+# Contact email
+PODONOS_CONTACT_EMAIL = "hello@podonos.com"
