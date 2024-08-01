7e456b3a62687f20ed739f27b6195c2dc446b437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:12 +0000

    Add package django-prose-editor/0.6.3/django_prose_editor-0.6.3.tar.gz

---
+++ packages/django-prose-editor/django_prose_editor-0.6.3.tar.gz/django_prose_editor-0.6.3/django_prose_editor/widgets.py
@@ -0,0 +1,40 @@
+import json
+
+from django import forms
+
+
+class ProseEditorWidget(forms.Textarea):
+    class Media:
+        css = {
+            "screen": [
+                "django_prose_editor/material-icons.css",
+                "django_prose_editor/editor.css",
+            ]
+        }
+        js = [
+            "django_prose_editor/editor.js",
+            "django_prose_editor/init.js",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        self.config = kwargs.pop("config", {})
+        super().__init__(*args, **kwargs)
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context["widget"]["attrs"]["data-django-prose-editor"] = json.dumps(
+            self.config
+            or {
+                "types": None,
+                "history": True,
+                "html": True,
+                "typographic": True,
+            },
+            separators=(",", ":"),
+        )
+        return context
+
+
+class AdminProseEditorWidget(ProseEditorWidget):
+    class Media:
+        css = {"screen": ["django_prose_editor/overrides.css"]}
