d88a33c05064ee2cf80061161ea382eeb58fe264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:12 +0000

    Add package netspresso/1.10.0b7/netspresso-1.10.0b7-py3-none-any.whl

---
+++ packages/netspresso/netspresso-1.10.0b7-py3-none-any.whl/netspresso/clients/launcher/v2/schemas/__init__.py
@@ -0,0 +1,37 @@
+from .common import (
+    AuthorizationHeader,
+    DeviceInfo,
+    ModelOption,
+    RequestPagination,
+    ResponseItem,
+    ResponseItems,
+    ResponsePaginationItems,
+    TaskInfo,
+    TaskOption,
+    UploadFile,
+)
+from .model import (
+    InputLayer,
+    ModelBase,
+    RequestModelUploadUrl,
+    RequestUploadModel,
+    RequestValidateModel,
+    ResponseModelItem,
+    ResponseModelItems,
+    ResponseModelOptions,
+    ResponseModelStatus,
+    ResponseModelUploadUrl,
+)
+from .task import (
+    RequestBenchmark,
+    RequestConvert,
+    ResponseBenchmarkFrameworkOptionItems,
+    ResponseBenchmarkOptionItems,
+    ResponseBenchmarkStatusItem,
+    ResponseBenchmarkTaskItem,
+    ResponseConvertDownloadModelUrlItem,
+    ResponseConvertFrameworkOptionItems,
+    ResponseConvertOptionItems,
+    ResponseConvertStatusItem,
+    ResponseConvertTaskItem,
+)
