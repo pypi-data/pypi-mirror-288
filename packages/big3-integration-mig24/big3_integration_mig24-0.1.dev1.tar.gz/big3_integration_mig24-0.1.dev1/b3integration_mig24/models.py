44e124b1b9ea4ac054c62af479bfd09a7bbd814f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:02:43 +0000

    Add package big3-integration-mig24/0.1.dev1/big3_integration_mig24-0.1.dev1.tar.gz

---
+++ packages/big3-integration-mig24/big3_integration_mig24-0.1.dev1.tar.gz/big3_integration_mig24-0.1.dev1/b3integration_mig24/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from integrations_market.models import AbstractSettingsSingleton
+
+
+class Settings(AbstractSettingsSingleton):
+
+    class Meta:
+        verbose_name = "Настройки"
+        verbose_name_plural = "Настройки"
+
+    def __str__(self):
+        return '<<App settings "b3integration_mig24">>'
