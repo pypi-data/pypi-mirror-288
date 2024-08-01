b4e2257f3a624eb9561740c9c4485c0c031f26a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:04:10 +0000

    Add package pyaesthetics/0.0.8.0/pyaesthetics-0.0.8.0-py3-none-any.whl

---
+++ packages/pyaesthetics/pyaesthetics-0.0.8.0-py3-none-any.whl/pyaesthetics/__init__.py
@@ -0,0 +1,12 @@
+from . import quadTreeDecomposition
+from . import spaceBasedDecomposition
+from . import colorfulness
+from . import brightness
+from . import symmetry
+from . import faceDetection
+from . import analysis
+from . import contrast
+from . import saturation
+
+# This is used to print out the package version
+__version__ = '0.0.7.8' #Version Control
