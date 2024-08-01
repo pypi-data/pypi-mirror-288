5d10d5706e91a464332a09ef0b62467f976fa7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:02:58 +0000

    Add package locobuzz-kafka-protobuf/0.1.1/locobuzz_kafka_protobuf-0.1.1-py3-none-any.whl

---
+++ packages/locobuzz-kafka-protobuf/locobuzz_kafka_protobuf-0.1.1-py3-none-any.whl/kafka_protobuf/__init__.py
@@ -0,0 +1,8 @@
+# # kafka_protobuf/__init__.py
+#
+# from .producer import produce_protobuf_messages
+# from .consumer import create_consumer, consume_messages
+# from .utils import (
+#     load_proto_module, create_message, set_message_fields,
+#     serialize_message, deserialize_message
+# )
