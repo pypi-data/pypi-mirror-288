199ae2f8e469cadd0fddb32907039cee1af011b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:02:56 +0000

    Add package picwish/0.3.1/picwish-0.3.1.tar.gz

---
+++ packages/picwish/picwish-0.3.1.tar.gz/picwish-0.3.1/picwish/__init__.py
@@ -0,0 +1,10 @@
+import asyncio
+import os
+
+if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+from .image_models import BackgroundRemovedImage, EnhancedImage
+from .main import EnhancedImage, PicWish, PicwishError
+
+__version__ = '0.3.1'
