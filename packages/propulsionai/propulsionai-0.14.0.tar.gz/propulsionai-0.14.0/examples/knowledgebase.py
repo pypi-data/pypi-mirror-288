d2e08cf292bc1d6d1b09e96e9a0520bad6f12fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:27 +0000

    Add package propulsionai/0.14.0/propulsionai-0.14.0.tar.gz

---
+++ packages/propulsionai/propulsionai-0.14.0.tar.gz/propulsionai-0.14.0/examples/knowledgebase.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+from propulsionai import PropulsionAI
+
+client = PropulsionAI(
+    # This is the default and can be omitted
+    bearer_token=os.environ.get("PROPULSIONAI_BEARER_TOKEN"),
+)
+
+
+def sync_main() -> None:
+    knowledgebase = client.knowledgebase.create(name="weather", description="A knowledgebase for weather", tags="uppto")
+    print(f"Knowledgebase created with id: ", knowledgebase.code)
+    # read ./12025248.pdf and convert it to bytes
+    file_path = Path("examples/12025248.pdf")
+    if knowledgebase.code:
+        with open(file_path, "rb") as f:
+            file_bytes = f.read()
+            upload_file_response = client.knowledgebase.file.upload(
+                knowledgebase_code=knowledgebase.code, file=file_bytes
+            )
+            print(f"File uploaded with id: ", upload_file_response.id)
+        if upload_file_response.id:
+            # delete the file
+            delete_file_response = client.knowledgebase.file.delete(
+                knowledgebase_code=knowledgebase.code, file_id=upload_file_response.id
+            )
+            print(f"File deleted with id: ", delete_file_response.id)
+
+
+sync_main()
