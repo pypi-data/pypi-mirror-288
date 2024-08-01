c72e3d913e722589fd4324aac40865710b25b9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:02:45 +0000

    Add package deepfolio/1.2.0/deepfolio-1.2.0.tar.gz

---
+++ packages/deepfolio/deepfolio-1.2.0.tar.gz/deepfolio-1.2.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='deepfolio',
+    version='1.2.0',
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    url='https://github.com/jialuechen/deepfolio',
+    license='BSD-2',
+    author='Jialue Chen',
+    author_email='jialuechen@outlook.com',
+    description='Python Library for Portfolio Optimization Built on top of Deep Learning'
+)
