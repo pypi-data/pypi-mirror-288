9e3645f8e8f550ed1a9c23abacc481b28b918e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:08 +0000

    Add package robotframework-xlibrary/12.0.3/robotframework-xlibrary-12.0.3.tar.gz

---
+++ packages/robotframework-xlibrary/robotframework-xlibrary-12.0.3.tar.gz/robotframework-xlibrary-12.0.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="robotframework-xlibrary",
+    version="12.0.3",
+    author="Tassana Khrueawan",
+    author_email="tassana.khr@gmail.com",
+    description="Library Custom For Automate",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Khrx1999/robotframework-xlibrary.git",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'robotframework>=6.1.1',
+        'selenium>=3.141.0',
+        'requests>=2.25.1',
+        'pymongo>=4.7.2',
+        'bson>=0.5.10',
+        'appium-python-client>=1.0.2',
+        'robotframework-appiumlibrary>=1.5.0',
+        'robotframework-requests>=0.7.0',
+        'robotframework-appiumflutterlibrary>=1.0.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Framework :: Robot Framework',
+    ],
+    python_requires='>=3.6',
+)
