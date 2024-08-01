f2a403b1ad2cc4247261b36d2bb6e6793d7902cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:53 +0000

    Add package mongo-update/1.0.8/mongo_update-1.0.8.tar.gz

---
+++ packages/mongo-update/mongo_update-1.0.8.tar.gz/mongo_update-1.0.8/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+# import setuptools
+
+packages = ['mongo_update']
+setup(name='mongo_update',
+      version='1.0.8',
+      author='xigua, ',
+      author_email="2587125111@qq.com",
+      long_description='''
+      mongodb数据更新。
+      ''',
+      packages=packages,
+      package_dir={'requests': 'requests'},
+      license="MIT",
+      python_requires='>=2.7',
+      )
