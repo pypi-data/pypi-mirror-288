6f80477aa44d56d4362056f2acd632927479f04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:10 +0000

    Add package memoryscope/0.1.0.4/memoryscope-0.1.0.4-py3-none-any.whl

---
+++ packages/memoryscope/memoryscope-0.1.0.4-py3-none-any.whl/memoryscope/__init__.py
@@ -0,0 +1,11 @@
+""" Version of MemoryScope."""
+__version__ = "0.1.0.4"
+
+import fire
+
+from memoryscope.core.config.arguments import Arguments  # noqa: F401
+from memoryscope.core.memoryscope import MemoryScope  # noqa: F401
+
+
+def cli():
+    fire.Fire(MemoryScope.cli_memory_chat)
