43d4dbb85d269bc4e220b3e950fd31c97727b6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:16 +0000

    Add package Annotations2Sub/2.18.0/annotations2sub-2.18.0.tar.gz

---
+++ packages/Annotations2Sub/annotations2sub-2.18.0.tar.gz/annotations2sub-2.18.0/src/Annotations2Sub/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""在 src 目录运行 python __main__.py 或 python -m Annotations2Sub"""
+
+if not __package__:
+    import os
+    import sys
+
+    path = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])
+    sys.path.append(path)
+    path = os.path.abspath(os.path.join(path, os.pardir))
+    sys.path.append(path)
+
+if __name__ == "__main__":
+    from Annotations2Sub.main import main
+
+    main()
---
+++ packages/Annotations2Sub/annotations2sub-2.18.0.tar.gz/annotations2sub-2.18.0/src/Annotations2Sub/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+""" 程序入口 """
+
+import sys
+from typing import NoReturn
+
+from Annotations2Sub.cli import Run
+
+
+def main() -> NoReturn:
+    sys.exit(Run())
