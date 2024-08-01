effabe50cd3245d39088f5b6ab87fcb5a4fa14a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:06 +0000

    Add package aioyoomoney-api/0.0.3/aioyoomoney_api-0.0.3.tar.gz

---
+++ packages/aioyoomoney-api/aioyoomoney_api-0.0.3.tar.gz/aioyoomoney_api-0.0.3/aioyoomoney/base/context_method.py
@@ -0,0 +1,25 @@
+from os.path import join
+
+from .context_session import ContextSession
+from ..globals import API_URL
+
+
+class ContextMethod(ContextSession):
+    def __init__(self, token: str, method: str, http_method: str = "POST", **kwargs):
+        self.token = token
+        self.headers = {
+            'Authorization': 'Bearer ' + self.token,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        super().__init__(
+            url=join(API_URL, method),
+            method=http_method,
+            headers=self.headers,
+            **kwargs
+        )
+
+    async def __aenter__(self) -> dict:
+        response = await super().__aenter__()
+
+        return await response.json()
