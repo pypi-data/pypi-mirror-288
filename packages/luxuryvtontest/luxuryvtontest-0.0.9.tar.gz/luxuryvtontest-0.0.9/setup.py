392bcd0c1916fa9fa2c3735abed10a5e8b7ac318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 14:03:14 +0000

    Add package luxuryvtontest/0.0.9/luxuryvtontest-0.0.9.tar.gz

---
+++ packages/luxuryvtontest/luxuryvtontest-0.0.9.tar.gz/luxuryvtontest-0.0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="luxuryvtontest",
+    version="0.0.9",
+    description="This is a test for my python package upload to pypi",
+    author="powerjsv",
+    author_email="powerjsv12@gmail.com",
+    url="https://github.com/powerjsv/jsv_package_test",
+    install_requires=[
+        "tqdm",
+        "pandas",
+        "scikit-learn",
+    ],
+    packages=find_packages(exclude=[]),
+    keywords=["vton", "powerjsv", "toy project", "pypi"],
+    python_requires=">=3.6",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
