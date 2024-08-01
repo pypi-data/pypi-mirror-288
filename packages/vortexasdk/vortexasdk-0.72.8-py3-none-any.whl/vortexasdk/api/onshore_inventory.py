dc46ad7a3c89af116912c3327899773f6c85314d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-01 15:03:47 +0000

    Add package vortexasdk/0.72.8/vortexasdk-0.72.8-py3-none-any.whl

---
+++ packages/vortexasdk/vortexasdk-0.72.8-py3-none-any.whl/vortexasdk/api/onshore_inventory.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+
+from vortexasdk.api.asset_tank import AssetTank
+
+from vortexasdk.api.shared_types import ISODate
+from vortexasdk.api.id import ID
+
+
+class OnshoreInventory(BaseModel):
+    """
+
+    Land Storage measurements are the base data set the Vortexa API is centred around.
+
+    Each measurement represents the total capacity and current amount being stored at each location.
+
+    [Land Storage Further Documentation](https://docs.vortexa.com/reference/intro-land-storage)
+
+    """
+
+    measurement_id: Optional[ID] = None
+    tank_id: Optional[ID] = None
+    tank_details: Optional[AssetTank] = None
+    measurement_timestamp: Optional[ISODate] = None
+    publish_timestamp: Optional[ISODate] = None
+    fill_bbl: Optional[int] = None
+    fill_tons: Optional[float] = None
+    fill_cbm: Optional[float] = None
+    reference_data_version: Optional[str] = None
