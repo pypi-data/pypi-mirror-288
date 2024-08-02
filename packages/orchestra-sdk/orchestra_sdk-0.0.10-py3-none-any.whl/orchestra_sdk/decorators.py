96bdbd2c1bf6f034e55f6c4a5708ee6915bb8b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:03:33 +0000

    Add package orchestra-sdk/0.0.10/orchestra_sdk-0.0.10-py3-none-any.whl

---
+++ packages/orchestra-sdk/orchestra_sdk-0.0.10-py3-none-any.whl/orchestra_sdk/decorators.py
@@ -0,0 +1,71 @@
+import logging
+import os
+import uuid
+
+from src.orchestra_sdk.errors import (
+    MissingEnvironmentKeysError,
+    NoEnvironmentVariablesFoundError,
+)
+from src.orchestra_sdk.models import TaskRunWebhookModel
+from src.orchestra_sdk.task_updates import (
+    send_failed_status,
+    send_running_status,
+    send_success_status,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def _check_env_vars(env_keys: list[str], env_vars: os._Environ[str] | None) -> None:
+    if env_vars is None or len(env_vars) == 0:
+        raise NoEnvironmentVariablesFoundError
+
+    missing_keys = set()
+
+    for key in env_keys:
+        if key not in env_vars:
+            missing_keys.add(key)
+
+    if missing_keys:
+        raise MissingEnvironmentKeysError(missing_keys)
+
+
+def orchestra_run(func):
+    def wrapper(*args, **kwargs):
+        env_keys = [
+            "orchestra_api_key",
+            "orchestra_pipeline_run_id",
+            "orchestra_task_run_id",
+            "orchestra_webhook_url",
+        ]
+        env_vars = None
+        task_run_webhook = None
+
+        try:
+            # Read webhook_url, task_run_id, pipeline_run_id from the environment
+            env_vars = os.environ
+            # Validate the environment variables
+            _check_env_vars(env_keys, env_vars)
+            # Create the model
+            task_run_webhook = TaskRunWebhookModel(
+                api_key=env_vars["orchestra_api_key"],
+                webhook_url=env_vars["orchestra_webhook_url"],
+                task_run_id=uuid.UUID(env_vars["orchestra_task_run_id"]),
+                pipeline_run_id=uuid.UUID(env_vars["orchestra_pipeline_run_id"]),
+            )
+        except MissingEnvironmentKeysError as e:
+            logger.error(f"Missing environment variables: {', '.join(e.missing_keys)}")
+        except NoEnvironmentVariablesFoundError:
+            logger.error("No environment variables loaded")
+        except Exception as e:
+            logger.error(f"Error processing environment variables: {e}")
+
+        send_running_status(task_run_webhook, func.__name__)
+
+        try:
+            func(*args, **kwargs)
+            send_success_status(task_run_webhook, func.__name__)
+        except Exception as failed_func_err:
+            send_failed_status(task_run_webhook, func.__name__, failed_func_err)
+
+    return wrapper
---
+++ packages/orchestra-sdk/orchestra_sdk-0.0.10-py3-none-any.whl/orchestra_sdk/http.py
@@ -0,0 +1,62 @@
+import logging
+from datetime import datetime
+from typing import Any, Literal
+from uuid import UUID
+
+import requests
+from tenacity import retry, stop_after_attempt, wait_exponential
+
+from src.orchestra_sdk.enum import WebhookEventType
+
+logger = logging.getLogger(__name__)
+
+
+def _get_response_json(r: requests.Response) -> dict | None:
+    try:
+        return r.json()
+    except Exception as e:
+        logger.warning(f"Could not get JSON response from request: {e}")
+        return None
+
+
+@retry(
+    reraise=True,
+    stop=stop_after_attempt(5),
+    wait=wait_exponential(multiplier=3, min=3, max=15),
+)
+def _make_request(
+    json: dict[str, Any], headers: dict[str, str], method: Literal["POST"], url: str
+) -> requests.Response:
+    r = requests.request(json=json, method=method, url=url, headers=headers)
+    r.raise_for_status()
+    return r
+
+
+def orchestra_http_request(
+    url: str,
+    api_key: str,
+    event_type: WebhookEventType,
+    task_run_id: UUID,
+    metadata: dict[str, Any] = {},
+    json: dict[str, Any] = {},
+    method: Literal["POST"] = "POST",
+):
+    try:
+        logger.info(f"Sending request to {url}...")
+        r = _make_request(
+            json={
+                "event_type": event_type.value,
+                "metadata": metadata,
+                "task_run_id": str(task_run_id),
+                "timestamp": datetime.now().isoformat(),
+                **json,
+            },
+            headers={"Authorization": f"Bearer {api_key}"},
+            method=method,
+            url=url,
+        )
+        logger.info(f"Response ({r.status_code}): {_get_response_json(r)}")
+    except requests.exceptions.HTTPError as e:
+        logger.error(f"Failed request ({e.response.status_code}): {_get_response_json(e.response)}")
+    except Exception as e:
+        logger.error(f"Could not send {method} request to {url} with JSON: {json} - {e}")
