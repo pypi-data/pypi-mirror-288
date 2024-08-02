ea0c0691a0a16f144ad35e89e4b0cb8aeabc4218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:04:27 +0000

    Add package JapaneseMatplotlib/1.0.4/japanesematplotlib-1.0.4.tar.gz

---
+++ packages/JapaneseMatplotlib/japanesematplotlib-1.0.4.tar.gz/japanesematplotlib-1.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="JapaneseMatplotlib",
+    version="1.0.4",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+    ],
+    author="kiendt",
+    author_email="kiendt226@gmail.com",
+    description="A package to support Japanese fonts for Matplotlib",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    include_package_data=True,
+    package_data={
+        'JapaneseMatplotlib': ['fonts/*'],
+    }
+)
