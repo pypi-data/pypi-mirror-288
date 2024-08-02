7b611cd6fac866afbf5046e26956e8c5e747445a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:02:58 +0000

    Add package BorgTestGenerator/0.3.3/BorgTestGenerator-0.3.3-py3-none-any.whl

---
+++ packages/BorgTestGenerator/BorgTestGenerator-0.3.3-py3-none-any.whl/BorgTestGenerator/utils/files.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+# import pkg_resources
+from os import path
+
+def read_text_file(file_path: str) -> str|None:
+    """
+    Read the contents of a text file.
+
+    Args:
+        file_path (str): The path to the text file.
+
+    Returns:
+        str|None: The contents of the text file as a string, or None if the file does not exist.
+    """
+    if not path.exists(file_path):
+        raise FileNotFoundError(f"Le fichier {file_path} est manquant")
+    
+    with open(file_path, 'r') as in_file:
+        return in_file.read()
