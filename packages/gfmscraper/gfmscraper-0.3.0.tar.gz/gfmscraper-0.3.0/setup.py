cabb8a17119eab0baf1fa779851e154521666e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 07:04:42 +0000

    Add package gfmscraper/0.3.0/gfmscraper-0.3.0.tar.gz

---
+++ packages/gfmscraper/gfmscraper-0.3.0.tar.gz/gfmscraper-0.3.0/setup.py
@@ -0,0 +1,46 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="gfmscraper",
+    version="0.3.0",
+    description="A small library to scrap GoFundMe campaigns.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PalestinianVoices/GoFundMeScrapper",
+    author="Ziad A. & Essam W.",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["gfmscraper"],
+    include_package_data=True,
+    install_requires=["requests", "bs4"],
+)
+
+# update:
+# pip install wheel
+# pip install twine
+# python setup.py sdist bdist_wheel     
+# twine check dist/*
+# twine upload dist/*
