4355061165d2615e338fa322958865524018e820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:11 +0000

    Add package tuma-ai/1.0/tuma-ai-1.0.tar.gz

---
+++ packages/tuma-ai/tuma-ai-1.0.tar.gz/tuma-ai-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tuma-ai",
+    version="1.0",
+    author="Tangyuqi",
+    url='https://gitee.com/tangyuqi-code/tuma-ai',
+    author_email="644853320@qq.com",
+    description="适合编程教学学习使用的AI库",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
