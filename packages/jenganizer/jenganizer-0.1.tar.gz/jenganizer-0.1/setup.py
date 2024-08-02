5bfa7fdd6addf036fe0ccda51a8d941a4fd2b7ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 04:03:05 +0000

    Add package jenganizer/0.1/jenganizer-0.1.tar.gz

---
+++ packages/jenganizer/jenganizer-0.1.tar.gz/jenganizer-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jenganizer",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "boto3",
+        "click",
+        "click-log",
+        "colorlog"
+    ],
+    entry_points={
+        "console_scripts": [
+            "jenganizer = jenganizer.jenganizer:cli",
+        ],
+    },
+)
