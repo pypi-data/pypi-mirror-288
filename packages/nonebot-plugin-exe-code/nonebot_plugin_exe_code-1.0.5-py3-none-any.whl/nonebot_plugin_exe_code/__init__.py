a0e9cdf0e4c28f018fdf5f50d2aeb8efcfa83e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 04:03:26 +0000

    Add package nonebot-plugin-exe-code/1.0.5/nonebot_plugin_exe_code-1.0.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-exe-code/nonebot_plugin_exe_code-1.0.5-py3-none-any.whl/nonebot_plugin_exe_code/__init__.py
@@ -0,0 +1,30 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+from nonebot.plugin.load import inherit_supported_adapters
+
+requirements = {
+    "nonebot_plugin_alconna",
+    "nonebot_plugin_datastore",
+    "nonebot_plugin_session",
+    "nonebot_plugin_userinfo",
+    "nonebot_plugin_waiter",
+}
+[require(i) for i in requirements]
+
+from . import matchers as _
+from .config import Config
+
+__version__ = "1.0.5"
+__plugin_meta__ = PluginMetadata(
+    name="exe_code",
+    description="在对话中执行 Python 代码",
+    usage="code {Your code here...}",
+    type="application",
+    homepage="https://github.com/wyf7685/nonebot-plugin-exe-code",
+    config=Config,
+    supported_adapters=inherit_supported_adapters(*requirements),
+    extra={
+        "author": "wyf7685",
+        "version": __version__,
+    },
+)
