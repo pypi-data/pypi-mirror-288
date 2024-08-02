470009b7e05698ec5aed6ce59d26161cf7d98402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:40 +0000

    Add package vtexpy/0.0.0a20/vtexpy-0.0.0a20.tar.gz

---
+++ packages/vtexpy/vtexpy-0.0.0a20.tar.gz/vtexpy-0.0.0a20/vtex/_api/__init__.py
@@ -0,0 +1,6 @@
+from .catalog import CatalogAPI
+from .custom import CustomAPI
+from .license_manager import LicenseManagerAPI
+from .logistics import LogisticsAPI
+from .orders import OrdersAPI
+from .transactions import TransactionsAPI
