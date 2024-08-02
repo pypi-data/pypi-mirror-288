94cb1b50dbdeb340de795dfb2656d6ffafb7063a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:29 +0000

    Add package talking-equipment-sdk/0.0.16/talking_equipment_sdk-0.0.16-py3-none-any.whl

---
+++ packages/talking-equipment-sdk/talking_equipment_sdk-0.0.16-py3-none-any.whl/talking_equipment/data/total_harmonic_distortion/data.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from typing import Union
+
+from ..data import Data
+from ..enums import DataValueType
+
+
+@dataclass
+class TotalHarmonicDistortionData(Data):
+    value: Union[float, int]
+    value_type: DataValueType = DataValueType.FLOAT
+    unit: str = 'Total Harmonic Distortion'
+    unit_abbreviation: str = 'THD'
+
+    def __post_init__(self):
+        self.value = float(self.value)
+
+
+@dataclass(kw_only=True)
+class ThreePhaseTotalHarmonicDistortionData(Data):
+    a: Union[TotalHarmonicDistortionData, float, int]
+    b: Union[TotalHarmonicDistortionData, float, int]
+    c: Union[TotalHarmonicDistortionData, float, int]
+
+    def __post_init__(self):
+        self.a = TotalHarmonicDistortionData(self.a) if not isinstance(self.a, TotalHarmonicDistortionData) else self.a
+        self.b = TotalHarmonicDistortionData(self.b) if not isinstance(self.b, TotalHarmonicDistortionData) else self.b
+        self.c = TotalHarmonicDistortionData(self.c) if not isinstance(self.c, TotalHarmonicDistortionData) else self.c
