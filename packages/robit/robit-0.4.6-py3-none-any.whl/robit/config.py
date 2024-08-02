8ca8c24d291b6a09b9f4298c689e0cd4f3a8fd59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 07:04:42 +0000

    Add package robit/0.4.6/robit-0.4.6-py3-none-any.whl

---
+++ packages/robit/robit-0.4.6-py3-none-any.whl/robit/config.py
@@ -0,0 +1,11 @@
+class Config:
+    VERSION: str = '0.4.5'
+    TIMEZONE: str = 'UTC'
+    LOG_FILE_NAME: str = 'robit'
+    LOG_BACKUP_DAYS: int = 7
+    DATABASE_LOGGING: bool = False
+    DATABASE_FILE_NAME: str = 'robit'
+    DATABASE_BACKUP_DAYS: int = 21
+
+
+config = Config()
