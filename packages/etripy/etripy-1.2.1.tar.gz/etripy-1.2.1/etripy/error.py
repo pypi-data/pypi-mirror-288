6f49b1d0a42c2e332b790453c837159ffcdea4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 15:04:12 +0000

    Add package etripy/1.2.1/etripy-1.2.1.tar.gz

---
+++ packages/etripy/etripy-1.2.1.tar.gz/etripy-1.2.1/etripy/error.py
@@ -0,0 +1,26 @@
+class HTTPException(Exception):
+    pass
+
+
+class SentencesException(Exception):
+    pass
+
+
+class QAException(Exception):
+    pass
+
+
+class AnalysisException(Exception):
+    pass
+
+
+class AudioFileException(Exception):
+    pass
+
+
+class VoiceRecognitionException(Exception):
+    pass
+
+
+class VoicePronunciationException(Exception):
+    pass
