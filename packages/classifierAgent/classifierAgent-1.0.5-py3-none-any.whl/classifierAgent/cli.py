f464d10c15c508f95ad3b9e2f97c160bfb5823a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:04:35 +0000

    Add package classifierAgent/1.0.5/classifierAgent-1.0.5-py3-none-any.whl

---
+++ packages/classifierAgent/classifierAgent-1.0.5-py3-none-any.whl/classifierAgent/cli.py
@@ -0,0 +1,14 @@
+# classifierAgent/cli.py
+__author__ = "Adnan Karol"
+__version__ = "1.0.4"
+__maintainer__ = "Adnan Karol"
+__email__ = "adnanmushtaq5@gmail.com"
+__status__ = "PROD"
+
+from classifierAgent import classifierAgent
+
+def main():
+    classifierAgent()
+
+if __name__ == "__main__":
+    main()
