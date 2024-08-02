ca1815c2f5e719e3d2649ae4486477857f09987f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:36 +0000

    Add package xurpas-data-quality/1.4.12/xurpas_data_quality-1.4.12-py3-none-any.whl

---
+++ packages/xurpas-data-quality/xurpas_data_quality-1.4.12-py3-none-any.whl/xurpas_data_quality/config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from typing import List
+
+class Colors(BaseModel):
+    base_colors: List[str] = ['#17A2B8', "#28A745", "#0D6EFD", "#DC3545", "#FFC107"]
+
+class Settings(BaseSettings):
+    colors: Colors = Colors()
+    
