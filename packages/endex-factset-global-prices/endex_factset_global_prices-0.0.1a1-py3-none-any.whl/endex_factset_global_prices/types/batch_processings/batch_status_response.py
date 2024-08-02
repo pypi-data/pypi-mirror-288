591d12ca3c2607a0693b8b6c7a5755542a537dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:35 +0000

    Add package endex-factset-global-prices/0.0.1a1/endex_factset_global_prices-0.0.1a1-py3-none-any.whl

---
+++ packages/endex-factset-global-prices/endex_factset_global_prices-0.0.1a1-py3-none-any.whl/endex_factset_global_prices/types/batch_processings/batch_status_response.py
@@ -0,0 +1,57 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from pydantic import Field as FieldInfo
+
+from ..._models import BaseModel
+
+__all__ = ["BatchStatusResponse", "Data", "DataError", "DataErrorLinks"]
+
+
+class DataErrorLinks(BaseModel):
+    about: Optional[str] = None
+    """details."""
+
+
+class DataError(BaseModel):
+    id: Optional[str] = None
+    """A UUID for this particular occurrence of the problem."""
+
+    code: Optional[str] = None
+    """status"""
+
+    links: Optional[DataErrorLinks] = None
+    """The Endpoint path {package}/version/{endpoint}"""
+
+    title: Optional[str] = None
+    """The plain text error message"""
+
+
+class Data(BaseModel):
+    id: Optional[str] = None
+    """the id of batch request."""
+
+    end_time: Optional[datetime] = FieldInfo(alias="endTime", default=None)
+    """Time when the batch request is ended.
+
+    This is in Eastern Time Zone. The date-time format is expressed as
+    [YYYY-MM-DD]T[HH:MM:SSS], following ISO 8601.
+    """
+
+    error: Optional[DataError] = None
+
+    start_time: Optional[datetime] = FieldInfo(alias="startTime", default=None)
+    """Time when the batch request is started.
+
+    This is in Eastern Time Zone. The date-time format is expressed as
+    [YYYY-MM-DD]T[HH:MM:SSS], following ISO 8601.
+    """
+
+    status: Optional[Literal["queued", "executing", "created", "failed"]] = None
+
+
+class BatchStatusResponse(BaseModel):
+    data: Optional[Data] = None
