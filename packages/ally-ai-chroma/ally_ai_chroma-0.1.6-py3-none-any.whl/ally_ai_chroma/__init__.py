6a0e045917758fab7490436f527c5046ff3365ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 15:04:05 +0000

    Add package ally-ai-chroma/0.1.6/ally_ai_chroma-0.1.6-py3-none-any.whl

---
+++ packages/ally-ai-chroma/ally_ai_chroma-0.1.6-py3-none-any.whl/ally_ai_chroma/__init__.py
@@ -0,0 +1,10 @@
+from ally_ai_chroma.Chroma import Chroma
+from ally_ai_chroma.ChromaEmbeddingsVisualisor import ChromaEmbeddingsVisualisor
+from ally_ai_core.embeddings.EmbeddingsVisualisor import EmbeddingsVisualisor
+
+
+__all__ = [
+    'Chroma',
+    'ChromaEmbeddingsVisualisor',
+    'EmbeddingsVisualisor'
+]
