2f52a9f4cb01dc020cc288d4f0f048140b20698f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:03:34 +0000

    Add package scarx-api-client/0.1.3/scarx_api_client-0.1.3-py3-none-any.whl

---
+++ packages/scarx-api-client/scarx_api_client-0.1.3-py3-none-any.whl/scarx_api_client/__init__.py
@@ -0,0 +1,2 @@
+from scarx_api_client.proto.grpc.api.v1 import IpServiceStub, IpDetails, IpRequest, LakGatewayServiceStub, LakGatewayStatus, LakGatewayInputMetadata, LakGatewayInputEmpty
+from scarx_api_client.Channel import ScarxApiChannel
