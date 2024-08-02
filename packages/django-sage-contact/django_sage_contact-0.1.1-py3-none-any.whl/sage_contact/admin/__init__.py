d22d99ea8c9c23f351b838909e0a18a515c1637d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:04:16 +0000

    Add package django-sage-contact/0.1.1/django_sage_contact-0.1.1-py3-none-any.whl

---
+++ packages/django-sage-contact/django_sage_contact-0.1.1-py3-none-any.whl/sage_contact/admin/__init__.py
@@ -0,0 +1,3 @@
+from .support import (FullSupportRequestAdmin, SupportRequestBaseParentAdmin,
+                      SupportRequestWithLocationAdmin,
+                      SupportRequestWithPhoneAdmin)
