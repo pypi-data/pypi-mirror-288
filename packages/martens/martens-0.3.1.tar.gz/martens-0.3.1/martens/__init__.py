d4f866df8e24c208410f80671b2ae978caad3af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:04:28 +0000

    Add package martens/0.3.1/martens-0.3.1.tar.gz

---
+++ packages/martens/martens-0.3.1.tar.gz/martens-0.3.1/martens/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Martens."""
+
+__author__ = 'Alex Rowley'
+__email__ = 'arowley@gmail.com'
+__version__ = '0.3.1'
+
+from .martens import Dataset, SourceFile, stack, initialise, average
