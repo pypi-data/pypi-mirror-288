687c714f83d7d442b9a536fbd4348fa53333d86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 04:03:34 +0000

    Add package sbcli-lvol/1.2.2/sbcli_lvol-1.2.2.zip

---
+++ packages/sbcli-lvol/sbcli_lvol-1.2.2.zip/sbcli_lvol-1.2.2/simplyblock_core/scripts/deploy_stack.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+export CLI_SSH_PASS=$1
+export CLUSTER_IP=$2
+export SIMPLYBLOCK_DOCKER_IMAGE=$3
+
+export GRAYLOG_ROOT_PASSWORD_SHA2=$4
+export GRAYLOG_PASSWORD_SECRET="is6SP2EdWg0NdmVGv6CEp5hRHNL7BKVMFem4t9pouMqDQnHwXMSomas1qcbKSt5yISr8eBHv4Y7Dbswhyz84Ut0TW6kqsiPs"
+
+export CLUSTER_SECRET=$5
+export CLUSTER_ID=$6
+export LOG_DELETION_INTERVAL=$7
+export RETENTION_PERIOD=$8
+export DIR="$(dirname "$(realpath "$0")")"
+
+if [ -s "/etc/foundationdb/fdb.cluster" ]
+then
+   FDB_CLUSTER_FILE_CONTENTS=$(tail /etc/foundationdb/fdb.cluster -n 1)
+   export FDB_CLUSTER_FILE_CONTENTS=$FDB_CLUSTER_FILE_CONTENTS
+fi
+
+docker network create monitoring-net -d overlay --attachable
+
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+#if [ -n "$INSTANCE_ID" ]
+#then
+#  export USE_EFS="rexray/efs"
+#fi
+
+docker stack deploy --compose-file="$DIR"/docker-compose-swarm-monitoring.yml monitoring
+
+# wait for the services to become online
+bash "$DIR"/stack_deploy_wait.sh monitoring
+
+docker stack deploy --compose-file="$DIR"/docker-compose-swarm.yml app
+
+# wait for the services to become online
+bash "$DIR"/stack_deploy_wait.sh app
