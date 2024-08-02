2415ecb281d6a19422bee1896e9028eb42df7714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 15:03:50 +0000

    Add package supy/2024.8.2.dev0/supy-2024.8.2.dev0-cp312-cp312-win_amd64.whl

---
+++ packages/supy/supy-2024.8.2.dev0-cp312-cp312-win_amd64.whl/supy/__init__.py
@@ -0,0 +1,56 @@
+###########################################################################
+# SuPy: SUEWS that speaks Python
+# Authors:
+# Ting Sun, ting.sun@reading.ac.uk
+# History:
+# 20 Jan 2018: first alpha release
+# 01 Feb 2018: performance improvement
+# 03 Feb 2018: improvement in output processing
+# 08 Mar 2018: pypi packaging
+# 01 Jan 2019: public release
+# 22 May 2019: restructure of module layout
+# 02 Oct 2019: logger restructured
+###########################################################################
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_7_2():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'supy.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_7_2()
+del _delvewheel_patch_1_7_2
+# end delvewheel patch
+
+# core functions
+from ._supy_module import (
+    init_supy,
+    load_SampleData,
+    load_forcing_grid,
+    run_supy,
+    save_supy,
+    check_forcing,
+    check_state,
+)
+
+
+# utilities
+from . import util
+
+
+# version info
+from ._version import show_version, __version__
+
+from .cmd import SUEWS
+
+# module docs
+__doc__ = """
+supy - SUEWS that speaks Python
+===============================
+
+**SuPy** is a Python-enhanced urban climate model with SUEWS as its computation core.
+
+"""
