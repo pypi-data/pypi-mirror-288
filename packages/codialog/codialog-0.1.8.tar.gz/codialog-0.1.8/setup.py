91ddb321a51b62ec38067d80a54386f09183dddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:02 +0000

    Add package codialog/0.1.8/codialog-0.1.8.tar.gz

---
+++ packages/codialog/codialog-0.1.8.tar.gz/codialog-0.1.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+# Read the requirements.txt file
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='codialog',
+    version='0.1.8',
+    #packages=find_packages(),
+    packages=find_namespace_packages(where="."),
+    package_dir={"": "."},
+    package_data={"codialog": ["*.txt", "*.html", "static/*", "templates/*"]},
+    include_package_data=True,
+    install_requires=required,
+    entry_points='''
+        [console_scripts]
+        codialog=codialog.app:run
+    ''',
+)
