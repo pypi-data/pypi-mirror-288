7f7acc4171100e53783db1ec0f16e0de17c60cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 15:04:19 +0000

    Add package moscot/0.3.5/moscot-0.3.5.tar.gz

---
+++ packages/moscot/moscot-0.3.5.tar.gz/moscot-0.3.5/src/moscot/problems/generic/__init__.py
@@ -0,0 +1,4 @@
+from moscot.problems.generic._generic import FGWProblem, GWProblem, SinkhornProblem
+from moscot.problems.generic._mixins import GenericAnalysisMixin
+
+__all__ = ["FGWProblem", "GWProblem", "SinkhornProblem", "GenericAnalysisMixin"]
