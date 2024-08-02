06b8550cc6f65fcf3ec75277f5ec788ab5f768fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:33 +0000

    Add package mcresources/1.7.1/mcresources-1.7.1.tar.gz

---
+++ packages/mcresources/mcresources-1.7.1.tar.gz/mcresources-1.7.1/mcresources/__init__.py
@@ -0,0 +1,15 @@
+#  Part of mcresources by Alex O'Neill
+#  Work under copyright. Licensed under MIT
+#  For more information see the project LICENSE file
+
+from mcresources.resource_manager import ResourceManager
+
+from mcresources.block_context import BlockContext
+from mcresources.item_context import ItemContext
+from mcresources.recipe_context import RecipeContext
+
+from mcresources import loot_tables
+from mcresources import block_states
+from mcresources import advancements
+from mcresources import utils
+from mcresources import atlases
