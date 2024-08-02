e127a5c4ed6ca97206b3918907a35a9cb6a66336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:51 +0000

    Add package cobroswap/0.0.2/cobroswap-0.0.2.tar.gz

---
+++ packages/cobroswap/cobroswap-0.0.2.tar.gz/cobroswap-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+setup(
+    name = "cobroswap",
+    version = "0.0.2",
+    author = "Alejo Prieto Dávalos",
+    author_email = "alejoprietodavalos@gmail.com",
+    packages = find_packages(),
+    description = "Python wrapper for CobrosWap.",
+    long_description = Path("README.md").read_text(),
+    long_description_content_type = "text/markdown",
+    url = "https://pypi.org/project/cobroswap/",
+    project_urls = {
+        "Source": "https://github.com/AlejoPrietoDavalos/cobroswap/"
+    },
+    python_requires = ">=3.11",
+    install_requires = [
+        "requests>=2.32.0"
+    ],
+    include_package_data = True
+)
