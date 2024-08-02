01ae4d384c949d3b18702df783124c0bb23b3573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:29 +0000

    Add package mjolnir-arq/0.0.3/mjolnir_arq-0.0.3.tar.gz

---
+++ packages/mjolnir-arq/mjolnir_arq-0.0.3.tar.gz/mjolnir_arq-0.0.3/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mjolnir_arq',
+    version='0.0.3',
+    description='A simple example package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Marlon Andres Leon Leon',
+    author_email='dandresleon64@gmail.com',
+    url='https://github.com/dandres10/mjolnir_arq',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'mj=mjolnir_arq.module:hello_world',  
+        ],
+    },
+)
