f3810d9a89da4bcd4c856cb208bd8f1210147651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:02:58 +0000

    Add package typegraph3/0.2.0/typegraph3-0.2.0.tar.gz

---
+++ packages/typegraph3/typegraph3-0.2.0.tar.gz/typegraph3-0.2.0/src/typegraph/__init__.py
@@ -0,0 +1,11 @@
+from .converter import (
+    PdtConverter,
+)
+
+from mapgraph.typevar import (
+    TypeVarModel,
+    infer_generic_type,
+    iter_deep_type,
+    gen_typevar_model,
+)
+from mapgraph.type_utils import deep_type
