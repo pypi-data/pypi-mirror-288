e779b1f0be2e71d29180661ea0f64aacd091c1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:05:12 +0000

    Add package aioyoomoney-api/0.0.4/aioyoomoney_api-0.0.4.tar.gz

---
+++ packages/aioyoomoney-api/aioyoomoney_api-0.0.4.tar.gz/aioyoomoney_api-0.0.4/aioyoomoney/globals.py
@@ -0,0 +1,14 @@
+from .exceptions import *
+
+API_URL = "https://yoomoney.ru/api/"
+ERROR_CODES = {
+    "illegal_param_type": IllegalParamType,
+    "illegal_param_start_record": IllegalParamStartRecord,
+    "illegal_param_records": IllegalParamRecords,
+    "illegal_param_label": IllegalParamLabel,
+    "illegal_param_from": IllegalParamFromDate,
+    "illegal_param_till": IllegalParamTillDate,
+    "illegal_param_details": IllegalParamDetails,
+
+    "illegal_param_operation_id": IllegalParamOperationId
+}
