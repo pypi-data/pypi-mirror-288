3cfefd0154aafc51fb99ca5a3e94defdb13fc05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 04:03:38 +0000

    Add package django-sage-mailbox/0.1.3/django_sage_mailbox-0.1.3-py3-none-any.whl

---
+++ packages/django-sage-mailbox/django_sage_mailbox-0.1.3-py3-none-any.whl/sage_mailbox/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class SageMailboxConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sage_mailbox"
+    verbose_name = _("Mailbox")
+
+    def ready(self):
+        from . import checks
+        from . import signals
