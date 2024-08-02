88ffbd0d4d28b6da6443aeade3d81a2db6ce222e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:03:40 +0000

    Add package policyengine-us/1.36.0/policyengine-us-1.36.0.tar.gz

---
+++ packages/policyengine-us/policyengine-us-1.36.0.tar.gz/policyengine-us-1.36.0/policyengine_us/variables/gov/local/tax/income/local_income_tax.py
@@ -0,0 +1,12 @@
+from policyengine_us.model_api import *
+
+
+class local_income_tax(Variable):
+    value_type = float
+    entity = TaxUnit
+    definition_period = YEAR
+    label = "Local income tax"
+    unit = USD
+
+    adds = ["nyc_income_tax"]
+    exhaustive_parameter_dependencies = "gov.local"
