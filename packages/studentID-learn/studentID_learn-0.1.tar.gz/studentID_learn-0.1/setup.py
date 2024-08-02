ee25d209d65ec4a327dfd96f1b4d20322055978f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:03:28 +0000

    Add package studentID-learn/0.1/studentID_learn-0.1.tar.gz

---
+++ packages/studentID-learn/studentID_learn-0.1.tar.gz/studentID_learn-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='studentID_learn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['pami'],
+    author='仲野龍太朗',
+    author_email='s1300017@u-aizu.ac.jp',
+    description='A package for learning purposes',
+    url='https://github.com/s1300017/s1300017_learn',
+)
