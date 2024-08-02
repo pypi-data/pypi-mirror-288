b9d72dfd2de5a099658a70d68129b4f129c9534c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:05:15 +0000

    Add package sync-pre-commit-lock/0.6.1/sync_pre_commit_lock-0.6.1.tar.gz

---
+++ packages/sync-pre-commit-lock/sync_pre_commit_lock-0.6.1.tar.gz/sync_pre_commit_lock-0.6.1/src/sync_pre_commit_lock/actions/sync_hooks.py
@@ -0,0 +1,150 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, NamedTuple
+
+from sync_pre_commit_lock.db import DEPENDENCY_MAPPING, REPOSITORY_ALIASES, PackageRepoMapping, RepoInfo
+from sync_pre_commit_lock.pre_commit_config import PreCommitHookConfig, PreCommitRepo
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from sync_pre_commit_lock import Printer
+    from sync_pre_commit_lock.config import SyncPreCommitLockConfig
+
+
+class GenericLockedPackage(NamedTuple):
+    name: str
+    version: str
+    # Add original data here?
+
+
+class SyncPreCommitHooksVersion:
+    def __init__(
+        self,
+        printer: Printer,
+        pre_commit_config_file_path: Path,
+        locked_packages: dict[str, GenericLockedPackage],
+        plugin_config: SyncPreCommitLockConfig,
+        dry_run: bool = False,
+    ) -> None:
+        self.printer = printer
+        self.pre_commit_config_file_path = pre_commit_config_file_path
+        self.locked_packages = locked_packages
+        self.plugin_config = plugin_config
+        self.dry_run = dry_run
+
+    def execute(self) -> None:
+        if self.plugin_config.disable_sync_from_lock:
+            self.printer.debug("Sync pre-commit lock is disabled")
+            return
+
+        try:
+            pre_commit_config_data = PreCommitHookConfig.from_yaml_file(self.pre_commit_config_file_path)
+        except FileNotFoundError:
+            self.printer.info(
+                f"No pre-commit config file detected at {self.pre_commit_config_file_path}, skipping sync."
+            )
+            return
+        except ValueError as e:
+            self.printer.error(f"Invalid pre-commit config file: {self.pre_commit_config_file_path}: {e}")
+            return
+
+        mapping, mapping_reverse_by_url = self.build_mapping()
+        # XXX We should have the list of packages mapped, but already up to date and print it
+        to_fix, in_sync = self.analyze_repos(pre_commit_config_data.repos_normalized, mapping, mapping_reverse_by_url)
+
+        if len(to_fix) == 0 and len(in_sync) == 0:
+            self.printer.info("No pre-commit hook detected that matches a locked package.")
+            return
+        if len(to_fix) == 0:
+            packages_str = ", ".join(f"{mapping_reverse_by_url[repo.repo]} ({rev})" for repo, rev in in_sync.items())
+            self.printer.info(f"All pre-commit hooks are already up to date with the lockfile: {packages_str}")
+            return
+
+        self.printer.info("Detected pre-commit hooks that can be updated to match the lockfile:")
+        self.printer.list_updated_packages(
+            {mapping_reverse_by_url[repo.repo]: (repo, new_ver) for repo, new_ver in to_fix.items()}
+        )
+
+        if self.dry_run:
+            self.printer.info("Dry run, skipping pre-commit hook update.")
+            return
+        pre_commit_config_data.update_pre_commit_repo_versions(to_fix)
+        self.printer.success(f"Pre-commit hooks have been updated in {self.pre_commit_config_file_path.name}!")
+
+    def get_pre_commit_repo_new_version(
+        self,
+        pre_commit_config_repo: PreCommitRepo,
+        mapping_db_repo_info: RepoInfo,
+        locked_package: GenericLockedPackage,
+    ) -> str | None:
+        if locked_package.name in self.plugin_config.ignore:
+            self.printer.debug(f"Ignoring {locked_package.name} from configuration.")
+            return None
+
+        self.printer.debug(
+            f"Found mapping between pre-commit hook `{pre_commit_config_repo.repo}` and locked package `{locked_package.name}`."
+        )
+        formatted_rev = mapping_db_repo_info["rev"].replace("${rev}", str(locked_package.version))
+        if formatted_rev != pre_commit_config_repo.rev:
+            self.printer.debug(
+                f"Pre-commit hook {pre_commit_config_repo.repo} and locked package {locked_package.name} have different versions:\n"
+                f" - Pre-commit hook ref: {pre_commit_config_repo.rev}\n"
+                f" - Locked package version: {locked_package.version}"
+            )
+            return formatted_rev
+
+        self.printer.debug(
+            f"Pre-commit hook {pre_commit_config_repo.repo} version already matches the version from the lockfile package."
+        )
+        return None
+
+    def build_mapping(self) -> tuple[PackageRepoMapping, dict[str, str]]:
+        """Merge the default mapping with the user-provided mapping. Also build a reverse mapping by URL."""
+        mapping: PackageRepoMapping = {**DEPENDENCY_MAPPING, **self.plugin_config.dependency_mapping}
+        mapping_reverse_by_url = {repo["repo"]: lib_name for lib_name, repo in mapping.items()}
+        for canonical_name, aliases in REPOSITORY_ALIASES.items():
+            for alias in aliases:
+                mapping_reverse_by_url[alias] = mapping_reverse_by_url[canonical_name]
+        # XXX Allow override / extend of aliases
+        return mapping, mapping_reverse_by_url
+
+    def analyze_repos(
+        self,
+        pre_commit_repos: set[PreCommitRepo],
+        mapping: PackageRepoMapping,
+        mapping_reverse_by_url: dict[str, str],
+    ) -> tuple[dict[PreCommitRepo, str], dict[PreCommitRepo, str]]:
+        to_fix: dict[PreCommitRepo, str] = {}
+        in_sync: dict[PreCommitRepo, str] = {}
+        for pre_commit_repo in pre_commit_repos:
+            if pre_commit_repo.repo not in mapping_reverse_by_url:
+                self.printer.debug(f"Pre-commit hook {pre_commit_repo.repo} not found in the DB mapping")
+                continue
+
+            dependency = mapping[mapping_reverse_by_url[pre_commit_repo.repo]]
+            dependency_name = mapping_reverse_by_url[pre_commit_repo.repo]
+            dependency_locked = self.locked_packages.get(dependency_name)
+
+            if not dependency_locked:
+                self.printer.debug(
+                    f"Pre-commit hook {pre_commit_repo.repo} has a mapping to Python package `{dependency_name}`, "
+                    "but was not found in the lockfile"
+                )
+                continue
+
+            if "+" in dependency_locked.version:
+                self.printer.debug(
+                    f"Pre-commit hook {pre_commit_repo.repo} has a mapping to Python package `{dependency_name}`, "
+                    f"but is skipped because the locked version `{dependency_locked.version}` contaims a `+`, "
+                    "which is a local version identifier."
+                )
+                continue
+
+            new_ver = self.get_pre_commit_repo_new_version(pre_commit_repo, dependency, dependency_locked)
+            if new_ver:
+                to_fix[pre_commit_repo] = new_ver
+            else:
+                in_sync[pre_commit_repo] = dependency_locked.version
+
+        return to_fix, in_sync
---
+++ packages/sync-pre-commit-lock/sync_pre_commit_lock-0.6.1.tar.gz/sync_pre_commit_lock-0.6.1/src/sync_pre_commit_lock/poetry_plugin.py
@@ -0,0 +1,168 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING, ClassVar
+
+from cleo.commands.command import Command
+from cleo.events.console_events import TERMINATE
+from cleo.events.console_terminate_event import ConsoleTerminateEvent
+from cleo.exceptions import CleoValueError
+from cleo.helpers import option
+from cleo.io.outputs.output import Verbosity
+from poetry.__version__ import __version__ as poetry_version
+from poetry.console.application import Application
+from poetry.console.commands.add import AddCommand
+from poetry.console.commands.install import InstallCommand
+from poetry.console.commands.lock import LockCommand
+from poetry.console.commands.self.self_command import SelfCommand
+from poetry.console.commands.update import UpdateCommand
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+from sync_pre_commit_lock import Printer
+from sync_pre_commit_lock.actions.install_hooks import SetupPreCommitHooks
+from sync_pre_commit_lock.actions.sync_hooks import GenericLockedPackage, SyncPreCommitHooksVersion
+from sync_pre_commit_lock.config import load_config
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from cleo.events.event import Event
+    from cleo.events.event_dispatcher import EventDispatcher
+    from cleo.io.io import IO
+
+    from sync_pre_commit_lock.pre_commit_config import PreCommitRepo
+
+
+class PoetryPrinter(Printer):
+    success_list_token: str = "<fg=green;options=bold>•</>"
+
+    def __init__(self, io: IO, with_prefix: bool = True) -> None:
+        self.io = io
+        self.plugin_prefix = "[sync-pre-commit-lock] " if with_prefix else ""
+
+    def debug(self, msg: str) -> None:
+        self.io.write_line(f"<info>{self.plugin_prefix}{msg}</info>", verbosity=Verbosity.DEBUG)
+
+    def info(self, msg: str) -> None:
+        self.io.write_line(f"<info>{self.plugin_prefix}{msg}</info>", verbosity=Verbosity.NORMAL)
+
+    def warning(self, msg: str) -> None:
+        return self.io.write_line(f"<warning>{self.plugin_prefix}{msg}</warning>", verbosity=Verbosity.NORMAL)
+
+    def error(self, msg: str) -> None:
+        return self.io.write_error_line(f"<error>{self.plugin_prefix}{msg}</error>", verbosity=Verbosity.NORMAL)
+
+    def success(self, msg: str) -> None:
+        return self.io.write_line(f"<success>{self.plugin_prefix} {msg}</success>", verbosity=Verbosity.NORMAL)
+
+    def list_updated_packages(self, packages: dict[str, tuple[PreCommitRepo, str]]) -> None:
+        from cleo.ui.table import Table
+
+        table = Table(self.io, style="compact")
+
+        table.set_rows(
+            [
+                [
+                    "<info>" + self.plugin_prefix + " " + self.success_list_token,
+                    self._format_repo_url(repo[0].repo, package),
+                    " ",
+                    "<warning>" + repo[0].rev + "</>",
+                    "->",
+                    "<success>" + repo[1] + "</>" + "</>",
+                ]
+                for package, repo in packages.items()
+            ]
+        )
+
+        table.render()
+
+    def _format_repo_url(self, repo_url: str, package_name: str) -> str:
+        return repo_url.replace(package_name, f"<c1>{package_name}</>")
+
+
+class PoetrySetupPreCommitHooks(SetupPreCommitHooks):
+    install_pre_commit_hooks_command: ClassVar[Sequence[str | bytes]] = ["poetry", "run", "pre-commit", "install"]
+    check_pre_commit_version_command: ClassVar[Sequence[str | bytes]] = ["poetry", "run", "pre-commit", "--version"]
+
+
+def run_sync_pre_commit_version(printer: PoetryPrinter, dry_run: bool, application: Application) -> None:
+    poetry_locked_packages = application.poetry.locker.locked_repository().packages
+    locked_packages = {str(p.name): GenericLockedPackage(p.name, str(p.version)) for p in poetry_locked_packages}
+    plugin_config = load_config(application.poetry.pyproject_path)
+    file_path = Path().cwd() / plugin_config.pre_commit_config_file
+    # Add poetry itself as it won't be part of the resolved dependencies
+    locked_packages["poetry"] = GenericLockedPackage("poetry", poetry_version)
+
+    SyncPreCommitHooksVersion(
+        printer,
+        pre_commit_config_file_path=file_path,
+        plugin_config=plugin_config,
+        locked_packages=locked_packages,
+        dry_run=dry_run,
+    ).execute()
+
+
+class SyncPreCommitLockPlugin(ApplicationPlugin):
+    application: Application | None
+
+    def activate(self, application: Application) -> None:
+        assert application.event_dispatcher is not None
+        application.event_dispatcher.add_listener(TERMINATE, self._handle_post_command)
+        application.command_loader.register_factory("sync-pre-commit", sync_pre_commit_poetry_command_factory)
+        self.application = application
+
+    def _handle_post_command(
+        self, event: ConsoleTerminateEvent | Event, event_name: str, dispatcher: EventDispatcher
+    ) -> None:
+        assert isinstance(event, ConsoleTerminateEvent)
+        if event.exit_code != 0:
+            # The command failed, so the plugin shouldn't do anything
+            return
+
+        command = event.command
+        printer = PoetryPrinter(event.io)
+        try:
+            dry_run: bool = bool(command.option("dry-run"))
+        except CleoValueError:
+            dry_run = False
+
+        if isinstance(command, SelfCommand):
+            printer.debug("Poetry pre-commit plugin does not run for 'self' command.")
+            return
+
+        if any(isinstance(command, t) for t in [InstallCommand, AddCommand]):
+            PoetrySetupPreCommitHooks(printer, dry_run=dry_run).execute()
+
+        if any(isinstance(command, t) for t in [InstallCommand, AddCommand, LockCommand, UpdateCommand]):
+            if self.application is None:
+                msg = "self.application is None"
+                raise RuntimeError(msg)
+
+            # Get all locked dependencies from self.application
+            run_sync_pre_commit_version(printer, dry_run, self.application)
+
+
+class SyncPreCommitPoetryCommand(Command):
+    name = "sync-pre-commit"
+    description = "Sync `.pre-commit-config.yaml` hooks versions with the lockfile"
+    help = "Sync `.pre-commit-config.yaml` hooks versions with the lockfile"
+    options = [
+        option(
+            "dry-run",
+            None,
+            "Output the operations but do not update the pre-commit file.",
+        ),
+    ]
+
+    def handle(self) -> int:
+        # XXX(dugab): handle return codes
+        if not self.application:
+            msg = "self.application is None"
+            raise RuntimeError(msg)
+        assert isinstance(self.application, Application)
+        run_sync_pre_commit_version(PoetryPrinter(self.io, with_prefix=False), False, self.application)
+        return 0
+
+
+def sync_pre_commit_poetry_command_factory() -> SyncPreCommitPoetryCommand:
+    return SyncPreCommitPoetryCommand()
