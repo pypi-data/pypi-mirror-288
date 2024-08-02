2585b8b57423197cf2d16ce912eb9281bf4e0e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:05:17 +0000

    Add package okazaki/0.0.8/okazaki-0.0.8-py3-none-any.whl

---
+++ packages/okazaki/okazaki-0.0.8-py3-none-any.whl/okazaki/ai/summarize.py
@@ -0,0 +1,43 @@
+# MIT License
+#
+# Copyright (c) 2022 Clivern
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from .client import Client
+
+
+class Summarize:
+
+    @staticmethod
+    def summarize(text, model_name="gpt-4o-mini", temperature=0, callbacks=[]):
+        """
+        Summarize the given text using a language model.
+        """
+        chain = Client.create_chat_chain(
+            "gpt-4o-mini",
+            0,
+            [
+                ("system", "You are a helpful assistant that summarizes text."),
+                ("user", "Summarize the following text:\n\n{text}")
+            ],
+            callbacks
+        )
+
+        return chain.invoke({"text": text})
