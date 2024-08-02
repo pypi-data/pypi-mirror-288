4f47ec9cd51e401347df9c6d0dadaa7b75ab4af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 15:03:53 +0000

    Add package sci-plot-style/1.0.2/sci_plot_style-1.0.2.tar.gz

---
+++ packages/sci-plot-style/sci_plot_style-1.0.2.tar.gz/sci_plot_style-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='sci_plot_style',
+    version='1.0.2',
+    classifiers=[
+        'Development Status :: 3 - Alpha', 'Environment :: Console',
+        'Operating System :: OS Independent'
+    ],
+    license='GPLv3',
+    description='SCI style',
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    author='Feifeng Wu',
+    author_email='wufeifeng_hust@163.com',
+    keywords=['matplotlib'],
+    python_requires=">=3.6",
+    install_requires=['matplotlib'],
+    packages=find_packages(),
+    package_data={
+      'sci_plot_style': ['styles/**/*.mplstyle', 'styles/**/*.ttf'],
+    },
+    include_package_data=True,
+)
