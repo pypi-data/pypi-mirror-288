87f41dea8fd07b18f70d83605ad100214f62a63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 04:03:01 +0000

    Add package buff-package/0.0.2/buff-package-0.0.2.tar.gz

---
+++ packages/buff-package/buff-package-0.0.2.tar.gz/buff-package-0.0.2/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="buff-package",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="buff",
+    author_email="buff@kakao.com",
+    description="Practice Package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/buff91/python-practice",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
