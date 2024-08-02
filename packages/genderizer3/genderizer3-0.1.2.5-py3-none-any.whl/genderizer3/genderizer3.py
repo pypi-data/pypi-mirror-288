59093edeef584b032e1e6018ba51f9e532b91dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:14 +0000

    Add package genderizer3/0.1.2.5/genderizer3-0.1.2.5-py3-none-any.whl

---
+++ packages/genderizer3/genderizer3-0.1.2.5-py3-none-any.whl/genderizer3/genderizer3.py
@@ -0,0 +1,85 @@
+from . import tokenizer
+from .classifier import Classifier
+from .namesCollection import NamesCollection
+from .cachedModel import CachedModel
+
+
+class Genderizer(object):
+    """@TODO: write docstring for Genderize"""
+
+    initialized = False
+    namesCollection = None
+    classifier = None
+    lang = "en"
+
+    significantDegree = 0.3
+
+    surelyMale = "M"
+    surelyFemale = "F"
+    mostlyMale = "?m"
+    mostlyFemale = "?f"
+    genderUnknown = "?"
+
+    @classmethod
+    def init(cls, lang="en", namesCollection=NamesCollection, classifier=None):
+
+        cls.lang = lang
+        cls.namesCollection = namesCollection
+
+        if classifier:
+            cls.classifier = classifier
+        else:
+            cls.classifier = Classifier(CachedModel.get(lang), tokenizer)
+
+        cls.initialized = True
+
+    @classmethod
+    def detect(cls, firstName=None, text=None, lang=None):
+
+        if not cls.initialized:
+            cls.init()
+
+        if cls.classifier is None:
+            raise Exception("No classifier found. You need to set a classifier.")
+
+        if cls.namesCollection is None:
+            raise Exception(
+                "No names collection found. You need to have names collection."
+            )
+
+        if firstName:
+            nameGender = cls.namesCollection.getGender(firstName, lang)
+            if nameGender:
+                if nameGender["gender"] == cls.surelyMale:
+                    return "male"
+                elif nameGender["gender"] == cls.surelyFemale:
+                    return "female"
+        else:
+            nameGender = None
+
+        if text:
+            probablities = dict(cls.classifier.classify(text))
+            classifierScoreLogF = probablities["female"] / sum(probablities.values())
+            classifierScoreLogM = probablities["male"] / sum(probablities.values())
+            classifierScoreM = classifierScoreLogF / (
+                classifierScoreLogM + classifierScoreLogF
+            )
+            classifierScoreF = classifierScoreLogM / (
+                classifierScoreLogM + classifierScoreLogF
+            )
+
+            if nameGender and nameGender["gender"].startswith("?"):
+                if nameGender["gender"] == cls.mostlyMale and classifierScoreM > 0.6:
+                    return "male"
+                elif (
+                    nameGender["gender"] == cls.mostlyFemale and classifierScoreF > 0.6
+                ):
+                    return "female"
+                elif nameGender["gender"] != cls.genderUnknown:
+                    return None
+
+            if abs(classifierScoreF - classifierScoreM) > cls.significantDegree:
+                if probablities["female"] > probablities["male"]:
+                    return "female"
+                else:
+                    return "male"
