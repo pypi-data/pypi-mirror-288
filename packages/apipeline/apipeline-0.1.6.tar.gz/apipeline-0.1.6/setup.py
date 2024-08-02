380626642f59f61b1be5c677f99a88e8c694963b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:05:02 +0000

    Add package apipeline/0.1.6/apipeline-0.1.6.tar.gz

---
+++ packages/apipeline/apipeline-0.1.6.tar.gz/apipeline-0.1.6/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="apipeline",
+    description="Python pipeline with asyncio support",
+    long_description=open("README.md").read().strip(),
+    long_description_content_type="text/markdown",
+    version="0.1.6",
+    license="BSD 3-Clause",
+    url="https://github.com/weedge/pipeline-py",
+    author="weedge",
+    author_email="weege007@gmail.com",
+    python_requires=">=3.10",
+    packages=["apipeline"],
+    install_requires=["pydantic >= 2.8.2"],
+    project_urls={
+        "Changes": "https://github.com/weedge/pipeline-py/releases",
+        "Code": "https://github.com/weedge/pipeline-py",
+        "Issue tracker": "https://github.com/weedge/pipeline-py/issues",
+    },
+    keywords=["pipeline", "asyncio"],
+)
