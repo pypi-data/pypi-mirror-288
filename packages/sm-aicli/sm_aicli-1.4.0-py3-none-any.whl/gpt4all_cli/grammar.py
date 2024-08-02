b69af759b66eb5090ffa68730b12cd2566010995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:02:54 +0000

    Add package sm-aicli/1.4.0/sm_aicli-1.4.0-py3-none-any.whl

---
+++ packages/sm-aicli/sm_aicli-1.4.0-py3-none-any.whl/gpt4all_cli/grammar.py
@@ -0,0 +1,32 @@
+CMD_HELP = "/help"
+CMD_ASK  = "/ask"
+CMD_EXIT = "/exit"
+CMD_ECHO = "/echo"
+CMD_MODEL = "/model"
+CMD_NTHREADS = "/nthreads"
+CMD_RESET = "/reset"
+CMD_TEMP = "/temp"
+CMD_APIKEY = "/apikey"
+CMD_VERBOSE = "/verbose"
+
+COMMANDS = [CMD_HELP, CMD_EXIT, CMD_ASK, CMD_ECHO, CMD_MODEL, CMD_NTHREADS, CMD_RESET, CMD_TEMP,
+            CMD_APIKEY]
+COMMANDS_ARG = [CMD_ECHO, CMD_MODEL, CMD_NTHREADS, CMD_TEMP, CMD_APIKEY, CMD_VERBOSE]
+COMMANDS_NOARG = r'|'.join(sorted(list(set(COMMANDS)-set(COMMANDS_ARG)))).replace('/','\\/')
+
+GRAMMAR = fr"""
+  start: (command | escape | text)? (command | escape | text)*
+  escape.3: /\\./
+  command.2: /{COMMANDS_NOARG}/ | \
+             /\/model/ / +/ string | \
+             /\/apikey/ / +/ string | \
+             /\/nthreads/ / +/ (number | def) | \
+             /\/verbose/ / +/ (number | def) | \
+             /\/temp/ / +/ (float | def ) | \
+             /\/echo/ | /\/echo/ / /
+  string: /"[^\"]+"/ | /""/
+  number: /[0-9]+/
+  float: /[0-9]+\.[0-9]*/
+  def: "default"
+  text: /(.(?!\/|\\))*./s
+"""
