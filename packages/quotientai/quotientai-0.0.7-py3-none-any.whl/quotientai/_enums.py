258fb6b039dafccdeec55e3d78ad1a8347f1ea6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:13 +0000

    Add package quotientai/0.0.7/quotientai-0.0.7-py3-none-any.whl

---
+++ packages/quotientai/quotientai-0.0.7-py3-none-any.whl/quotientai/_enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class GenerateDatasetType(Enum):
+    """
+    Enum for the type of dataset to generate.
+    """
+
+    grounded_qa: str = "grounded-question-answering"
+    summarization: str = "summarization"
+    translation: str = "translation"
