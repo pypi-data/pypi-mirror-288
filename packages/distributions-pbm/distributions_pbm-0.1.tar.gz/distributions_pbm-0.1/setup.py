567ea15669750697b7c7eba31a7cb0d058115d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:04:04 +0000

    Add package distributions-pbm/0.1/distributions_pbm-0.1.tar.gz

---
+++ packages/distributions-pbm/distributions_pbm-0.1.tar.gz/distributions_pbm-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pbm',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['distributions_pbm'],
+      author = 'Pablo Bartolomé Molina',
+      author_email = 'pablobart.me@gmail.com',
+      zip_safe=False)
