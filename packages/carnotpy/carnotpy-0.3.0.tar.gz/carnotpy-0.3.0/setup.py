1efdac8683513861350f3b1b2dc2df43d58fb700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:03:09 +0000

    Add package carnotpy/0.3.0/carnotpy-0.3.0.tar.gz

---
+++ packages/carnotpy/carnotpy-0.3.0.tar.gz/carnotpy-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='carnotpy',
+    version='0.3.0',
+    author='Carnot Innovations',
+    description='Carnot Connection as a Python Package',
+    py_modules=['carnotpy'],
+    install_requires=[
+        'pandas',
+        'requests',
+        'python-dateutil',
+    ],
+)
