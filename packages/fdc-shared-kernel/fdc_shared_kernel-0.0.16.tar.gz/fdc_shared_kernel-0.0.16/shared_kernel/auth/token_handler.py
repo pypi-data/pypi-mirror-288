f67a5889b13919e6cf1b1233c6e2580aeb163742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:03:10 +0000

    Add package fdc-shared-kernel/0.0.16/fdc_shared_kernel-0.0.16.tar.gz

---
+++ packages/fdc-shared-kernel/fdc_shared_kernel-0.0.16.tar.gz/fdc_shared_kernel-0.0.16/shared_kernel/auth/token_handler.py
@@ -0,0 +1,31 @@
+from flask import request
+from shared_kernel.auth import JWTTokenHandler
+from shared_kernel.config import Config
+from shared_kernel.exceptions import Unauthorized
+
+config = Config()
+token_handler = JWTTokenHandler(config.get('JWT_SECRET_KEY'))
+
+
+# Decorator to protect routes
+def token_required(f):
+    def decorator(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].replace('Bearer ', '')
+
+        if not token:
+            raise Unauthorized('Token is missing!')
+
+        payload = token_handler.decode_token(token)
+        if 'error' in payload:
+            raise Unauthorized("Failed to parse token")
+
+        # Add user information to the request context
+        current_user = {
+            "user_id": payload['user_id'],
+            "org_id": payload['org_id']
+        }
+        return f(current_user, *args, **kwargs)
+
+    return decorator
