9d0e263c42667c7ce41e024a7df364d52640949f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:15 +0000

    Add package owlapp/0.1.2/owlapp-0.1.2-py3-none-any.whl

---
+++ packages/owlapp/owlapp-0.1.2-py3-none-any.whl/app/cli/app_cmd.py
@@ -0,0 +1,22 @@
+import secrets
+from inspect import cleandoc
+from textwrap import indent
+
+import click
+from flask.cli import AppGroup
+
+cmd = AppGroup(help="App commands.")
+
+
+@cmd.command(name="generate-secret")
+def generate_secret():
+    value = secrets.token_urlsafe(32)
+    click.echo(value)
+
+
+@cmd.command(name="generate-env")
+def generate_env():
+    from app.settings import settings
+
+    content = settings.generate_env_content()
+    click.echo(content)
