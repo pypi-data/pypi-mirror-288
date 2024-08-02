6d7705717ff1eaec88191ce8eff6b4ebdc2c1cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:28 +0000

    Add package elifepubmed/0.7.0/elifepubmed-0.7.0.tar.gz

---
+++ packages/elifepubmed/elifepubmed-0.7.0.tar.gz/elifepubmed-0.7.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+import elifepubmed
+
+with open("README.md") as fp:
+    readme = fp.read()
+
+setup(
+    name="elifepubmed",
+    version=elifepubmed.__version__,
+    description="eLife PubMed deposit of journal articles.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=["elifepubmed"],
+    license="MIT",
+    install_requires=[
+        "elifetools>=0.33.0",
+        "elifearticle>=0.16.0",
+        "configparser",
+        "PyYAML",
+    ],
+    url="https://github.com/elifesciences/elife-pubmed-xml-generation",
+    maintainer="eLife Sciences Publications Ltd.",
+    maintainer_email="tech-team@elifesciences.org",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
