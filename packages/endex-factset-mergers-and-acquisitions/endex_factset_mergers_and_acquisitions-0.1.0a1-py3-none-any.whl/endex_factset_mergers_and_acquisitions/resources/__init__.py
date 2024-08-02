69afc2f58681ca128c4673c25201b232be40f591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:51 +0000

    Add package endex-factset-mergers-and-acquisitions/0.1.0a1/endex_factset_mergers_and_acquisitions-0.1.0a1-py3-none-any.whl

---
+++ packages/endex-factset-mergers-and-acquisitions/endex_factset_mergers_and_acquisitions-0.1.0a1-py3-none-any.whl/endex_factset_mergers_and_acquisitions/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .deals import (
+    DealsResource,
+    AsyncDealsResource,
+    DealsResourceWithRawResponse,
+    AsyncDealsResourceWithRawResponse,
+    DealsResourceWithStreamingResponse,
+    AsyncDealsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "DealsResource",
+    "AsyncDealsResource",
+    "DealsResourceWithRawResponse",
+    "AsyncDealsResourceWithRawResponse",
+    "DealsResourceWithStreamingResponse",
+    "AsyncDealsResourceWithStreamingResponse",
+]
