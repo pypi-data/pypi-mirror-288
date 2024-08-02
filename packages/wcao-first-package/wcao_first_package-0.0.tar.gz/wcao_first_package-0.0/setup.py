b1ebfc766ca48c4f01e217431a847a990b0ea84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:03:57 +0000

    Add package wcao-first-package/0.0/wcao_first_package-0.0.tar.gz

---
+++ packages/wcao-first-package/wcao_first_package-0.0.tar.gz/wcao_first_package-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='wcao_first_package',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['wcao_first_package'],
+      author_email='wenleicao@gmail.com',
+      zip_safe=False)
