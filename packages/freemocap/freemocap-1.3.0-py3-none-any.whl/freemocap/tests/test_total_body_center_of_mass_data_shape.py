c1ca81b07ae785225bd10e6686ac21d0b575cb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 03:03:14 +0000

    Add package freemocap/1.3.0/freemocap-1.3.0-py3-none-any.whl

---
+++ packages/freemocap/freemocap-1.3.0-py3-none-any.whl/freemocap/tests/test_total_body_center_of_mass_data_shape.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+import pytest
+
+from freemocap.utilities.get_number_of_frames_of_videos_in_a_folder import (
+    get_number_of_frames_of_videos_in_a_folder,
+)
+
+
+@pytest.mark.usefixtures("synchronized_video_folder_path", "total_body_center_of_mass_file_path")
+def test_total_body_center_of_mass_data_shape(
+    synchronized_video_folder_path: Union[str, Path],
+    total_body_center_of_mass_file_path: Union[str, Path],
+):
+    assert Path(
+        total_body_center_of_mass_file_path
+    ).is_file(), f"No file found at {total_body_center_of_mass_file_path}"
+
+    total_body_center_of_mass_fr_xyz = np.load(total_body_center_of_mass_file_path)
+
+    frame_counts = list(get_number_of_frames_of_videos_in_a_folder(synchronized_video_folder_path).values())
+    assert (
+        len(set(frame_counts)) == 1
+    ), f"Videos in {synchronized_video_folder_path} have different frame counts: {frame_counts}"
+
+    number_of_frames = frame_counts[0]
+
+    assert (
+        total_body_center_of_mass_fr_xyz.shape[0] == number_of_frames
+    ), f"Number of frames in {total_body_center_of_mass_file_path} does not match number of frames of videos in {synchronized_video_folder_path}"
+
+    assert (
+        total_body_center_of_mass_fr_xyz.shape[1] == 3
+    ), "`total_body_center_of_mass_fr_xyz.shape[1]` should have 3 dimensions (X,Y,Z) "
