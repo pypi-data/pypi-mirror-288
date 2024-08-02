e773bc838b4c4c8e6599f755cc81a9b11bba9c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:15 +0000

    Add package seahorse/1.1.0/seahorse-1.1.0-py3-none-any.whl

---
+++ packages/seahorse/seahorse-1.1.0-py3-none-any.whl/seahorse/player/player.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+import builtins
+from abc import abstractmethod
+from typing import TYPE_CHECKING
+
+from seahorse.game.action import Action
+from seahorse.utils.custom_exceptions import MethodNotImplementedError
+from seahorse.utils.serializer import Serializable
+
+if TYPE_CHECKING:
+    from seahorse.game.game_state import GameState
+
+
+class Player(Serializable):
+    """
+    A base class representing a player in the game.
+
+    Attributes:
+        id (int): The ID of the player.
+        name (str) : the name of the player
+    """
+
+    def __init__(self, name: str = "bob", *, identifier:int | None = None,**_) -> None:
+        """
+        Initializes a new instance of the Player class.
+
+        Args:
+            name (str, optional): The name of the player. Defaults to "bob".
+            hard_id (int, optional, keyword-only): Set the player's id in case of distant loading
+        """
+        self.name = name
+        if identifier is None:
+            self.identifier = builtins.id(self)
+        else:
+            self.identifier = identifier
+
+
+    def play(self, current_state: GameState, remaining_time: int) -> Action:
+        """
+        Implements the player's logic and calls compute_action with minimal information.
+
+        Args:
+            current_state (GameState): The current game state.
+
+        Raises:
+            MethodNotImplementedError: If the method is not implemented in the derived class.
+
+        Returns:
+            Action: The resulting action.
+        """
+        return self.compute_action(current_state=current_state, remaining_time=remaining_time)
+
+    @abstractmethod
+    def compute_action(self, **kwargs) -> Action:
+        """
+        Should be dedicated to adversarial search.
+
+        Args:
+            **kwargs: Additional arguments.
+
+        Raises:
+            MethodNotImplementedError: If the method is not implemented in the derived class.
+
+        Returns:
+            Action: The action to play.
+        """
+        raise MethodNotImplementedError()
+
+    def get_id(self) -> int:
+        """
+        Returns:
+            int: The ID of the player.
+        """
+        return self.identifier
+
+    def get_name(self) -> str:
+        """
+        Returns:
+            str: The name of the player.
+        """
+        return self.name
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Player object.
+
+        Returns:
+            str: The string representation.
+        """
+        return f"Player {self.get_name()}({self.get_id()})"
