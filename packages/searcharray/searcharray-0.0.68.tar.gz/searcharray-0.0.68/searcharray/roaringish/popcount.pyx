2479befd2be8441a1411ea3cc6a38fb2065579d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 04:03:28 +0000

    Add package searcharray/0.0.68/searcharray-0.0.68.tar.gz

---
+++ packages/searcharray/searcharray-0.0.68.tar.gz/searcharray-0.0.68/searcharray/roaringish/popcount.pyx
@@ -0,0 +1,194 @@
+# These are modified versions of sortednp:
+#   https://gitlab.sauerburger.com/frank/sortednp
+# cython: boundscheck=False
+# cython: wraparound=False
+# cython: initializedcheck=False
+# cython: cdivision=True
+# cython: nonecheck=False
+# cython: language_level=3
+cimport numpy as np
+import numpy as np
+
+
+# cimport snp_ops
+# from snp_ops cimport _galloping_search, DTYPE_t, ALL_BITS
+cimport searcharray.roaringish.snp_ops
+from searcharray.roaringish.snp_ops cimport DTYPE_t
+
+
+cdef extern from "stddef.h":
+    # Assuming size_t is available via stddef.h for the example's simplicity
+    # and portability, though it's not directly used here.
+    int __builtin_popcountll(unsigned long long x)
+
+    int __builtin_ctzll(unsigned long long x)
+    int __builtin_clzll(unsigned long long x)
+
+
+# Include mach performance timer
+# cdef extern from "mach/mach_time.h":
+#     uint64_t mach_absolute_time()
+
+
+cdef popcount64_arr(DTYPE_t[:] arr):
+    cdef np.uint64_t[:] result = np.empty(arr.shape[0], dtype=np.uint64)
+    # cdef int i = 0
+    cdef DTYPE_t* result_ptr = &result[0]
+    cdef DTYPE_t* arr_ptr = &arr[0]
+
+    for _ in range(arr.shape[0]):
+        result_ptr[0] = __builtin_popcountll(arr_ptr[0])
+        result_ptr += 1
+        arr_ptr += 1
+    return result
+
+
+cdef ctz_arr(DTYPE_t[:] arr):
+    cdef np.uint64_t[:] result = np.empty(arr.shape[0], dtype=np.uint64)
+    # cdef int i = 0
+    cdef DTYPE_t* result_ptr = &result[0]
+    cdef DTYPE_t* arr_ptr = &arr[0]
+
+    for _ in range(arr.shape[0]):
+        result_ptr[0] = __builtin_ctzll(arr_ptr[0])
+        result_ptr += 1
+        arr_ptr += 1
+    return result
+
+
+cdef clz_arr(DTYPE_t[:] arr):
+    cdef np.uint64_t[:] result = np.empty(arr.shape[0], dtype=np.uint64)
+    # cdef int i = 0
+    cdef DTYPE_t* result_ptr = &result[0]
+    cdef DTYPE_t* arr_ptr = &arr[0]
+
+    for _ in range(arr.shape[0]):
+        result_ptr[0] = __builtin_clzll(arr_ptr[0])
+        result_ptr += 1
+        arr_ptr += 1
+    return result
+
+
+cdef popcount64_arr_naive(DTYPE_t[:] arr):
+    cdef np.uint64_t[:] result = np.empty(arr.shape[0], dtype=np.uint64)
+    cdef int i = 0
+
+    for i in range(arr.shape[0]):
+        result[i] = __builtin_popcountll(arr[i])
+    return result
+
+
+def popcount64(np.ndarray[DTYPE_t, ndim=1] arr):
+    """Count the number of set bits in a 64-bit integer."""
+    return np.array(popcount64_arr(arr))
+
+
+cdef _popcount_reduce_at(DTYPE_t[:] ids, DTYPE_t[:] payload, float[:] output):
+    cdef DTYPE_t idx = 1
+    cdef DTYPE_t popcount_sum = __builtin_popcountll(payload[0])
+
+    # We already have 0, now add new values
+    while idx < ids.shape[0]:
+        if ids[idx] != ids[idx - 1]:
+            output[ids[idx - 1]] = popcount_sum
+            popcount_sum = 0
+        popcount_sum += __builtin_popcountll(payload[idx])
+        idx += 1
+    # Save final value
+    output[ids[idx - 1]] = popcount_sum
+
+
+def popcount_reduce_at(np.ndarray[DTYPE_t, ndim=1] ids,
+                       np.ndarray[DTYPE_t, ndim=1] payload,
+                       np.ndarray[np.float32_t, ndim=1] output):
+    """Write the sum of popcount of the payload at the indices in ids to the output array."""
+    if len(ids) != len(payload):
+        raise ValueError("ids and payload must have the same length")
+    return np.array(_popcount_reduce_at(ids, payload, output))
+
+
+cdef _key_sum_over(DTYPE_t[:] ids, DTYPE_t[:] count, float[:] output):
+    cdef DTYPE_t i = 0
+    for i in range(ids.shape[0]):
+        output[ids[i]] += count[i]
+
+
+def key_sum_over(np.ndarray[DTYPE_t, ndim=1] ids,
+                 np.ndarray[DTYPE_t, ndim=1] count,
+                 np.ndarray[np.float32_t, ndim=1] output):
+    """Write the last value of the payload at the indices in ids to the output array."""
+    _key_sum_over(ids, count, output)
+
+
+# Popcount reduce key-value pair
+# for words 0xKKKKKKKK...KKKKVVVV...VVVV
+# Returning two parallel arrays:
+#   - keys   - the keys
+#   - values - the popcount of the values with those keys
+cdef _popcount64_reduce(DTYPE_t[:] arr,
+                        DTYPE_t key_shift,
+                        DTYPE_t value_mask):
+    cdef float[:] popcounts = np.zeros(arr.shape[0], dtype=np.float32)
+    cdef DTYPE_t[:] keys = np.empty(arr.shape[0], dtype=np.uint64)
+    cdef float* popcounts_ptr = &popcounts[0]
+    cdef DTYPE_t* keys_ptr = &keys[0]
+    cdef DTYPE_t* arr_ptr = &arr[0]
+
+    cdef DTYPE_t last_key = arr_ptr[0] >> key_shift
+    cdef DTYPE_t key = last_key
+    keys_ptr[0] = last_key
+
+    for _ in range(arr.shape[0]):
+        key = arr_ptr[0] >> key_shift
+        if key == last_key:
+            popcounts_ptr[0] += __builtin_popcountll(arr_ptr[0] & value_mask)
+        else:
+            last_key = key
+            popcounts_ptr += 1
+            keys_ptr += 1
+            # Init next key
+            keys_ptr[0] = last_key
+            popcounts_ptr[0] = __builtin_popcountll(arr_ptr[0] & value_mask)
+        arr_ptr += 1
+    return keys, popcounts, (keys_ptr - &keys[0] + 1)
+
+
+# Branchless version using lookup table instead of if...
+# Popcount reduce key-value pair
+# for words 0xKKKKKKKK...KKKKVVVV...VVVV
+# Returning two parallel arrays:
+#   - keys   - the keys
+#   - values - the popcount of the values with those keys
+cdef _popcount64_reduce_nobranch(DTYPE_t[:] arr,
+                                 DTYPE_t key_shift,
+                                 DTYPE_t value_mask):
+    cdef float[:] popcounts = np.zeros(arr.shape[0], dtype=np.float32)
+    cdef DTYPE_t[:] keys = np.empty(arr.shape[0], dtype=np.uint64)
+    cdef float* popcounts_ptr = &popcounts[0]
+    cdef DTYPE_t* keys_ptr = &keys[0]
+    cdef DTYPE_t* arr_ptr = &arr[0]
+
+    cdef DTYPE_t last_key = arr_ptr[0] >> key_shift
+    cdef DTYPE_t key = last_key
+
+    keys_ptr[0] = last_key
+
+    for _ in range(arr.shape[0]):
+        key = arr_ptr[0] >> key_shift
+        popcounts_ptr += (key != last_key)
+        keys_ptr += (key != last_key)
+        popcounts_ptr[0] += __builtin_popcountll(arr_ptr[0] & value_mask)
+        keys_ptr[0] = key
+        last_key = key
+        arr_ptr += 1
+    return keys, popcounts, (keys_ptr - &keys[0] + 1)
+
+
+def popcount64_reduce(arr,
+                      key_shift,
+                      value_mask):
+    cdef DTYPE_t[:] arr_view = arr
+    if len(arr_view) == 0:
+        return np.array([]), np.array([])
+    keys, popcounts, results_idx = _popcount64_reduce(arr_view, key_shift, value_mask)
+    return np.array(keys[:results_idx]), np.array(popcounts[:results_idx])
---
+++ packages/searcharray/searcharray-0.0.68.tar.gz/searcharray-0.0.68/searcharray/similarity.py
@@ -0,0 +1,124 @@
+"""Similarity functions given term stats."""
+from typing import Protocol, Dict, Any
+import numpy as np
+from searcharray.bm25 import bm25_score
+
+
+class ScoringContext:
+    """Scoring context for similarity functions."
+
+    May be reused across multiple calls to similarity functions over
+    the lifetime of the index, allowing caching of any non-query-specific
+    information.
+
+    """
+
+    def __init__(self, doc_lens: np.ndarray, avg_doc_lens: int, num_docs: int):
+        """Initialize."""
+        self.doc_lens = doc_lens
+        self.avg_doc_lens = avg_doc_lens
+        self.num_docs = num_docs
+        self.working: Dict[str, Any] = {}
+
+    def same_as(self, other: "ScoringContext") -> bool:
+        """Check if the context is the same as another."""
+        if other is None:
+            return False
+        return (self.doc_lens is other.doc_lens
+                and self.avg_doc_lens == other.avg_doc_lens
+                and self.num_docs == other.num_docs)
+
+
+class Similarity(Protocol):
+    """Similarity function protocol."""
+
+    def __call__(self, term_freqs: np.ndarray, doc_freqs: np.ndarray,
+                 doc_lens: np.ndarray, avg_doc_lens: int, num_docs: int) -> np.ndarray:
+        """Calculate similarity scores."""
+        ...
+
+
+def compute_idf(num_docs, dfs):
+    """Calculate idf."""
+    return np.sum(np.log(1 + (num_docs - dfs + 0.5) / (dfs + 0.5)))
+
+
+def compute_adj_doc_lens(doc_lens, avg_doc_lens, k1, b):
+    if avg_doc_lens == 0:
+        adj_doc_lens = np.zeros_like(doc_lens, dtype=np.float32)
+    else:
+        adj_doc_lens = np.divide(doc_lens, avg_doc_lens, dtype=np.float32)
+    adj_doc_lens *= b
+    adj_doc_lens += 1 - b
+    adj_doc_lens *= k1
+    assert adj_doc_lens.dtype == np.float32
+    # Divide tf in place for perf, but this means
+    # we can't use the same term_freqs for different k1, b
+    return adj_doc_lens
+
+
+def bm25_similarity(k1: float = 1.2, b: float = 0.75) -> Similarity:
+    """BM25 similarity function, as in Lucene 9."""
+    context = None
+    def bm25(term_freqs: np.ndarray, doc_freqs: np.ndarray,
+             doc_lens: np.ndarray, avg_doc_lens: int, num_docs: int) -> np.ndarray:
+        """Calculate BM25 scores."""
+        # Sum doc freqs
+        # Calculate idf
+        nonlocal context
+        assert term_freqs.dtype == np.float32
+        new_context = ScoringContext(doc_lens, avg_doc_lens, num_docs)
+        if context is None or not context.same_as(new_context):
+            context = new_context
+
+        idf = compute_idf(context.num_docs, doc_freqs)
+        try:
+            adj_doc_lens = context.working["adj_doc_lens"]
+            bm25_score(term_freqs, adj_doc_lens, idf)
+            return term_freqs
+        except (KeyError, ValueError):
+            try:
+                adj_doc_lens = compute_adj_doc_lens(context.doc_lens, context.avg_doc_lens, k1, b)
+                context.working["adj_doc_lens"] = adj_doc_lens
+                assert term_freqs.dtype == np.float32
+                bm25_score(term_freqs, adj_doc_lens, idf)
+                return term_freqs
+            except ValueError:
+                adj_doc_lens = compute_adj_doc_lens(doc_lens, avg_doc_lens, k1, b)
+                assert term_freqs.dtype == np.float32
+                bm25_score(term_freqs, adj_doc_lens, idf)
+                return term_freqs
+    return bm25
+
+
+def bm25_legacy_similarity(k1: float = 1.2, b: float = 0.75) -> Similarity:
+    """BM25 similarity prior to LUCENE-8563 with k1 + 1 in numerator."""
+    # (freq * (k1 + 1)) / (freq + k1 * (1 - b + b * fieldLength / avgFieldLength))
+    def bm25(term_freqs: np.ndarray, doc_freqs: np.ndarray,
+             doc_lens: np.ndarray, avg_doc_lens: int, num_docs: int) -> np.ndarray:
+        """Calculate BM25 scores."""
+        # Calculate idf
+        idf = compute_idf(num_docs, doc_freqs)
+        # Calculate tf
+        tf = (term_freqs * (k1 + 1)) / (term_freqs + k1 * (1 - b + b * doc_lens / avg_doc_lens))
+        return idf * tf
+    return bm25
+
+
+def classic_similarity() -> Similarity:
+    """Classic Lucene TF-IDF similarity function."""
+    def classic(term_freqs: np.ndarray, doc_freqs: np.ndarray,
+                doc_lens: np.ndarray, avg_doc_lens: int, num_docs: int) -> np.ndarray:
+        """Calculate classic TF-IDF scores."""
+        # Sum doc freqs
+        sum_dfs = np.sum(doc_freqs, axis=0)
+        # Calculate idf as log((docCount+1)/(docFreq+1)) + 1
+        idf = np.log((num_docs + 1) / (sum_dfs + 1)) + 1
+        length_norm = 1.0 / np.sqrt(doc_lens)
+        # Calculate tf
+        tf = np.sqrt(term_freqs)
+        return idf * tf * length_norm
+    return classic
+
+
+default_bm25 = bm25_similarity()
