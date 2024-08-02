d3308d46d31fd4d5c7f69f85205b68562c455396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:08 +0000

    Add package gpxtable/1.5.2/gpxtable-1.5.2-py3-none-any.whl

---
+++ packages/gpxtable/gpxtable-1.5.2-py3-none-any.whl/gpxtable/__init__.py
@@ -0,0 +1,9 @@
+"""
+gpxtable - Create a markdown template from a Garmin GPX file for route information
+"""
+
+__version__ = "1.5.2"
+__all__ = ["GPXTableCalculator"]
+__author__ = "Paul Traina"
+
+from .gpxtable import GPXTableCalculator
