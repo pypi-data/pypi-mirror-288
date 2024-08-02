f94a657ad294082e8a053a17e78d0a936949a6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:04:30 +0000

    Add package FiinQuant/0.7.19/FiinQuant-0.7.19-py3-none-any.whl

---
+++ packages/FiinQuant/FiinQuant-0.7.19-py3-none-any.whl/FiinQuant/TickerData.pyi
@@ -0,0 +1,76 @@
+import pandas as pd
+
+# class TickerData:
+#     def __init__(self, data: pd.DataFrame) -> None:
+#         self.__private_attribute: pd.DataFrame = data
+#         self.TotalMatchVolume: int
+#         self.MarketStatus: str
+#         self.TradingDate: str
+#         self.MatchType: str
+#         self.ComGroupCode: str
+#         self.OrganCode: str
+#         self.Ticker: str
+#         self.ReferencePrice: float
+#         self.OpenPrice: float
+#         self.ClosePrice: float
+#         self.CeilingPrice: float
+#         self.FloorPrice: float
+#         self.HighestPrice: float
+#         self.LowestPrice: float
+#         self.MatchPrice: float
+#         self.PriceChange: float
+#         self.PercentPriceChange: float
+#         self.MatchVolume: int
+#         self.MatchValue: float
+#         self.TotalMatchValue: float
+#         self.TotalBuyTradeVolume: int
+#         self.TotalSellTradeVolume: int
+#         self.DealPrice: float
+#         self.TotalDealVolume: int
+#         self.TotalDealValue: float
+#         self.ForeignBuyVolumeTotal: int
+#         self.ForeignBuyValueTotal: float
+#         self.ForeignSellVolumeTotal: int
+#         self.ForeignSellValueTotal: float
+#         self.ForeignTotalRoom: int
+#         self.ForeignCurrentRoom: int
+
+#     def get_data(self) -> pd.DataFrame: ...
+
+class TickerData:
+    def __init__(self, data: pd.DataFrame) -> None:
+        self.__private_attribute: pd.DataFrame = data
+        self.TotalMatchVolume: str
+        self.MarketStatus: str
+        self.TradingDate: str
+        self.MatchType: str
+        self.ComGroupCode: str
+        self.OrganCode: str
+        self.Ticker: str
+        self.ReferencePrice: str
+        self.OpenPrice: str
+        self.ClosePrice: str
+        self.CeilingPrice: str
+        self.FloorPrice: str
+        self.HighestPrice: str
+        self.LowestPrice: str
+        self.MatchPrice: str
+        self.PriceChange: str
+        self.PercentPriceChange: str
+        self.MatchVolume: str
+        self.MatchValue: str
+        self.TotalMatchValue: str
+        self.TotalBuyTradeVolume: str
+        self.TotalSellTradeVolume: str
+        self.DealPrice: str
+        self.TotalDealVolume: str
+        self.TotalDealValue: str
+        self.ForeignBuyVolumeTotal: str
+        self.ForeignBuyValueTotal: str
+        self.ForeignSellVolumeTotal: str
+        self.ForeignSellValueTotal: str
+        self.ForeignTotalRoom: str
+        self.ForeignCurrentRoom: str
+
+    def get_data(self) -> pd.DataFrame: ...
+
