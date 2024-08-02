53b7a812eebfb85f508d417984d10b365db31a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:10 +0000

    Add package codialog/0.1.6/codialog-0.1.6-py3-none-any.whl

---
+++ packages/codialog/codialog-0.1.6-py3-none-any.whl/build/lib/flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/api/data')
+def get_data():
+    data = {
+        "message": "Hello from Flask!"
+    }
+    return jsonify(data)
+
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
+
+
