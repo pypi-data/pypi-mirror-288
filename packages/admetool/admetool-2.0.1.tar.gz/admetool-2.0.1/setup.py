df80c88ea1293514d7c871c481840e213a59085b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:46 +0000

    Add package admetool/2.0.1/admetool-2.0.1.tar.gz

---
+++ packages/admetool/admetool-2.0.1.tar.gz/admetool-2.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import pathlib
+
+setup(name='admetool',
+    version='2.0.1',
+    license='MIT License',
+    author='Júlio César Xavier',
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author_email='jcaxavier2@gmail.com',
+    keywords='admetlab',
+    description=u'Tool that uses Pharmit data to perform an ADMET analysis',
+    packages= find_packages(),
+    entry_points={
+        'console_scripts': [
+            'admetscore = admetool.app:main',
+        ],
+    },
+    
+    install_requires=["pandas==2.2.2","rdkit==2023.9.6","numpy==1.26.4","XlsxWriter==3.2.0","openpyxl==3.1.5","colorama==0.4.6"],
+    scripts=['admetool/app.py'])
