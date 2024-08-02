48169deccb7a2dc4954523c0e1f3026b6d416b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 15:04:26 +0000

    Add package GridCal/5.1.21/GridCal-5.1.21.tar.gz

---
+++ packages/GridCal/GridCal-5.1.21.tar.gz/GridCal-5.1.21/GridCal/ExecuteGridCal.py
@@ -0,0 +1,40 @@
+# This file is part of GridCal.
+#
+# GridCal is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GridCal is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GridCal.  If not, see <http://www.gnu.org/licenses/>.
+import os
+import sys
+import ctypes
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+from GridCal.__version__ import about_msg
+from GridCal.Gui.Main.GridCalMain import runGridCal
+import platform
+
+if platform.system() == 'Windows':
+    # this makes the icon display properly under windows
+    myappid = 'mycompany.myproduct.subproduct.version'  # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+if __name__ == "__main__":
+    print('Loading GridCal...')
+    print(about_msg)
+
+    # Set the application style to the clear theme
+    os.environ["QT_QUICK_CONTROLS_STYLE"] = "Basic"
+    # os.environ["QT_QPA_PLATFORMTHEME"] = "qt5ct"  # this forces QT-only menus and look and feel
+
+    runGridCal()
+
