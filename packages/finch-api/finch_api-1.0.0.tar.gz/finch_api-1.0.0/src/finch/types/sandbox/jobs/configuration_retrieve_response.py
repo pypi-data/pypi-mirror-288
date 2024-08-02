0c0b48c1c516d4a7b8484b7ee05bda2c31b8f293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 05:05:12 +0000

    Add package finch-api/1.0.0/finch_api-1.0.0.tar.gz

---
+++ packages/finch-api/finch_api-1.0.0.tar.gz/finch_api-1.0.0/src/finch/types/sandbox/jobs/configuration_retrieve_response.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .sandbox_job_configuration import SandboxJobConfiguration
+
+__all__ = ["ConfigurationRetrieveResponse"]
+
+ConfigurationRetrieveResponse = List[SandboxJobConfiguration]
