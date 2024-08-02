35ddb6199d5872a029175367a163c0e09635a2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:53 +0000

    Add package multiaihub/0.1.5/multiaihub-0.1.5.tar.gz

---
+++ packages/multiaihub/multiaihub-0.1.5.tar.gz/multiaihub-0.1.5/setup.py
@@ -0,0 +1,21 @@
+"""Module providing info about the package and its dependencies."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='multiaihub',
+    version='0.1.5',  # Start with a version number
+    description='Short description of your project',
+    license='Apache License 2.0',
+    long_description="MAH - Multi AI Hub is a project designed to make it easy to send the same prompt to multiple LLMs to help with testing and comparison.",
+    author='Mike Wolfson',
+    author_email='mike@ableandroid.com',
+    packages=find_packages(),
+    py_modules=["multi_ai_hub"],
+    install_requires=[
+        'python-dotenv==1.0.0',
+        'google.generativeai==0.5.4',
+        'openai==1.35.2',
+        'anthropic==0.17.0',
+    ],
+)
