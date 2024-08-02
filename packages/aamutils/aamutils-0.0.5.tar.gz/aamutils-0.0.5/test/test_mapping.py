93d12603357193739f5ec29b4fd23eafe53ce8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 15:04:06 +0000

    Add package aamutils/0.0.5/aamutils-0.0.5.tar.gz

---
+++ packages/aamutils/aamutils-0.0.5.tar.gz/aamutils-0.0.5/test/test_mapping.py
@@ -0,0 +1,16 @@
+from aamutils.mapping import Mapper
+from aamutils.algorithm.aaming import get_its, get_rc
+from aamutils.utils import smiles_to_graph
+
+
+def test_mapper_with_expected_rc():
+    # Diels-Alder reaction example
+    smiles = "C1=CC=CC1.C1=CC=CC1>>C1=CC2CC1C3C2C=CC3"
+    mapper = Mapper()
+    aam_smiles = mapper.get_aam(smiles, expected_rc=(6, 6))
+    assert isinstance(aam_smiles, str)
+    g, h = smiles_to_graph(aam_smiles)
+    its = get_its(g, h)
+    rc = get_rc(its)
+    assert len(rc.nodes) == 6
+    assert len(rc.edges) == 6
