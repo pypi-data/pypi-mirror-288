ac6bd945fb0bc21b1e40eff7a5147ecc94df2b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 07:04:12 +0000

    Add package vm-x-ai-sdk/0.7.0/vm_x_ai_sdk-0.7.0-py3-none-any.whl

---
+++ packages/vm-x-ai-sdk/vm_x_ai_sdk-0.7.0-py3-none-any.whl/vmxai/__init__.py
@@ -0,0 +1,32 @@
+"""VM-X Python SDK"""
+
+from vmxai.auth.api_key import VMXClientAPIKey
+from vmxai.auth.oauth import VMXClientOAuth
+from vmxai.auth.provider import VMXClientAuthProvider
+from vmxai.client import VMXClient
+from vmxai.protos.completion.completion_pb2 import (
+    CompletionResponse,
+    CompletionUsage,
+    RequestMessageToolCall,
+    RequestMessageToolCallFunction,
+    RequestToolChoiceFunction,
+    RequestToolChoiceItem,
+)
+from vmxai.types import CompletionRequest, RequestMessage, RequestToolFunction, RequestTools
+
+__all__ = [
+    VMXClient,
+    VMXClientAuthProvider,
+    VMXClientOAuth,
+    VMXClientAPIKey,
+    CompletionRequest,
+    CompletionResponse,
+    CompletionUsage,
+    RequestMessage,
+    RequestToolChoiceItem,
+    RequestToolChoiceFunction,
+    RequestTools,
+    RequestMessageToolCall,
+    RequestMessageToolCallFunction,
+    RequestToolFunction,
+]
