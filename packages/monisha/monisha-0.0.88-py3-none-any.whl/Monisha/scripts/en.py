8b25f7f596eff7958ca5ef3a9d5e5bd6650a1bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 06:04:06 +0000

    Add package monisha/0.0.88/monisha-0.0.88-py3-none-any.whl

---
+++ packages/monisha/monisha-0.0.88-py3-none-any.whl/Monisha/scripts/en.py
@@ -0,0 +1,16 @@
+
+class Scripted(object):
+
+    DATA01 = ""
+    DATA04 = "-"
+    DATA02 = " "
+    DATA03 = "ㅤ"
+    DATA05 = "NA"
+    DATA08 = "0 𝙱"
+    DATA09 = "0 B"
+    DATA12 = "{}{}"
+    DATA06 = ".tmp"
+    DATA15 = "0𝚜𝚎𝚌"
+    DATA16 = "0sec"
+    DATA13 = "Unknown"
+    DATA14 = "Unknown.tmp"
