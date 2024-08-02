c7c62014e032f60465b7b9e312e8c8d88aea53ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-02 14:03:52 +0000

    Add package three-d-city-model-generator/0.1.0/three_d_city_model_generator-0.1.0.tar.gz

---
+++ packages/three-d-city-model-generator/three_d_city_model_generator-0.1.0.tar.gz/three_d_city_model_generator-0.1.0/src/three_d_city_model_generator/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for 3D City Model Generator."""
+
+__author__ = """Kunihiko Fujiwara"""
+__email__ = 'kunihiko@nus.edu.sg'
+__version__ = '0.1.0'
+
+from .three_d_city_model_generator import generate_model
