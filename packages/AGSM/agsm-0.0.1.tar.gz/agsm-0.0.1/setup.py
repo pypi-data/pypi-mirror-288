6c098e9a04ffd308e9ed5200141c0efdc2a6c360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 15:03:48 +0000

    Add package AGSM/0.0.1/agsm-0.0.1.tar.gz

---
+++ packages/AGSM/agsm-0.0.1.tar.gz/agsm-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AGSM',
+    version='0.0.1',
+    author='Ayato',
+    author_email='ayato.yofukashi@gmail.com',
+    description='???',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/YFKS/AGSM',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
