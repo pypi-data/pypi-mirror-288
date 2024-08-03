47df5d8db8469678590b59f6b2c30aee3bd21fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:04:41 +0000

    Add package gpu-runner/0.0.3/gpu_runner-0.0.3.tar.gz

---
+++ packages/gpu-runner/gpu_runner-0.0.3.tar.gz/gpu_runner-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from grun import __version__
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="gpu-runner",
+    version=__version__,
+    description="Run a command requiring GPU resources",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    install_requires=[
+        "filelock",
+        "nvidia-ml-py3",
+    ],
+    entry_points={"console_scripts": ["grun=grun.__main__:main"]},
+    url="https://github.com/cosmoquester/gpu-runner.git",
+    author="Park Sangjun",
+    packages=find_packages(exclude=["tests"]),
+)
