04b8e116c141b257ca648286b2ec0f3571c992dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:49 +0000

    Add package fastrpa/1.0.0/fastrpa-1.0.0-py3-none-any.whl

---
+++ packages/fastrpa/fastrpa-1.0.0-py3-none-any.whl/fastrpa/__init__.py
@@ -0,0 +1,29 @@
+from fastrpa.app import FastRPA, Web
+from fastrpa.core.elements import (
+    Element,
+    InputElement,
+    FileInputElement,
+    RadioInputElement,
+    CheckboxElement,
+    SelectElement,
+    ListElement,
+    ButtonElement,
+    FormElement,
+    TableElement,
+)
+
+
+__all__ = (
+    'FastRPA',
+    'Web',
+    'Element',
+    'InputElement',
+    'FileInputElement',
+    'RadioInputElement',
+    'CheckboxElement',
+    'SelectElement',
+    'ListElement',
+    'ButtonElement',
+    'FormElement',
+    'TableElement',
+)
