26fb12e11659f75d5f50ad820872b26e45e04300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:04:33 +0000

    Add package haskellian/0.4.10/haskellian-0.4.10.tar.gz

---
+++ packages/haskellian/haskellian-0.4.10.tar.gz/haskellian-0.4.10/src/haskellian/dicts/__init__.pyi
@@ -0,0 +1,15 @@
+from .basics import Mapper, map, map_k, map_kv, filter, filter_k, flatmap, flatmap_k
+from .grouping import group_by, zip, unzip, aggregate
+from .dict import Dict
+from .lifting import lift
+from .misc import unpack, evolve, insert, remove, rename
+
+__all__ = [
+  'Mapper',
+  'map', 'map_k', 'map_kv',
+  'filter', 'filter_k',
+  'flatmap', 'flatmap_k',
+  'group_by', 'zip', 'unzip', 'aggregate',
+  'Dict',
+  'lift', 'unpack', 'evolve', 'insert', 'remove', 'rename'
+]
