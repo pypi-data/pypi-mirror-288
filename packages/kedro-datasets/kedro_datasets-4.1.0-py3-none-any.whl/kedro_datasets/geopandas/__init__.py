2bbc28f6c877382810a90faf40f52eb275cdfad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:03:35 +0000

    Add package kedro-datasets/4.1.0/kedro_datasets-4.1.0-py3-none-any.whl

---
+++ packages/kedro-datasets/kedro_datasets-4.1.0-py3-none-any.whl/kedro_datasets/geopandas/__init__.py
@@ -0,0 +1,12 @@
+"""``GeoJSONDataset`` is an ``AbstractVersionedDataset`` to save and load GeoJSON files."""
+
+from typing import Any
+
+import lazy_loader as lazy
+
+# https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
+GeoJSONDataset: Any
+
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__, submod_attrs={"geojson_dataset": ["GeoJSONDataset"]}
+)
