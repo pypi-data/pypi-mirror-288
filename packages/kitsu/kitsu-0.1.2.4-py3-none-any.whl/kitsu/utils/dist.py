a03fd4159427651017b327c4bfef8339afbb9bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:44 +0000

    Add package kitsu/0.1.2.4/kitsu-0.1.2.4-py3-none-any.whl

---
+++ packages/kitsu/kitsu-0.1.2.4-py3-none-any.whl/kitsu/utils/dist.py
@@ -0,0 +1,51 @@
+import torch
+import torch.distributed as dist
+
+
+def safe_all_reduce(x, reduce_op=dist.ReduceOp.SUM) -> torch.Tensor:
+    if dist.is_initialized():
+        dist.all_reduce(x, reduce_op)
+    return x
+
+
+def safe_all_mean(x) -> torch.Tensor:
+    x = safe_all_reduce(x)
+    if dist.is_initialized():
+        x /= dist.get_world_size()
+    return x
+
+
+def safe_all_gather(x, dim=0) -> torch.Tensor:
+    if dist.is_initialized():
+        xs = [torch.empty_like(x) for _ in range(dist.get_world_size())]
+        dist.all_gather(xs, x)
+        x = torch.cat(xs, dim=dim)
+    return x
+
+
+def safe_barrier():
+    if dist.is_initialized():
+        dist.barrier()
+
+
+def safe_broadcast(x, src) -> torch.Tensor:
+    if dist.is_initialized():
+        dist.broadcast(x, src)
+    return x
+
+
+def is_rankzero():
+    if dist.is_initialized():
+        return dist.get_rank() == 0
+    else:
+        return True
+
+
+def rankzero_only(func):
+    def wrapper(*args, **kwargs):
+        if not is_rankzero():
+            return
+
+        return func(*args, **kwargs)
+
+    return wrapper
