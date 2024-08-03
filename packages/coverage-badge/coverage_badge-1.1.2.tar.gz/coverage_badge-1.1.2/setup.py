04067c20ba34cfac03c50da8f05d7e65d9ca9f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:05:16 +0000

    Add package coverage-badge/1.1.2/coverage_badge-1.1.2.tar.gz

---
+++ packages/coverage-badge/coverage_badge-1.1.2.tar.gz/coverage_badge-1.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+readme = open('README.rst').read()
+
+setup(name='coverage-badge',
+      version='1.1.2',
+      description='Generate coverage badges for Coverage.py.',
+      author='Danilo Bargen',
+      author_email='mail@dbrgn.ch',
+      url='https://github.com/dbrgn/coverage-badge',
+      install_requires=['coverage', 'setuptools'],
+      packages=['coverage_badge'],
+      zip_safe=True,
+      include_package_data=True,
+      license='MIT',
+      keywords='coverage badge shield',
+      long_description=readme,
+      entry_points={
+          'console_scripts': [
+              'coverage-badge = coverage_badge.__main__:main',
+          ]
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Testing',
+      ],
+)
