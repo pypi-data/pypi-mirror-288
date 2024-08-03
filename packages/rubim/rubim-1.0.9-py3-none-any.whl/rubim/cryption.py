6042a49a99cd6da0a9e999ee10e8319d6dc0e4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 15:03:54 +0000

    Add package rubim/1.0.9/rubim-1.0.9-py3-none-any.whl

---
+++ packages/rubim/rubim-1.0.9-py3-none-any.whl/rubim/cryption.py
@@ -0,0 +1,58 @@
+from base64 import b64encode, urlsafe_b64decode
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+
+class cryption:
+
+    def __init__(self, auth:str):
+        self.key = bytearray(self.secret(auth), 'UTF-8')
+        self.iv = bytearray.fromhex('0' * 32)
+
+    def replaceCharAt(self, e, t, i):
+        return e[0:t] + i + e[t + len(i):]
+
+    def secret(self, e):
+        t = e[0:8]
+        i = e[8:16]
+        n = e[16:24] + t + e[24:32] + i
+        s = 0
+        while s < len(n):
+            e = n[s]
+            if e >= '0' and e <= '9':
+                t = chr((ord(e[0]) - ord('0') + 5) % 10 + ord('0'))
+                n = self.replaceCharAt(n, s, t)
+            else:
+                t = chr((ord(e[0]) - ord('a') + 9) % 26 + ord('a'))
+                n = self.replaceCharAt(n, s, t)
+            s += 1
+        return n
+
+    def encrypt(self, text):
+        return b64encode(
+            AES.new(self.key, AES.MODE_CBC, self.iv)
+            .encrypt(pad(text.encode('UTF-8'),AES.block_size))
+        ).decode('UTF-8')
+
+
+    def decrypt(self, text):
+        return unpad(
+            AES.new(self.key, AES.MODE_CBC, self.iv)
+            .decrypt(urlsafe_b64decode(text.encode('UTF-8')))
+            ,AES.block_size
+        ).decode('UTF-8')
+    
+    def changeAuthType(self, auth_enc):
+        lowercase = 'abcdefghijklmnopqrstuvwxyz'
+        uppercase = 'abcdefghijklmnopqrstuvwxyz'.upper()
+        digits = '0123456789'
+        n = ''
+        for s in auth_enc:
+            if s in lowercase:
+                n += chr(((32 - (ord(s) - 97)) % 26) + 97)
+            elif s in uppercase:
+                n += chr(((29- (ord(s) - 65)) % 26) + 65)
+            elif s in digits:
+                n += chr(((13 - (ord(s)- 48)) % 10) + 48)
+            else:
+                n += s
+        return n
