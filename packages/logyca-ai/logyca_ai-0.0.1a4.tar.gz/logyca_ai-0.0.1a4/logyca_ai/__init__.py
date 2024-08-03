da3a89c99735f849f6795a6f30a5e1e23a0ec19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 06:04:21 +0000

    Add package logyca-ai/0.0.1a4/logyca_ai-0.0.1a4.tar.gz

---
+++ packages/logyca-ai/logyca_ai-0.0.1a4.tar.gz/logyca_ai-0.0.1a4/logyca_ai/__init__.py
@@ -0,0 +1,6 @@
+from logyca_ai.utils.constants.image import ImageResolution
+from logyca_ai.utils.constants.content import ContentRole, ContentType
+from logyca_ai.utils.helpers.azure_openai_chatgpt import AzureOpenAIChatGPT
+from logyca_ai.utils.schemes.input.conversations import Content, UserMessage, AssistantMessage, PDFMessage, ImageMessage
+
+from samples.assets_for_conversations.conversation_samples import get_content_image_sample, get_content_pdf_sample
