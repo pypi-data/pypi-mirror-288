a22d0ef0e46355f393de29e81296caa9810e833f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 06:04:31 +0000

    Add package starbear/0.2.0/starbear-0.2.0-py3-none-any.whl

---
+++ packages/starbear/starbear-0.2.0-py3-none-any.whl/starbear/__init__.py
@@ -0,0 +1,12 @@
+from hrepr import H, J, returns
+from hrepr.resource import Resource
+
+from .common import UsageError, here
+from .config import config
+from .core.app import bear, simplebear
+from .core.constructors import BrowserEvent, FormData, NamespaceDict
+from .core.page import Component, Page, selector_for
+from .core.reg import Reference
+from .core.templating import Template, template
+from .core.utils import ClientWrap, FeedbackQueue, Queue, VirtualFile, rewrap
+from .version import version
