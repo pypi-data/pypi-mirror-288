ca4b512080db04056d0b154e172f6a6fc39a92cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:04:23 +0000

    Add package cututil/0.0.2/cututil-0.0.2-py3-none-any.whl

---
+++ packages/cututil/cututil-0.0.2-py3-none-any.whl/cututil/cututil.py
@@ -0,0 +1,8 @@
+import csv
+
+
+def proc():
+    with open('eggs.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            print(', '.join(row))
