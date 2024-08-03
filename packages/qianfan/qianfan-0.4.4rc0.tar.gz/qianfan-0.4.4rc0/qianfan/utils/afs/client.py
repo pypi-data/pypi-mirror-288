20b0a438576a42c2c33511db52ccd0628e042346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:04:08 +0000

    Add package qianfan/0.4.4rc0/qianfan-0.4.4rc0.tar.gz

---
+++ packages/qianfan/qianfan-0.4.4rc0.tar.gz/qianfan-0.4.4rc0/qianfan/utils/afs/client.py
@@ -0,0 +1,12 @@
+# class AFSClient(object):
+#     def __init__(self, *args, **kwargs):
+#         self.dfs_endpoint = kwargs.get("dfs_endpoint", None)
+#         self.ugi = kwargs.get("ugi", None)
+
+#         pass
+
+#     def put(self, local_path, remote_path=None):
+#         pass
+
+#     def get(self, remote_path, local_path=None):
+#         pass
