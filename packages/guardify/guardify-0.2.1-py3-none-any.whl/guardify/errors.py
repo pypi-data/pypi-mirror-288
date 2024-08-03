892ff857f3c2a041c10118e4c316e5a43b00e627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:05:12 +0000

    Add package guardify/0.2.1/guardify-0.2.1-py3-none-any.whl

---
+++ packages/guardify/guardify-0.2.1-py3-none-any.whl/guardify/errors.py
@@ -0,0 +1,22 @@
+class TokenError(ValueError):
+    pass
+
+
+class ClockError(TokenError):
+    pass
+
+
+class ExpirationError(TokenError):
+    pass
+
+
+class PermissionError(TokenError):
+    pass
+
+
+class SignatureError(TokenError):
+    pass
+
+
+class RevocationError(TokenError):
+    pass
