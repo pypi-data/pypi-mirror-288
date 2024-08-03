63d9c3f06d1c4e21b92c919723f67f88dee7c5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 06:04:29 +0000

    Add package sammo/0.2.1/sammo-0.2.1.tar.gz

---
+++ packages/sammo/sammo-0.2.1.tar.gz/sammo-0.2.1/sammo/components_test.py
@@ -0,0 +1,26 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+import pytest
+
+from sammo.base import Template
+from sammo.components import Output, Union, ForEach, GenerateText
+from sammo.runners import MockedRunner
+
+
+@pytest.mark.asyncio
+async def test_union():
+    res = await Union("a", "b", "c")(None, dict())
+    assert res.value == ["a", "b", "c"]
+
+
+@pytest.mark.asyncio
+async def test_for_each():
+    res = await ForEach("x", Union("a", "b", "c"), Template(".{{x}}"))(None, dict())
+    assert res.value == [".a", ".b", ".c"]
+
+
+@pytest.mark.asyncio
+async def test_generate_text():
+    runner = MockedRunner("Return value.")
+    res = await GenerateText("This is a simple test.")(runner, dict())
+    assert res.value == "Return value."
