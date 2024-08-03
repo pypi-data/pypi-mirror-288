33dc7662726e63dcb484cba0670066e481ad547c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:46 +0000

    Add package hal9/2.6.0/hal9-2.6.0-py3-none-any.whl

---
+++ packages/hal9/hal9-2.6.0-py3-none-any.whl/hal9/__init__.py
@@ -0,0 +1,7 @@
+from hal9.create import create
+from hal9.run import run
+from hal9.deploy import deploy
+from hal9.iobind import load, save, input
+from hal9.code import extract
+from hal9.urls import is_url, url_contents
+from hal9.events import event
