e21854803839859f4fa65755743e591b75e8f0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:41 +0000

    Add package UzbekistanWeather/1.0.5/uzbekistanweather-1.0.5.tar.gz

---
+++ packages/UzbekistanWeather/uzbekistanweather-1.0.5.tar.gz/uzbekistanweather-1.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from UzbekistanWeather.weather import UzbekistanWeather
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name="UzbekistanWeather",
+    version="1.0.5",
+    packages=find_packages(),
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author="Sino",
+    author_email="farmonovsino@gmail.com",
+    url="https://github.com/farmonovsino4",
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ]
+)
