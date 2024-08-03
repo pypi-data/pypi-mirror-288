cf873ebc8269b237accd139d8cccd4e66dc70c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:04:55 +0000

    Add package carbon-verifier/0.1.1/carbon_verifier-0.1.1.tar.gz

---
+++ packages/carbon-verifier/carbon_verifier-0.1.1.tar.gz/carbon_verifier-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='carbon_verifier',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='carbon',
+    author_email='team@carbon.ai',
+    description='A library to verify Carbon webhook events',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Carbon-for-Developers/webhook-library.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
