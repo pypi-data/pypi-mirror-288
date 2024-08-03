a1436c83d05614f313ae77ed00cac54c9d6ea262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 05:04:02 +0000

    Add package codiac/0.0.3/codiac-0.0.3.tar.gz

---
+++ packages/codiac/codiac-0.0.3.tar.gz/codiac-0.0.3/README.md
@@ -0,0 +1,12 @@
+# Comprehensive Domain Analysis of Interaction Contacts (CoDIAC)
+This is the source code for CoDIAC, an open source Python toolkit for harnessing InterPro, Uniprot, PDB, and AlphaFold for generating references of proteins containing domains of interest and analyzing the contacts that exist between that domain and other regions of interest (other domains on the same protein or ligands)
+
+CoDIAC is published under the GNU General Public License as published by the Free Software Foundation, version 3 of the License.  See LICENSE.txt
+
+Python version 3.0 or greater is required to use CoDIAC.
+
+Documentation: https://naeglelab.github.io/CoDIAC/index.html
+
+Authors: The Naegle Lab, including: Kristen M. Naegle, Alekhya Kandoor, Saqib Rizvi, Julianna Hitchock, Logan Campbell, and Gabi Martinez
+
+How to Cite: Please cite our [BioRxiv paper](https://www.biorxiv.org/content/10.1101/2024.07.18.604100v1)
---
+++ packages/codiac/codiac-0.0.3.tar.gz/codiac-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+# Run setup
+setup(
+    name="codiac",
+    version="0.0.3",
+    author="Naegle Lab",
+    author_email="kmn4mj@virginia.edu",
+    url="https://github.com/NaegleLab/CoDIAC",
+    install_requires=['pandas', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'statsmodels', 'biopython','requests'],
+    license='GNU General Public License v3',
+    description='CoDIAC: COmprehensive Domain Interface Anlysis of Contacts',
+#    long_description=This is the source code for CoDIAC, an open source Python toolkit for harnessing InterPro, Uniprot, PDB, and AlphaFold for generating references of proteins containing domains of interest and analyzing the contacts that exist between that domain and other regions of interest."""   
+    project_urls = {'Documentation': 'https://naeglelab.github.io/CoDIAC/index.html'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data = True,
+    python_requires=">=3.6",
+    package_data={'': ['CoDIAC/data/proteomescout_everything_20190701/*.tsv']},
+    zip_safe = False
+)
