c505acbd19a1154ba8b7bc7d5576ed819f7e9a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:03:43 +0000

    Add package pysails/0.1.1/pysails-0.1.1-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/pysails/pysails-0.1.1-cp311-cp311-macosx_11_0_arm64.whl/sails/__init__.py
@@ -0,0 +1,20 @@
+from .sails_module import (
+                           HKL,
+                           Reflection,
+                           Pair,
+                           Structure,
+                           Model,
+                           Chain,
+                           Residue,
+                           Atom,
+                           Position,
+                           Element,
+                           SeqId,
+                           n_glycosylate_from_objects,
+                           c_glycosylate_from_objects,
+                           Cell,
+                           MTZ,
+                           GlycoSite,
+                           Dot)
+from .__version__ import __version__
+from .main import glycosylate
