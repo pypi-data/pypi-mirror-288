82bea0ecc76cebb6f1feeab9e2c72dae2991f5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:04:13 +0000

    Add package cli-base-utilities/0.10.1/cli_base_utilities-0.10.1.tar.gz

---
+++ packages/cli-base-utilities/cli_base_utilities-0.10.1.tar.gz/cli_base_utilities-0.10.1/cli_base/demo/settings.py
@@ -0,0 +1,33 @@
+import dataclasses
+
+from cli_base.systemd.data_classes import BaseSystemdServiceInfo, BaseSystemdServiceTemplateContext
+
+
+@dataclasses.dataclass
+class SystemdServiceTemplateContext(BaseSystemdServiceTemplateContext):
+    """
+    CLI-Base Demo - Context values for the systemd service file content.
+    """
+
+    verbose_service_name: str = 'CLI-Base Demo'
+
+
+@dataclasses.dataclass
+class SystemdServiceInfo(BaseSystemdServiceInfo):
+    """
+    CLI-Base Demo - Information for systemd helper functions.
+    """
+
+    template_context: SystemdServiceTemplateContext = dataclasses.field(default_factory=SystemdServiceTemplateContext)
+
+
+@dataclasses.dataclass
+class DemoSettings:
+    """
+    This are just settings for the "cli-base-utilities" DEMO.
+    Will be used in cli_base example commands.
+    See "./cli.py --help" for more information.
+    """
+
+    # Information how to setup the systemd services:
+    systemd: dataclasses = dataclasses.field(default_factory=SystemdServiceInfo)
