cbccb9237f7306b75598b77196b3852585b2a867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 06:04:25 +0000

    Add package libem/0.0.23/libem-0.0.23.tar.gz

---
+++ packages/libem/libem-0.0.23.tar.gz/libem-0.0.23/libem/parameter.py
@@ -0,0 +1,12 @@
+from libem.core.struct import Parameter
+
+model = Parameter(
+    default="gpt-4o",
+    options=["gpt-4o-mini", "gpt-4",
+             "gpt-4-turbo", "gpt-3.5-turbo",
+             "llama3"]
+)
+temperature = Parameter(
+    default=0,
+    options=[0.1, 0.5, 0.9, 1.4]
+)
