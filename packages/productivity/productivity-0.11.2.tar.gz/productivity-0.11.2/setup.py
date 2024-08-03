4c1a84b8a5090b95cdd487340b9343ca0e629912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:25 +0000

    Add package productivity/0.11.2/productivity-0.11.2.tar.gz

---
+++ packages/productivity/productivity-0.11.2.tar.gz/productivity-0.11.2/setup.py
@@ -0,0 +1,52 @@
+"""Python driver for AutomationDirect Productivity Series PLCs."""
+from setuptools import setup
+
+with open('README.md') as in_file:
+    long_description = in_file.read()
+
+setup(
+    name='productivity',
+    version='0.11.2',
+    description="Python driver for AutomationDirect Productivity Series PLCs.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alexrudd2/productivity/',
+    author='Patrick Fuller',
+    author_email='patrickfuller@gmail.com',
+    maintainer="Alex Ruddick",
+    maintainer_email="alex@ruddick.tech",
+    packages=['productivity'],
+    entry_points={
+        'console_scripts': [('productivity = productivity:command_line')]
+    },
+    install_requires=[
+        'pymodbus>=2.4.0; python_version == "3.8"',
+        'pymodbus>=2.4.0; python_version == "3.9"',
+        'pymodbus>=3.0.2,<3.8.0; python_version >= "3.10"',
+        'PyYAML',
+    ],
+    extras_require={
+        'test': [
+            'mypy==1.11.1',
+            'pytest',
+            'pytest-cov',
+            'pytest-asyncio',
+            'ruff==0.5.5',
+            'types-PyYAML'
+        ],
+    },
+    license='GPLv2',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Scientific/Engineering :: Human Machine Interfaces'
+    ]
+)
