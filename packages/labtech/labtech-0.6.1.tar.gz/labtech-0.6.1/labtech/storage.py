b3be6c8f0037682822206fc1abd5265b57e0dd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:24 +0000

    Add package labtech/0.6.1/labtech-0.6.1.tar.gz

---
+++ packages/labtech/labtech-0.6.1.tar.gz/labtech-0.6.1/labtech/storage.py
@@ -0,0 +1,94 @@
+"""Storage providers for cached task results."""
+
+import os
+import shutil
+from pathlib import Path
+from typing import IO, Sequence, Union
+
+from .exceptions import StorageError
+from .types import Storage
+
+
+class NullStorage(Storage):
+    """Storage provider that does not store cached results."""
+
+    def find_keys(self) -> Sequence[str]:
+        return []
+
+    def exists(self, key: str) -> bool:
+        return False
+
+    def file_handle(self, key: str, filename: str, *, mode: str = 'r') -> IO:
+        return open(os.devnull, mode=mode)
+
+    def delete(self, key: str):
+        pass
+
+
+class LocalStorage(Storage):
+    """Storage provider that stores cached results in a local filesystem
+    directory."""
+
+    def __init__(self, storage_dir: Union[str, Path], *, with_gitignore: bool = True):
+        """
+        Args:
+            storage_dir: Path to the directory where cached results will be
+                stored. The directory will be created if it does not already
+                exist.
+            with_gitignore: If `True`, a `.gitignore` file will be created
+                inside the storage directory to ignore the entire storage
+                directory. If an existing `.gitignore` file exists, it will be
+                replaced.
+        """
+        if isinstance(storage_dir, str):
+            storage_dir = Path(storage_dir)
+        self._storage_path = storage_dir.resolve()
+        if not self._storage_path.exists():
+            self._storage_path.mkdir()
+
+        if with_gitignore:
+            gitignore_path = self._storage_path / '.gitignore'
+            with gitignore_path.open('w') as gitignore_file:
+                gitignore_file.write('*\n')
+
+    def _key_path(self, key: str) -> Path:
+        if not key:
+            raise StorageError("Key cannot be empty")
+
+        disallowed_key_chars = ['.', '/', '\\', os.path.sep, os.path.altsep]
+        for char in disallowed_key_chars:
+            if char is not None and char in key: # altsep can be None
+                raise StorageError(f"Key '{key}' must not contain the forbidden character '{char}'")
+
+        key_path = (self._storage_path / key).resolve()
+        if key_path.parent != self._storage_path:
+            raise StorageError((f"Key '{key}' should only reference a directory directly "
+                                f"under the storage directory '{self._storage_path}'"))
+        return key_path
+
+    def find_keys(self) -> Sequence[str]:
+        return sorted([
+            key_path.name for key_path in self._storage_path.iterdir()
+            if key_path.is_dir()
+        ])
+
+    def exists(self, key: str) -> bool:
+        key_path = self._key_path(key)
+        return key_path.exists()
+
+    def file_handle(self, key: str, filename: str, *, mode: str = 'r') -> IO:
+        key_path = self._key_path(key)
+        try:
+            key_path.mkdir()
+        except FileExistsError:
+            pass
+        file_path = (key_path / filename).resolve()
+        if file_path.parent != key_path:
+            raise StorageError((f"Filename '{filename}' should only reference a directory directly "
+                                f"under the storage key directory '{key_path}'"))
+        return file_path.open(mode=mode)
+
+    def delete(self, key: str):
+        key_path = self._key_path(key)
+        if key_path.exists():
+            shutil.rmtree(key_path)
---
+++ packages/labtech/labtech-0.6.1.tar.gz/labtech-0.6.1/labtech/types.py
@@ -0,0 +1,175 @@
+"""Core types of labtech."""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from inspect import isclass
+from typing import (
+    IO,
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Literal,
+    Optional,
+    Protocol,
+    Sequence,
+    Type,
+    TypeVar,
+)
+
+
+@dataclass(frozen=True)
+class TaskInfo:
+    orig_post_init: Optional[Callable]
+    cache: 'Cache'
+    max_parallel: Optional[int]
+    mlflow_run: bool
+
+
+CovariantResultT = TypeVar('CovariantResultT', covariant=True)
+ResultT = TypeVar('ResultT')
+"""Type variable for result returned by the `run` method of a
+[`Task`][labtech.types.Task]."""
+ResultsMap = Dict['Task[CovariantResultT]', CovariantResultT]
+
+
+@dataclass(frozen=True)
+class ResultMeta:
+    """Metadata about the execution of a task. If the task is loaded from
+    cache, the metadata is also loaded from the cache."""
+    start: Optional[datetime]
+    """The timestamp when the task's execution began."""
+    duration: Optional[timedelta]
+    """The time that the task took to execute."""
+
+
+@dataclass(frozen=True)
+class TaskResult(Generic[ResultT]):
+    value: ResultT
+    meta: ResultMeta
+
+
+@dataclass
+class Task(Protocol, Generic[CovariantResultT]):
+    """Interface provided by any class that is decorated by
+    [`labtech.task`][labtech.task]."""
+    _lt: TaskInfo
+    _is_task: Literal[True]
+    _results_map: Optional[ResultsMap]
+    cache_key: str
+    """The key that uniquely identifies the location for this task within cache storage."""
+    context: Optional[dict[str, Any]]
+    """Context variables from the Lab that can be accessed when the task is running."""
+    result_meta: Optional[ResultMeta]
+    """Metadata about the execution of the task."""
+
+    def _set_results_map(self, results_map: ResultsMap):
+        pass
+
+    def _set_result_meta(self, result_meta: ResultMeta):
+        pass
+
+    @property
+    def result(self) -> CovariantResultT:
+        """Returns the result executed/loaded for this task. If no result is
+        available in memory, accessing this property raises a `TaskError`."""
+
+    def set_context(self, context: dict[str, Any]):
+        """Set the context that is made available to the task while it is
+        running."""
+
+    def run(self):
+        """User-provided method that executes the task parameterised by the
+        attributes of the task.
+
+        Usually executed by [`Lab.run_tasks()`][labtech.Lab.run_tasks]
+        instead of being called directly.
+
+        """
+
+
+TaskT = TypeVar("TaskT", bound=Task)
+
+
+def is_task_type(cls):
+    """Returns `True` if the given `cls` is a class decorated with
+    [`labtech.task`][labtech.task]."""
+    return isclass(cls) and isinstance(getattr(cls, '_lt', None), TaskInfo)
+
+
+def is_task(obj):
+    """Returns `True` if the given `obj` is an instance of a task class."""
+    return is_task_type(type(obj)) and hasattr(obj, '_is_task')
+
+
+class Storage(ABC):
+    """Storage provider for persisting cached task results."""
+
+    @abstractmethod
+    def find_keys(self) -> Sequence[str]:
+        """Returns the keys of all currently cached task results."""
+
+    @abstractmethod
+    def exists(self, key: str) -> bool:
+        """Returns `True` if the given task `key` is present in the storage
+        cache."""
+
+    @abstractmethod
+    def file_handle(self, key: str, filename: str, *, mode: str = 'r') -> IO:
+        """Opens and returns a File-like object for a single file within the
+        storage cache.
+
+        Args:
+            key: The task key of the cached result containing the file.
+            filename: The name of the file to open.
+            mode: The file mode to open the file with.
+
+        """
+
+    @abstractmethod
+    def delete(self, key: str) -> None:
+        """Deletes the cached result for the task with the given `key`."""
+
+
+class Cache(ABC):
+    """Cache that controls saving task results into
+    [Storage][labtech.storage.Storage] providers."""
+
+    @abstractmethod
+    def cache_key(self, task: Task) -> str:
+        """Returns the key to identify the given `task` within this cache."""
+
+    @abstractmethod
+    def is_cached(self, storage: Storage, task: Task) -> bool:
+        """Returns `True` if a result is cached for the given `task` within
+        the given `storage`."""
+
+    @abstractmethod
+    def save(self, storage: Storage, task: Task[ResultT], result: TaskResult[ResultT]) -> None:
+        """Save the given `result` for the given `task` into the given
+        `storage`."""
+
+    @abstractmethod
+    def load_task(self, storage: Storage, task_type: Type[TaskT], key: str) -> TaskT:
+        """Loads the task instance of the given `task_type` for the given
+        `key` from the given `storage`.
+
+        The task's result is not loaded by this method.
+        """
+
+    @abstractmethod
+    def load_result_with_meta(self, storage: Storage, task: Task[ResultT]) -> TaskResult[ResultT]:
+        """Loads the result and metadata for the given task from the storage
+        provider.
+
+        Args:
+            storage: Storage provider to load the result from
+            task: task instance to load the result for
+
+        """
+
+    @abstractmethod
+    def delete(self, storage: Storage, task: Task) -> None:
+        """Deletes the cached result for the given `task` from the given
+        `storage`."""
