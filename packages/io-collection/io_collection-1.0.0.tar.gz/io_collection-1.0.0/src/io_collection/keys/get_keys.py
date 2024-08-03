e768f84cdcb755446ca1a5b64610568da364b4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:05:45 +0000

    Add package io-collection/1.0.0/io_collection-1.0.0.tar.gz

---
+++ packages/io-collection/io_collection-1.0.0.tar.gz/io_collection-1.0.0/src/io_collection/keys/get_keys.py
@@ -0,0 +1,98 @@
+import os
+from glob import glob
+
+import boto3
+
+
+def get_keys(location: str, prefix: str) -> list[str]:
+    """
+    Get list of objects at specified location with given prefix.
+
+    Method will check in the S3 bucket if the location begins with the **s3://**
+    protocol, otherwise it assumes the location is a local path.
+
+    Parameters
+    ----------
+    location
+        Object location (local path or S3 bucket).
+    prefix
+        Object key prefix.
+
+    Returns
+    -------
+    :
+        List of all object keys at location.
+    """
+
+    if location[:5] == "s3://":
+        return _get_keys_on_s3(location[5:], prefix)
+    return _get_keys_on_fs(location, prefix)
+
+
+def _get_keys_on_fs(path: str, prefix: str) -> list[str]:
+    """
+    Get list of objects on local file system with given prefix.
+
+    Parameters
+    ----------
+    location
+        Local object path.
+    prefix
+        Object key prefix.
+
+    Returns
+    -------
+    :
+        List of all object keys on the local file system.
+    """
+
+    glob_pattern = f"{path}/{prefix}/**/*".replace("//", "/")
+    all_files = glob(glob_pattern, recursive=True)
+    regular_files = [file for file in all_files if not os.path.isdir(file)]
+    keys = [file.replace(path, "").strip("/") for file in regular_files]
+    return keys
+
+
+def _get_keys_on_s3(bucket: str, prefix: str) -> list[str]:
+    """
+    Get list of objects in AWS S3 bucket with given prefix.
+
+    Parameters
+    ----------
+    location
+        AWS S3 bucket name.
+    prefix
+        Object key prefix.
+
+    Returns
+    -------
+    :
+        List of all object keys in the AWS bucket.
+    """
+
+    s3_client = boto3.client("s3")
+
+    bucket = bucket.replace("s3://", "")
+    prefix = f"{prefix}/"
+    response = s3_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+
+    get_response = True
+    keys: list[str] = []
+
+    while get_response:
+        if "Contents" not in response:
+            break
+
+        content_keys = [content["Key"] for content in response["Contents"]]
+        keys = keys + content_keys
+
+        if response["IsTruncated"]:
+            response = s3_client.list_objects_v2(
+                Bucket=bucket,
+                Prefix=prefix,
+                ContinuationToken=response["NextContinuationToken"],
+            )
+        else:
+            get_response = False
+
+    return keys
---
+++ packages/io-collection/io_collection-1.0.0.tar.gz/io_collection-1.0.0/src/io_collection/load/load_buffer.py
@@ -0,0 +1,87 @@
+import io
+import os
+
+import boto3
+
+MAX_CONTENT_LENGTH = 2**31 - 1
+
+
+def load_buffer(location: str, key: str) -> io.BytesIO:
+    """
+    Load key into in-memory bytes buffer from specified location.
+
+    Method will load from the S3 bucket if the location begins with the
+    **s3://** protocol, otherwise it assumes the location is a local path.
+
+    Parameters
+    ----------
+    location
+        Object location (local path or S3 bucket).
+    key
+        Object key.
+
+    Returns
+    -------
+    :
+        Loaded object buffer.
+    """
+
+    if location[:5] == "s3://":
+        return _load_buffer_from_s3(location[5:], key)
+    return _load_buffer_from_fs(location, key)
+
+
+def _load_buffer_from_fs(path: str, key: str) -> io.BytesIO:
+    """
+    Load key into in-memory bytes buffer from local file system.
+
+    Parameters
+    ----------
+    path
+        Local object path.
+    key
+        Object key.
+
+    Returns
+    -------
+    :
+        Loaded object buffer.
+    """
+
+    full_path = os.path.join(path, key)
+    return io.BytesIO(open(full_path, "rb").read())
+
+
+def _load_buffer_from_s3(bucket: str, key: str) -> io.BytesIO:
+    """
+    Load key into in-memory bytes buffer from AWS S3 bucket.
+
+    Objects larger than `MAX_CONTENT_LENGTH` are loaded in chunks.
+
+    Parameters
+    ----------
+    bucket
+        AWS S3 bucket name.
+    key
+        Object key.
+
+    Returns
+    -------
+    :
+        Loaded object buffer.
+    """
+
+    s3_client = boto3.client("s3")
+    obj = s3_client.get_object(Bucket=bucket, Key=key)
+
+    # Check if body needs to be loaded in chunks.
+    content_length = obj["ContentLength"]
+
+    if content_length > MAX_CONTENT_LENGTH:
+        print("Loading chunks ...")
+        body = bytearray()
+        for chunk in obj["Body"].iter_chunks(chunk_size=MAX_CONTENT_LENGTH):
+            body += chunk
+        return io.BytesIO(body)
+
+    return io.BytesIO(obj["Body"].read())
