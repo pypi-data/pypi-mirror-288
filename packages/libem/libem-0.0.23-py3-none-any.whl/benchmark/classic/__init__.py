20c2b8c6286e08902c7d840c82d0bbc13c3358eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 06:04:24 +0000

    Add package libem/0.0.23/libem-0.0.23-py3-none-any.whl

---
+++ packages/libem/libem-0.0.23-py3-none-any.whl/benchmark/classic/__init__.py
@@ -0,0 +1,24 @@
+# similarity thresholds for blocking to
+# achieve 100% recall
+block_similarities = {
+    'abt-buy': 50,
+    'amazon-google': 54,
+    'beer': 79,
+    'dblp-acm': 79,
+    'dblp-scholar': 59,
+    'fodors-zagats': 83,
+    'itunes-amazon': 63,
+    'walmart-amazon': 50
+}
+
+from benchmark.classic import (
+    abt_buy,
+    amazon_google,
+    beer,
+    dblp_acm,
+    dblp_scholar,
+    fodors_zagats,
+    itunes_amazon,
+    walmart_amazon,
+    challenging,
+)
---
+++ packages/libem/libem-0.0.23-py3-none-any.whl/libem/match/prompt.py
@@ -0,0 +1,44 @@
+from libem.core.struct import Prompt, Shots, Index
+from libem.core.struct.prompt import (
+    Shot, Rules, Experiences
+)
+
+"""System prompts"""
+role = Prompt(
+    default="You are an entity matcher that determines whether "
+            "two entity descriptions refer to the same real-world entity.",
+    options=[""],
+)
+
+rules = Prompt(
+    default=Rules(),
+    options=[],
+)
+
+experiences = Prompt(
+    default=Experiences(),
+    options=[],
+)
+
+output = Prompt(
+    default=Index("plain"),
+    options={
+        "plain": "At the end, give your answer in the form of a "
+                 "single 'yes' or 'no'.",
+        "likelihood": "At the end, give your answer strictly in the "
+                      "format of a single number between 0.0 and 1.0, "
+                      "estimating the likelihood that the two entities "
+                      "are the same."
+    },
+)
+
+"""Assistant prompts"""
+shots = Shots(
+    default=[Shot()]
+)
+
+"""User prompts"""
+query = Prompt(
+    default="Entity 1: {left}.\nEntity 2: {right}.",
+    options=[],
+)
