02ece9de1a493e91af491f546e584ec54a2ffa3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:04:08 +0000

    Add package pyiron-contrib/0.1.18/pyiron_contrib-0.1.18.tar.gz

---
+++ packages/pyiron-contrib/pyiron_contrib-0.1.18.tar.gz/pyiron_contrib-0.1.18/setup.py
@@ -0,0 +1,64 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+
+setup(
+    name='pyiron_contrib',
+    version=versioneer.get_version(),
+    description='Repository for user-generated plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_contrib',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='huber@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[        
+        'matplotlib>=3.9.1',
+        'numpy>=1.26.4',
+        'pyiron_snippets>=0.1.3',
+        'pyiron_base>=0.9.11',
+        'scipy>=1.13.0',
+        'seaborn>=0.13.2',
+        'pyparsing>=3.1.2',
+    ],
+    extras_require={
+        'atomistic': [
+            'ase>=3.23.0',
+            'pyiron_atomistics>=0.6.10',
+            'pycp2k>=0.2.2',
+        ],
+        'fenics': [
+            'fenics>=2019.1.0',
+            'mshr>=2019.1.0',
+        ],
+        'image': ['scikit-image>=0.24.0'],
+        'generic': [
+            'boto3>=1.34.152', 
+            'moto>=5.0.9'
+        ],
+        'tinybase': [
+            'distributed>=2024.7.1',
+            'pymatgen>=2024.7.18',
+            'pympipool>=0.8.4',
+            'h5io_browser>=0.0.17',
+        ]
+    },
+    cmdclass=versioneer.get_cmdclass(),
+)
