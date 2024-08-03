0e4d229b767b08ec11df7fea0bf5b8e1b4a66ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:44 +0000

    Add package kandinskylib/0.1.1/kandinskylib-0.1.1.tar.gz

---
+++ packages/kandinskylib/kandinskylib-0.1.1.tar.gz/kandinskylib-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Получить путь к текущему каталогу
+here = pathlib.Path(__file__).parent.resolve()
+
+# Прочитать содержимое README.md
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='kandinskylib',  # Имя вашего пакета
+    version='0.1.1',  # Обновите версию здесь
+    description='A library for working with Kandinsky AI image generator API',  # Краткое описание
+    long_description=long_description,  # Длинное описание из README.md
+    long_description_content_type='text/markdown',  # Тип содержимого длинного описания
+    url='https://github.com/Read1dno/kandinskylib',  # URL вашего проекта
+    author='Read1dno',
+    author_email='ef8ser@gmail.com',
+    classifiers=[  # Классификаторы для PyPI
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='kandinsky, ai, image generation, api',  # Ключевые слова
+    packages=find_packages(),  # Автоматический поиск пакетов
+    python_requires='>=3.6, <4',
+    install_requires=['requests', 'pillow'],  # Зависимости
+)
