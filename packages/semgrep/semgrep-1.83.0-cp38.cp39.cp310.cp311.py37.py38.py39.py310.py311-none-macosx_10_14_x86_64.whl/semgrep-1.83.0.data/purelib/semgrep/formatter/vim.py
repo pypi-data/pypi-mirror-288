2480c3d1b87d923a0b309256c08674c1d9ced063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:03:50 +0000

    Add package semgrep/1.83.0/semgrep-1.83.0-cp38.cp39.cp310.cp311.py37.py38.py39.py310.py311-none-macosx_10_14_x86_64.whl

---
+++ packages/semgrep/semgrep-1.83.0-cp38.cp39.cp310.cp311.py37.py38.py39.py310.py311-none-macosx_10_14_x86_64.whl/semgrep-1.83.0.data/purelib/semgrep/formatter/vim.py
@@ -0,0 +1,44 @@
+from typing import Any
+from typing import Iterable
+from typing import Mapping
+from typing import Sequence
+
+import semgrep.semgrep_interfaces.semgrep_output_v1 as out
+from semgrep.error import SemgrepError
+from semgrep.formatter.base import BaseFormatter
+from semgrep.rule import Rule
+from semgrep.rule_match import RuleMatch
+
+
+class VimFormatter(BaseFormatter):
+    @staticmethod
+    def _get_parts(rule_match: RuleMatch) -> Sequence[str]:
+        # TODO is there a spec for vim format?
+        severity = {
+            out.Info(): "I",
+            out.Warning(): "W",
+            out.Error(): "E",
+            out.Low(): "L",
+            out.Medium(): "M",
+            out.High(): "H",
+            out.Critical(): "C",
+        }
+        return [
+            str(rule_match.path),
+            str(rule_match.start.line),
+            str(rule_match.start.col),
+            severity[rule_match.severity.value],
+            rule_match.rule_id,
+            rule_match.message,
+        ]
+
+    def format(
+        self,
+        rules: Iterable[Rule],
+        rule_matches: Iterable[RuleMatch],
+        semgrep_structured_errors: Sequence[SemgrepError],
+        cli_output_extra: out.CliOutputExtra,
+        extra: Mapping[str, Any],
+        is_ci_invocation: bool,
+    ) -> str:
+        return "\n".join(":".join(self._get_parts(rm)) for rm in rule_matches)
