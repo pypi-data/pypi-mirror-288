732bfc59cfe79f5394c44a432b16bf46f6fc73f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 05:03:58 +0000

    Add package nodestream/0.12.5a14/nodestream-0.12.5a14-py3-none-any.whl

---
+++ packages/nodestream/nodestream-0.12.5a14-py3-none-any.whl/nodestream/pipeline/step.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import Any, AsyncGenerator
+
+
+class Step(ABC):
+    """A `Step` represents a phase of an ETl pipeline."""
+
+    @classmethod
+    def from_file_data(cls, **kwargs):
+        return cls(**kwargs)
+
+    @abstractmethod
+    async def handle_async_record_stream(
+        self, record_stream: AsyncGenerator[Any, Any]
+    ) -> AsyncGenerator[Any, Any]:
+        raise NotImplementedError
+
+    async def finish(self):
+        pass
+
+
+class PassStep(Step):
+    """A `PassStep` is a step that does nothing."""
+
+    def __init__(self, **kwargs) -> None:
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    async def handle_async_record_stream(
+        self, record_stream: AsyncGenerator[Any, Any]
+    ) -> AsyncGenerator[Any, Any]:
+        async for record in record_stream:
+            yield record
