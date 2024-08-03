a8a7d2c4be4d8ff2fcd5e9e3aeb0fb8339e35e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:33 +0000

    Add package genutility/0.0.107/genutility-0.0.107-py3-none-any.whl

---
+++ packages/genutility/genutility-0.0.107-py3-none-any.whl/genutility/error.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from typing import Callable
+from warnings import warn
+
+
+def deprecated(msg: str, stacklevel: int = 2) -> Callable[[Callable], Callable]:
+    def decorator(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            warn(msg, DeprecationWarning, stacklevel)  # noqa: B028
+            print("DeprecationWarning:", msg)
+            return func(*args, **kwargs)
+
+        return inner
+
+    return decorator
