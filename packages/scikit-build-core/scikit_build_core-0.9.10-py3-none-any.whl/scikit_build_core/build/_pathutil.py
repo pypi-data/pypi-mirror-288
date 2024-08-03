9148635dc0b0805b391c3b755616e891c7f11d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 05:04:16 +0000

    Add package scikit-build-core/0.9.10/scikit_build_core-0.9.10-py3-none-any.whl

---
+++ packages/scikit-build-core/scikit_build_core-0.9.10-py3-none-any.whl/scikit_build_core/build/_pathutil.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pathspec
+
+from ._file_processor import each_unignored_file
+
+if TYPE_CHECKING:
+    from collections.abc import Generator, Sequence
+
+__all__ = ["is_valid_module", "packages_to_file_mapping", "path_to_module", "scantree"]
+
+
+def __dir__() -> list[str]:
+    return __all__
+
+
+def scantree(path: Path) -> Generator[Path, None, None]:
+    """Recursively yield Path objects for given directory."""
+    for entry in os.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scantree(Path(entry))
+        else:
+            yield Path(entry)
+
+
+def path_to_module(path: Path) -> str:
+    path = path.with_name(path.name.split(".", 1)[0])
+    if path.name == "__init__":
+        path = path.parent
+    return ".".join(path.parts)
+
+
+def packages_to_file_mapping(
+    *,
+    packages: Sequence[str],
+    platlib_dir: Path,
+    include: Sequence[str],
+    src_exclude: Sequence[str],
+    target_exclude: Sequence[str],
+) -> dict[str, str]:
+    mapping = {}
+    exclude_spec = pathspec.GitIgnoreSpec.from_lines(target_exclude)
+    for package in packages:
+        source_package = Path(package)
+        base_path = source_package.parent
+        for filepath in each_unignored_file(
+            source_package,
+            include=include,
+            exclude=src_exclude,
+        ):
+            rel_path = filepath.relative_to(base_path)
+            target_path = platlib_dir / rel_path
+            if not exclude_spec.match_file(rel_path) and not target_path.is_file():
+                mapping[str(filepath)] = str(target_path)
+
+    return mapping
+
+
+def is_valid_module(path: Path) -> bool:
+    parts = path.parts
+    return (
+        all(p.isidentifier() for p in parts[:-1])
+        and parts[-1].split(".", 1)[0].isidentifier()
+    )
