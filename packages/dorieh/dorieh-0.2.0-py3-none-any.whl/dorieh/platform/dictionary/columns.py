fd7856ed91cd18729cb91b362fd492ede69d408b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:05:01 +0000

    Add package dorieh/0.2.0/dorieh-0.2.0-py3-none-any.whl

---
+++ packages/dorieh/dorieh-0.2.0-py3-none-any.whl/dorieh/platform/dictionary/columns.py
@@ -0,0 +1,281 @@
+#  Copyright (c) 2023. Harvard University
+#
+#  Developed by Research Software Engineering,
+#  Faculty of Arts and Sciences, Research Computing (FAS RC)
+#  Author: Michael A Bouzinier
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+from typing import Dict, List, Optional, Set
+
+import sqlparse
+import yaml
+from sqlparse.sql import IdentifierList, Parenthesis, Function, Identifier
+import html
+
+from dorieh.platform.dictionary.element import HTML, DataModelElement, qstr, attrs2string
+from dorieh.platform.data_model.domain import Domain
+
+
+def noop(x):
+    return
+
+
+class Column(DataModelElement):
+    def __init__(self, table_name: str, column_block: Dict):
+        if isinstance(column_block, dict):
+            for name in column_block:
+                self.name = name
+                super().__init__(column_block[name])
+                break
+        else:
+            self.name = str(column_block)
+            super().__init__(None)
+        self.qualified_name = table_name + '.' + self.name
+        self.predecessors: Set[str] = set()
+        self.functions: List[str] = []
+        self.datatype = "string"
+        self.column_type = ""
+        self.expression = []
+        self.copied = False
+        self.casts = dict()
+        self.requires = []
+        if self.block is None:
+            return
+        if "type" in self.block:
+            self.datatype = self.block["type"]
+        if "source" in self.block:
+            self.expand_macro2()
+            src_block = self.block["source"]
+            if isinstance(src_block, list):
+                for item in src_block:
+                    self.predecessors.add(item)
+            elif isinstance(src_block, dict):
+                if "type" in src_block:
+                    self.column_type = src_block["type"]
+                if "code" in src_block:
+                    self.parse_expr(src_block["code"])
+            elif isinstance(src_block, str):
+                self.parse_expr(src_block)
+        if "requires" in self.block:
+            req = self.block["requires"]
+            if isinstance(req, list):
+                self.requires = req
+            else:
+                self.requires = [req]
+        if "cast" in self.block:
+            for t in self.block["cast"]:
+                self.casts[t] = self.block["cast"][t]
+        return
+
+    def is_transformed(self):
+        return self.expression or self.casts
+
+    @classmethod
+    def expand_macro1(cls, block: Dict) -> Optional[List[Dict]]:
+        if not isinstance(block, dict):
+            return [block]
+        name = list(block.keys())[0]
+        if '$' in name:
+            x = Domain.parse_wildcard_column_spec(name)
+            if x is not None:
+                prefix, var, values, postfix = x
+                b1 = yaml.safe_dump(block[name])
+                expansion = []
+                for v in values:
+                    bi = b1.replace(f'${var}', str(v))
+                    b = {f'{prefix}{str(v)}': yaml.safe_load(bi)}
+                    expansion.append(b)
+                return expansion
+        return [block]
+
+    def expand_macro2(self):
+        src_block = self.block["source"]
+        if isinstance(src_block, list):
+            original = src_block
+        elif isinstance(src_block, str):
+            original = [src_block]
+        else:
+            return
+        expansion = []
+        expanded = False
+        for s in original:
+            if '$' in s:
+                var = s[s.find('$') + 1]
+                values = self.block[var]
+                for v in values:
+                    expansion.append(s.replace(f'${var}', v))
+                expanded = True
+            else:
+                expansion.append(s)
+        if expanded:
+            self.block["source"] = expansion
+        return
+
+    def describe(self) -> str:
+        text = f'{self.name} ({self.datatype}) {self.column_type}\n'
+        if self.reference:
+            text += "See: " + self.reference + '\n\n'
+        try:
+            if self.description:
+                if "text" in self.description:
+                    text += self.description["text"] + '\n'
+                for key in self.description:
+                    if key == "text":
+                        continue
+                    text += key + ': ' + str(self.description[key]) + '\n'
+        except:
+            print("ERROR:")
+            print(str(self.description))
+        if self.expression:
+            exp = ';\n'.join(self.expression)
+            text += "\n\n" + exp + '\n'
+        return text
+
+    def describe_html(self) -> str:
+        text = "\n<TABLE>\n"
+        text += "<tr>"
+        text += f'<td align = "center" border = "0"><FONT POINT-SIZE="20"><b>{self.qualified_name}</b></FONT></td>'
+        text += f'<td align = "center" border = "0">{self.datatype}</td>'
+        text += "</tr>\n"
+
+        if self.column_type:
+            text += f'<tr><td  align = "center" border = "0"><i>{self.column_type}</i></td></tr>\n'
+
+        if self.reference:
+            text += f'<tr><td  align = "center" border = "0"><i>For more information see: {self.reference}</i></td></tr>\n'
+        if self.description and "text" in self.description:
+            value = html.escape(self.description["text"])
+            text += f'<tr><td  align = "center" border = "0">{value}</td></tr>\n'
+
+        n = 0
+        if self.description is not None:
+            for key in self.description:
+                if key == "text":
+                    continue
+                value = html.escape(str(self.description[key]))
+                text += f'\t<tr><td align = "left">{key}</td><td align = "left">{value}</td></tr>\n'
+                if n == 0:
+                    text += "<hr/>\n"
+                n += 1
+        if self.expression:
+            if n > 0:
+                text += "<hr/>\n"
+                n = 0
+            for exp in self.expression:
+                value = html.escape(exp)
+                text += f'\t<tr><td align = "left">{value}</td></tr>\n'
+        for key in self.casts:
+            if n > 0:
+                text += "<hr/>\n"
+            value = html.escape(str(self.casts[key]))
+            text += f'<tr><td align = "left">{key} &rarr;</td><td align = "left">{value}</td></tr>\n'
+        text += "\n</TABLE>\n"
+        return text
+
+    def html(self, of: str, svg = None):
+        body = self.describe_html()
+        if svg:
+            body += "<hr/>\n"
+            body += f'<object data="{svg}" type="image/svg+xml"> </object>'
+        block = HTML.format(
+            title = f"Column {self.qualified_name}",
+            body = body
+        )
+        with open(of, "wt") as out:
+            print(block, file=out)
+
+    def to_dot(self):
+        node_id = qstr(self.qualified_name)
+        node_label = '<' + self.describe_html() + '>'
+        attrs = {
+            "label": node_label,
+            "shape": "box"
+        }
+        if self.reference:
+            attrs["URL"] = qstr(self.reference)
+            attrs["target"] = "_blank"
+        return f"\t{node_id} [{attrs2string(attrs)}];"
+
+    def __repr__(self):
+        return self.to_dot()
+
+    def parse_expr(self, exp: str):
+        parsed = sqlparse.parse(exp)[0]
+        self.find_all_names(parsed)
+        self.expression.append(exp)
+
+    def find_all_names(self, element):
+        if hasattr(element, "tokens"):
+            n = len(element.tokens)
+            for i in range(0, n):
+                t = element.tokens[i]
+                if str(t.ttype) == "Token.Name":
+                    if isinstance(element, IdentifierList) or isinstance(element, Parenthesis):
+                        self.predecessors.add(str(t))
+                    elif isinstance(element, Function):
+                        self.functions.append(str(t))
+                    elif isinstance(element, Identifier) and hasattr(element, "parent"):
+                        if isinstance(element.parent, Function):
+                            self.functions.append(str(t))
+                        elif isinstance(element.parent, IdentifierList) or isinstance(element.parent, Parenthesis):
+                            self.predecessors.add(str(t))
+                        else:
+                             noop(t)
+                    else:
+                        noop(t)
+                else:
+                    self.find_all_names(t)
+        return
+
+    def __str__(self) -> str:
+        repr = "Column: " + self.name
+        if self.predecessors:
+            repr += " <= " + ', '.join(self.predecessors)
+        if self.functions:
+            repr += " [" + ', '.join(self.functions) + ']'
+        return repr
+
+
+c1 = """
+age_min:
+    source: |
+      CASE
+        WHEN MIN(age) <> MAX(age) THEN MIN(age)
+      END
+"""
+
+c2 = """
+fips3_valdiated:
+    source: public.validate_zip_fips(MAX(zip), MAX(fips2), MAX(fips3))
+"""
+
+c3="""
+ssa2_list:
+    source: "string_agg(distinct ssa2, ',')"
+"""
+
+c4 = """
+fips5:
+    source: "(MAX(fips2) || MAX(fips3))"
+
+"""
+
+
+if __name__ == '__main__':
+    for c in [c1, c2, c3, c4]:
+        a = yaml.safe_load(c)
+        column = Column("xxx", a)
+        print(column)
+
+
---
+++ packages/dorieh/dorieh-0.2.0-py3-none-any.whl/dorieh/platform/dictionary/tables.py
@@ -0,0 +1,446 @@
+#  Copyright (c) 2023. Harvard University
+#
+#  Developed by Research Software Engineering,
+#  Faculty of Arts and Sciences, Research Computing (FAS RC)
+#  Author: Michael A Bouzinier
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+"""
+Module to create list of tables from the Domain Data Model
+"""
+import math
+import re
+from typing import Dict, Optional, List, Set
+import html
+import sqlparse
+
+from dorieh.platform.data_model.domain import Domain
+from dorieh.platform.dictionary.columns import Column
+from dorieh.platform.dictionary.element import HTML, DataModelElement, Graph, fqn, qstr, \
+    attrs2string, add_row, start_table, add_html_row, end_table, add_header_row, \
+    hr, start_invisible_row, end_invisible_row
+
+
+class Table(DataModelElement):
+    def __init__(self, schema: str, name: str, domain: Domain, domain_dict):
+        self.name = name
+        self.schema = schema
+        self.qualified_name = fqn(self.schema, self.name)
+        super().__init__(domain.find(self.qualified_name))
+        print("Adding: " + fqn(schema, name))
+        self.domain = domain
+        self.domain_dict = domain_dict
+        self.domain_dict.tables[fqn(schema, name)] = self
+        self.aggregation:Optional[Aggregation] = None
+        self.union:Optional[Union] = None
+        self.join:Optional[Join] = None
+        self.parent = None
+        self.type: str = "Table"
+        self.master = None
+        create_block = None
+        if "create" in self.block:
+            create_block:Dict[str, Dict] = self.block["create"]
+            self.aggregation = self.get_aggregation(schema, name, create_block)
+            if "type" in create_block:
+                self.type = str(create_block["type"])
+            if "from" in create_block:
+                from_block = create_block["from"]
+                if self.aggregation is None:
+                    if isinstance(from_block, list):
+                        self.union = Union(schema, from_block, self.domain_dict)
+                    elif isinstance(from_block, str):
+                        if "join" in from_block:
+                            self.join = Join(schema, from_block, self.domain_dict)
+                        else:
+                            tname = fqn(schema, from_block)
+                            self.master = self.domain_dict.tables[tname]
+        if "children" in self.block:
+            children_block = self.block["children"]
+            assert isinstance(children_block, dict)
+            for child in children_block:
+                child_table = Table(schema, child, self.domain,
+                                    self.domain_dict)
+                child_table.parent = self
+                relation = Relation("parent-child", self, child_table)
+                self.domain_dict.relations.append(relation)
+        if self.aggregation is not None:
+            relation = Relation(
+                "aggregation",
+                self.aggregation.parent,
+                self,
+                self.aggregation.on
+            )
+        elif self.parent is not None:
+            relation = Relation("parent-child", self.parent, self)
+        elif self.master is not None:
+            relation = Relation("transformation", self.master, self)
+        elif self.join is not None:
+            for t in self.join.tables:
+                relation = Relation("join", t, self, self.join.on)
+                self.domain_dict.relations.append(relation)
+            relation = None
+        elif self.union is not None:
+            for t in self.union.tables:
+                relation = Relation("union", t, self)
+                self.domain_dict.relations.append(relation)
+            relation = None
+        else:
+            relation = None
+        if relation is not None:
+            self.domain_dict.relations.append(relation)
+        self.columns = dict()
+        if "columns" in self.block:
+            columns_block = self.block["columns"]
+            for column_block in columns_block:
+                cblocks = Column.expand_macro1(column_block)
+                for cblock in cblocks:
+                    column = Column(self.qualified_name, cblock)
+                    self.columns[column.name.lower()] = column
+        if self.master is not None and create_block and "select" in create_block:
+            self.add_master_columns()
+        self.all_predecessors = None
+        self.base_predecessors = None
+
+    def add_master_columns(self):
+        identifiers = Domain.get_select_from(self.block)
+        if not identifiers:
+            return
+        for identifier in identifiers:
+            if isinstance(identifier, sqlparse.sql.Identifier):
+                c = identifier.get_name()
+                if c in self.master.columns:
+                    self.add_column_from_master(c)
+                else:
+                    self.add_column_from_sql(identifier)
+            elif identifier.ttype[0] == "Wildcard":
+                for c in self.master.columns:
+                    self.add_column_from_master(c)
+
+    def add_column_from_master(self, c: str):
+        p_column = self.master.columns[c]
+        column_block = {c: p_column.block}
+        column = Column(self.qualified_name, column_block)
+        column.copied = True
+        self.columns[column.name.lower()] = column
+
+    def add_column_from_sql(self, identifier: sqlparse.sql.Identifier):
+        c = identifier.get_name()
+        column = Column(self.qualified_name, {c: None})
+        column.expression = [identifier.value]
+        column.copied = False
+        identifiers = self.get_source_columns(identifier)
+        column.predecessors.update(identifiers)
+        self.columns[column.name.lower()] = column
+
+    def get_source_columns(self, identifier: sqlparse.sql.Identifier) -> Set[str]:
+        identifiers = set()
+        for t in identifier.tokens:
+            if t.is_keyword and t.value.lower() == "as":
+                break
+            if isinstance(t, sqlparse.sql.Identifier) or t.is_group:
+                tname = t.get_name()
+                if tname in self.master.columns:
+                    identifiers.add(tname)
+                elif len(t.tokens) > 1:
+                    identifiers.update(self.get_source_columns(t))
+        return identifiers
+
+    def get_aggregation(self, schema: str, name: str, create_block: dict):
+        if "group by" not in create_block:
+            return None
+        group_by_block :Dict[str, Dict] = create_block["group by"]
+        assert isinstance(group_by_block, list)
+        parent = create_block["from"]
+        assert isinstance(parent, str)
+        return Aggregation(schema, name, parent, group_by_block, self.domain_dict)
+
+    def __str__(self) -> str:
+        return self.type + " " + self.qualified_name
+
+    def __eq__(self, __value):
+        if isinstance(__value, Table):
+            if self.name == __value.name and self.schema == __value.schema:
+                return True
+        return False
+
+    def is_downstream(self, predecessor: str):
+        if predecessor not in self.domain_dict.tables:
+            raise ValueError(f"Unknown requirement {predecessor}")
+        t = self.domain_dict.tables[predecessor]
+        for r in self.domain_dict.relations:
+            if r.x == t and r.y == self:
+                return True
+        return False
+
+    def add_predecessors_for_column(self, c: str, lst: List):
+        for t in self.columns[c.lower()].requires:
+            if self.is_downstream(t):
+                continue
+            table = self.domain_dict.tables[t]
+            relation = Relation("custom-select", table, self)
+            self.domain_dict.relations.append(relation)
+            lst.append(relation)
+        return
+
+    def get_predecessors(self) -> List:
+        if self.base_predecessors is None:
+            self.base_predecessors = []
+            for relation in self.domain_dict.relations:
+                if relation.y == self:
+                    self.base_predecessors.append(relation)
+            self.all_predecessors = []
+            self.all_predecessors.extend(self.base_predecessors)
+            for c in self.columns:
+                self.add_predecessors_for_column(c, self.all_predecessors)
+        return self.all_predecessors
+
+    def get_predecessors_for_column(self, column: str) -> List:
+        if self.base_predecessors is None:
+            self.get_predecessors()
+        predecessors = []
+        predecessors.extend(self.base_predecessors)
+        self.add_predecessors_for_column(column, predecessors)
+        return predecessors
+
+    def get_column_links(self, column: Column) -> List:
+        pc = column.predecessors
+        pt = self.get_predecessors_for_column(column.name)
+        links = []
+        for c in pc:
+            c = c.lower()
+            for r in pt:
+                t = r.x
+                if c in t.columns:
+                    link = ColumnLink(r, t.columns[c], column)
+                    links.append(link)
+        return links
+
+    def calculate_column_lineage(self, column: Column, graph: Graph):
+        links = self.get_column_links(column)
+        if len(links) > 3:
+            group = "parent_" + column.qualified_name
+        else:
+            group = None
+        for link in links:
+            graph.add_edge(link)
+            parent = qstr(link.x.qualified_name)
+            graph.add_node(parent, repr(link.x), group)
+            link.relation.x.calculate_column_lineage(link.x, graph)
+        return
+
+    def column_lineage_to_dot(self, column_name: str, out):
+        column = self.columns[column_name]
+        graph = Graph()
+        graph.add_node(qstr(column.qualified_name), repr(column))
+        self.calculate_column_lineage(column, graph)
+        graph.print(indent=1, file=out)
+
+    def describe_column_type(self, column: Column):
+        if column.column_type:
+            return column.column_type
+        if column.copied:
+            return "copied"
+        if column.expression:
+            return "computed"
+        if self.aggregation:
+            if column.name in self.aggregation.columns:
+                return "grouping"
+            else:
+                return "aggregated"
+        if column.is_transformed():
+            return "transformed"
+        return ""
+
+    @staticmethod
+    def link_to_table(name: str):
+        ref = f"{name}.html"
+        return f'<a href = "{ref}">{name}</a>'
+
+    @staticmethod
+    def link_to_column(name: str, basedir: str):
+        cpath = f'{basedir}/{name}.dot.html'
+        return f'<a href="{cpath}">{name}</a>'
+
+    def describe_html(self, basedir) -> str:
+        text = start_table(border=0)
+        text += start_invisible_row()
+        text += start_table(border=2)
+        header = f'<FONT POINT-SIZE="20"><b>{self.type}: {self.qualified_name}</b></FONT>'
+        text += add_html_row([header], border=2)
+
+        if self.reference:
+            text += f'<tr><td  align = "center" border = "0"><i>For more information see: {self.reference}</i></td></tr>\n'
+        if self.description and "text" in self.description:
+            value = html.escape(self.description["text"])
+            text += add_row([value])
+        if self.parent:
+            name = self.parent.qualified_name
+            text += add_row([f"Child table of {self.link_to_table(name)}"])
+        if self.aggregation:
+            name = self.aggregation.parent.qualified_name
+            text += add_row([f"Aggregated from {self.link_to_table(name)} on {self.aggregation.on}"])
+        if self.master:
+            name = self.master.qualified_name
+            text += add_row([f"Transformed from {self.link_to_table(name)}"])
+        elif self.join:
+            name = self.join.sql
+            text += add_row([f"Join of tables: {name}"])
+        if "primary_key" in self.block:
+            pk = self.block["primary_key"]
+            pk_text = ", ".join(pk)
+            text += add_row([f"Primary Key:  {pk_text}"])
+        text += end_table()
+        text += end_invisible_row()
+        text += hr()
+        text += start_invisible_row()
+        text += start_table(border=2)
+        text += add_header_row(["Column name", "Column type", "Datatype"])
+        for c in sorted(self.columns):
+            column = self.columns[c]
+            text += add_row([
+                self.link_to_column(column.name, basedir),
+                self.describe_column_type(column),
+                column.datatype
+            ])
+        text += end_table()
+        text += end_invisible_row()
+        text += end_table()
+        return text
+
+    def html(self, of: str, svg = None):
+        body = self.describe_html(self.qualified_name)
+        if svg:
+            body += "<hr/>\n"
+            body += f'<object data="{svg}" type="image/svg+xml"> </object>'
+        block = HTML.format(
+            title = f"Table {self.qualified_name}",
+            body = body
+        )
+        with open(of, "wt") as out:
+            print(block, file=out)
+
+
+class Aggregation:
+    def __init__(self, schema: str, name: str, parent: str, columns: List, domain_dict):
+        self.domain_dict = domain_dict
+        self.table = self.domain_dict.tables[fqn(schema, name)]
+        if '.' not in parent:
+            parent = fqn(schema, parent)
+        self.parent = self.domain_dict.tables[parent]
+        self.columns = columns
+        self.on = "On " + ", ".join(self.columns)
+
+
+class Union:
+    def __init__(self, schema: str, tables: List[str], domain_dict):
+        self.domain_dict = domain_dict
+        self.tables = []
+        for t in tables:
+            if '.' not in t:
+                t = fqn(schema, t)
+            pattern = re.compile(t.replace('*', ".*"))
+            for t2 in self.domain_dict.tables:
+                if pattern.fullmatch(t2):
+                    self.tables.append(self.domain_dict.tables[t2])
+        return
+
+
+class Join:
+    def __init__(self, schema: str, s: str, domain_dict):
+        self.domain_dict = domain_dict
+        self.sql = s
+        natural_join = "natural" in s
+        s = s.lower()
+        for keyword in ["natural", "left", "right", "full"]:
+            s = s.replace(keyword, '')
+        i = s.find("on")
+        if i > 0:
+            s = s[:i]
+            self.on = s[i:]
+        elif natural_join:
+            self.on = "natural"
+        else:
+            self.on = None
+        tables = s.split("join")
+        self.tables = []
+        for table in tables:
+            table = table.strip()
+            if '.' not in table:
+                table = fqn(schema, table)
+            self.tables.append(self.domain_dict.tables[table])
+        return
+
+
+class Relation:
+    def __init__(self, reltype: str, x: Table, y: Table, data = None):
+        self.type = reltype
+        self.x = x
+        self.y = y
+        self.data = data
+
+    def label(self):
+        return self.data if self.data is not None else ''
+
+    def as_edge_attr(self) -> Dict:
+        attrs = dict()
+        if self.data is None:
+            attrs["label"] = '\t' + qstr(self.type)
+        else:
+            attrs["label"] = f'"\t{self.type} {self.label()}"'
+        if self.type == "aggregation":
+            attrs["penwidth"] = 5
+            attrs["color"] = "blue"
+        elif self.type == "union":
+            attrs["penwidth"] = 2
+            attrs["color"] = "chocolate"
+        elif self.type == "parent-child":
+            attrs["penwidth"] = 2
+            attrs["color"] = "darkorchid"
+        elif self.type == "custom-select":
+            attrs["penwidth"] = 1
+            attrs["color"] = "chartreuse"
+        else:
+            attrs["penwidth"] = 1
+            attrs["color"] = "black"
+        return attrs
+
+    def __str__(self) -> str:
+        data = self.label()
+        return f'{self.type}: {str(self.x)} => {str(self.y)} {data}'
+
+
+class ColumnLink:
+    def __init__(self, r: Relation, x: Column, y: Column):
+        self.relation = r
+        self.x: Column = x
+        self.y: Column = y
+
+    def to_dot(self):
+        node1 = qstr(self.x.qualified_name)
+        node2 = qstr(self.y.qualified_name)
+        attrs = self.relation.as_edge_attr()
+        if self.y.copied:
+            attrs["label"] = "Copied"
+        elif "transformation" in attrs["label"]:
+            attrs["label"] = attrs["label"].replace("transformation", "transformed")
+        elif "aggregation" in attrs["label"]:
+            attrs["label"] = attrs["label"].replace("aggregation", "aggregated")
+        return f"\t{node1} -> {node2} [{attrs2string(attrs)}];"
+
+    def __str__(self):
+        return f"{self.x.qualified_name} -> {self.y.qualified_name}"
+
+    def __repr__(self):
+        return self.to_dot()
