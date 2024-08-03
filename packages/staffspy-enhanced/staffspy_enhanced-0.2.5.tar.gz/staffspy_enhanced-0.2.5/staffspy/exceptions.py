c2aea720116ea69e6be9a05885602f556264ae4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 15:03:50 +0000

    Add package staffspy-enhanced/0.2.5/staffspy_enhanced-0.2.5.tar.gz

---
+++ packages/staffspy-enhanced/staffspy_enhanced-0.2.5.tar.gz/staffspy_enhanced-0.2.5/staffspy/exceptions.py
@@ -0,0 +1,16 @@
+from requests.exceptions import RequestException
+
+
+class TooManyRequests(RequestException):
+    """Too many requests."""
+
+
+class BadCookies(RequestException):
+    """Login expiration."""
+
+
+class GeoUrnNotFound(RequestException):
+    """Could not find geo urn for given location."""
+
+class BlobException(Exception):
+    pass
