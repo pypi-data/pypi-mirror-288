03445696b8da6daeda8b957edadcd7c3c675814f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:05:22 +0000

    Add package quke/0.6.0/quke-0.6.0-py3-none-any.whl

---
+++ packages/quke/quke-0.6.0-py3-none-any.whl/quke/llm_chat.py
@@ -0,0 +1,213 @@
+"""Sets up all elements required for a chat session."""
+
+import importlib
+import logging  # functionality managed by Hydra
+from collections import defaultdict
+from datetime import datetime
+from pathlib import Path
+from typing import Literal
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+from quke import ClassImportDefinition
+
+
+def chat(
+    vectordb_location: str,
+    embedding_import: ClassImportDefinition,
+    vectordb_import: ClassImportDefinition,
+    llm_import: ClassImportDefinition,
+    llm_parameters: dict,
+    prompt_parameters: dict,
+    output_file: dict,
+) -> object:
+    """Initiates a chat with an LLM.
+
+    Sets up all components required for the chat including the LLM,
+    embedding model, vector store, chat memory, retriever and actual
+    questions.
+
+    Args:
+        vectordb_location: Folder of vector store.
+        embedding_import: Definition of embedding model.
+        vectordb_import: Definition of vector store.
+        llm_import: Definition of LLM.
+        llm_parameters: dict provided as **kwargs to LLM model class.
+        prompt_parameters: List of questions to ask the LLM.
+        output_file: Folder where result file will be saved.
+
+    Returns:
+        Object containing chat history.
+    """
+    module = importlib.import_module(embedding_import.module_name)
+    class_ = getattr(module, embedding_import.class_name)
+    embedding = class_()
+
+    logging.warning(
+        "CAUTION: This function uses external compute services "
+        "(like OpenAI or HuggingFace). This is likely to cost money."
+    )
+    module = importlib.import_module(vectordb_import.module_name)
+    class_ = getattr(module, vectordb_import.class_name)
+    vectordb = class_(embedding_function=embedding, persist_directory=vectordb_location)
+
+    module = importlib.import_module(llm_import.module_name)
+    class_ = getattr(module, llm_import.class_name)
+    llm = class_(**llm_parameters)
+
+    if llm_parameters.get("rate_limiter") is not None:
+        logging.info("Rate limiter used by LLM.")
+    else:
+        logging.info("No rate limiter used by LLM.")
+
+    condense_question_system_template = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
+    )
+
+    condense_question_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", condense_question_system_template),
+            ("placeholder", "{chat_history}"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, vectordb.as_retriever(), condense_question_prompt
+    )
+
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("placeholder", "{chat_history}"),
+            ("human", "{input}"),
+        ]
+    )
+    qa_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    convo_qa_chain = create_retrieval_chain(history_aware_retriever, qa_chain)
+
+    # NOTE: trial API keys may have very restrictive rules. It is plausible that you run into
+    # constraints after the 2nd question.
+
+    results = [
+        convo_qa_chain.invoke(
+            {
+                "input": question,
+                "chat_history": [],
+            }
+        )
+        for question in prompt_parameters
+    ]
+
+    # results = [qa({"question": question}) for question in prompt_parameters]
+    chat_output_to_html(
+        results, output_file
+    )  # TODO: infer output from output file name in cfg?
+    chat_output_to_html(results, output_file, output_extension=".md")
+    chat_output_to_html(results, output_file, output_extension="logging")
+
+    logging.info("=======================")
+
+    return results
+
+
+def chat_output_to_html(
+    results: list[dict],
+    output_file: dict,
+    output_extension: Literal[".html", ".md", "logging"] = ".html",
+) -> None:
+    """Write summary of chat experiment into HTML file.
+
+    Args:
+        results: list of dicts with the answer from the LLM. Expects 'question', 'answer'
+        and 'source' keys; 'page' key optionally.
+        output_file: path and other information regarding the output file.
+        output_extension: .html or .md. Alteratively logging for python logging.
+    """
+    env = Environment(loader=PackageLoader("quke"), autoescape=select_autoescape())
+
+    if output_extension.lower() == ".html":
+        template_name = "chat_session.html.jinja"
+    elif output_extension.lower() == ".md":
+        template_name = "chat_session.md.jinja"
+    elif output_extension.lower() == "logging":
+        template_name = "chat_session.logging.jinja"
+    else:
+        template_name = "chat_session.html.jinja"
+
+    template = env.get_template(template_name)
+    func_dict = {"dict_crosstab": _dict_crosstab_for_jinja}
+    template.globals.update(func_dict)
+
+    output = template.render(
+        chat_time=datetime.now().astimezone().strftime("%a %d-%b-%Y %H:%M %Z"),
+        llm_results=results,
+        config=output_file["conf_yaml"],
+    )
+
+    if output_extension.lower() == "logging":
+        logging.info(output)
+    else:
+        file_path = Path(output_file["path"]).with_suffix(output_extension)
+        with file_path.open("w") as fp:
+            fp.write(output)
+
+
+def _dict_crosstab_for_jinja(sources: list) -> dict:
+    """Wrapper around dict_crostab for use from within Jinja.
+
+    Args:
+        sources (list): _description_
+
+    Returns:
+        dict: _description_
+    """
+    src_docs = [doc.metadata for doc in sources]
+    return dict_crosstab(src_docs, "source", "page")
+
+
+def dict_crosstab(source: list, key: str, listed: str, missing: str = "NA") -> dict:
+    """Limited and simple version of a crosstab query on a dict.
+
+    Args:
+        source: List of dicts. Two elements per dict will be considered: 'key' and 'listed'.
+        key: Every dict should contain an entry for 'key'.
+        listed: The key for the element in the dict considered to contain the value.
+        missing: Value to be used if dict has no key for 'listed'.
+
+    Returns:
+        A dictionary containing 'keys' and a list of values for each 'key'.
+
+    >>> a = {'name': 'a', 'number': 2}
+    >>> b = {'name': 'a', 'number': 3, 'number_2': 3}
+    >>> c = {'name': 'a', 'number': 2}
+    >>> d = {'name': 'd', 'number': 1}
+    >>> e = {'name': 'e', 'number_3': 2}
+    >>> dict_crosstab([e, b, c, d, a], 'name', 'number')
+    {'e': ['NA'], 'a': [2, 3], 'd': [1]}
+    """
+    dict_subs = [{key: d[key], listed: d.get(listed, missing)}.values() for d in source]
+
+    d = defaultdict(list)
+    for k, v in dict_subs:
+        d[k].append(v)
+        d[k] = list(set(d[k]))
+
+    return dict(d)  # TODO: consider sorted(d.items())
---
+++ packages/quke/quke-0.6.0-py3-none-any.whl/quke/quke.py
@@ -0,0 +1,231 @@
+"""Main module to initiate quke, to compare chat results.
+
+LLMs, embedding model, vector store and other components can be congfigured.
+"""
+
+import logging  # functionality managed by Hydra
+from pathlib import Path
+
+import hydra
+from dotenv import find_dotenv, load_dotenv
+from hydra.utils import to_absolute_path
+from omegaconf import DictConfig, OmegaConf
+from rich.console import Console
+
+from quke import ClassImportDefinition, ClassRateLimit, DatabaseAction, embed, llm_chat
+from quke import rate_limiter as qrate_limiter
+
+_ = load_dotenv(find_dotenv())
+
+
+class ConfigParser:
+    """Turns config into a more programming friendly and consistent format.
+
+    For core classes and functions groups configuration by
+    class/function arguments for convenience.
+
+    Provides an abstraction layer on top of the structure of the
+    Hydra configuration files.
+    """
+
+    def __init__(self, cfg: DictConfig) -> None:
+        """Creates configuration abstraction."""
+        self.cfg = cfg
+
+        # TODO: Read from cfg
+        self.src_doc_folder = cfg.source_document_folder
+
+        # TODO: Is this sufficiently robust? What if the user wants a folder not related to wcd/pwd?
+        self.vectordb_location = str(
+            Path.cwd()
+            / cfg.internal_data_folder
+            / cfg.embedding.vectordb.vectorstore_location
+        )
+        self.embedding_import = ClassImportDefinition(
+            cfg.embedding.embedding.module_name, cfg.embedding.embedding.class_name
+        )
+        self.vectordb_import = ClassImportDefinition(
+            cfg.embedding.vectordb.module_name, cfg.embedding.vectordb.class_name
+        )
+        self.llm_import = ClassImportDefinition(
+            cfg.llm.module_name_llm, cfg.llm.class_name_llm
+        )
+        self.embedding_rate_limit = ClassRateLimit(
+            cfg.embedding.embedding.rate_limit_chunks,
+            cfg.embedding.embedding.rate_limit_delay,
+        )
+        self.embedding_kwargs = self.get_embedding_kwargs(cfg)
+
+        self.splitter_import = ClassImportDefinition(
+            cfg.embedding.splitter.module_name, cfg.embedding.splitter.class_name
+        )
+        self.splitter_args = self.get_args_dict(cfg.embedding.splitter.args)
+
+        try:
+            self.write_mode = DatabaseAction[
+                (cfg.embedding.vectordb.vectorstore_write_mode).upper()
+            ]
+        except Exception:
+            logging.warning(
+                f"Invalid value configured for cfg.embedding.vectorstore_write_mode: "
+                f"{cfg.embedding.vectordb.vectorstore_write_mode}. Using no_overwrite instead."
+            )
+            self.write_mode = DatabaseAction.NO_OVERWRITE
+
+        self.questions = cfg.question.questions
+
+        try:
+            if not cfg.embed_only:
+                self.embed_only = False
+            else:
+                self.embed_only = True
+        except Exception:
+            self.embed_only = False
+
+        # TODO: need something better for output folder
+        # https://hydra.cc/docs/tutorials/basic/running_your_app/working_directory/
+        try:  # try statement done for testing suite
+            self.output_file = str(
+                Path(hydra.core.hydra_config.HydraConfig.get()["runtime"]["output_dir"])
+                / cfg.experiment_summary_file
+            )
+        except Exception:
+            self.output_file = cfg.experiment_summary_file
+
+        self.llm_rate_limiter_name = getattr(cfg.llm, "rate_limiter", None)
+
+    def get_rate_limiter_kwargs(self) -> dict:
+        """Based on the config files returns the set of parameters needed to setup a rate limiter."""
+        if not self.llm_rate_limiter_name:
+            logging.info("No rate_limiter used as none specified in llm config file.")
+            return {}
+
+        rate_limiters = OmegaConf.to_container(self.cfg.rate_limiters, resolve=True)
+        limiter_index = next(
+            (i for i, d in enumerate(rate_limiters) if self.llm_rate_limiter_name in d),
+            -1,
+        )
+
+        if limiter_index != -1:
+            rate_limiter_config = rate_limiters[limiter_index][
+                self.llm_rate_limiter_name
+            ]
+        else:
+            logging.warning(
+                f"No rate_limiter used as the rate limiter specified in llm config file ({self.llm_rate_limiter_name}) cannot be found in config.yaml."
+            )
+            rate_limiter_config = {}
+
+        return rate_limiter_config
+
+    def get_embed_params(self) -> dict:
+        """Based on the config files returns the set of parameters need to start embedding."""
+        return {
+            "src_doc_folder": self.src_doc_folder,
+            "vectordb_location": self.vectordb_location,
+            "embedding_import": self.embedding_import,
+            "embedding_kwargs": self.embedding_kwargs,
+            "vectordb_import": self.vectordb_import,
+            "rate_limit": self.embedding_rate_limit,
+            "splitter_params": self.get_splitter_params(),
+            "write_mode": self.write_mode,
+        }
+
+    def get_chat_params(self) -> dict:
+        """Based on the config files returns the set of parameters need to start a chat."""
+        return {
+            "vectordb_location": self.vectordb_location,
+            "embedding_import": self.embedding_import,
+            "vectordb_import": self.vectordb_import,
+            "llm_import": self.llm_import,
+            "llm_parameters": self.get_llm_parameters(),
+            "prompt_parameters": self.questions,
+            "output_file": self.get_chat_session_file_parameters(self.cfg),
+        }
+
+    def get_splitter_params(self) -> dict:
+        """Based on the config files returns the set of parameters needed to split source documents."""
+        return {
+            "splitter_import": self.splitter_import,
+            "splitter_args": self.splitter_args,
+        }
+
+    def get_args_dict(self, cfg_sub: dict) -> dict:
+        """Takes a subset of the Hydra configs and returns the same as a dict."""
+        res = OmegaConf.to_container(cfg_sub, resolve=True)
+        return res if isinstance(res, dict) else {}
+
+    def create_rate_limiter(self) -> qrate_limiter.InMemoryRateLimiter:
+        """Create a new rate limiter and add it to the global dictionary."""
+        limiter_kwargs = self.get_rate_limiter_kwargs()
+
+        if limiter_kwargs:
+            rate_limiter = qrate_limiter.get_rate_limiter(
+                self.llm_rate_limiter_name, **limiter_kwargs
+            )
+            return rate_limiter
+
+        return None
+
+    def get_llm_parameters(self) -> dict:
+        """Based on the config files returns the set of parameters needed to setup an LLM."""
+        res = OmegaConf.to_container(self.cfg.llm.llm_args, resolve=True)
+
+        rate_limiter = self.create_rate_limiter()
+        if rate_limiter:
+            res["rate_limiter"] = rate_limiter
+
+        return res if isinstance(res, dict) else {}
+
+    def get_chat_session_file_parameters(self, cfg: DictConfig) -> dict:
+        """Returns the full configuration in a single yaml and file location for output."""
+        return {
+            "path": self.output_file,
+            "conf_yaml": OmegaConf.to_yaml(cfg),
+        }
+
+    def get_embedding_kwargs(self, cfg: DictConfig) -> dict:
+        """Based on the config files returns the set of parameters needed for embedding."""
+        try:
+            embedding_kwargs = (
+                cfg.embedding.embedding.kwargs if cfg.embedding.embedding.kwargs else {}
+            )
+        except Exception:
+            embedding_kwargs = {}
+        return embedding_kwargs
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def quke(cfg: DictConfig) -> None:
+    """The main function to initiate a chat.
+
+    Including the embedding of the provided source documents.
+
+    Questions, LLM, embedding model, vectordb are specified in config files (using Hydra).
+    """
+    console = Console()
+    config_parser = ConfigParser(cfg)
+
+    embed_parameters = config_parser.get_embed_params()
+
+    with console.status("Embedding...", spinner="aesthetic"):
+        # python -m rich.spinner to see options
+        embed.embed(**embed_parameters)
+        # Used to log config here: logging.info("\n" + OmegaConf.to_yaml(cfg))
+
+    if not config_parser.embed_only:
+        with console.status("Chatting...", spinner="aesthetic"):
+            chat_parameters = config_parser.get_chat_params()
+            llm_chat.chat(**chat_parameters)
+
+    logging.info(
+        f"Source documents loaded from: {to_absolute_path(config_parser.src_doc_folder)}"
+    )
+    logging.info(
+        f"Vector store created in: {to_absolute_path(config_parser.vectordb_location)}"
+    )
+    logging.info(f"Results captured in: {to_absolute_path(config_parser.output_file)}")
+
+
+if __name__ == "__main__":
+    quke()
