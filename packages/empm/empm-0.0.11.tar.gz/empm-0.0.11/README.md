0a7b80ee84465ddb29b7c0412dac56d15e4a423b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 15:03:49 +0000

    Add package empm/0.0.11/empm-0.0.11.tar.gz

---
+++ packages/empm/empm-0.0.11.tar.gz/empm-0.0.11/README.md
@@ -0,0 +1,21 @@
+# empm
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/empm?logo=python)
+![PyPI - Version](https://img.shields.io/pypi/v/empm?logo=pypi)
+![Codecov](https://img.shields.io/codecov/c/github/paopaozhi/empm?logo=codecov)
+![PyPI - License](https://img.shields.io/pypi/l/empm?logo=apache)
+
+灵感来源于，pip和cargo这两个包管理工具，在嵌入式开发中，总是需要自己处理各种包，并且需要将其嵌入到自己的工程中，这就会可能会导致一个简单的demo工程变得非常庞大，在python和rust开发中，我们总是可以使用优秀的包管理器管理包，但是，在嵌入式中并没有一个让我中意的包管理器，所以，该项目应于而生。
+
+empm，帮助嵌入式软件工程师完成关于包的管理，让其专注在逻辑代码的实现。
+
+## 支持命令
+
+目前支持的功能还很少，但是，新功能正在紧张的开发中。
+
+|命令|描述|
+|---|---|
+|add|添加库|
+|home|GUI管理界面|
+|install|读取toml文件，安装库|
+|remove|删除库|
---
+++ packages/empm/empm-0.0.11.tar.gz/empm-0.0.11/empm/_internal/web/main.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from empm.utility import TomlDepend
+
+web_app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+web_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+base_path = Path(Path(__file__).parent, "frontend/dist")
+
+web_app.mount("/assets", StaticFiles(directory=f"{base_path}/assets"), name="assets")
+
+
+@web_app.get("/")
+async def root():
+    html_path = Path(base_path, "index.html")
+    return FileResponse(html_path)
+
+
+@web_app.get("/pack/info")
+async def get_pack_info():
+    cfg = TomlDepend()
+    return cfg.info()
+
+
+@web_app.get("/pack/dependencies")
+async def get_pack_dependencies():
+    cfg = TomlDepend()
+    dependencies = cfg.get_depend()
+    depend = []
+    for i in dependencies:
+        try:
+            depend.append(
+                {
+                    "name": i,
+                    "url": dependencies[i]["url"],
+                    "version": dependencies[i]["version"],
+                }
+            )
+        except Exception:
+            depend.append({"name": i, "url": dependencies[i]["url"], "version": None})
+
+    return depend
