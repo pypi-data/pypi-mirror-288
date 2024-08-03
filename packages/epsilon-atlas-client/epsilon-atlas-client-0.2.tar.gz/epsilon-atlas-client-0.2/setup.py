4747b52d32772d3f0138990f9b7a0762e3565b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:24 +0000

    Add package epsilon-atlas-client/0.2/epsilon-atlas-client-0.2.tar.gz

---
+++ packages/epsilon-atlas-client/epsilon-atlas-client-0.2.tar.gz/epsilon-atlas-client-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='epsilon-atlas-client',
+    version='0.2',
+    packages=find_packages(include=['atlas_client', 'atlas_client.*']),
+    install_requires=[
+        'pandas',
+        'requests',
+        'python-dotenv',
+    ],
+    keywords='atlas client, apache atlas',
+    include_package_data=True,
+    package_data={
+        '': ['*.csv', '*.env'],
+    },
+    author='Khajiev Nizomjon',
+    author_email='nizom7812@example.com',
+    description='A package for reading data from CSV or XLS files and publishing to Apache Atlas.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Epsilon-Data/py-packages',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
