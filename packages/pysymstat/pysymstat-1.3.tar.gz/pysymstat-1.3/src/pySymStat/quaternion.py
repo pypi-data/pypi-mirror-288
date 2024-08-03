e1e6adc5e40558b277f6c7e0666ac7cd376b1d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 15:03:55 +0000

    Add package pysymstat/1.3/pysymstat-1.3.tar.gz

---
+++ packages/pysymstat/pysymstat-1.3.tar.gz/pysymstat-1.3/src/pySymStat/quaternion.py
@@ -0,0 +1,73 @@
+__all__ = [
+    'quat_norm', # hide this
+    'quat_conj',
+    'quat_mult',
+    'quat_same', # hide this
+    'quat_rotate'
+]
+
+import numpy as np
+
+from numpy.typing import NDArray
+
+def quat_norm(q : NDArray[np.float64]) -> float:
+    return np.linalg.norm(q)
+
+def quat_conj(q : NDArray[np.float64]) -> NDArray[np.float64]:
+    '''
+    The `quat_conj` functions calculates the conjugate of an input quaternion.
+    - `q`: This is a quaternion and should be a NumPy vector of type `np.float64` with a length of 4.
+    '''
+    return q * np.array([1, -1, -1, -1])
+
+def quat_mult(
+    q1 : NDArray[np.float64],
+    q2 : NDArray[np.float64]
+) -> NDArray[np.float64]:
+    '''
+    The `quat_mult` functions calculates the product of two input quaternions.
+    - `q1` and `q2`: This are two quaternions and should be NumPy vectors of type `np.float64` with a length of 4.
+    '''
+    w1, x1, y1, z1 = q1
+    w2, x2, y2, z2 = q2
+    return np.array([w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2,
+                     w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2,
+                     w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2,
+                     w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2], dtype = np.float64)
+
+def quat_same(
+    q1 : NDArray[np.float64],
+    q2 : NDArray[np.float64],
+    eps : float = 1e-3
+) -> bool:
+    return np.linalg.norm(q1 - q2) < eps or np.linalg.norm(q1 + q2) < eps
+
+def quat_rotate(
+    q : NDArray[np.float64], 
+    v : NDArray[np.float64]
+) -> NDArray[np.float64]:
+    '''
+    The `quat_rotate` functions rotate this a vector based on a spatial rotation represented by a unit quarternion.
+    - `q`: This is a quaternion and should be a NumPy vector of type `np.float64` with a length of 4. It represents the spatial rotation.
+    - `v`: This is a vector in 3D space, which to be rotated.
+    '''
+    v = np.array([0, v[0], v[1], v[2]], dtype = np.float64)
+    qv = quat_mult(quat_mult(q, v), quat_conj(q))
+    return qv[1:]
+
+if __name__ == '__main__':
+
+    q1 = np.random.randn(4)
+    q1 /= np.linalg.norm(q1)
+    q2 = np.random.randn(4)
+    q2 /= np.linalg.norm(q2)
+
+    v = np.random.randn(3)
+    v /= np.linalg.norm(v)
+
+    print(r'R_q v == R_{-q} v:',
+          np.allclose(quat_rotate(q1, v), quat_rotate(-q1, v)))
+    print(r'R_{conj(q)} R_q v == v:',
+          np.allclose(quat_rotate(quat_conj(q1), quat_rotate(q1, v)), v))
+    print(r'R_{q1} R_{q2} v == R_{q1 q2}v:',
+          np.allclose(quat_rotate(q1, quat_rotate(q2, v)), quat_rotate(quat_mult(q1, q2), v)))
