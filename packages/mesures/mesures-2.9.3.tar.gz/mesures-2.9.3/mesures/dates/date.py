eb0245831d4457da5d163703c85033240515098b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:03:48 +0000

    Add package mesures/2.9.3/mesures-2.9.3.tar.gz

---
+++ packages/mesures/mesures-2.9.3.tar.gz/mesures-2.9.3/mesures/dates/date.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+
+DATE_MASK = '%Y/%m/%d %H'
+DATETIME_HOUR_MASK = '%Y/%m/%d %H:%M'
+DATETIME_MASK = '%Y/%m/%d %H:%M:%S'
+REE_END_DATE = '30000101'
+REE_END_DATE_HOUR = '3000/01/01 01'
+SIMPLE_DATE_MASK = '%Y%m%d'
