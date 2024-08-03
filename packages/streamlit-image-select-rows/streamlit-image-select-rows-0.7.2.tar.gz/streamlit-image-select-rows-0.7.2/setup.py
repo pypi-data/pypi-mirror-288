65feb9625f06f2bb35a5300efc270ea50b00fd6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:04:23 +0000

    Add package streamlit-image-select-rows/0.7.2/streamlit-image-select-rows-0.7.2.tar.gz

---
+++ packages/streamlit-image-select-rows/streamlit-image-select-rows-0.7.2.tar.gz/streamlit-image-select-rows-0.7.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_desc = f.read()
+
+setuptools.setup(
+name = "streamlit-image-select-rows",
+version = "0.7.2",
+description = "🖼️ An image select component for Streamlit",
+authors = ["Johannes Rieke <johannes.rieke@gmail.com>", "Fabian Lucas <fl@msr.de>"] ,
+license = "MIT",
+readme = "README.md",
+setup_requires=['wheel'],
+long_description=long_desc,
+long_description_content_type='text/markdown',
+packages=setuptools.find_packages(),
+include_package_data=True,
+classifiers=[],
+python_requires=">=3.6",
+install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+],
+)
