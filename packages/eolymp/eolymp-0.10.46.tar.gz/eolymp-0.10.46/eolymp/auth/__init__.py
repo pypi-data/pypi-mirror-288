4b2c1bb459fec13ca4f4157e0179ae134a927590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:04:59 +0000

    Add package eolymp/0.10.46/eolymp-0.10.46.tar.gz

---
+++ packages/eolymp/eolymp-0.10.46.tar.gz/eolymp-0.10.46/eolymp/auth/__init__.py
@@ -0,0 +1,5 @@
+from .oauth2_service_pb2 import *
+from .sso_service_pb2 import *
+from .sso_service_http import *
+from .claims_pb2 import *
+from .oauth2_service_http import *
---
+++ packages/eolymp/eolymp-0.10.46.tar.gz/eolymp-0.10.46/eolymp/keeper/__init__.py
@@ -0,0 +1,3 @@
+from .keeper_http import *
+from .keeper_pb2 import *
+from .blob_pb2 import *
