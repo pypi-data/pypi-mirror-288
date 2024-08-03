eb576a63ac78eaa8ad8dde0f1ac329181d15bbef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 15:03:49 +0000

    Add package e2hbaseapi/0.1.9.4/e2hbaseapi-0.1.9.4-py3-none-any.whl

---
+++ packages/e2hbaseapi/e2hbaseapi-0.1.9.4-py3-none-any.whl/e2hbaseapi/__init__.py
@@ -0,0 +1,12 @@
+"""
+Modules:
+- client: Contains the E2hBaseApiClient class for interacting with e2hapi.
+
+Version: 1.0.1
+"""
+
+from .client import E2hApiClient
+
+__version__ = "1.0.0"
+__author__ = "E2H"
+__license__ = "MIT"
