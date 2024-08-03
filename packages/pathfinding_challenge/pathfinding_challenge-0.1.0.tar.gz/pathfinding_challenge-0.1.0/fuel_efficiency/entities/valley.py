9c0992981a8ecc4f6e492dd5bc180a59d35e046e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:53 +0000

    Add package pathfinding_challenge/0.1.0/pathfinding_challenge-0.1.0.tar.gz

---
+++ packages/pathfinding_challenge/pathfinding_challenge-0.1.0.tar.gz/pathfinding_challenge-0.1.0/fuel_efficiency/entities/valley.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+
+from fuel_efficiency.entities.node import Node
+from fuel_efficiency.entities.position import Position
+
+
+@dataclass(slots=True)
+class Valley(Node):
+    weight: float = float(1)
+    position: Position = field(default_factory=Position)
+
+    def __hash__(self):
+        """Return a hash value for the object."""
+        return hash((self.position.x, self.position.y))
+
+    def __eq__(self, other):
+        if (
+            not hasattr(self, 'weight')
+            or not hasattr(self, 'position')
+            or not hasattr(other, 'weight')
+            or not hasattr(other, 'position')
+        ):
+            raise NotImplementedError(
+                'Missing `position` or `weight` attribute'
+            )
+        # if not isinstance(other, Valley):
+        #     raise NotImplementedError(
+        #         f'Cannot compare Valley with {type(other)}'
+        #     )
+        return self.position == other.position
+
+    # def __lt__(self, other) -> bool:
+    #     if not isinstance(other, Valley):
+    #         return NotImplemented
+    #     return (self.position.x, self.position.y) < (
+    #         other.position.x,
+    #         other.position.y,
+    #     )
+
+    def __lt__(self, other: 'Valley') -> bool:
+        if not hasattr(self, 'weight') or not hasattr(other, 'weight'):
+            raise NotImplementedError('Missing `weight` attribute')
+        return self.weight < other.weight
+
+    def __gt__(self, other: 'Valley') -> bool:
+        if not hasattr(self, 'weight') or not hasattr(other, 'weight'):
+            raise NotImplementedError('Missing `weight` attribute')
+        return self.weight > other.weight
+
+    def __ne__(self, other: 'Valley') -> bool:
+        if not hasattr(self, 'weight') or not hasattr(other, 'weight'):
+            raise NotImplementedError('Missing `weight` attribute')
+        return self.weight != other.weight
