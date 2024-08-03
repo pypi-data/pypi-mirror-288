b7f0c7dddd19b20d995e9bbb6dd1693272db0a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 06:04:24 +0000

    Add package pymoc/0.5.2/pymoc-0.5.2.tar.gz

---
+++ packages/pymoc/pymoc-0.5.2.tar.gz/pymoc-0.5.2/lib/pymoc/util/catalog.py
@@ -0,0 +1,215 @@
+# Copyright (C) 2015-2017 East Asian Observatory.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+
+from math import pi
+
+from astropy.coordinates import SkyCoord
+from astropy.io import ascii
+from astropy.units import arcsecond, hour, degree, radian
+from astropy.units.quantity import Quantity
+from healpy import query_disc
+from healpy.pixelfunc import \
+    ang2pix, ang2vec, get_all_neighbours, pix2ang, vec2pix
+import numpy as np
+
+from ..moc import MOC
+
+
+def catalog_to_moc(catalog, radius, order, **kwargs):
+    """
+    Convert a catalog to a MOC.
+
+    The catalog is given as an Astropy SkyCoord object containing
+    multiple coordinates.  The radius of catalog entries can be
+    given as an Astropy Quantity (with units), otherwise it is assumed
+    to be in arcseconds.
+
+    Any additional keyword arguments are passed on to `catalog_to_cells`.
+    """
+
+    # Generate list of MOC cells.
+    cells = catalog_to_cells(catalog, radius, order, **kwargs)
+
+    # Create new MOC object using our collection of cells.
+    moc = MOC(moctype='CATALOG')
+    moc.add(order, cells, no_validation=True)
+    return moc
+
+
+def _catalog_to_cells_neighbor(catalog, radius, order):
+    """
+    Convert a catalog to a list of cells.
+
+    This is the original implementation of the `catalog_to_cells`
+    function which does not make use of the Healpy `query_disc` routine.
+
+    Note: this function uses a simple flood-filling approach and is
+    very slow, especially when used with a large radius for catalog objects
+    or a high resolution order.
+    """
+
+    if not isinstance(radius, Quantity):
+        radius = radius * arcsecond
+
+    nside = 2 ** order
+
+    # Ensure catalog is in ICRS coordinates.
+    catalog = catalog.icrs
+
+    # Determine central cell for each catalog entry.
+    phi = catalog.ra.radian
+    theta = (pi / 2) - catalog.dec.radian
+
+    cells = np.unique(ang2pix(nside, theta, phi, nest=True))
+
+    # Iteratively consider the neighbors of cells within our
+    # catalog regions.
+    new_cells = cells
+    rejected = np.array((), dtype=np.int64)
+    while True:
+        # Find new valid neighboring cells which we didn't already
+        # consider.
+        neighbors = np.unique(np.ravel(
+            get_all_neighbours(nside, new_cells, nest=True)))
+
+        neighbors = np.extract(
+            [(x != -1) and (x not in cells) and (x not in rejected)
+             for x in neighbors], neighbors)
+
+        # Get the coordinates of each of these neighbors and compare them
+        # to the catalog entries.
+        (theta, phi) = pix2ang(nside, neighbors, nest=True)
+
+        coords = SkyCoord(phi, (pi / 2) - theta, frame='icrs', unit='rad')
+
+        (idx, sep2d, dist3d) = coords.match_to_catalog_sky(catalog)
+
+        within_range = (sep2d < radius)
+
+        # If we didn't find any new cells within range,
+        # end the iterative process.
+        if not np.any(within_range):
+            break
+
+        new_cells = neighbors[within_range]
+        cells = np.concatenate((cells, new_cells))
+        rejected = np.concatenate((
+            rejected, neighbors[np.logical_not(within_range)]))
+
+    return cells
+
+
+def catalog_to_cells(catalog, radius, order, include_fallback=True, **kwargs):
+    """
+    Convert a catalog to a set of cells.
+
+    This function is intended to be used via `catalog_to_moc` but
+    is available for separate usage.  It takes the same arguments
+    as that function.
+
+    This function uses the Healpy `query_disc` function to get a list
+    of cells for each item in the catalog in turn.  Additional keyword
+    arguments, if specified, are passed to `query_disc`.  This can include,
+    for example, `inclusive` (set to `True` to include cells overlapping
+    the radius as well as those with centers within it) and `fact`
+    (to control sampling when `inclusive` is specified).
+
+    If cells at the given order are bigger than the given radius, then
+    `query_disc` may find none inside the radius.  In this case,
+    if `include_fallback` is `True` (the default), the cell at each
+    position is included.
+
+    If the given radius is zero (or smaller) then Healpy `query_disc`
+    is not used -- instead the fallback position is used automatically.
+    """
+
+    nside = 2 ** order
+
+    # Ensure catalog is in ICRS coordinates.
+    catalog = catalog.icrs
+
+    # Ensure radius is in radians.
+    if isinstance(radius, Quantity):
+        radius = radius.to(radian).value
+    else:
+        radius = radius * pi / (180.0 * 3600.0)
+
+    # Convert coordinates to position vectors.
+    phi = catalog.ra.radian
+    theta = (pi / 2) - catalog.dec.radian
+
+    vectors = ang2vec(theta, phi)
+
+    # Ensure we can iterate over vectors (it might be a single position).
+    if catalog.isscalar:
+        vectors = [vectors]
+
+    # Query for a list of cells for each catalog position.
+    cells = set()
+    for vector in vectors:
+        if radius > 0.0:
+            # Try "disc" query.
+            vector_cells = query_disc(nside, vector, radius, nest=True, **kwargs)
+
+            if vector_cells.size > 0:
+                cells.update(vector_cells.tolist())
+                continue
+
+            elif not include_fallback:
+                continue
+
+        # The query didn't find anything -- include the cell at the
+        # given position at least.
+        cell = vec2pix(nside, vector[0], vector[1], vector[2], nest=True)
+        cells.add(cell.item())
+
+    return cells
+
+
+def read_ascii_catalog(filename, format_, unit=None):
+    """
+    Read an ASCII catalog file using Astropy.
+
+    This routine is used by pymoctool to load coordinates from a
+    catalog file in order to generate a MOC representation.
+    """
+
+    catalog = ascii.read(filename, format=format_)
+    columns = catalog.columns
+
+    if 'RA' in columns and 'Dec' in columns:
+        if unit is None:
+            unit = (hour, degree)
+
+        coords = SkyCoord(catalog['RA'],
+                          catalog['Dec'],
+                          unit=unit,
+                          frame='icrs')
+
+    elif 'Lat' in columns and 'Lon' in columns:
+        if unit is None:
+            unit = (degree, degree)
+
+        coords = SkyCoord(catalog['Lon'],
+                          catalog['Lat'],
+                          unit=unit,
+                          frame='galactic')
+
+    else:
+        raise Exception('columns RA,Dec or Lon,Lat not found')
+
+    return coords
---
+++ packages/pymoc/pymoc-0.5.2.tar.gz/pymoc-0.5.2/lib/pymoc/util/tool.py
@@ -0,0 +1,443 @@
+# Copyright (C) 2014 Science and Technology Facilities Council.
+# Copyright (C) 2015-2017 East Asian Observatory.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""pymoctool - PyMOC utility program
+
+Usage::
+
+    pymoctool [INPUT]... [COMMAND [FILE]]... [--output OUTPUT]
+
+This program can be used to manipulate MOC files.  All formats handled
+by PyMOC (FITS, JSON and ASCII) are supported.  The program maintains a
+"running" MOC into which all input files are merged.  Each command operates
+on the "running" MOC.  Input files and commands are processed in the order
+given to the command.
+
+For example, this command::
+
+    pymoctool a.fits --output a.json b.fits --output merged.txt
+
+would load a MOC "a.fits", re-write it as "a.json", merge (forming the union
+with) "b.fits" and write the combined MOC to "merged.txt".
+"""
+
+
+from __future__ import absolute_import, print_function
+
+import os.path
+import sys
+import textwrap
+
+from .. import MOC
+from ..io.ascii import read_moc_ascii, write_moc_ascii
+from ..version import version
+
+
+class CommandDict(dict):
+    """Decorator to record commands in a dictionary."""
+
+    def __init__(self):
+        """Constructor."""
+        dict.__init__(self)
+        self.documentation = {}
+
+    def __call__(self, *aliases):
+        """Callable method.
+
+        Adds the function which it is decorating to the dictionary,
+        indexed by each of the given command aliases.
+        """
+
+        def command(f):
+            for alias in aliases:
+                self[alias] = f
+
+            self.documentation[aliases[0].lstrip('-')] = (aliases, f.__doc__)
+
+            return f
+        return command
+
+
+class CommandError(Exception):
+    """Class representing expected errors from PyMOC tool commands."""
+
+    pass
+
+
+class MOCTool(object):
+    """Class implementing a basic tool to manipulate MOC files."""
+
+    command = CommandDict()
+
+    def __init__(self):
+        """Constructor.
+
+        Initializes the running MOC object to None.
+        """
+
+        self.moc = None
+
+    def run(self, params):
+        """Main run method for PyMOC tool.
+
+        Takes a list of command line arguments to process.
+
+        Each operation is performed on a current "running" MOC
+        object.
+        """
+
+        self.params = list(reversed(params))
+
+        if not self.params:
+            self.help()
+            return
+
+        while self.params:
+            p = self.params.pop()
+
+            if p in self.command:
+                # If we got a known command, execute it.
+                self.command[p](self)
+
+            elif p == '-':
+                self.read_moc_stdin()
+
+            elif os.path.exists(p):
+                # If we were given the name of an existing file, read it.
+                self.read_moc(p)
+
+            else:
+                # Otherwise raise an error.
+                raise CommandError('file or command {0} not found'.format(p))
+
+    def read_moc(self, filename):
+        """Read a file into the current running MOC object.
+
+        If the running MOC object has not yet been created, then
+        it is created by reading the file, which will import the
+        MOC metadata.  Otherwise the metadata are not imported.
+        """
+
+        if self.moc is None:
+            self.moc = MOC(filename=filename)
+
+        else:
+            self.moc.read(filename)
+
+    def read_moc_stdin(self):
+        """Read from stdin into the current running MOC object.
+
+        Create the running MOC object if it does not already
+        exist, then attempt to read ASCII format data
+        from standard input.
+        """
+
+        if self.moc is None:
+            self.moc = MOC()
+
+        read_moc_ascii(self.moc, file=sys.stdin)
+
+    @command('--catalog')
+    def catalog(self):
+        """Create MOC from catalog of coordinates.
+
+        This command requires that the Healpy and Astropy libraries
+        be available.  It attempts to load the given catalog,
+        and merges it with the running MOC.
+
+        The name of an ASCII catalog file should be given.  The file
+        should contain either "RA" and "Dec" columns (for ICRS coordinates)
+        or "Lon" and "Lat" columns (for galactic coordinates).  The MOC
+        order and radius (in arcseconds) can be given with additional
+        options.
+
+        ::
+
+            pymoctool --catalog coords.txt
+                [order 12]
+                [radius 3600]
+                [unit (hour | deg | rad) (deg | rad)]
+                [format commented_header]
+                [inclusive]
+
+        Units (if not specified) are assumed to be hours and degrees for ICRS
+        coordinates and degrees for galactic coordinates.  The format, if not
+        specified (as an Astropy ASCII table format name) is assumed to be
+        commented header, e.g.:
+
+        ::
+
+            # RA Dec
+            01:30:00 +45:00:00
+            22:30:00 +45:00:00
+        """
+
+        from .catalog import catalog_to_moc, read_ascii_catalog
+
+        filename = self.params.pop()
+        order = 12
+        radius = 3600
+        unit = None
+        format_ = 'commented_header'
+        kwargs = {}
+
+        while self.params:
+            if self.params[-1] == 'order':
+                self.params.pop()
+                order = int(self.params.pop())
+            elif self.params[-1] == 'radius':
+                self.params.pop()
+                radius = float(self.params.pop())
+            elif self.params[-1] == 'unit':
+                self.params.pop()
+                unit_x = self.params.pop()
+                unit_y = self.params.pop()
+                unit = (unit_x, unit_y)
+            elif self.params[-1] == 'format':
+                self.params.pop()
+                format_ = self.params.pop()
+            elif self.params[-1] == 'inclusive':
+                self.params.pop()
+                kwargs['inclusive'] = True
+            else:
+                break
+
+        coords = read_ascii_catalog(filename, format_=format_, unit=unit)
+        catalog_moc = catalog_to_moc(coords, radius, order, **kwargs)
+
+        if self.moc is None:
+            self.moc = catalog_moc
+        else:
+            self.moc += catalog_moc
+
+    @command('--help', '-h')
+    def help(self):
+        """Display command usage information."""
+
+        if self.params:
+            command = self.params.pop().lstrip('-')
+
+            if command in self.command.documentation:
+                (aliases, doc) = self.command.documentation[command]
+                (synopsis, body) = self._split_docstring(doc)
+
+                print(synopsis)
+                if body:
+                    print()
+                    print(body)
+
+            else:
+                raise CommandError('command {0} not known'.format(command))
+        else:
+            (synopsis, body) = self._split_docstring(__doc__)
+
+            print(synopsis)
+            print()
+            print(body)
+            print()
+            print('Commands:')
+            for command in sorted(self.command.documentation.keys()):
+                print('   ', ', '.join(self.command.documentation[command][0]))
+            print()
+            print('Use "pymoctool --help COMMAND" for additional '
+                  'information about a command.')
+
+    @command('--id')
+    def identifier(self):
+        """Set the identifier of the current MOC.
+
+        The new identifier should be given after this option.
+
+        ::
+
+            pymoctool ... --id 'New MOC identifier' --output new_moc.fits
+        """
+
+        if self.moc is None:
+            self.moc = MOC()
+
+        self.moc.id = self.params.pop()
+
+    @command('--info', '-i')
+    def display_info(self):
+        """Display basic information about the running MOC."""
+
+        if self.moc is None:
+            print('No MOC information present')
+            return
+
+        if self.moc.name is not None:
+            print('Name:', self.moc.name)
+        if self.moc.id is not None:
+            print('Identifier:', self.moc.id)
+        print('Order:', self.moc.order)
+        print('Cells:', self.moc.cells)
+        print('Area:', self.moc.area_sq_deg, 'square degrees')
+
+    @command('--intersection')
+    def intersection(self):
+        """Compute the intersection with the given MOC.
+
+        This command takes the name of a MOC file and forms the intersection
+        of the running MOC with that file.
+
+        ::
+
+            pymoctool a.fits --intersection b.fits --output intersection.fits
+        """
+
+        if self.moc is None:
+            raise CommandError('No MOC information present for intersection')
+
+        filename = self.params.pop()
+        self.moc = self.moc.intersection(MOC(filename=filename))
+
+    @command('--name')
+    def name(self):
+        """Set the name of the current MOC.
+
+        The new name should be given after this option.
+
+        ::
+
+            pymoctool ... --name 'New MOC name' --output new_moc.fits
+        """
+
+        if self.moc is None:
+            self.moc = MOC()
+
+        self.moc.name = self.params.pop()
+
+    @command('--normalize')
+    def normalize(self):
+        """Normalize the MOC to a given order.
+
+        This command takes a MOC order (0-29) and normalizes the MOC so that
+        its maximum order is the given order.
+
+        ::
+
+            pymoctool a.fits --normalize 10 --output a_10.fits
+        """
+
+        if self.moc is None:
+            raise CommandError('No MOC information present for normalization')
+
+        order = int(self.params.pop())
+        self.moc.normalize(order)
+
+    @command('--output', '-o')
+    def write_moc(self):
+        """Write the MOC to a given file."""
+
+        if self.moc is None:
+            raise CommandError('No MOC information present for output')
+
+        filename = self.params.pop()
+
+        if filename == '-':
+            write_moc_ascii(self.moc, file=sys.stdout)
+            print()
+        else:
+            self.moc.write(filename)
+
+    @command('--subtract')
+    def subtract(self):
+        """Subtract the given MOC from the running MOC.
+
+        This command takes the name of a MOC file to be subtracted from the
+        running MOC.
+
+        ::
+
+            pymoctool a.fits --subtract b.fits --output difference.fits
+        """
+
+        if self.moc is None:
+            raise CommandError('No MOC information present for subtraction')
+
+        filename = self.params.pop()
+        self.moc -= MOC(filename=filename)
+
+    @command('--plot')
+    def plot(self):
+        """Show the running MOC on an all-sky map.
+
+        This command requires that the Healpy and matplotlib libraries be
+        available.  It plots the running MOC, which should be normalized to
+        a lower order first if it would generate an excessively large pixel
+        array.
+
+        ::
+
+            pymoctool a.moc --normalize 8 --plot
+
+        It also accepts additional arguments which can be used to control
+        the plot.  The 'order' option can be used instead of normalizing the
+        MOC before plotting.  The 'antialias' option specifies an additional
+        number of MOC orders which should be used to smooth the edges as
+        plotted -- 1 or 2 is normally sufficient.  The 'file' option can
+        be given to specify a file to which the plot should be saved.
+
+        ::
+
+            pymoctool ... --plot [order <order>] [antialias <level>] [file <filename>] ...
+        """
+
+        if self.moc is None:
+            raise CommandError('No MOC information present for plotting')
+
+        from .plot import plot_moc
+
+        order = self.moc.order
+        antialias = 0
+        filename = None
+
+        while self.params:
+            if self.params[-1] == 'order':
+                self.params.pop()
+                order = int(self.params.pop())
+            elif self.params[-1] == 'antialias':
+                self.params.pop()
+                antialias = int(self.params.pop())
+            elif self.params[-1] == 'file':
+                self.params.pop()
+                filename = self.params.pop()
+            else:
+                break
+
+        plot_moc(self.moc, order=order, antialias=antialias,
+                 filename=filename, projection='moll')
+
+    @command('--version')
+    def version(self):
+        """Show PyMOC version number."""
+
+        print('PyMOC', version)
+
+    def _split_docstring(self, docstring):
+        """Separate a docstring into the synopsis (first line) and body."""
+
+        lines = docstring.strip().splitlines()
+
+        synopsis = lines[0].strip()
+        body = textwrap.dedent('\n'.join(lines[2:]))
+
+        # Remove RST preformatted text markers.
+        body = body.replace('\n::\n', '')
+        body = body.replace('::\n', ':')
+
+        return (synopsis, body)
