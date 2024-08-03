da7ad5766c48bce119b8d2ffc553940bd3a6d4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 05:04:16 +0000

    Add package cat-win/1.8.3/cat_win-1.8.3-py3-none-any.whl

---
+++ packages/cat-win/cat_win-1.8.3-py3-none-any.whl/cat_win/tests/src/const/test_regex.py
@@ -0,0 +1,18 @@
+# from unittest import TestCase
+
+# from cat_win.src.const.regex import compile_re
+# # import sys
+# # sys.path.append('../cat_win')
+
+
+# class TestRegex(TestCase):
+#     def test_unique_parameters(self):
+#         parameters_short_form = [x.short_form for x in ALL_ARGS]
+#         parameters_long_form = [x.long_form for x in ALL_ARGS]
+#         parameters_help = [x.arg_help for x in ALL_ARGS]
+#         parameters_id= [x.arg_id for x in ALL_ARGS]
+#         self.assertEqual(len(set(parameters_short_form)), len(parameters_short_form))
+#         self.assertEqual(len(set(parameters_long_form)), len(parameters_long_form))
+#         self.assertEqual(len(set(parameters_help)), len(parameters_help))
+#         self.assertEqual(len(set(parameters_id)), len(parameters_id))
+        
