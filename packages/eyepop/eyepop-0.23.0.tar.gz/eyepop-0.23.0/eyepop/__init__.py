a31686235ae93f5cbd81a016d43dbe63a3213d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 14:04:26 +0000

    Add package eyepop/0.23.0/eyepop-0.23.0.tar.gz

---
+++ packages/eyepop/eyepop-0.23.0.tar.gz/eyepop-0.23.0/eyepop/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '0.22.0'
+
+import eyepop.eyepopsdk
+import eyepop.worker.worker_jobs
+import eyepop.visualize
+
+EyePopSdk = eyepopsdk.EyePopSdk
+Job = eyepop.worker.worker_jobs.WorkerJob
+Plot = visualize.EyePopPlot
