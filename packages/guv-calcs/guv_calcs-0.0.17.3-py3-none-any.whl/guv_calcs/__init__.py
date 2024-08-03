5078e94707098b6efcb2d98e7234f89dce6945b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 05:04:14 +0000

    Add package guv-calcs/0.0.17.3/guv_calcs-0.0.17.3-py3-none-any.whl

---
+++ packages/guv-calcs/guv_calcs-0.0.17.3-py3-none-any.whl/guv_calcs/__init__.py
@@ -0,0 +1,14 @@
+from .room import Room
+from .lamp import Lamp
+from .calc_zone import CalcVol, CalcPlane
+from .trigonometry import to_polar, to_cartesian, attitude
+
+__all__ = [
+    "Room",
+    "Lamp",
+    "CalcVol",
+    "CalcPlane",
+    "to_polar",
+    "to_cartesian",
+    "attitude",
+]
