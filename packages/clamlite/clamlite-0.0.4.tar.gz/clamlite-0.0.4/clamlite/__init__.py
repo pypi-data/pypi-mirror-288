b4643773219342cb8f1c45e871f68f1df6e80508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 06:04:15 +0000

    Add package clamlite/0.0.4/clamlite-0.0.4.tar.gz

---
+++ packages/clamlite/clamlite-0.0.4.tar.gz/clamlite-0.0.4/clamlite/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.4"
+
+from clamlite.create_patches_fp import seg_and_patch
+from clamlite.extract_features_fp import extract_features
+
+
+__all__ = [
+    'seg_and_patch',
+    'extract_features',
+]
