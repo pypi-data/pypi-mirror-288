45fb79be5fb447389ab778ed0afa75e092ec533c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 15:03:56 +0000

    Add package z80/1.0b3/z80-1.0b3.tar.gz

---
+++ packages/z80/z80-1.0b3.tar.gz/z80-1.0b3/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import inspect
+import os
+import platform
+from setuptools import Extension, setup
+
+
+here = os.path.abspath(os.path.dirname(inspect.getsource(lambda: 0)))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+cxx_flags = []
+if platform.system() == 'Windows':
+    pass
+else:
+    cxx_flags.extend([
+        '-std=c++11', '-Wall', '-fno-exceptions', '-fno-rtti',
+        '-O3',
+        # '-S', '-fverbose-asm',  # TODO
+    ])
+
+z80_emulator_module = Extension(
+    name='z80._z80',
+    extra_compile_args=cxx_flags,
+    sources=['z80/_z80module.cpp'],
+    language='c++')
+
+
+# TODO: Update the URL once we have a published documentation.
+# TODO: Do we have a name for the emulator?
+setup(name='z80',
+      version='1.0b3',
+      description='Fast and flexible Z80/i8080 Emulator',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Ivan Kosarev',
+      author_email='mail@ivankosarev.com',
+      url='https://github.com/kosarev/z80',
+      license='MIT',
+      ext_modules=[z80_emulator_module],
+      packages=['z80'],
+      install_requires=[],
+      entry_points={
+          'console_scripts': [
+              'z80 = z80:main',
+          ],
+      },
+      test_suite='tests.testsuite.suite',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Education',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: C++',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: System :: Emulators',
+      ],
+      )
