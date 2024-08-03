0bbbff8f0d68d9b6cada0ab91ed7dcbf7047da7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:04:12 +0000

    Add package campbell-quest/0.0.7/campbell_quest-0.0.7-py3-none-any.whl

---
+++ packages/campbell-quest/campbell_quest-0.0.7-py3-none-any.whl/campbell_quest/quest_generator.py
@@ -0,0 +1,14 @@
+from .questAgents import quest_brainstormer, quest_refiner, quest_formatter
+
+def generate_quest(quest_prompt, schema, objective_info, location_info, character_info, rewards):
+    
+    initial_generated_quest = quest_brainstormer.generate_quest(objective_info, quest_prompt, location_info, character_info)
+    
+    quest_with_objectives = quest_refiner.define_quest_objectives(initial_generated_quest, location_info, character_info)
+    
+    quest_reward = quest_refiner.define_quest_reward(initial_generated_quest, rewards)
+    print(quest_reward)
+    formatted_quest = quest_formatter.format_quest(quest_with_objectives, quest_reward, schema)
+    
+    return formatted_quest
+    
