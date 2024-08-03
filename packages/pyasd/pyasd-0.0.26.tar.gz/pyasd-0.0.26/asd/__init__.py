4125fd3f39044d3ca17127968dc0a3719b122624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 04:04:19 +0000

    Add package pyasd/0.0.26/pyasd-0.0.26.tar.gz

---
+++ packages/pyasd/pyasd-0.0.26.tar.gz/pyasd-0.0.26/asd/__init__.py
@@ -0,0 +1,8 @@
+__built_time__        =  "built at Fri Aug  2 21:46:37 2024_CST"
+__name__              =  "pyasd"
+__version__           =  "0.0.26"
+__author__            =  "Shunhong Zhang"
+__author_email__      =  "zhangshunhong.pku@gmail.com"
+__url__               =  "https://to_be_posted"
+__license__           =  "MIT LICENSE"
+__platform__          =  "linux"
