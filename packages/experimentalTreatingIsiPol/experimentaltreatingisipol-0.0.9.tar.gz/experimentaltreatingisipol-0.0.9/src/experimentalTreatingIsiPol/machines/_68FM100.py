7c2b93efa8067ee982eb87277e3598c4f89aa981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-03 03:04:29 +0000

    Add package experimentalTreatingIsiPol/0.0.9/experimentaltreatingisipol-0.0.9.tar.gz

---
+++ packages/experimentalTreatingIsiPol/experimentaltreatingisipol-0.0.9.tar.gz/experimentaltreatingisipol-0.0.9/src/experimentalTreatingIsiPol/machines/_68FM100.py
@@ -0,0 +1,14 @@
+class _68FM100():
+    def __init__(self) -> None:
+
+        self.colunas  = [
+                             r'Tempo'
+                            ,r'Deslocamento [mm]'
+                            ,r'Força'
+                            ,r'Tensão à tração'
+                            ,r'Deformação à tração (Deslocamento)'
+                            ,r'Extens Bi Axial - Connector 6'
+                            ,r'Extens Bi Trans - Conector 5'
+                         ]
+        self.column_delimitador = ';'
+        self.decimal = ','
