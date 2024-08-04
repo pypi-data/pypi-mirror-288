2698336748869a756be9b94461eb5e73871cce6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 15:03:59 +0000

    Add package pym3u8downloader/0.1.3/pym3u8downloader-0.1.3-py3-none-any.whl

---
+++ packages/pym3u8downloader/pym3u8downloader-0.1.3-py3-none-any.whl/pym3u8downloader/__init__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    "M3U8Downloader",
+    "M3U8DownloaderError",
+    "__author__",
+    "__description__",
+    "__name__",
+    "__version__"
+]
+__author__ = "coldsofttech"
+__description__ = """
+M3U8 Downloader is a Python class designed to download and concatenate video files from M3U8 playlists.
+This class provides functionality to handle M3U8 playlist files, download video segments, concatenate them
+into a single video file, and manage various error conditions.
+"""
+__name__ = "pym3u8downloader"
+__version__ = "0.1.3"
+
+from pym3u8downloader.__main__ import M3U8Downloader, M3U8DownloaderError
