122b65d94a5bdafa47f51024658ea1a95def5f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 15:04:06 +0000

    Add package tool-calling-llm/0.0.3/tool_calling_llm-0.0.3.tar.gz

---
+++ packages/tool-calling-llm/tool_calling_llm-0.0.3.tar.gz/tool_calling_llm-0.0.3/src/tool_calling_llm/__init__.py
@@ -0,0 +1,13 @@
+from tool_calling_llm.tool_calling_llm import (
+    ToolCallingLLM,
+    convert_to_tool_definition,
+    DEFAULT_RESPONSE_FUNCTION,
+    DEFAULT_SYSTEM_TEMPLATE
+)
+
+__all__ = [
+    "ToolCallingLLM",
+    "convert_to_tool_definition",
+    "DEFAULT_SYSTEM_TEMPLATE",
+    "DEFAULT_RESPONSE_FUNCTION",
+]
