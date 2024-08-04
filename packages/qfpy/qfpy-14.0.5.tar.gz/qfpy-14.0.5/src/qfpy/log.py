a8478b9145ff5a99a34e612e16b0b1a5a298c060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:03:55 +0000

    Add package qfpy/14.0.5/qfpy-14.0.5.tar.gz

---
+++ packages/qfpy/qfpy-14.0.5.tar.gz/qfpy-14.0.5/src/qfpy/log.py
@@ -0,0 +1,20 @@
+"""
+logger
+
+def config()
+"""
+
+import sys
+
+from loguru import logger
+
+
+def config():
+    logger.configure(
+        handlers=[
+            dict(
+                sink=sys.stderr,
+                format="<green>{time:HH:mm:ss.SSS}</green> <cyan>{file}</cyan>:<cyan>{line}</cyan> <level>{message}</level>",
+            )
+        ]
+    )
