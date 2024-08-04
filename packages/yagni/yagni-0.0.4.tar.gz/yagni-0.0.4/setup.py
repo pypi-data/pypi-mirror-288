c29b907b2574cdf4dfb28e3977ccfc0bb98a026c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 04:05:26 +0000

    Add package yagni/0.0.4/yagni-0.0.4.tar.gz

---
+++ packages/yagni/yagni-0.0.4.tar.gz/yagni-0.0.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="yagni",
+    version="0.0.4",
+    description="You are probably gonna need it: a collection of useful Python utilities aggregated over many years of development.",
+    packages=find_packages(),
+    install_requires=[
+        # 'six',
+    ],
+    extras_require={
+        'dev': [
+            'black',
+            'isort',
+            'pytest',
+            'pytest-cov',
+            'radon',
+            'xenon',
+        ],
+    },
+    python_requires=">=3.8",
+)
