83a084e65d0047d3b2316b4f73a57f444d9d09d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:03:55 +0000

    Add package netty-snmp/0.1.0/netty_snmp-0.1.0-py3-none-any.whl

---
+++ packages/netty-snmp/netty_snmp-0.1.0-py3-none-any.whl/netty_snmp/factory/manufactures/aruba.py
@@ -0,0 +1,15 @@
+from netty_snmp.factory import consts
+from netty_snmp.factory.snmp_factory import SnmpFactory, SnmpV3Params
+
+
+class ArubaSnmpFactory(SnmpFactory):
+    def __init__(
+        self,
+        ip: str,
+        port: int = consts.SNMP_DEFAULT_PORT,
+        version: consts.SnmpVersion = consts.SnmpVersion.v2c,
+        community: str | None = consts.SNMP_DEFAULT_COMMUNITY,
+        v3_params: SnmpV3Params | None = None,
+        model: str | None = None,
+    ) -> None:
+        super().__init__(ip, port, version, community, v3_params, model)
