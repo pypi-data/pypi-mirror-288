67b96beaf36d914d1b9430393c2a05d39099bf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:03:51 +0000

    Add package shaarpy/1.1.0/shaarpy-1.1.0.tar.gz

---
+++ packages/shaarpy/shaarpy-1.1.0.tar.gz/shaarpy-1.1.0/shaarpy/templatetags/shaarpy_extras.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+"""
+    ShaarPy :: Extra balls (because that reminds me pinball:)
+"""
+from django.urls import reverse
+from django import template
+import pypandoc
+
+register = template.Library()
+
+
+@register.filter(name='tags')
+# draw the tags in the "card footer"
+def tags(value):
+    out = ''
+    for tag in value.split(','):
+        out += '<a href="' + reverse('links_by_tag_list', args=[tag]) + '">' \
+               '<span class="badge rounded-pill text-bg-secondary">' + tag + '</span></a> '
+
+    return out
+
+
+@register.filter(name='markdown')
+def markdown(text):
+    # convert into Github_Flavor_Markdown
+    return pypandoc.convert_text(text, "html", format="gfm")
