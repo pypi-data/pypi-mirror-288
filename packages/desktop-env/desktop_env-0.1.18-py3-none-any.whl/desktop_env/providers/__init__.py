258b6e8756e8d50470d8e0cfe1226eaa7a190832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:05:00 +0000

    Add package desktop-env/0.1.18/desktop_env-0.1.18-py3-none-any.whl

---
+++ packages/desktop-env/desktop_env-0.1.18-py3-none-any.whl/desktop_env/providers/__init__.py
@@ -0,0 +1,25 @@
+from desktop_env.providers.base import VMManager, Provider
+from desktop_env.providers.vmware.manager import VMwareVMManager
+from desktop_env.providers.vmware.provider import VMwareProvider
+from desktop_env.providers.aws.manager import AWSVMManager
+from desktop_env.providers.aws.provider import AWSProvider
+from desktop_env.providers.azure.manager import AzureVMManager
+from desktop_env.providers.azure.provider import AzureProvider
+from desktop_env.providers.virtualbox.manager import VirtualBoxVMManager
+from desktop_env.providers.virtualbox.provider import VirtualBoxProvider
+
+def create_vm_manager_and_provider(provider_name: str, region: str):
+    """
+    Factory function to get the Virtual Machine Manager and Provider instances based on the provided provider name.
+    """
+    provider_name = provider_name.lower().strip()
+    if provider_name == "vmware":
+        return VMwareVMManager(), VMwareProvider(region)
+    elif provider_name == "virtualbox":
+        return VirtualBoxVMManager(), VirtualBoxProvider(region)
+    elif provider_name in ["aws", "amazon web services"]:
+        return AWSVMManager(), AWSProvider(region)
+    elif provider_name == "azure":
+        return AzureVMManager(), AzureProvider(region)
+    else:
+        raise NotImplementedError(f"{provider_name} not implemented!")
