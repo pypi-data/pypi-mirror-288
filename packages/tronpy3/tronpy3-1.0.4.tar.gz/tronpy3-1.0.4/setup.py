353b0b9a9c2bc1cf1597d63c3e701ee7a189d362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:03:52 +0000

    Add package tronpy3/1.0.4/tronpy3-1.0.4.tar.gz

---
+++ packages/tronpy3/tronpy3-1.0.4.tar.gz/tronpy3-1.0.4/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+setup(name='tronpy3',  # 包名
+      version='1.0.4',  # 版本号
+      description='A small example package',
+      long_description=long_description,
+      author='mikezhou_talk',
+      author_email='',
+      url='',
+      install_requires=[
+          "base58",
+          "coincurve",
+          "eth-abi>=5.0.0,<6.0.0",
+          "httpx",
+          "pycryptodome<4",
+          "requests",
+      ],
+      license='BSD License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
