3ce7e9cf9e45aa71efeab450f6da0f557abef5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:03:53 +0000

    Add package autonomy-dev/0.2.60/autonomy_dev-0.2.60.tar.gz

---
+++ packages/autonomy-dev/autonomy_dev-0.2.60.tar.gz/autonomy_dev-0.2.60/auto_dev/test.py
@@ -0,0 +1,31 @@
+"""
+Module for testing the project.
+"""
+from auto_dev.cli_executor import CommandExecutor
+from multiprocessing import cpu_count
+
+
+def test_path(
+    path: str,
+    verbose: bool = False,
+    watch: bool = False,
+) -> bool:
+    """
+    Check the path for linting errors.
+    :param path: The path to check
+    """
+    available_cores = cpu_count()
+    command = CommandExecutor(
+        [
+            "poetry",
+            "run",
+            "pytest",
+            str(path),
+            "-vv",
+            "-n",
+            str(available_cores),
+        ]
+        + (["-ff"] if watch else [])
+    )
+    result = command.execute(verbose=verbose, stream=True)
+    return result
