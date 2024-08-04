939246c95ef15b0b571fd8f5e30f0bd065236821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 14:04:17 +0000

    Add package snailpy/1.1.0/snailpy-1.1.0-py3-none-any.whl

---
+++ packages/snailpy/snailpy-1.1.0-py3-none-any.whl/snailpy/__init__.py
@@ -0,0 +1,6 @@
+# snailpy/__init__.py
+
+from .snailpy import *
+
+print('You are successfully using SnailPy v1.1.0!')
+print('Anything made with 1.0.9 or under will most likely be broken due to server changes and the data loss.')
