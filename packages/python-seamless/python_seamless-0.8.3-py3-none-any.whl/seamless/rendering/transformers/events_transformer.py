d2cba515a69cf02bee33adacb1f396949fbc9daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 14:04:09 +0000

    Add package python-seamless/0.8.3/python_seamless-0.8.3-py3-none-any.whl

---
+++ packages/python-seamless/python_seamless-0.8.3-py3-none-any.whl/seamless/rendering/transformers/events_transformer.py
@@ -0,0 +1,37 @@
+from seamless.context.database import DB
+from seamless.internal import SEAMLESS_ELEMENT_ATTRIBUTE, SEAMLESS_INIT_ATTRIBUTE
+
+
+def events_transformer():
+    def matcher(key: str, value):
+        return key.startswith("on_") and callable(value)
+
+    def event_transformer(key: str, value, props):
+        if not callable(value):
+            props[key] = value
+            return
+        
+        event_name = key.removeprefix("on_")
+        action = DB.add_event(value)
+        props[SEAMLESS_INIT_ATTRIBUTE] = props.get(SEAMLESS_INIT_ATTRIBUTE, "") + \
+        f"""this.addEventListener('{event_name}', (event) => {{
+            const outEvent = seamless.instance.eventObjectTransformer(
+              event, 
+              seamless.instance.serializeEventObject(event)
+            );
+          seamless.instance.socket.emit("event", "{action.id}", outEvent);
+        }});"""
+
+        props[SEAMLESS_ELEMENT_ATTRIBUTE] = True
+
+    return matcher, event_transformer
+
+def js_events_transformer():
+    def matcher(key: str, value):
+        return key.startswith("on_") and isinstance(value, str)
+
+    def event_transformer(key: str, value, element_props):       
+        event_name = key.removeprefix("on_")
+        element_props[f"on{event_name}"] = value
+
+    return matcher, event_transformer
