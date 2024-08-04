edc91cedc60edeee6363c182957e5f45a103d2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 04:05:11 +0000

    Add package tiddl/1.4.0/tiddl-1.4.0.tar.gz

---
+++ packages/tiddl/tiddl-1.4.0.tar.gz/tiddl-1.4.0/tiddl/config.py
@@ -0,0 +1,93 @@
+import json
+import logging
+
+from typing import TypedDict, Any
+from .types import TrackQuality
+
+
+class Settings(TypedDict, total=False):
+    download_path: str
+    track_quality: TrackQuality
+    file_template: str
+
+
+class User(TypedDict, total=False):
+    user_id: str
+    country_code: str
+
+
+class ConfigData(TypedDict, total=False):
+    token: str
+    refresh_token: str
+    token_expires_at: int
+    settings: Settings
+    user: User
+
+
+DEFAULT_PATH = ".tiddl_config.json"
+DEFAULT_CONFIG: ConfigData = {
+    "token": "",
+    "refresh_token": "",
+    "token_expires_at": 0,
+    "settings": {
+        "download_path": "tidal_download",
+        "track_quality": "HIGH",
+        "file_template": "{artist} - {title}",
+    },
+    "user": {"user_id": "", "country_code": ""},
+}
+
+
+class Config:
+    def __init__(self, config_path=DEFAULT_PATH) -> None:
+        self.config_path = config_path
+        self._config: ConfigData = DEFAULT_CONFIG
+        self._logger = logging.getLogger("Config")
+
+        try:
+            with open(self.config_path, "r") as f:
+                loaded_config: dict = json.load(f)
+                loaded_settings = loaded_config.get("settings")
+                self._logger.debug(f"loaded {loaded_settings}")
+                merged_config: ConfigData = merge(loaded_config, self._config)
+                self._config.update(merged_config)
+                self._save()
+
+        except FileNotFoundError:
+            self._logger.debug("creating new file")
+            self._save()  # save default config if file does not exist
+            self._logger.debug("created new file")
+
+    def _save(self) -> None:
+        with open(self.config_path, "w") as f:
+            self._logger.debug(self._config.get("settings"))
+            json.dump(self._config, f, indent=2)
+
+    def __getitem__(self, key: str) -> Any:
+        return self._config[key]
+
+    def __iter__(self):
+        return iter(self._config)
+
+    def __str__(self) -> str:
+        return json.dumps(self._config, indent=2)
+
+    def update(self, data: ConfigData) -> ConfigData:
+        self._logger.debug("updating")
+        self._config.update(data)
+        self._save()
+        self._logger.debug("updated")
+        return self._config.copy()
+
+
+def merge(source, destination):
+    # https://stackoverflow.com/a/20666342
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge(value, node)
+        else:
+            destination[key] = value
+
+    return destination
---
+++ packages/tiddl/tiddl-1.4.0.tar.gz/tiddl-1.4.0/tiddl/download.py
@@ -0,0 +1,169 @@
+import logging
+import requests
+import json
+import os
+
+from xml.etree.ElementTree import fromstring
+from base64 import b64decode
+from typing import TypedDict, List
+
+from .types.track import ManifestMimeType
+
+
+logger = logging.getLogger("download")
+
+
+def decodeManifest(manifest: str):
+    return b64decode(manifest).decode()
+
+
+def parseManifest(manifest: str):
+    class AudioFileInfo(TypedDict):
+        mimeType: str
+        codecs: str
+        encryptionType: str
+        urls: List[str]
+
+    data: AudioFileInfo = json.loads(manifest)
+    return data
+
+
+def parseManifestXML(xml_content: str):
+    """
+    Parses XML manifest file of the track.
+    """
+
+    NS = "{urn:mpeg:dash:schema:mpd:2011}"
+
+    tree = fromstring(xml_content)
+
+    representationElement = tree.find(
+        f"{NS}Period/{NS}AdaptationSet/{NS}Representation"
+    )
+    if representationElement is None:
+        raise ValueError("Representation element not found")
+
+    codecs = representationElement.get("codecs")
+
+    segmentElement = representationElement.find(f"{NS}SegmentTemplate")
+    if segmentElement is None:
+        raise ValueError("SegmentTemplate element not found")
+
+    url_template = segmentElement.get("media")
+    if url_template is None:
+        raise ValueError("No `media` attribute in SegmentTemplate")
+
+    timelineElements = segmentElement.findall(f"{NS}SegmentTimeline/{NS}S")
+    if not timelineElements:
+        raise ValueError("SegmentTimeline elements not found")
+
+    total = 0
+    for element in timelineElements:
+        total += 1
+        count = element.get("r")
+        if count is not None:
+            total += int(count)
+
+    urls = [url_template.replace("$Number$", str(i)) for i in range(0, total + 1)]
+
+    return urls, codecs
+
+
+def showProgressBar(iteration: int, total: int, text: str, length=30):
+    SQUARE, SQUARE_FILL = "□", "■"
+    iteration_mb = iteration / 1024 / 1024
+    total_mb = total / 1024 / 1024
+    percent = 100 * (iteration / total)
+    progress = int(length * iteration // total)
+    bar = f"{SQUARE_FILL * progress}{SQUARE * (length - progress)}"
+    print(
+        f"\r{text} {bar} {percent:.0f}% {iteration_mb:.2f} / {total_mb:.2f} MB",
+        end="\r",
+    )
+    if iteration >= total:
+        print()
+
+
+def download(url: str) -> bytes:
+    logger.debug(url)
+    # use session for performance
+    session = requests.Session()
+    req = session.get(url, stream=True)
+    total_size = int(req.headers.get("content-length", 0))
+    block_size = 1024 * 1024
+    data = b""
+
+    for block in req.iter_content(block_size):
+        data += block
+        showProgressBar(len(data), total_size, "Single URL")
+
+    return data
+
+
+def threadDownload(urls: list[str]) -> bytes:
+    # TODO: implement threaded download ⚡️
+    data = b""
+    for index, url in enumerate(urls):
+        req = requests.get(url)
+        data += req.content
+        showProgressBar(index, len(urls), f"{len(urls)} URLs")
+
+    return data
+
+
+def downloadTrackStream(
+    full_path: str,
+    encoded_manifest: str,
+    mime_type: ManifestMimeType,
+):
+    logger.debug(f"mime_type: {mime_type}")
+    manifest = decodeManifest(encoded_manifest)
+
+    match mime_type:
+        case "application/dash+xml":
+            track_urls, codecs = parseManifestXML(manifest)
+        case "application/vnd.tidal.bts":
+            data = parseManifest(manifest)
+            track_urls, codecs = data["urls"], data["codecs"]
+        case _:
+            raise ValueError(f"Unknown `mime_type`: {mime_type}")
+
+    if len(track_urls) == 1:
+        track_data = download(track_urls[0])
+    else:
+        track_data = threadDownload(track_urls)
+
+    """
+    known codecs
+        flac (master)
+        mp4a.40.2 (high)
+        mp4a.40.5 (low)
+    """
+
+    # TODO: use proper file extension ✨
+
+    # quick fix for file extension
+
+    if codecs is None:
+        raise Exception("Missing codecs")
+
+    if codecs == "flac":
+        extension = "flac"
+    elif codecs.startswith("mp4a"):
+        extension = "m4a"
+    else:
+        extension = "flac"
+        logger.warning(
+            f'unknown file codecs: "{codecs}", please submit this as issue on GitHub'
+        )
+
+    file_path = os.path.dirname(full_path)
+    file_name = f"{full_path}.{extension}"
+    logger.debug(f"file_path: {file_path}, file_name: {file_name}")
+
+    os.makedirs(file_path, exist_ok=True)
+
+    with open(file_name, "wb+") as f:
+        f.write(track_data)
+
+    return file_name
