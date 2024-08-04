677b2b1a4b5aa80bdbc4a1b21cb0df2c152e6c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 04:05:17 +0000

    Add package openai-commands/3.61.1/openai_commands-3.61.1-py3-none-any.whl

---
+++ packages/openai-commands/openai_commands-3.61.1-py3-none-any.whl/openai_commands/.abcli/literature_review.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+function literature_review() {
+    local options=$1
+
+    if [ $(abcli_option_int "$options" help 0) == 1 ]; then
+        options="dryrun,~download,~upload"
+        local args="[--choices <choices.yaml>]$ABCUL[--count <-1>]$ABCUL[--filename <filename.csv>]$ABCUL[--overwrite 1]$ABCUL[--suffix <suffix>]$ABCUL[--verbose 1]"
+        abcli_show_usage "@litrev$ABCUL[$options]$ABCUL[$LITERATURE_REVIEW_OBJECT|<object-name>]$ABCUL$args" \
+            "<object-name>: literature review @ choices."
+        return
+    fi
+
+    local do_dryrun=$(abcli_option_int "$options" dryrun 0)
+    local do_download=$(abcli_option_int "$options" download $(abcli_not $do_dryrun))
+    local do_upload=$(abcli_option_int "$options" upload $(abcli_not $do_dryrun))
+
+    local object_name=$(abcli_clarify_object $2 $LITERATURE_REVIEW_OBJECT)
+
+    [[ "$do_download" == 1 ]] &&
+        abcli_download - $object_name
+
+    abcli_log "literature review: $object_name ..."
+
+    abcli_eval dryrun=$do_dryrun \
+        python3 -m openai_commands.literature_review \
+        --object_name $object_name \
+        "${@:3}"
+    [[ $? -ne 0 ]] && return 1
+
+    [[ "$do_upload" == 1 ]] &&
+        abcli_upload - $object_name
+
+    return 0
+}
