dcea16aa492af8e75e187f34506191da06950959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:05:00 +0000

    Add package seiryu/0.9/seiryu-0.9.tar.gz

---
+++ packages/seiryu/seiryu-0.9.tar.gz/seiryu-0.9/setup.py
@@ -0,0 +1,40 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="seiryu",
+    version="0.9",
+    packages=find_packages(),
+    install_requires=[
+        'argon2',
+        'argon2-cffi',
+        'bcrypt',
+        'numpy',
+        'passlib',
+        'siphashc',
+        'spookyhash',
+        'uuid',
+        'xxhash',
+        'argon2-cffi-bindings',
+        'cffi',
+    ],
+    entry_points={
+        'console_scripts': [
+            'seiryu=seiryu:main',
+        ],
+    },
+    author="Veilwr4ith",
+    author_email="hidetheveil@gmail.com",
+    description="The Pinnacle of Advanced Hash Generator",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/veilwr4ith/Seiryu",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
+
