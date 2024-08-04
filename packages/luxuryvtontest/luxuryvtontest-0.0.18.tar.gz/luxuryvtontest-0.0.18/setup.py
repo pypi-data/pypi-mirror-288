55614cf725eccf9c231ddae3eada635ead23943a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 15:04:06 +0000

    Add package luxuryvtontest/0.0.18/luxuryvtontest-0.0.18.tar.gz

---
+++ packages/luxuryvtontest/luxuryvtontest-0.0.18.tar.gz/luxuryvtontest-0.0.18/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="luxuryvtontest",
+    version="0.0.18",
+    description="This is a test for my python package upload to pypi",
+    author="powerjsv",
+    author_email="powerjsv12@gmail.com",
+    url="https://github.com/powerjsv/jsv_package_test",
+    install_requires=[
+        # "pip==23.3.1",
+        # "accelerate==0.25.0",
+        # "tqdm==4.66.1",
+        # "transformers==4.36.2",
+        # "diffusers==0.25.0",
+        # "einops==0.7.0",
+        # "bitsandbytes==0.39.0",
+        # "scipy==1.11.1",
+        # "opencv-python",
+    ],
+    packages=find_packages(exclude=[]),
+    keywords=["vton", "powerjsv", "toy project", "pypi"],
+    python_requires=">=3.10",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
