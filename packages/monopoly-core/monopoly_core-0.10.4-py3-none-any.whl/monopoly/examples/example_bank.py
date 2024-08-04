1eac530a5adb5238f47e78cb0310da6031146ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:04:16 +0000

    Add package monopoly-core/0.10.4/monopoly_core-0.10.4-py3-none-any.whl

---
+++ packages/monopoly-core/monopoly_core-0.10.4-py3-none-any.whl/monopoly/examples/example_bank.py
@@ -0,0 +1,31 @@
+from monopoly.banks.base import BankBase
+from monopoly.config import CreditStatementConfig
+from monopoly.constants import InternalBankNames, SharedPatterns
+from monopoly.identifiers import MetadataIdentifier, TextIdentifier
+
+
+class ExampleBank(BankBase):
+    """Dummy class to help with reading the example PDF statement"""
+
+    credit_config = CreditStatementConfig(
+        bank_name=InternalBankNames.EXAMPLE,
+        statement_date_pattern=r"(\d{2}\-\d{2}\-\d{4})",
+        prev_balance_pattern=(
+            r"(?P<description>LAST MONTH'S BALANCE?)\s+"
+            + SharedPatterns.AMOUNT_EXTENDED_WITHOUT_EOL
+        ),
+        transaction_pattern=(
+            r"(?P<transaction_date>\d+/\d+)\s*"
+            + SharedPatterns.DESCRIPTION
+            + SharedPatterns.AMOUNT_EXTENDED
+        ),
+    )
+
+    identifiers = [
+        [
+            MetadataIdentifier(
+                creator="Adobe Acrobat 23.3", producer="Adobe Acrobat Pro (64-bit)"
+            ),
+            TextIdentifier(text="HCBS12345(12345)"),
+        ]
+    ]
