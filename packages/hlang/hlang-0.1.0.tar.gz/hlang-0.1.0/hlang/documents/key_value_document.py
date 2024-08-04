b49dc6934de3fa01c4f72eb742a233819e836d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:08:26 +0000

    Add package hlang/0.1.0/hlang-0.1.0.tar.gz

---
+++ packages/hlang/hlang-0.1.0.tar.gz/hlang-0.1.0/hlang/documents/key_value_document.py
@@ -0,0 +1,10 @@
+from openlangchain.documents.document import ABCDocument
+
+
+class KeyValueDocument(ABCDocument):
+    def __init__(self, content, key: str):
+        self.key = key
+        self.value = content
+
+    def embed_text(self) -> str:
+        return self.key
