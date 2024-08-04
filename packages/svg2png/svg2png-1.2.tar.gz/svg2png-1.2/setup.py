3efa09ca0c1a06fa1838c7e661c5878480b67c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:04:14 +0000

    Add package svg2png/1.2/svg2png-1.2.tar.gz

---
+++ packages/svg2png/svg2png-1.2.tar.gz/svg2png-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from svg2png import version
+
+setup(
+    name='svg2png',
+    version=version,
+    packages=find_packages(),
+    install_requires=[],
+    author='foxypiratecove37350',
+    author_email='foxypiratecovefnaf12@gmail.com',
+    description='Convert a SVG file into a PNG file using Inkscape',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/foxypiratecove37350/svg2png',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+    ],
+    license='GPL-2.0',
+)
