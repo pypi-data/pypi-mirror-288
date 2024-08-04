ab29cbb51356359b970a0f15c4807fefc160d4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 14:04:34 +0000

    Add package milesianpy/0.0.2/milesianpy-0.0.2.tar.gz

---
+++ packages/milesianpy/milesianpy-0.0.2.tar.gz/milesianpy-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='milesianpy',
+      version="0.0.2",
+      description='A math calculation package.',
+      url='https://github.com/n0tanna/milesianpy.git',
+      author='Anna St Germain',
+      author_email='anna.stgermain885@gmail.com',
+      package_dir={"": "src"},
+      packages=find_packages(where="src"),
+      include_package_data=True,
+      classifiers=['Programming Language :: Python :: 3'],
+)
