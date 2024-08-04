7fe4d45eef9fa49868dcf9fa298061991b6bf32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 14:04:26 +0000

    Add package dbsys/0.1.13/dbsys-0.1.13.tar.gz

---
+++ packages/dbsys/dbsys-0.1.13.tar.gz/dbsys-0.1.13/dbsys/__init__.py
@@ -0,0 +1,5 @@
+from .dbsys import manage_db, DatabaseError, TableNotFoundError, ColumnNotFoundError, DatabaseManager
+
+__version__ = "0.1.12"
+__all__ = ["manage_db", "DatabaseError", "TableNotFoundError", "ColumnNotFoundError", "DatabaseManager"]
+__version__ = "0.1.1"
