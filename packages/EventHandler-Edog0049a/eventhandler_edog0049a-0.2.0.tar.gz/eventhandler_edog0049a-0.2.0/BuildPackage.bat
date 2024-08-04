ff3c5b11de94d550b6284c211248d725021b6b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 14:04:12 +0000

    Add package EventHandler-Edog0049a/0.2.0/eventhandler_edog0049a-0.2.0.tar.gz

---
+++ packages/EventHandler-Edog0049a/eventhandler_edog0049a-0.2.0.tar.gz/eventhandler_edog0049a-0.2.0/BuildPackage.bat
@@ -0,0 +1,9 @@
+@echo off
+if not exist distbackup (mkdir distbackup)
+move /y dist\*.*  distbackup
+rmdir /s /q dist
+py -m pip install --upgrade pip
+py -m pip install --upgrade build
+py -m pip install --upgrade twine
+py -m build   
+py -m twine upload --config-file "D:\\.pypirc" --repository EventHandler_Edog0049a dist/*
