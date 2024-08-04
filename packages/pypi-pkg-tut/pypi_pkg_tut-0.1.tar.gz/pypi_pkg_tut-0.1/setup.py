2e2db7c0efcbc3078f178c0312b153a685370f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 04:05:20 +0000

    Add package pypi-pkg-tut/0.1/pypi_pkg_tut-0.1.tar.gz

---
+++ packages/pypi-pkg-tut/pypi_pkg_tut-0.1.tar.gz/pypi_pkg_tut-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypi_pkg_tut',
+    version='0.1',
+    packages= find_packages(),
+    install_requires=[
+        #Add dependencies
+    ],
+)
