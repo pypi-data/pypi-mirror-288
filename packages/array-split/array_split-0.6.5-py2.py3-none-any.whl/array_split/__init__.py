3e35a9325cd9ddd8d3938b22d860f522d8fca016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 15:04:06 +0000

    Add package array-split/0.6.5/array_split-0.6.5-py2.py3-none-any.whl

---
+++ packages/array-split/array_split-0.6.5-py2.py3-none-any.whl/array_split/__init__.py
@@ -0,0 +1,81 @@
+"""
+==============================
+The :mod:`array_split` Package
+==============================
+
+.. currentmodule:: array_split
+
+Python package for splitting a :obj:`numpy.ndarray` (or just an array shape)
+into a number of sub-arrays.
+
+The two main functions are:
+
+   :func:`array_split.array_split`
+       Similar to :func:`numpy.array_split`, returns a list of
+       sub-array *views* of the input :obj:`numpy.ndarray`.
+       Can split along multiple axes and has more splitting
+       criteria (parameters) than :func:`numpy.array_split`.
+
+   :func:`array_split.shape_split`
+      Instead taking an :obj:`numpy.ndarray` as an argument, it
+      takes the array *shape* and returns tuples of :obj:`slice`
+      objects which indicate the extents of the sub-arrays.
+
+These two functions use an instance of the :obj:`array_split.ShapeSplitter` class
+which contains the bulk of the *split* implementation.
+Instances of :obj:`array_split.ShapeSplitter` also
+maintain state related to the computed split.
+
+Splitting of multi-dimensional arrays can be performed according to several criteria:
+
+   * Per-axis indices indicating the *cut* positions.
+   * Per-axis number of sub-arrays.
+   * Total number of sub-arrays (with optional per-axis *number of sections* constraints).
+   * Specific sub-array shape.
+   * Specification of *halo* (*ghost*) elements for sub-arrays.
+   * Arbitrary *start index* for the shape to be partitioned.
+   * Maximum number of bytes for a sub-array with constraints:
+
+        - sub-arrays are an even multiple of a specified sub-tile shape
+        - upper limit on the per-axis sub-array shape
+
+The usage documentation is given in the :ref:`array_split-examples` section.
+
+
+Classes and Functions
+=====================
+
+.. autosummary::
+   :toctree: generated/
+
+   shape_split - Splits a shape and returns :obj:`numpy.ndarray` of :obj:`slice` elements.
+   array_split - Equivalent to :func:`numpy.array_split`.
+   ShapeSplitter - Array shape splitting class.
+
+Attributes
+==========
+
+.. autodata:: ARRAY_BOUNDS
+.. autodata:: NO_BOUNDS
+
+
+"""
+from __future__ import absolute_import
+from .license import license as _license, copyright as _copyright
+from . import split  # noqa: E402,F401
+from .split import array_split, shape_split, ShapeSplitter  # noqa: E402,F401
+from . import _version
+
+__author__ = "Shane J. Latham"
+__license__ = _license()
+__copyright__ = _copyright()
+__version__ = _version.get_versions()['version']
+
+
+#: See :data:`array_split.split.ARRAY_BOUNDS`
+ARRAY_BOUNDS = split.ARRAY_BOUNDS
+
+#: See :data:`array_split.split.NO_BOUNDS`
+NO_BOUNDS = split.NO_BOUNDS
+
+__all__ = [s for s in dir() if not s.startswith('_')]
---
+++ packages/array-split/array_split-0.6.5-py2.py3-none-any.whl/array_split/tests.py
@@ -0,0 +1,147 @@
+"""
+===================================
+The :mod:`array_split.tests` Module
+===================================
+
+Module for running all :mod:`array_split` unit-tests, including :mod:`unittest` test-cases
+and :mod:`doctest` tests for module doc-strings and sphinx (RST) documentation.
+Execute as::
+
+   python -m array_split.tests
+
+.. currentmodule:: array_split.tests
+
+
+Classes and Functions
+=====================
+
+.. autosummary::
+   :toctree: generated/
+
+   MultiPlatformAnd23Checker - Customised doctest output checking.
+   DocTestTestSuite - Loads all module and file doctests as single :mod:`unittest` suite.
+   load_tests - Returns suite of :mod:`doctest` and :mod:`unittest` tests.
+
+"""
+# pylint: disable=unused-import
+from __future__ import absolute_import
+import sys as _sys
+import re as _re
+import unittest as _unittest
+import doctest as _doctest
+import os.path
+import array_split as _array_split
+from array_split import split as _split
+
+from .license import license as _license, copyright as _copyright, version as _version
+from .split_test import SplitTest  # noqa: F401,F403
+
+__author__ = "Shane J. Latham"
+__license__ = _license()
+__copyright__ = _copyright()
+__version__ = _version()
+
+_doctest_OuputChecker = _doctest.OutputChecker
+
+
+class MultiPlatformAnd23Checker(_doctest_OuputChecker):
+
+    """
+    Overrides the :meth:`doctest.OutputChecker.check_output` method
+    to remove the :samp:`'L'` from integer literals
+    """
+
+    def check_output(self, want, got, optionflags):
+        """
+        For python-2 replaces "124L" with "124". For python 2 and 3,
+        replaces :samp:`", dtype=int64)"` with :samp:`")"`.
+
+        See :meth:`doctest.OutputChecker.check_output`.
+
+        """
+        if _sys.version_info[0] <= 2:
+            got = _re.sub("([0-9]+)L", "\\1", got)
+
+        got = _re.sub(", dtype=int64\\)", ")", got)
+
+        return _doctest_OuputChecker.check_output(self, want, got, optionflags)
+
+
+_doctest.OutputChecker = MultiPlatformAnd23Checker
+
+
+class DocTestTestSuite(_unittest.TestSuite):
+
+    """
+    Adds :mod:`array_split` doctests as `unittest.TestCase` objects.
+    """
+
+    def __init__(self):
+        """
+        Uses :meth:`unittest.TestSuite.addTests` to add :obj:`doctest.DocFileSuite`
+        and :obj:`doctest.DocTestSuite` tests.
+        """
+        readme_file_name = \
+            os.path.realpath(
+                os.path.join(os.path.dirname(__file__), "..", "README.rst")
+            )
+        examples_rst_file_name = \
+            os.path.realpath(
+                os.path.join(
+                    os.path.dirname(__file__),
+                    "..",
+                    "docs",
+                    "source",
+                    "examples",
+                    "index.rst"
+                )
+            )
+        suite = _unittest.TestSuite()
+        if os.path.exists(readme_file_name):
+            suite.addTests(
+                _doctest.DocFileSuite(
+                    readme_file_name,
+                    module_relative=False,
+                    optionflags=_doctest.NORMALIZE_WHITESPACE
+                )
+            )
+        if os.path.exists(examples_rst_file_name):
+            suite.addTests(
+                _doctest.DocFileSuite(
+                    examples_rst_file_name,
+                    module_relative=False,
+                    optionflags=_doctest.NORMALIZE_WHITESPACE
+                )
+            )
+        suite.addTests(
+            _doctest.DocTestSuite(
+                _array_split,
+                optionflags=_doctest.NORMALIZE_WHITESPACE
+            )
+        )
+        suite.addTests(
+            _doctest.DocTestSuite(
+                _split,
+                optionflags=_doctest.NORMALIZE_WHITESPACE
+            )
+        )
+
+        _unittest.TestSuite.__init__(self, suite)
+
+
+def load_tests(loader, tests, pattern):  # pylint: disable=unused-argument
+    """
+    Loads :mod:`array_split.split_test` tests and :obj:`DocTestTestSuite`
+    tests.
+    """
+    suite = loader.loadTestsFromNames(["array_split.split_test", ])
+    suite.addTests(DocTestTestSuite())
+    return suite
+
+
+__all__ = [s for s in dir() if not s.startswith('_')]
+
+if __name__ == "__main__":
+    # import logging
+    # logging.basicConfig(level=logging.DEBUG)
+    _unittest.main()
