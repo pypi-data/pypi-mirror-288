c7f82d33ef9c59a7a1748cd14ad995f6471e3f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 04:05:12 +0000

    Add package tq-utils/0.1.1.post2/tq_utils-0.1.1.post2.tar.gz

---
+++ packages/tq-utils/tq_utils-0.1.1.post2.tar.gz/tq_utils-0.1.1.post2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os.path
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '0.1.1-2'
+DESCRIPTION = 'Some simple utils.'
+
+setup(
+    name='tq_utils',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://gitee.com/torchW/tqutils',
+    author='TripleQuiz',
+    author_email='triple_quiz@163.com',
+    license='MIT',
+    keywords=['python', 'util', 'code analyze', 'file manager', 'graph', 'dot', 'profile'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows',
+        'Natural Language :: Chinese (Simplified)',
+    ],
+    packages=find_packages(include=['src', 'src.*']),
+    install_requires=[
+        'pydot==3.0.1',
+        'pygraphviz==1.13',
+        'pyinstrument==4.6.2',
+    ],
+    include_package_data=True,
+    exclude_package_data={},
+    zip_safe=False,
+)
