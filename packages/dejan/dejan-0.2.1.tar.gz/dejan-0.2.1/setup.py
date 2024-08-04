84d2ba520c54d3883866bd7d61fce5da0541cc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 14:04:43 +0000

    Add package dejan/0.2.1/dejan-0.2.1.tar.gz

---
+++ packages/dejan/dejan-0.2.1.tar.gz/dejan-0.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dejan",
+    version="0.2.1",  # Updated version
+    description="A collection of utilities for various tasks, including SEO tools and data processing.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="DejanSEO",
+    author_email="enquiries@dejanmarketing.com",
+    url="https://github.com/dejanmarketing/dejan", 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "pandas",
+        "streamlit",
+        "transformers",
+        "torch",
+        "click",  # Add click for CLI handling
+    ],
+    entry_points={
+        'console_scripts': [
+            'dejan=dejan.cli:cli',
+        ],
+    },
+    include_package_data=True,
+)
