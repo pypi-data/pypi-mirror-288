7010f9337b2eaa32ea605129bc96f334a6512963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 15:04:04 +0000

    Add package roboquant/0.7.1/roboquant-0.7.1-py3-none-any.whl

---
+++ packages/roboquant/roboquant-0.7.1-py3-none-any.whl/roboquant/journals/runmetric.py
@@ -0,0 +1,25 @@
+from roboquant.journals.metric import Metric
+
+
+class RunMetric(Metric):
+    """Calculates a number of basic metrics during a run:
+    - total number of events, items, signals and orders
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.items = 0
+        self.orders = 0
+        self.signals = 0
+        self.events = 0
+
+    def calc(self, event, account, signals, orders) -> dict[str, float]:
+        self.items += len(event.items)
+        self.orders += len(orders)
+        self.events += 1
+
+        return {
+            "run/items": self.items,
+            "run/orders": self.orders,
+            "run/events": self.events,
+        }
