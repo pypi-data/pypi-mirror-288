6de693e2b7c1a8a73e63075b7e273f28b9bb6ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:05:01 +0000

    Add package slurm-completed-logs-utils/0.1.0/slurm_completed_logs_utils-0.1.0.tar.gz

---
+++ packages/slurm-completed-logs-utils/slurm_completed_logs_utils-0.1.0.tar.gz/slurm_completed_logs_utils-0.1.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=7.0',
+    "Rich",
+    "PyYAML",
+]
+
+test_requirements = [ ]
+
+setup(
+    author="Jaideep Sundaram",
+    author_email='jai.python3@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Collection of utilities for parsing the SLURM Completed Logs file.",
+    # entry_points={
+    #     'console_scripts': [
+    #         'make-simple-template-toolkit=slurm_completed_logs_utils.make_shell_scripts_and_aliases:main',
+    #         'insert-lines=slurm_completed_logs_utils.insert_lines:main',
+    #         'make-substitutions=slurm_completed_logs_utils.make_substitutions:main',
+    #     ],
+    # },
+    install_requires=requirements,
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='slurm_completed_logs_utils',
+    name='slurm_completed_logs_utils',
+    packages=find_packages(include=['slurm_completed_logs_utils', 'slurm_completed_logs_utils.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/jai-python3/slurm-completed-logs-utils',
+    version='0.1.0',
+    zip_safe=False,
+)
