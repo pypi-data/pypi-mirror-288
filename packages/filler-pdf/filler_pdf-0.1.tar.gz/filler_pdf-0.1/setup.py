be63da7d09000b1884637b9279008dd404c1b1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 15:04:02 +0000

    Add package filler-pdf/0.1/filler_pdf-0.1.tar.gz

---
+++ packages/filler-pdf/filler_pdf-0.1.tar.gz/filler_pdf-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "filler_pdf",
+    version= '0.1',
+    packages= find_packages(),
+    author='Mayur Talreja',
+    author_email='talrejamayur6@gmail.com',
+    install_requires = [
+        ## add dependencies
+    ],
+
+    # entry_points = {
+    #     "console_scripts": [
+    #         "pixegami-hello = pixegami_hello:hello",
+
+    #     ],
+    # },
+)
