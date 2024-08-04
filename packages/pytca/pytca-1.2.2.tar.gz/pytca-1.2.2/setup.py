407a1d18bb0cb11ff9ae7a88b4c9100429d6a2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 14:04:32 +0000

    Add package pytca/1.2.2/pytca-1.2.2.tar.gz

---
+++ packages/pytca/pytca-1.2.2.tar.gz/pytca-1.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+from setuptools.command.build_ext import build_ext
+from pytca import __version__ as versionInfo
+setup(
+    name='pytca',
+    version=versionInfo,
+    description='Python Library for Transaction Cost Analysis',
+    author='Jialue Chen',
+    author_email='jialuechen@outlook.com',
+    url='https://github.com/jialuechen/pytca',
+    packages=find_packages(),
+    install_requires=[
+        'pandas', 'matplotlib', 'plotly', 'bokeh', 'dash', 'flask', 'pybind11', 'numpy', 'geopandas', 'scikit-learn', 'textblob', 'requests', 'web3'
+    ],
+    cmdclass={'build_ext': build_ext},
+)
+
