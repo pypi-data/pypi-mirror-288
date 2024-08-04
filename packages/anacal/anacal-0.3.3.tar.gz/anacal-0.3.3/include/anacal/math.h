5fb767e6b5e74f6f0e10bb1e15c44bf4e20522ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 04:05:09 +0000

    Add package anacal/0.3.3/anacal-0.3.3.tar.gz

---
+++ packages/anacal/anacal-0.3.3.tar.gz/anacal-0.3.3/include/anacal/math.h
@@ -0,0 +1,7 @@
+#ifndef ANACAL_MATH_H
+#define ANACAL_MATH_H
+
+#include "math/pooling.h"
+#include "math/qnumber.h"
+
+#endif // ANACAL_MATH_H
---
+++ packages/anacal/anacal-0.3.3.tar.gz/anacal-0.3.3/include/anacal/math/pooling.h
@@ -0,0 +1,67 @@
+#ifndef ANACAL_MATH_POOLING_H
+#define ANACAL_MATH_POOLING_H
+
+#include <cmath>
+
+namespace anacal {
+namespace math {
+
+    inline double ssfunc1(double x, double mu, double sigma) {
+        // Returns the C1 smooth step weight funciton
+        auto _func = [](double t) -> double {
+            return -2.0 * t * t * t + 3 * t * t;
+        };
+
+        double t = (x - mu) / (sigma * 2.0) + 0.5;
+        if (t < 0) return 0.0;
+        else if (t <= 1) return _func(t);
+        else return 1.0;
+    }
+
+    inline double ssfunc1_deriv(double x, double mu, double sigma) {
+        // Returns the derivative of C1 smooth step weight funciton
+        auto _func = [](double t) -> double {
+            return -6.0 * t * t + 6 * t;
+        };
+
+        double t = (x - mu) / (sigma * 2.0) + 0.5;
+        if (t < 0) return 0.0;
+        else if (t <= 1) return _func(t) / (sigma * 2.0);
+        else return 0.0;
+    }
+
+    inline double ssfunc2(double x, double mu, double sigma) {
+        // Returns the C2 smooth step weight funciton
+        auto _func = [](double t) -> double {
+            double t3 = t * t * t;
+            return (6 * t * t - 15 * t + 10) * t3 ;
+        };
+
+        double t = (x - mu) / (sigma * 2.0) + 0.5;
+        if (t < 0) return 0.0;
+        else if (t <= 1) return _func(t);
+        else return 1.0;
+    }
+
+    inline double ssfunc2_deriv(double x, double mu, double sigma) {
+        // Returns the derivative of C2 smooth step weight funciton
+        auto _func = [](double t) -> double {
+            double t2 = t * t;
+            return (30 * t2 - 60 * t + 30) * t2 ;
+        };
+
+        double t = (x - mu) / (sigma * 2.0) + 0.5;
+        if (t < 0) return 0.0;
+        else if (t <= 1) return _func(t) / (sigma * 2.0);
+        else return 0.0;
+    }
+
+    inline double sigmoid(double x, double mu, double sigma) {
+        double t = (x - mu) / sigma;
+        return 1.0 / (1.0 + std::exp(-t));
+    }
+
+}
+}
+
+#endif // ANACAL_MATH_POOLING_H
