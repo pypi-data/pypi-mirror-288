a6eed531911adba06a63d4a62276d141169d4c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-04 03:04:12 +0000

    Add package picwish/0.4.4/picwish-0.4.4.tar.gz

---
+++ packages/picwish/picwish-0.4.4.tar.gz/picwish-0.4.4/picwish/__init__.py
@@ -0,0 +1,20 @@
+"""
+PicWish Python
+==============
+A library designed to provide free and unlimited access to PicWish
+services such as AI image generation, image enhancement, image OCR,
+and background removal.
+"""
+
+__version__ = '0.4.4'
+__license__ = 'MIT'
+
+import asyncio
+import os
+
+from .enums import OCRFormat, OCRLanguage, T2IQuality, T2ISize, T2ITheme
+from .image_models import BackgroundRemovedImage, ColorizeResult, EnhancedImage, OCRResult, T2IResult
+from .main import EnhancedImage, PicWish, PicwishError
+
+if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
