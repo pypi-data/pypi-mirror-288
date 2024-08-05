d5503d67ab6aa2d9b34d55a70132024579d32ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:04:43 +0000

    Add package rship-sdk/0.1.11/rship_sdk-0.1.11.tar.gz

---
+++ packages/rship-sdk/rship_sdk-0.1.11.tar.gz/rship_sdk-0.1.11/src/rship_sdk/proxies/action.py
@@ -0,0 +1,35 @@
+from ..models import Action
+
+from typing import Callable, Dict
+
+class ActionArgs():
+  def __init__(self, name: str, short_id: str, schema):
+    self.name = name
+    self.short_id = short_id
+    self.schema = schema
+
+class ActionProxy():
+  def __init__(self, target_proxy, args:ActionArgs, client, handler: Callable[[ActionArgs, Dict], None]):
+    self.target = target_proxy
+    self.args = args
+    self.client = client
+    self.handler = handler
+
+
+  async def save(self):
+    action = Action(
+      id=self.id(),
+      name=self.args.name,
+      schema=self.args.schema,
+      target_id=self.target.id(),
+      service_id=self.target.instance.args.service_id,
+    )
+
+    await self.client.set_data(action, 'Action')
+    
+
+  def id(self) -> str:
+    return f"{self.target.instance.args.service_id}:{self.target.args.short_id}:{self.args.short_id}"
+  
+  def take(self, data: any):
+    self.handler(self.args, data)
