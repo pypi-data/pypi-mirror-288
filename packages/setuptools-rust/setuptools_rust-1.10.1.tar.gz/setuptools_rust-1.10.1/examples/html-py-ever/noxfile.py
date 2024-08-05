a502dda698fd993a46e86e9dc93060fb1f129d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:02 +0000

    Add package setuptools-rust/1.10.1/setuptools_rust-1.10.1.tar.gz

---
+++ packages/setuptools-rust/setuptools_rust-1.10.1.tar.gz/setuptools_rust-1.10.1/examples/html-py-ever/noxfile.py
@@ -0,0 +1,16 @@
+from os.path import dirname
+
+import nox
+
+SETUPTOOLS_RUST = dirname(dirname(dirname(__file__)))
+
+
+@nox.session()
+def test(session: nox.Session):
+    session.install(
+        SETUPTOOLS_RUST, "wheel", "pytest", "pytest-benchmark", "beautifulsoup4"
+    )
+    # Ensure build uses version of setuptools-rust under development
+    session.install("--no-build-isolation", ".")
+    # Test Python package
+    session.run("pytest", *session.posargs)
