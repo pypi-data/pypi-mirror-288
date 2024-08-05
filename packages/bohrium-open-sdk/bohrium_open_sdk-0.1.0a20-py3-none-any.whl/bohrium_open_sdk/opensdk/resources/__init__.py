71fbe2a31b31ff9b9615b8d18babb73ac657671d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:04:52 +0000

    Add package bohrium-open-sdk/0.1.0a20/bohrium_open_sdk-0.1.0a20-py3-none-any.whl

---
+++ packages/bohrium-open-sdk/bohrium_open_sdk-0.1.0a20-py3-none-any.whl/bohrium_open_sdk/opensdk/resources/__init__.py
@@ -0,0 +1,8 @@
+from .app.app import App
+from .app.app_job import AppJob
+from .app.stock import Stock
+from .job import Job
+from .user import User
+from .sku import Sku
+
+__all__ = ["Job", "User", "App", "AppJob", "Sku", "Stock"]
