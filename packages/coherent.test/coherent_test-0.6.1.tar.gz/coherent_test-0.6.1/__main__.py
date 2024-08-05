9cb7c66f4a0911ff4e1970834e5025f48941a59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:04:30 +0000

    Add package coherent.test/0.6.1/coherent_test-0.6.1.tar.gz

---
+++ packages/coherent.test/coherent_test-0.6.1.tar.gz/coherent_test-0.6.1/__main__.py
@@ -0,0 +1,29 @@
+import contextlib
+import os
+import runpy
+import sys
+
+import pip_run
+from coherent.build import bootstrap
+
+
+@contextlib.contextmanager
+def project_on_path():
+    """
+    Install the project under test to sys.path.
+    """
+    deps = pip_run.deps.load('--editable', '.[test]')
+    with bootstrap.write_pyproject(), deps as home:
+        sys.path.insert(0, str(home))
+        yield
+
+
+def run():
+    os.environ.update(
+        PYTEST_ADDOPTS='--doctest-modules',
+    )
+    with project_on_path():
+        runpy.run_module('pytest', run_name='__main__')
+
+
+__name__ == '__main__' and run()
