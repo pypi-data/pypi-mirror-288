79b17821fdd9015a3ae7036e657eff6fa22c2cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:48 +0000

    Add package peek-plugin-eventdb/3.4.16/peek-plugin-eventdb-3.4.16.tar.gz

---
+++ packages/peek-plugin-eventdb/peek-plugin-eventdb-3.4.16.tar.gz/peek-plugin-eventdb-3.4.16/peek_plugin_eventdb/_private/admin-app/edit-setting-table/edit.component.ts
@@ -0,0 +1,52 @@
+import { Component } from "@angular/core";
+import { BalloonMsgService } from "@synerty/peek-plugin-base-js";
+import {
+    extend,
+    NgLifeCycleEvents,
+    TupleLoader,
+    VortexService,
+} from "@synerty/vortexjs";
+import { eventdbFilt } from "../PluginNames";
+import { SettingPropertyTuple } from "../tuples/SettingPropertyTuple";
+
+@Component({
+    selector: "pl-eventdb-edit-setting",
+    templateUrl: "./edit.component.html",
+})
+export class EditSettingComponent extends NgLifeCycleEvents {
+    items: SettingPropertyTuple[] = [];
+    loader: TupleLoader;
+    // This must match the dict defined in the admin_backend handler
+    private readonly filt = {
+        key: "admin.Edit.SettingProperty",
+    };
+
+    constructor(
+        private balloonMsg: BalloonMsgService,
+        vortexService: VortexService
+    ) {
+        super();
+
+        this.loader = vortexService.createTupleLoader(this, () =>
+            extend({}, this.filt, eventdbFilt)
+        );
+
+        this.loader.observable.subscribe(
+            (tuples: SettingPropertyTuple[]) => (this.items = tuples)
+        );
+    }
+
+    saveClicked() {
+        this.loader
+            .save()
+            .then(() => this.balloonMsg.showSuccess("Save Successful"))
+            .catch((e) => this.balloonMsg.showError(e));
+    }
+
+    resetClicked() {
+        this.loader
+            .load()
+            .then(() => this.balloonMsg.showSuccess("Reset Successful"))
+            .catch((e) => this.balloonMsg.showError(e));
+    }
+}
