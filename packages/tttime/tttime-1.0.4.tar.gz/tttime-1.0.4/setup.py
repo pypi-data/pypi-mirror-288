40b0c64cb0cd2e2c3df1fc101d47f3fd4b6d050c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:05:51 +0000

    Add package tttime/1.0.4/tttime-1.0.4.tar.gz

---
+++ packages/tttime/tttime-1.0.4.tar.gz/tttime-1.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tttime',
+    # this version is automatically copied from the typescript package.json
+    # so does not need to be updated manually, this does mean that sometimes
+    # it might apear out of sync
+    version='1.0.4',
+    author='tonyaellie',
+    author_email='tonya@tokia.dev',
+    description='An implementation of a TTTime in Python.',
+    long_description=open('../README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tonyaellie/tttime',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
