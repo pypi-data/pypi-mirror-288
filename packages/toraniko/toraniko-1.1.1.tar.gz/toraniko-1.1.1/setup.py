aac3748c6e84f4b9a90d65da8aa8a27f4ae20e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:05:04 +0000

    Add package toraniko/1.1.1/toraniko-1.1.1.tar.gz

---
+++ packages/toraniko/toraniko-1.1.1.tar.gz/toraniko-1.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    for line in fh:
+        requirements.append(line.strip())
+
+setup(
+    name="toraniko",
+    version="1.1.1",
+    packages=find_packages(),
+    install_requires = requirements
+)
