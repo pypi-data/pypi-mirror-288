aacabd7bbd4f0d2c5161400e685a274feab73ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:59 +0000

    Add package config-server-client-python/1.0.0/config_server_client_python-1.0.0.tar.gz

---
+++ packages/config-server-client-python/config_server_client_python-1.0.0.tar.gz/config_server_client_python-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='config-server-client-python',
+      version='1.0.0',
+      description='Package to fetch configurations from spring-cloud-config-server in Python',
+      packages=['src'],
+      author_email='jayantreddy181198@gmail.com',
+      zip_safe=False)
