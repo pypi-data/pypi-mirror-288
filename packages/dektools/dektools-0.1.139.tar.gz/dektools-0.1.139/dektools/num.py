1f50278e06fa6a71c84aeb58b5542c4baf3f2972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:50 +0000

    Add package dektools/0.1.139/dektools-0.1.139.tar.gz

---
+++ packages/dektools/dektools-0.1.139.tar.gz/dektools-0.1.139/dektools/num.py
@@ -0,0 +1,27 @@
+import string
+
+default_digs = string.digits + string.ascii_uppercase
+
+
+def int2base(x, base, digs=None):
+    digs = digs or default_digs
+    if x < 0:
+        sign = -1
+    elif x == 0:
+        return digs[0]
+    else:
+        sign = 1
+
+    x *= sign
+    digits = []
+
+    while x:
+        digits.append(digs[int(x % base)])
+        x = int(x / base)
+
+    if sign < 0:
+        digits.append('-')
+
+    digits.reverse()
+
+    return ''.join(digits)
