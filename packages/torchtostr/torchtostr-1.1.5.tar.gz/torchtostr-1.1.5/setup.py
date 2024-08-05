9958bb08168226c84a83cf4179f9f3c4217ec6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:05:07 +0000

    Add package torchtostr/1.1.5/torchtostr-1.1.5.tar.gz

---
+++ packages/torchtostr/torchtostr-1.1.5.tar.gz/torchtostr-1.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='torchtostr',
+  version='1.1.5',
+  author='ankov',
+  description='Module for quick and simple transportation of torch tensors arrays by converting them to str and back',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  install_requires=['torch', 'torchvision', 'torchaudio', 'numpy'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='tensors strings torch pytorch',
+  project_urls={
+    'GitHub': 'https://github.com/Ankov404/torchtostr'
+  }
+)
