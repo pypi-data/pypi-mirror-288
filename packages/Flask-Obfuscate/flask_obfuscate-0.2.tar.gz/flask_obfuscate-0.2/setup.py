db3f20ec76e6b31344dab44c1f83b1b06f20ccf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:17 +0000

    Add package Flask-Obfuscate/0.2/flask_obfuscate-0.2.tar.gz

---
+++ packages/Flask-Obfuscate/flask_obfuscate-0.2.tar.gz/flask_obfuscate-0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="Flask-Obfuscate",
+    version="0.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Flask",
+    ],
+    author="Zachariah Michael Lagden",
+    author_email="zach@zachlagden.uk",
+    description="A Flask extension to obfuscate HTML responses.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lagden-Development/flask-obfuscate",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Framework :: Flask",
+    ],
+)
