23505836f481d5f73520ad2cb543d9762d330b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:04:41 +0000

    Add package fzf-python/0.0.6/fzf_python-0.0.6.tar.gz

---
+++ packages/fzf-python/fzf_python-0.0.6.tar.gz/fzf_python-0.0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fzf-python",
+    version="0.0.6",
+    packages=find_packages(),
+    install_requires=[],
+    long_description="fzfpy is a python wrapper for fzf",
+)
