c1b13c244bd1000209a23566850d2e0af7b60293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:05:35 +0000

    Add package kappamodules/0.1.75/kappamodules-0.1.75.tar.gz

---
+++ packages/kappamodules/kappamodules-0.1.75.tar.gz/kappamodules-0.1.75/kappamodules/layers/drop_path.py
@@ -0,0 +1,81 @@
+import torch
+from torch import nn
+
+
+class DropPath(nn.Sequential):
+    """
+    Efficiently drop paths (Stochastic Depth) per sample such that dropped samples are not processed.
+    This is a subclass of nn.Sequential and can be used either as standalone Module or like nn.Sequential.
+    Examples::
+        >>> # use as nn.Sequential module
+        >>> sequential_droppath = DropPath(nn.Linear(4, 4), drop_prob=0.2)
+        >>> y = sequential_droppath(torch.randn(10, 4))
+
+        >>> # use as standalone module
+        >>> standalone_layer = nn.Linear(4, 4)
+        >>> standalone_droppath = DropPath(drop_prob=0.2)
+        >>> y = standalone_droppath(torch.randn(10, 4), standalone_layer)
+    """
+
+    def __init__(self, *args, drop_prob: float = 0., scale_by_keep: bool = True, stochastic_drop_prob: bool = False):
+        super().__init__(*args)
+        assert 0. <= drop_prob < 1.
+        self._drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
+        self.stochastic_drop_prob = stochastic_drop_prob
+
+    @property
+    def drop_prob(self):
+        return self._drop_prob
+
+    @drop_prob.setter
+    def drop_prob(self, value):
+        assert 0. <= value < 1.
+        self._drop_prob = value
+
+    @property
+    def keep_prob(self):
+        return 1. - self.drop_prob
+
+    def forward(self, x, residual_path=None, residual_path_kwargs=None):
+        assert (len(self) == 0) ^ (residual_path is None)
+        residual_path_kwargs = residual_path_kwargs or {}
+        if self.drop_prob == 0. or not self.training:
+            if residual_path is None:
+                return x + super().forward(x, **residual_path_kwargs)
+            else:
+                return x + residual_path(x, **residual_path_kwargs)
+        # generate indices to keep (propagated through transform path)
+        bs = len(x)
+        if self.stochastic_drop_prob:
+            perm = torch.empty(bs, device=x.device).bernoulli_(self.keep_prob).nonzero().squeeze(1)
+            scale = 1 / self.keep_prob
+        else:
+            keep_count = max(int(bs * self.keep_prob), 1)
+            scale = bs / keep_count
+            perm = torch.randperm(bs, device=x.device)[:keep_count]
+
+        # propagate
+        if self.scale_by_keep:
+            alpha = scale
+        else:
+            alpha = 1.
+        # reduce kwargs (e.g. used for DiT block where scale/shift/gate is passed and also has to be reduced)
+        residual_path_kwargs = {
+            key: value[perm] if torch.is_tensor(value) else value
+            for key, value in residual_path_kwargs.items()
+        }
+        if residual_path is None:
+            residual = super().forward(x[perm], **residual_path_kwargs)
+        else:
+            residual = residual_path(x[perm], **residual_path_kwargs)
+        return torch.index_add(
+            x.flatten(start_dim=1),
+            dim=0,
+            index=perm,
+            source=residual.to(x.dtype).flatten(start_dim=1),
+            alpha=alpha,
+        ).view_as(x)
+
+    def extra_repr(self):
+        return f'drop_prob={round(self.drop_prob, 3):0.3f}'
