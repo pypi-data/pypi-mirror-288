0003a64d69e78d08dbe4c6d3675a6f910e8e1beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:05:00 +0000

    Add package legal-citation-parser/0.5.0/legal_citation_parser-0.5.0-py3-none-any.whl

---
+++ packages/legal-citation-parser/legal_citation_parser-0.5.0-py3-none-any.whl/legal_citation_parser/citation_parser.py
@@ -0,0 +1,51 @@
+# citation_parser.py
+
+from .canlii_rules import CanLIICitationParser
+
+def create_citation(citation: str, citation_type: str = "canlii", **kwargs) -> dict:
+    """
+    Creates a citation object from a citation string.
+
+    Args:
+        citation (str): The citation string to parse.
+        type (str): The type of citation to parse. Default is "canlii".
+
+    Keyword Args:
+        include_url (bool): A flag to determine whether to include the CanLII URL in the output.
+        
+
+    Returns:
+        ParsedCitation: An object containing the parsed information, including the style of cause,
+        citation, citation type (neutral or CanLII), year, court code, decision number,
+        jurisdiction, court name, and court level.
+    """
+
+    parser = CanLIICitationParser(citation, **kwargs)
+    citation_info = parser.parse()
+    
+    return citation_info
+
+
+def parse_citation(
+    citation: str, citation_type: str = "canlii", **kwargs
+) -> dict:
+    """
+    Parses a citation string to extract key information about the court case.
+
+    Args:
+        citation (str): The citation string to parse.
+        type (str): The type of citation to parse. Default is "canlii".
+
+    Keyword Args:
+        include_url (bool): A flag to determine whether to include the CanLII URL in the output.
+        
+
+    Returns:
+        ParsedCitation: An object containing the parsed information, including the style of cause,
+        citation, citation type (neutral or CanLII), year, court code, decision number,
+        jurisdiction, court name, and court level.
+    """
+
+    citation_info = create_citation(citation, citation_type, **kwargs)
+    
+    return citation_info.parse()
