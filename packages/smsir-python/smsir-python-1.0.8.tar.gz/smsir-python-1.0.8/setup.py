f845d1269bd7fbe80168388328be0c5b8e26a0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:05:59 +0000

    Add package smsir-python/1.0.8/smsir-python-1.0.8.tar.gz

---
+++ packages/smsir-python/smsir-python-1.0.8.tar.gz/smsir-python-1.0.8/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setuptools.setup(
+    name="smsir-python",
+    version="1.0.8",
+    author="Mojtaba Akbari",
+    author_email="mojtaba.akbari.221b@gmail.com",
+    packages=["sms_ir"],
+    description="Python Package of SMS.ir Panel ",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IPeCompany/SmsPanelV2.Python",
+    license='MIT',
+    python_requires='>=3.7',
+    install_requires=[],
+)
