8e165f957dc198c29f54a0c06e2b8f8038c8bbf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:06:11 +0000

    Add package airport-manager/0.2.21/airport_manager-0.2.21.tar.gz

---
+++ packages/airport-manager/airport_manager-0.2.21.tar.gz/airport_manager-0.2.21/airport_manager/flight_tracker_v1/main_menu.py
@@ -0,0 +1,18 @@
+import click
+from rich.console import Console
+from rich.prompt import Prompt
+from airport_manager.flight_tracker_v1.menu import display_flight_tracker_v1_menu, handle_flight_tracker_v1_menu
+from airport_manager.utils import clear_console
+
+console = Console()
+
+@click.command()
+@click.pass_context
+def flight_tracker_v1_menu(ctx):
+    while True:
+        clear_console()
+        display_flight_tracker_v1_menu(console)
+        choice = Prompt.ask("Select an option", choices=["1", "2", "3", "4", "5", "6", "7"])
+        if choice == "7":
+            break
+        handle_flight_tracker_v1_menu(choice)
