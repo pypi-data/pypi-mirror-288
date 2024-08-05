3421e1e9085dfaa404d6315fffae4043049fa7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:42 +0000

    Add package validacion-terrenos/0.2.0/validacion_terrenos-0.2.0-py3-none-any.whl

---
+++ packages/validacion-terrenos/validacion_terrenos-0.2.0-py3-none-any.whl/validacion_terrenos/__init__.py
@@ -0,0 +1,11 @@
+# terrenos_sahagun/__init__.py
+
+from .terrenos_sahagun import obtener_capa_activa, obtener_seleccionados, validar_fila, validate_attributes, confirmar_cambios
+
+__all__ = [
+    'obtener_capa_activa',
+    'obtener_seleccionados',
+    'validar_fila',
+    'validate_attributes',
+    'confirmar_cambios'
+]
