bb44592ee3972a1a7a1b38d5b63820e0f3d1a648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:47 +0000

    Add package peek-plugin-branch/3.4.16/peek-plugin-branch-3.4.16.tar.gz

---
+++ packages/peek-plugin-branch/peek-plugin-branch-3.4.16.tar.gz/peek-plugin-branch-3.4.16/peek_plugin_branch/_private/admin-app/branch.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "branch-admin",
+    templateUrl: "branch.component.html",
+})
+export class BranchComponent implements OnInit {
+    ngOnInit() {}
+}
