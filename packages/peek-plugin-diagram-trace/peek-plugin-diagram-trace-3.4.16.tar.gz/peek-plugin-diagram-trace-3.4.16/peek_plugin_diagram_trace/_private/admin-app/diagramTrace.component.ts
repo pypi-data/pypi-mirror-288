5af3289526cb3916620c40f9327ab70a2db9a9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:48 +0000

    Add package peek-plugin-diagram-trace/3.4.16/peek-plugin-diagram-trace-3.4.16.tar.gz

---
+++ packages/peek-plugin-diagram-trace/peek-plugin-diagram-trace-3.4.16.tar.gz/peek-plugin-diagram-trace-3.4.16/peek_plugin_diagram_trace/_private/admin-app/diagramTrace.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "diagramTrace-admin",
+    templateUrl: "diagramTrace.component.html",
+})
+export class DiagramTraceComponent implements OnInit {
+    ngOnInit() {}
+}
