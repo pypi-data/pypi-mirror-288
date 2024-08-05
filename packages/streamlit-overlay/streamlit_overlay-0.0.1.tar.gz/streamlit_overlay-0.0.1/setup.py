ae0d1848f030d8efb31345cde711b2f95cb53c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:05:10 +0000

    Add package streamlit-overlay/0.0.1/streamlit_overlay-0.0.1.tar.gz

---
+++ packages/streamlit-overlay/streamlit_overlay-0.0.1.tar.gz/streamlit_overlay-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+
+with open(Path(this_directory) / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="streamlit-overlay",
+    version="0.0.1",
+    author="Jens Rahnfeld",
+    author_email="",
+    description="Streamlit component that simplifies adding overlays to images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.8",
+    install_requires=[
+        "opencv-python>=4.6.0.66",
+        "streamlit>=1.22.0",
+    ],
+)
