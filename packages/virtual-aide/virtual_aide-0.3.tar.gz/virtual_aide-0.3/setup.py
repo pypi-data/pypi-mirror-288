4781a9944cc962ec674b8390a382116a10a41b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:24 +0000

    Add package virtual-aide/0.3/virtual_aide-0.3.tar.gz

---
+++ packages/virtual-aide/virtual_aide-0.3.tar.gz/virtual_aide-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='virtual_aide',
+    version='0.3',  
+    packages=find_packages(),
+    description='Required package to run assistant',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
