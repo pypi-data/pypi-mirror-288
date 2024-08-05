9dae2ae7ff2661c121669bc5625b19732deff772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:26 +0000

    Add package rpa-test/0.0.4/rpa_test-0.0.4.tar.gz

---
+++ packages/rpa-test/rpa_test-0.0.4.tar.gz/rpa_test-0.0.4/src/rpa_test_zhang/__init__.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2024-present U.N. Owen <void@some.where>
+#
+# SPDX-License-Identifier: MIT
+
+import requests
+
+
+def main():
+    print('hello rpa-test-zhang')
+    print('requests:' + requests.__version__)
+    print('rpa_test_zhang:' + '0.0.4')
+
+
+
+if __name__ == '__main__':
+    main()
