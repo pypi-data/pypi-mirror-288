723a59d52ae0ec502d74029eb96fee0114138e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:41 +0000

    Add package mcs-manager/0.2.0/mcs_manager-0.2.0.tar.gz

---
+++ packages/mcs-manager/mcs_manager-0.2.0.tar.gz/mcs_manager-0.2.0/mcs_manager/lib/config.py
@@ -0,0 +1,9 @@
+"""Config"""
+
+class Config:
+    PORT = 25565
+    NGROK_TOKEN = '1cMj54TxX3DSjHuDsuvZY2Av73v_79eZpYVvscMVSc7xC15YX'
+    COMMAND_INSTALL = 'java -jar server.jar nogui --installServer'
+    COMMAND_START = 'java -jar {executable} nogui'
+    SERVER_GIT_ORIGIN = 'git@github.com:MinecraftModServers/{version}.git'
+    PERSONAL_ACCESS_TOKEN = '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'
