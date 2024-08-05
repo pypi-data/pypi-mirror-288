0e62456d4fe6e4c3c3b910eb4d946b9d038dae7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:05:54 +0000

    Add package machinemind-bpe/0.4.0/machinemind_bpe-0.4.0.tar.gz

---
+++ packages/machinemind-bpe/machinemind_bpe-0.4.0.tar.gz/machinemind_bpe-0.4.0/README.md
@@ -0,0 +1,12 @@
+BPE Tokenizer
+This package provides an implementation of Byte Pair Encoding (BPE) tokenizer, designed to be efficient and fast. It includes training on both CPU and GPU, along with encoding and decoding functionalities.
+
+Features
+Train BPE tokenizer on CPU and GPU
+Encode and decode text using the trained tokenizer
+Save and load merges and vocabulary to/from JSON files
+Support for special tokens and customizable regex patterns
+Installation
+To install the package, use the following command:
+
+pip install machinemind_bpe
---
+++ packages/machinemind-bpe/machinemind_bpe-0.4.0.tar.gz/machinemind_bpe-0.4.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="machinemind_bpe",
+    version="0.4.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.18.0",
+        "torch>=1.7.0",
+        "tqdm>=4.45.0",
+        "regex>=20.4.4"
+    ],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    description="A Byte Pair Encoding (BPE) tokenizer package for CPU and GPU.",
+    author="Javokhir",
+    author_email="machinemind60@gmail.com",
+    url="https://github.com/machineminduzb_v2/bpe_tokenizer",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
