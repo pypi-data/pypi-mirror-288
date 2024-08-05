41629c2ada5e6a02fdd4be168914371d14cebcd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:42 +0000

    Add package one-api-cli/0.2.0/one_api_cli-0.2.0.tar.gz

---
+++ packages/one-api-cli/one_api_cli-0.2.0.tar.gz/one_api_cli-0.2.0/src/one_api_cli/constant.py
@@ -0,0 +1,23 @@
+import os
+
+# Environment variables
+base_url = os.getenv("ONE_API_BASE_URL")
+access_token = os.getenv("ONE_API_ACCESS_TOKEN")
+section_token = os.getenv("ONE_API_SECTION_TOKEN")
+
+
+assert base_url, "ONE_API_BASE_URL is not set"
+assert access_token or section_token, "Either ONE_API_ACCESS_TOKEN or ONE_API_SECTION_TOKEN must be set"
+
+# Headers
+if access_token:
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Authorization": f"Bearer {access_token}",
+    }
+else:
+    section_token = section_token.lstrip("section=").strip("=")
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Cookie": f"section={section_token}=",
+    }
