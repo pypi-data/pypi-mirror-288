950fcd486e98693919d984ce130034e2a5663691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:48 +0000

    Add package matrix-gptbot/0.3.16/matrix_gptbot-0.3.16.tar.gz

---
+++ packages/matrix-gptbot/matrix_gptbot-0.3.16.tar.gz/matrix_gptbot-0.3.16/src/gptbot/commands/botinfo.py
@@ -0,0 +1,23 @@
+from nio.events.room_events import RoomMessageText
+from nio.rooms import MatrixRoom
+
+
+async def command_botinfo(room: MatrixRoom, event: RoomMessageText, bot):
+    bot.logger.log("Showing bot info...")
+
+    body = f"""GPT Info:
+
+Model: {bot.model}
+Maximum context tokens: {bot.max_tokens}
+Maximum context messages: {bot.max_messages}
+
+Room info:
+
+Bot user ID: {bot.matrix_client.user_id}
+Current room ID: {room.room_id}
+System message: {bot.get_system_message(room)}
+
+For usage statistics, run !gptbot stats
+"""
+
+    await bot.send_message(room, body, True)
