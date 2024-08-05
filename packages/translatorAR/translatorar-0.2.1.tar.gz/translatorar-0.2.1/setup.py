e1ebb8e13ac47a2db7ede9031b8d0325373b2774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:59 +0000

    Add package translatorAR/0.2.1/translatorar-0.2.1.tar.gz

---
+++ packages/translatorAR/translatorar-0.2.1.tar.gz/translatorar-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='translatorAR',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'transformers',
+        'pymupdf',
+        'tqdm',
+        'pdf2image',
+        'pytesseract',
+        'pillow'
+        # Add other dependencies here
+    ],
+    include_package_data=True,
+    license='CC BY-NC 4.0',
+    description='A translation tool for multiple languages.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Amin Boulouma',
+    author_email='amin@boulouma.com',
+    url='https://pypi.org/project/translatorAR/0.1.0/',
+)
