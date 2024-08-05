9fcd442df9a5d23497c6d8264cb10bc70d32ce30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:05:55 +0000

    Add package oarepo-oai-pmh-harvester/4.1.2/oarepo_oai_pmh_harvester-4.1.2-py3-none-any.whl

---
+++ packages/oarepo-oai-pmh-harvester/oarepo_oai_pmh_harvester-4.1.2-py3-none-any.whl/oarepo_oaipmh_harvester/oai_harvester/services/records/search.py
@@ -0,0 +1,19 @@
+from oarepo_runtime.services.search import SearchOptions
+
+from . import facets
+
+
+class OaiHarvesterSearchOptions(SearchOptions):
+    """OaiHarvesterRecord search options."""
+
+    facet_groups = {}
+
+    facets = {
+        "batch_size": facets.batch_size,
+        "loader": facets.loader,
+        "max_records": facets.max_records,
+        "metadataprefix": facets.metadataprefix,
+        "setspecs": facets.setspecs,
+        "transformers": facets.transformers,
+        **getattr(SearchOptions, "facets", {}),
+    }
