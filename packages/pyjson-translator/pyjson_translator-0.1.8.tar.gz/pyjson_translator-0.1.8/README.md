639f9fe8556fba307dec19b8a4fe982d8f9a8002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:26 +0000

    Add package pyjson-translator/0.1.8/pyjson_translator-0.1.8.tar.gz

---
+++ packages/pyjson-translator/pyjson_translator-0.1.8.tar.gz/pyjson_translator-0.1.8/README.md
@@ -0,0 +1,9 @@
+# pyjson_translator
+
+A simple JSON to Python object translator.
+
+## Installation
+
+```bash
+pip install pyjson_translator
+```
---
+++ packages/pyjson-translator/pyjson_translator-0.1.8.tar.gz/pyjson_translator-0.1.8/pyjson_translator/__init__.py
@@ -0,0 +1,13 @@
+from .logger_setting import (
+    pyjson_translator_logging,
+    set_logging_level
+)
+
+from .pyjson_translator import (
+    serialize_value,
+    deserialize_value
+)
+
+from .db_sqlalchemy_instance import (
+    default_sqlalchemy_instance
+)
