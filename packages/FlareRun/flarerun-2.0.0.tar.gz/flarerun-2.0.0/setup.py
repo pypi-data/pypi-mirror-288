9eeefab69b866e6bd369f1458952d2a47fe31688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:03 +0000

    Add package FlareRun/2.0.0/flarerun-2.0.0.tar.gz

---
+++ packages/FlareRun/flarerun-2.0.0.tar.gz/flarerun-2.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FlareRun",
+    version="2.0.0",
+    author="Aryan Chander",
+    author_email="aryanchander5@gmail.com",
+    description="A comprehensive Python library designed for seamless interaction with FlareRun's trading APIs",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://flarerun.online",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
