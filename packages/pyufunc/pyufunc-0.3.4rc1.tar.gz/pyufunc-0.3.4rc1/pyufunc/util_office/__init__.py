a684b6e3d26b2c7ceb4e3499bf1393b994aac5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:04:54 +0000

    Add package pyufunc/0.3.4rc1/pyufunc-0.3.4rc1.tar.gz

---
+++ packages/pyufunc/pyufunc-0.3.4rc1.tar.gz/pyufunc-0.3.4rc1/pyufunc/util_office/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+from ._email import is_valid_email, send_email
+
+__all__ = [
+    "is_valid_email",
+    "send_email"
+]
