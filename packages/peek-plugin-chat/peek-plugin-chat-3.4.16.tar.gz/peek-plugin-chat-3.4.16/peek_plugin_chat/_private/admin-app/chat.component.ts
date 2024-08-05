fcef983dc7ff697442d3ded86ec20f218b389eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:47 +0000

    Add package peek-plugin-chat/3.4.16/peek-plugin-chat-3.4.16.tar.gz

---
+++ packages/peek-plugin-chat/peek-plugin-chat-3.4.16.tar.gz/peek-plugin-chat-3.4.16/peek_plugin_chat/_private/admin-app/chat.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "chat-admin",
+    templateUrl: "chat.component.html",
+})
+export class ChatComponent implements OnInit {
+    ngOnInit() {}
+}
---
+++ packages/peek-plugin-chat/peek-plugin-chat-3.4.16.tar.gz/peek-plugin-chat-3.4.16/peek_plugin_chat/_private/admin-app/chat.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+// Import our components
+import { ChatComponent } from "./chat.component";
+
+// Define the routes for this Angular module
+export const pluginRoutes: Routes = [
+    {
+        path: "",
+        component: ChatComponent,
+    },
+];
+
+// Define the module
+@NgModule({
+    imports: [CommonModule, RouterModule.forChild(pluginRoutes), FormsModule],
+    exports: [],
+    providers: [],
+    declarations: [ChatComponent],
+})
+export class ChatModule {}
