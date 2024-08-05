a8125983f4c9e15bcc2ae46522138ccb854dc326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:54 +0000

    Add package pyview-web/0.0.20/pyview_web-0.0.20.tar.gz

---
+++ packages/pyview-web/pyview_web-0.0.20.tar.gz/pyview_web-0.0.20/pyview/live_view.py
@@ -0,0 +1,53 @@
+from typing import TypeVar, Generic, Optional, Union, Any
+from .live_socket import LiveViewSocket, UnconnectedSocket
+from pyview.template import LiveTemplate, template_file, RenderedContent, LiveRender
+import inspect
+from pyview.events import InfoEvent
+from urllib.parse import ParseResult
+
+T = TypeVar("T")
+
+AnySocket = Union[LiveViewSocket[T], UnconnectedSocket[T]]
+Session = dict[str, Any]
+
+# TODO: ideally this would always be a ParseResult, but we need to update push_patch
+URL = Union[ParseResult, str]
+
+
+class LiveView(Generic[T]):
+    def __init__(self):
+        pass
+
+    async def mount(self, socket: AnySocket, session: Session):
+        pass
+
+    async def handle_event(self, event, payload, socket: LiveViewSocket[T]):
+        pass
+
+    async def handle_info(self, event: InfoEvent, socket: LiveViewSocket[T]):
+        pass
+
+    async def handle_params(self, url: URL, params, socket: AnySocket):
+        pass
+
+    async def disconnect(self, socket: LiveViewSocket[T]):
+        pass
+
+    async def render(self, assigns: T) -> RenderedContent:
+        html_render = _find_render(self)
+
+        if html_render:
+            return LiveRender(html_render, assigns)
+
+        raise NotImplementedError()
+
+
+def _find_render(m: LiveView) -> Optional[LiveTemplate]:
+    cf = inspect.getfile(m.__class__)
+    return _find_template(cf)
+
+
+def _find_template(cf: str) -> Optional[LiveTemplate]:
+    if cf.endswith(".py"):
+        cf = cf[:-3]
+        return template_file(cf + ".html")
