d13ca8671d5453a911daf91eb81507bb33bc09c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 16:04:50 +0000

    Add package py-pde/0.41.0/py_pde-0.41.0-py3-none-any.whl

---
+++ packages/py-pde/py_pde-0.41.0-py3-none-any.whl/pde/__init__.py
@@ -0,0 +1,38 @@
+"""The py-pde package provides classes and methods for solving partial differential
+equations."""
+
+# determine the package version
+try:
+    # try reading version of the automatically generated module
+    from ._version import __version__
+except ImportError:
+    # determine version automatically from CVS information
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        __version__ = version("pde")
+    except PackageNotFoundError:
+        # package is not installed, so we cannot determine any version
+        __version__ = "unknown"
+    del PackageNotFoundError, version  # clean name space
+
+# initialize the configuration
+from .tools.config import Config, environment
+
+config = Config()  # initialize the default configuration
+del Config  # clean name space
+
+# import all other modules that should occupy the main name space
+from .fields import *  # @UnusedWildImport
+from .grids import *  # @UnusedWildImport
+from .pdes import *  # @UnusedWildImport
+from .solvers import *  # @UnusedWildImport
+from .storage import *  # @UnusedWildImport
+from .tools.parameters import Parameter
+from .trackers import *  # @UnusedWildImport
+from .visualization import *  # @UnusedWildImport
+
+try:
+    from .tools.modelrunner import *
+except ImportError:
+    pass  # modelrunner extensions are simply not loaded
