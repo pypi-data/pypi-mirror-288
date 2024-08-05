50b1694afb893eca654a9e4e2d9c5f8456ccf391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 03:04:43 +0000

    Add package pyjson-translator/0.1.11/pyjson_translator-0.1.11.tar.gz

---
+++ packages/pyjson-translator/pyjson_translator-0.1.11.tar.gz/pyjson_translator-0.1.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyjson_translator',
+    version='0.1.11',
+    description='A simple JSON to Python object translator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='moqimoqidea',
+    author_email='moqimoqidea@gmail.com',
+    url='https://github.com/moqimoqidea/pyjson_translator',
+    packages=find_packages(),
+    install_requires=[
+        'flask_sqlalchemy',
+        'marshmallow',
+        'marshmallow_sqlalchemy',
+        'pydantic',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    project_urls={
+        'Documentation': 'https://github.com/moqimoqidea/pyjson_translator#readme',
+        'Source': 'https://github.com/moqimoqidea/pyjson_translator',
+        'Tracker': 'https://github.com/moqimoqidea/pyjson_translator/issues',
+    },
+)
