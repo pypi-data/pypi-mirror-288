a46a6b421985a28a5493b4a881313788f52ebccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:49 +0000

    Add package mango-pycore/0.1.33a0/mango_pycore-0.1.33a0-py3-none-any.whl

---
+++ packages/mango-pycore/mango_pycore-0.1.33a0-py3-none-any.whl/mango_pycore/api/rest_api.py
@@ -0,0 +1,68 @@
+import json
+import traceback
+
+from .base_api import Api
+
+from .request import RestApiRequest
+from .response import Response
+from .exceptions import BadRequest, MiddlewareShortCircuit
+
+default_cors_headers = {
+    "Access-Control-Allow-Headers": "Content-Type, X-Amz-Date, Authorization,X-Api-Key,X-Amz-Security-Token",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Max-Age": 600,
+    "Content-Type": "application/json"
+}
+
+
+class RestApi(Api):
+    def __init__(self, name="SAM-API", cors_headers=None, cors_function=None):
+        super().__init__(name=name, cors_headers=None, cors_function=None)
+
+    def __call__(self, event, context, *args, **kwargs):
+        try:
+            request = self._process_middlewares(RestApiRequest(event=event, app=self, debug=self.debug)) if \
+            event['requestContext']['httpMethod'].lower() != "options" else RestApiRequest(event=event, app=self,
+                                                                                           debug=self.debug)
+            self.log.debug(event)
+
+            route_key = request.route_key
+
+            function = self._resources[route_key]
+            response = function(request)
+
+            base_headers = self.cors_headers.copy()
+
+            if isinstance(response, Response):
+                rslt = response.to_dict()
+                rslt_headers = rslt.get('headers', {})
+                base_headers.update(rslt_headers)
+                rslt['headers'] = base_headers
+                return rslt
+
+            response_headers = response.get('headers', {})
+            base_headers.update(response_headers)
+            response['headers'] = base_headers
+            return response
+
+        except MiddlewareShortCircuit as e:
+            return e.response
+
+        except BadRequest as e:
+            return {
+                "isBase64Encoded": False,
+                "statusCode": 400,
+                "body": json.dumps({
+                    "error": e.message
+                })
+            }
+        except Exception as e:
+            self.log.error(str(e))
+            debug_error = traceback.format_exc()
+            return {
+                "isBase64Encoded": False,
+                "statusCode": 500,
+                "body": json.dumps({
+                    "error": debug_error if self.debug else "Internal Server Error"
+                })
+            }
