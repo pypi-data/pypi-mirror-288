efbff12df085fdf8818e018691abff3d5f388d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:45 +0000

    Add package peek-core-email/3.4.16/peek-core-email-3.4.16.tar.gz

---
+++ packages/peek-core-email/peek-core-email-3.4.16.tar.gz/peek-core-email-3.4.16/peek_core_email/_private/admin-app/core-email-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "core-email-admin",
+    templateUrl: "core-email-admin.component.html",
+    styleUrls: ["core-email-admin.component.css"],
+})
+export class CoreEmailAdminComponent implements OnInit {
+    ngOnInit() {}
+}
---
+++ packages/peek-core-email/peek-core-email-3.4.16.tar.gz/peek-core-email-3.4.16/peek_core_email/_private/admin-app/setting-list/admin-setting-list.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from "@angular/core";
+import {
+    extend,
+    NgLifeCycleEvents,
+    Tuple,
+    TupleLoader,
+    VortexService,
+} from "@synerty/vortexjs";
+import { coreEmailFilt } from "../PluginNames";
+import { BalloonMsgService } from "@synerty/peek-plugin-base-js";
+
+class SettingProperty extends Tuple {
+    id: number;
+    settingId: number;
+    key: string;
+    type: string;
+
+    int_value: number;
+    char_value: string;
+    boolean_value: boolean;
+
+    constructor() {
+        super("c.s.p.setting.property");
+    }
+}
+
+@Component({
+    selector: "core-email-setting-list",
+    templateUrl: "./admin-setting-list.component.html",
+})
+export class AdminSettingListComponent
+    extends NgLifeCycleEvents
+    implements OnInit
+{
+    items: SettingProperty[] = [];
+    loader: TupleLoader;
+    private readonly filt = extend(
+        {
+            key: "server.setting.data",
+        },
+        coreEmailFilt
+    );
+
+    constructor(
+        private balloonMsg: BalloonMsgService,
+        vortexService: VortexService
+    ) {
+        super();
+
+        this.loader = vortexService.createTupleLoader(this, this.filt);
+
+        this.loader.observable.subscribe(
+            (tuples) => (this.items = <SettingProperty[]>tuples)
+        );
+    }
+
+    ngOnInit() {}
+
+    saveClicked() {
+        this.loader
+            .save()
+            .then(() => this.balloonMsg.showSuccess("Save Successful"))
+            .catch((e) => this.balloonMsg.showError(e));
+    }
+
+    resetClicked() {
+        this.loader
+            .load()
+            .then(() => this.balloonMsg.showSuccess("Reset Successful"))
+            .catch((e) => this.balloonMsg.showError(e));
+    }
+}
