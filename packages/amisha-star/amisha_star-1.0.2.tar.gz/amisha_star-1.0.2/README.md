a0820190539e5d7a2bf9f2f6bc53bf39d7314df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 15:05:41 +0000

    Add package amisha-star/1.0.2/amisha_star-1.0.2.tar.gz

---
+++ packages/amisha-star/amisha_star-1.0.2.tar.gz/amisha_star-1.0.2/README.md
@@ -0,0 +1,7 @@
+A simple Python package for printing a star pattern 
+
+
+You can install the package using pip:
+
+
+pip install .
---
+++ packages/amisha-star/amisha_star-1.0.2.tar.gz/amisha_star-1.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+long_description = ""
+with open("README.md") as fid:
+    long_description = fid.read()
+
+setup(
+    name='amisha-star',
+    version='1.0.2',
+    description='A package for printing various star patterns.',
+    long_description="long_description", 
+    long_description_content_type='text/plain',  
+    author='Amisha',  
+    author_email='amishaagarwal397@gmail.com', 
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+    ],
+    keywords='printing star pattern',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[],
+    extras_require={
+        'dev': ['pytest'],
+    },
+    test_suite='tests',
+)
