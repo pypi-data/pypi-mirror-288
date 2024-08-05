10b684638f2322d0daa04347eea68b191eb91b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 14:04:55 +0000

    Add package imagecompression4/0.1.2/imagecompression4-0.1.2.tar.gz

---
+++ packages/imagecompression4/imagecompression4-0.1.2.tar.gz/imagecompression4-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='imagecompression4',
+    version='0.1.2',
+    packages=find_packages(),
+    description='Image compression using REIS algorithms',
+    author='Katie Wen Ling Kuo',
+    author_email='katie20030705@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+)
