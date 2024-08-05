853383257fab603dbdbd435c5d9387e5e84fd60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-05 04:04:59 +0000

    Add package py-solana-cli/1.2.4/py_solana_cli-1.2.4.tar.gz

---
+++ packages/py-solana-cli/py_solana_cli-1.2.4.tar.gz/py_solana_cli-1.2.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='py_solana_cli',
+      version='1.2.4',
+      description='SolanaCLI on Python',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      packages=['py_solana_cli'],
+      install_requires=[
+        'requests',  'os'
+      ],
+      author_email='hzhz191119111911@gmail.com',
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+      ],
+      zip_safe=False)
+
