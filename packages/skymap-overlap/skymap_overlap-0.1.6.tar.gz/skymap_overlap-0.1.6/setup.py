804731f0262f0f85bc9caf1ae26362b4b2bd26ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:06:07 +0000

    Add package skymap-overlap/0.1.6/skymap_overlap-0.1.6.tar.gz

---
+++ packages/skymap-overlap/skymap_overlap-0.1.6.tar.gz/skymap_overlap-0.1.6/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+from pathlib import Path
+
+# Code from StackOverflow: https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+import re
+VERSIONFILE="skymap_overlap/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+setuptools.setup(
+    name="skymap-overlap",
+    version=verstr,
+    author="Rico Ka Lok Lo",
+    author_email="ka-lok.lo@ligo.org",
+    description="Compute overlap between two skymaps",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    url="https://git.ligo.org/ka-lok.lo/skymap-overlap",
+    packages=[
+        "skymap_overlap",
+    ],
+    entry_points={
+        "console_scripts": [
+            "compute_overlap=skymap_overlap.compute_overlap:main",
+            "download_skymap=skymap_overlap.download_skymap:main",
+            "compute_overlap_from_skymaps_pipe=skymap_overlap.compute_overlap_from_skymaps_pipe:main",
+            "combine_skymaps=skymap_overlap.combine_skymaps:main",
+        ]
+    },
+    install_requires=[
+        "healpy",
+        "ligo.skymap",
+        "pycondor",
+    ],
+    python_requires='>=3.9',
+)
