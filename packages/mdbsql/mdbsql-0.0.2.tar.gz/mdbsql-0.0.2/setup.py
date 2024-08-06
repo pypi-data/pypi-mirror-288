e547370a65e4c3373862068c10292bb07db549b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:49 +0000

    Add package mdbsql/0.0.2/mdbsql-0.0.2.tar.gz

---
+++ packages/mdbsql/mdbsql-0.0.2.tar.gz/mdbsql-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+# import setuptools
+
+packages = ['mdbsql']
+setup(name='mdbsql',
+      version='0.0.2',
+      author='xigua, ',
+      author_email="2587125111@qq.com",
+      long_description='''
+      mongodb/mysql 数据库更新, 移除冗余数据。
+      ''',
+      packages=packages,
+      package_dir={'requests': 'requests'},
+      license="MIT",
+      python_requires='>=3.6',
+      )
