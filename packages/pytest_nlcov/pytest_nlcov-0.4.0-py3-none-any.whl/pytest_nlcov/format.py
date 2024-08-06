3477b896381e122c9ca9558ab486b656eb1df244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 19:05:42 +0000

    Add package pytest_nlcov/0.4.0/pytest_nlcov-0.4.0-py3-none-any.whl

---
+++ packages/pytest_nlcov/pytest_nlcov-0.4.0-py3-none-any.whl/pytest_nlcov/format.py
@@ -0,0 +1,56 @@
+from typing import Iterator, List, Tuple
+
+
+def list_to_ranges(lines: List[int]) -> List[Tuple[int, int]]:
+    """Produce a list of ranges for `format_lines`.
+
+    >>> list_to_ranges([1,2,3,4])
+    [(1, 4)]
+    >>> list_to_ranges([1,4])
+    [(1, 1), (4, 4)]
+    >>> list_to_ranges([1,2,4,5,7])
+    [(1, 2), (4, 5), (7, 7)]
+    >>> list_to_ranges([-2, -1, 0, 1])
+    [(-2, 1)]
+    >>> list_to_ranges([])
+    []
+
+
+    """
+    lines = sorted(lines)
+
+    pairs = []
+    start = -1
+    prev_line = -1
+    for line in lines:
+        if start == -1:
+            start = prev_line = line
+            continue
+
+        if line == prev_line + 1:
+            prev_line = line
+            continue
+
+        pairs.append((start, prev_line))
+        start = prev_line = line
+
+    if start != -1 and prev_line:
+        pairs.append((start, prev_line))
+
+    return pairs
+
+
+def format_ranges(pairs: List[Tuple[int, int]]) -> Iterator[str]:
+    """
+    >>> list(format_ranges([(1,2), (3,3), (4,20)]))
+    ['1-2', '3', '4-20']
+    """
+    for start, end in pairs:
+        if start == end:
+            yield f"{start}"
+        else:
+            yield f"{start}-{end}"
+
+
+def format_lines(lines: List[int]):
+    return ", ".join(format_ranges(list_to_ranges(lines)))
---
+++ packages/pytest_nlcov/pytest_nlcov-0.4.0-py3-none-any.whl/pytest_nlcov/git.py
@@ -0,0 +1,35 @@
+import pathlib
+from io import StringIO
+from typing import Optional
+
+import git
+from unidiff import PatchSet
+
+from pytest_nlcov.data import LinesPerFile
+from pytest_nlcov.data import lines_per_file_factory
+
+
+def get_diff(revision: str, root_dir: str) -> str:
+    return git.Repo(root_dir).git.diff(revision, ignore_blank_lines=True, ignore_space_at_eol=True)
+
+
+def get_new_lines_per_file(revision: str, glob: Optional[str] = None, root_dir: str = ".") -> LinesPerFile:
+    """
+    Get all lines which are new according to git.
+    """
+    uni_diff_text = get_diff(revision, root_dir)
+
+    lines_per_file = lines_per_file_factory()
+
+    for patched_file in PatchSet(StringIO(uni_diff_text)):
+        file_path = pathlib.Path(patched_file.path).resolve()
+        if glob and not file_path.match(glob):
+            continue
+        for hunk in patched_file:
+            for hunk_line in hunk:
+                if hunk_line.target_line_no is None or not hunk_line.is_added:
+                    continue
+                line = lines_per_file[file_path][hunk_line.target_line_no]
+                line.is_empty = hunk_line.value.strip() == ""
+
+    return lines_per_file
