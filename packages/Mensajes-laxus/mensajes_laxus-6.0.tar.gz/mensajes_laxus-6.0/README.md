10e4f88d90d636bca51f1c1ca8ca004278d3e789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:05:16 +0000

    Add package Mensajes-laxus/6.0/mensajes_laxus-6.0.tar.gz

---
+++ packages/Mensajes-laxus/mensajes_laxus-6.0.tar.gz/mensajes_laxus-6.0/README.md
@@ -0,0 +1,5 @@
+# Mensajes
+
+El paquete de mensajes tiene como finalidad explorar pruebas unitarias.
+
+Además del uso de modulos  y paquetes  en una distribución pública.
---
+++ packages/Mensajes-laxus/mensajes_laxus-6.0.tar.gz/mensajes_laxus-6.0/mensajes/despedida/despedirse.py
@@ -0,0 +1,7 @@
+def despedir():
+    print("Bye, l say goodbye from despeirse.espedir()")
+
+
+class Despedida():
+    def __init__(self):
+        print("Im the Despedida's constructor")
