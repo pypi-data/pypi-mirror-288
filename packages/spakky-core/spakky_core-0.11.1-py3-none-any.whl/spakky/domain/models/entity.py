d60d78626f786d47b0195f8fcb7b4d42ebf6ec98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:25 +0000

    Add package spakky-core/0.11.1/spakky_core-0.11.1-py3-none-any.whl

---
+++ packages/spakky-core/spakky_core-0.11.1-py3-none-any.whl/spakky/domain/models/entity.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from typing import Any, Generic
+from dataclasses import field
+
+from spakky.core.interfaces.equatable import EquatableT, IEquatable
+from spakky.core.mutability import mutable
+from spakky.domain.error import SpakkyDomainError
+
+
+class CannotMonkeyPatchEntityError(SpakkyDomainError):
+    message: str = "Cannot monkey patch an entity."
+
+
+@mutable
+class Entity(IEquatable, Generic[EquatableT], ABC):
+    __initialized: bool = field(init=False, repr=False, default=False)
+    uid: EquatableT
+
+    @classmethod
+    @abstractmethod
+    def next_id(cls) -> EquatableT: ...
+
+    def validate(self) -> None:
+        return
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, type(self)):
+            return False
+        return self.uid == other.uid
+
+    def __hash__(self) -> int:
+        return hash(self.uid)
+
+    def __post_init__(self) -> None:
+        self.validate()
+        self.__initialized = True
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name not in self.__dataclass_fields__:
+            raise CannotMonkeyPatchEntityError
+        __old: Any | None = getattr(self, __name, None)
+        super().__setattr__(__name, __value)
+        if self.__initialized:
+            try:
+                self.validate()
+            except:
+                super().__setattr__(__name, __old)
+                raise
