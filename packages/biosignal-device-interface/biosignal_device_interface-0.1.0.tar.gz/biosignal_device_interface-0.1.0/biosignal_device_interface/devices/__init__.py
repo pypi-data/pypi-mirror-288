c818b96aacf4c20d72bb75fecc1629ade317ff23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:35 +0000

    Add package biosignal-device-interface/0.1.0/biosignal_device_interface-0.1.0.tar.gz

---
+++ packages/biosignal-device-interface/biosignal_device_interface-0.1.0.tar.gz/biosignal_device_interface-0.1.0/biosignal_device_interface/devices/__init__.py
@@ -0,0 +1,9 @@
+# Import Devices to be used from biosignal_device_interface.devices
+# import Muovi, MindRoveBracelet, Quattrocento, QuattrocentoLight, ...
+
+# OTB Devices
+from biosignal_device_interface.devices.otb.otb_quattrocento import (
+    OTBQuattrocento,
+    OTBQuattrocentoLight,
+)
+from biosignal_device_interface.devices.otb.otb_muovi import OTBMuovi
