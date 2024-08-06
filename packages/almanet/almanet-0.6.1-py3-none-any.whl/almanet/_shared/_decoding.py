35721589673d9d492ca8ab8739bd177f0b74fd46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 15:05:36 +0000

    Add package almanet/0.6.1/almanet-0.6.1-py3-none-any.whl

---
+++ packages/almanet/almanet-0.6.1-py3-none-any.whl/almanet/_shared/_decoding.py
@@ -0,0 +1,26 @@
+import typing
+
+import pydantic
+import pydantic_core
+
+__all__ = [
+    "serialize",
+    "serialize_json",
+]
+
+
+def serialize[T: typing.Any](
+    annotation: type[T],
+) -> typing.Callable[[typing.Any], T]:
+    model = pydantic.TypeAdapter(annotation)
+    return lambda v: model.validate_python(v)
+
+
+def serialize_json[T: typing.Any](
+    annotation: type[T] | typing.Any = ...,
+) -> typing.Callable[[bytes | str], T]:
+    if annotation is ...:
+        return lambda v: pydantic_core.from_json(v)
+
+    model = pydantic.TypeAdapter(annotation)
+    return lambda v: model.validate_json(v)
