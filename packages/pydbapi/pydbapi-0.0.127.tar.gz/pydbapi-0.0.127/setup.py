2deddf8df61219f5c75e3a7bd6532d259391fe02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:04:55 +0000

    Add package pydbapi/0.0.127/pydbapi-0.0.127.tar.gz

---
+++ packages/pydbapi/pydbapi-0.0.127.tar.gz/pydbapi-0.0.127/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# @Author: longfengpili
+# @Date:   2023-06-02 15:27:41
+# @Last Modified by:   longfengpili
+# @Last Modified time: 2024-08-06 10:54:54
+# @github: https://github.com/longfengpili
+
+
+import setuptools
+
+VERSION = '0.0.127'
+PROJECT_NAME = 'pydbapi'
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requires = f.read()
+
+setuptools.setup(
+    name=PROJECT_NAME,  # Replace with your own username
+    version=VERSION,
+    author="longfengpili",
+    author_email="398745129@qq.com",
+    description="A simple database API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://pypi.org/project/{PROJECT_NAME}/",
+    packages=setuptools.find_packages(exclude=["tests.*", "tests"]),
+    install_requires=requires,
+    entry_points={
+        'ipython.magic': [
+            'load_ipython_extension = pydbapi:load_ipython_extension',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords=["dbapi", "sqlite3", "redshift", 'snowflake', 'doris', 'trino'],
+    python_requires=">=3.9",
+    project_urls={
+        'Documentation': f'https://github.com/longfengpili/{PROJECT_NAME}/blob/master/README.md',
+        'Source': f'https://github.com/longfengpili/{PROJECT_NAME}',
+    },
+)
