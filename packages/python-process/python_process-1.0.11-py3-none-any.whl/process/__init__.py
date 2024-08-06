c8354259bec6949be17991543e5a601ad05427c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:06:02 +0000

    Add package python-process/1.0.11/python_process-1.0.11-py3-none-any.whl

---
+++ packages/python-process/python_process-1.0.11-py3-none-any.whl/process/__init__.py
@@ -0,0 +1,36 @@
+from process.asyncio.process import Process as AsyncProcess
+from process.core.constants import DEVNULL, PIPE, STDOUT
+from process.core.errors import (
+    ProcessAlreadyRunError,
+    ProcessError,
+    ProcessInvalidStreamError,
+    ProcessNotRunError,
+    ProcessTimeoutError,
+)
+from process.core.process import AbstractProcess
+from process.core.protocol import ProcessProtocol
+from process.core.types import Buffer, File, StrOrPath
+from process.process import Process
+from process.types import StreamReader, StreamWriter
+
+__version__ = "1.0.11"
+
+__all__ = [
+    "DEVNULL",
+    "PIPE",
+    "STDOUT",
+    "AbstractProcess",
+    "AsyncProcess",
+    "Buffer",
+    "File",
+    "Process",
+    "ProcessAlreadyRunError",
+    "ProcessError",
+    "ProcessInvalidStreamError",
+    "ProcessNotRunError",
+    "ProcessProtocol",
+    "ProcessTimeoutError",
+    "StreamReader",
+    "StreamWriter",
+    "StrOrPath",
+]
