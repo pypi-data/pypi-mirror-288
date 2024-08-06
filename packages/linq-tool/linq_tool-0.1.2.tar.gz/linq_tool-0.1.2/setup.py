8dc1619bdfc78ba3d84d8e1be18f4927f99d5899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 18:05:52 +0000

    Add package linq-tool/0.1.2/linq_tool-0.1.2.tar.gz

---
+++ packages/linq-tool/linq_tool-0.1.2.tar.gz/linq_tool-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='linq-tool',
+    version='0.1.2',
+    description='A LINQ-like library for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Zozimo Fernandez',
+    author_email='zozi.fer96@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/Zozi96/linq_tool',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
