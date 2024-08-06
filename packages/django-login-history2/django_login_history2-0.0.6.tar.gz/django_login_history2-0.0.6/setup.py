85f3f7acfb575fcbf292ce843b17b715d93671a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:05:36 +0000

    Add package django-login-history2/0.0.6/django_login_history2-0.0.6.tar.gz

---
+++ packages/django-login-history2/django_login_history2-0.0.6.tar.gz/django_login_history2-0.0.6/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_login_history2",
+    version="0.0.6",
+    author="Henrique da Silva Santos",
+    author_email="rique_dev@hotmail.com",
+    description="It's easy to use, plug-in django app that once included, stores logins history (with device data, like IP, user-agent, location etc.) of all users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT License",
+    url="https://github.com/riquedev/django-login-history2",
+    keywords="security, django, login, location, session, ip, authenticate",
+    project_urls={
+        "Bug Tracker": "https://github.com/riquedev/django-login-history2/issues",
+        "Repository": "https://github.com/riquedev/django-login-history2",
+    },
+    install_requires=[
+        'Django>=3.2.13',
+        'django_ipware>=4.0.2',
+        'requests>=2.31.0',
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=setuptools.find_packages(exclude=("tests",'django_login_history', "manage.py")),
+    include_package_data=True,
+    python_requires=">=3.8"
+)
