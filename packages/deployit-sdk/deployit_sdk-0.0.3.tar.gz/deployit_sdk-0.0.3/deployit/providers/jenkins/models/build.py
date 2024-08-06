7929cb593dbc578d4a937734e8c9e0fd3a7ace1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:05:38 +0000

    Add package deployit-sdk/0.0.3/deployit_sdk-0.0.3.tar.gz

---
+++ packages/deployit-sdk/deployit_sdk-0.0.3.tar.gz/deployit_sdk-0.0.3/deployit/providers/jenkins/models/build.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Build(BaseModel):
+    """
+    Data model representing a Jenkins build.
+
+    Attributes
+    ----------
+    id : int
+        The ID of the build.
+    number : int
+        The number of the build.
+    url : str
+        The URL of the build.
+    status : str, optional
+        The status of the build (default is 'pending').
+    result : str, optional
+        The result of the build (default is None).
+    console_log : str, optional
+        The console log of the build (default is an empty string).
+    """
+
+    id: int
+    number: int
+    url: str
+    timestamp: Optional[datetime]
+    status: Optional[str] = "pending"
+    result: Optional[str] = None
+    console_log: Optional[str] = ""
+
+    def update_status(self, status: str) -> None:
+        """
+        Update the status of the build.
+
+        Parameters
+        ----------
+        status : str
+            The new status of the build.
+        """
+        self.status = status
+
+    def set_result(self, result: str) -> None:
+        """
+        Set the result of the build.
+
+        Parameters
+        ----------
+        result : str
+            The result of the build.
+        """
+        self.result = result
---
+++ packages/deployit-sdk/deployit_sdk-0.0.3.tar.gz/deployit_sdk-0.0.3/deployit/providers/jenkins/repositories/job.py
@@ -0,0 +1,106 @@
+from deployit.providers.jenkins.models.build import Build
+from deployit.providers.jenkins.models.job import Job
+from deployit.providers.jenkins.presentation.rich import RichPresenter
+from deployit.providers.jenkins.services.job import JenkinsJobApiService
+from deployit.providers.jenkins.utils.errors import JenkinsAPIError
+
+
+class JobRepository:
+    """
+    Repository for interacting with Jenkins job data.
+    """
+
+    def __init__(
+        self,
+        api_service: JenkinsJobApiService,
+        presenter: RichPresenter = RichPresenter(),
+    ):
+        """
+        Initialize the JobRepository with a Jenkins API client.
+
+        Parameters
+        ----------
+        api_service : JenkinsJobApiService
+            The Jenkins API service
+        """
+        self.api_service = api_service
+        self.presenter = presenter
+
+    def fetch_job_details(self, job: Job) -> Job:
+        """
+        Fetch details for a given job and update the job instance.
+
+        Parameters
+        ----------
+        job : Job
+            The job for which to fetch details.
+
+        Returns
+        -------
+        Job
+            The updated job instance.
+        """
+        self.presenter.info(
+            f"Fetching details for job '{job.name}' with URL '{job.url}'"
+        )
+        try:
+            job_info = self.api_service.get_job_info(
+                folder_url=job.url, short_name=job.name
+            )
+            self.presenter.debug(f"Job info received: {job_info}")
+
+            job.name = job_info.get("name", job.name)
+            job.url = job_info.get("url", job.url)
+            job.color = job_info.get("color")
+            job.subjobs = [
+                Job(name=subjob["name"], url=subjob["url"])
+                for subjob in job_info.get("jobs", [])
+            ]
+            job.build_history = self.fetch_all_builds(job)
+            job.requires_parameters = bool(
+                job_info.get("actions", [{}])[0].get("parameterDefinitions", [])
+            )
+            job.required_parameters = [
+                param["name"]
+                for param in job_info["actions"][0]["parameterDefinitions"]
+                if param.get("defaultParameterValue") is None
+            ]
+
+            self.presenter.info(f"Successfully fetched details for job '{job.name}'")
+        except Exception as e:
+            raise JenkinsAPIError(f"Error fetching job details: {e}") from e
+        return job
+
+    def fetch_all_builds(self, job: Job) -> list:
+        """
+        Fetch all builds for a given job.
+
+        Parameters
+        ----------
+        job : Job
+            The job for which to fetch builds.
+
+        Returns
+        -------
+        list
+            A list of builds associated with the job.
+        """
+        self.presenter.info(
+            f"Fetching all builds for job '{job.name}' with URL '{job.url}'"
+        )
+        try:
+            builds_data = self.api_service.get_all_builds(
+                folder_url=job.url, short_name=job.name, tree="allBuilds[number,url]"
+            )
+            self.presenter.debug(f"Builds data received: {builds_data}")
+
+            builds = [
+                Build(id=build["number"], number=build["number"], url=build["url"])
+                for build in builds_data.get("allBuilds", [])
+            ]
+            self.presenter.info(f"Fetched {len(builds)} builds for job '{job.name}'")
+            return builds
+        except Exception as e:
+            raise JenkinsAPIError(
+                f"Error fetching builds for job '{job.name}': {e}"
+            ) from e
