f5770fe40642d71765b9233e6e27163adbbc9765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:49 +0000

    Add package image-to-psd/1.0.5/image_to_psd-1.0.5.tar.gz

---
+++ packages/image-to-psd/image_to_psd-1.0.5.tar.gz/image_to_psd-1.0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='image_to_psd',
+    version='1.0.5',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'colorthief',
+        'scikit-learn',
+        'Pillow',
+        'colormath',
+        'wand',
+        'colorama',
+    ],
+    tests_require=[
+        'unittest',
+    ],
+    entry_points={
+        'console_scripts': [
+            # Define any command-line scripts here
+        ],
+    },
+    author='Yatharth Doshi',
+    author_email='',
+    description='A library for processing images and saving layers as PSD',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yatharth-doshi/PSD_Generator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
