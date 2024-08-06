1c480fb44431a8194c4884016f6f12426f42062e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 18:05:40 +0000

    Add package formant/1.181.0/formant-1.181.0-py2.py3-none-any.whl

---
+++ packages/formant/formant-1.181.0-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationTemplate, ScopeFilterTypesItem, AnnotationField, AnnotationFieldValuesRequestTags, AnnotationType, AnnotationMetadata, AnnotationFieldValuesResponse, ScopeFilter, AnnotationStreamType, UserParametersRolesItem, Annotation, TagParameters, AnnotationTemplateTags, FocusedDatapoint, UserParameters, AnnotationFieldValueTags, SheetParameters, AnnotationFieldType, AnnotationFieldValuesRequest, AnnotationFieldValue, AnnotationTemplateListResponse, AnnotationTags
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.181.0-py2.py3-none-any.whl/formant/sdk/cloud/v2/src/resources/views/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import ImageViewConfigurationMode, BitsetViewConfiguration, Filter, LocationViewConfiguration, TransformTreeViewConfiguration, PartialViewLayoutType, PointCloudViewConfiguration, LocationViewport, View, ImageViewConfiguration, PartialViewTags, ViewConfigurationType, PartialView, ViewLayoutType, ViewListResponse, ViewTags, Vector3, LocalizationViewConfiguration, Location, Camera, ViewConfiguration, LocationViewConfigurationBasemap, NumericViewConfiguration
+from formant.sdk.cloud.v2.formant_admin_api_client.api.view import view_controller_get_one, view_controller_get_all, view_controller_patch
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
