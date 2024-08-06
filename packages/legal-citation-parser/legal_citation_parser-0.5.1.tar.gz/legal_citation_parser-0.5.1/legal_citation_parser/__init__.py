c2fcdd4c6e45b05dc523674ef76898a96713200c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:23 +0000

    Add package legal-citation-parser/0.5.1/legal_citation_parser-0.5.1.tar.gz

---
+++ packages/legal-citation-parser/legal_citation_parser-0.5.1.tar.gz/legal_citation_parser-0.5.1/legal_citation_parser/__init__.py
@@ -0,0 +1,12 @@
+"""
+__init__.py file
+"""
+
+from .citation_parser import parse_citation, create_citation
+
+__all__ = ['parse_citation', 'create_citation']
+__version__ = '0.5.0'
+__author__ = 'Daniel Nathan Booy'
+__url__ = ''
+__description__ = 'Extracts metadata from CanLII case citations'
+__license__ = 'GPL-3.0'
