887c32374b533de9a18d48b1ec68602e9f37efef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:05:34 +0000

    Add package mtnlog/1.1.0/mtnlog-1.1.0.tar.gz

---
+++ packages/mtnlog/mtnlog-1.1.0.tar.gz/mtnlog-1.1.0/src/mtnlog/__init__.py
@@ -0,0 +1,11 @@
+from .json import *
+from .performance import *
+
+__version__ = '1.0'
+
+__doc__ = """Performance logger for tracking resource usage."""
+
+__all__ = [
+    'JSONLogger',
+    'PerformanceLogger',
+]
