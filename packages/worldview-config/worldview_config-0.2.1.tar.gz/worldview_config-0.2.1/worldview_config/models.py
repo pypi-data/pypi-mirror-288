f794fd8321474fca0099e5971c0f220a84990420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:53 +0000

    Add package worldview-config/0.2.1/worldview_config-0.2.1.tar.gz

---
+++ packages/worldview-config/worldview_config-0.2.1.tar.gz/worldview_config-0.2.1/worldview_config/models.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field, RootModel
+
+class Projection(BaseModel):
+    source: str
+    matrixSet: str
+
+class LayerConfig(BaseModel):
+    id: str
+    satellite: str
+    instrument: str
+    measurement: str
+    discipline: str
+    title: str
+    format: str
+    colormap_id: str
+    projections: dict[str, Projection]
+    start_date: datetime
+    group: str = Field(default="overlays")
+    description: str = Field(default="")
+    tags: list[str] = Field(default=[])
+    period: str | None = Field(default="daily")
+    end_date: datetime | None = Field(default=None)
+    temporal_start: datetime | None = Field(default=None)
+    temporal_end: datetime | None = Field(default=None)
+    date_interval: int | None = Field(default=None)
+
+
+class LayerConfigs(RootModel):
+    root: list[LayerConfig]
