a4c1622db6abe09808c9c5b30e479a70413cd631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:06:09 +0000

    Add package acquire-imaging/0.3.1/acquire_imaging-0.3.1-cp38-abi3-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl

---
+++ packages/acquire-imaging/acquire_imaging-0.3.1-cp38-abi3-macosx_10_12_x86_64.macosx_11_0_arm64.macosx_10_12_universal2.whl/acquire/__init__.pyi
@@ -0,0 +1,22 @@
+from typing import List, Optional, Union
+
+import napari  # type: ignore
+
+from .acquire import *
+
+__version__: str
+
+def setup(
+    runtime: Runtime,
+    camera: Union[str, List[str]] = ...,
+    storage: Union[str, List[str]] = ...,
+    output_filename: Optional[str] = ...,
+) -> Properties: ...
+def setup_one_streams(runtime: Runtime, frame_count: int) -> Properties: ...
+def setup_two_streams(runtime: Runtime, frame_count: int) -> Properties: ...
+
+g_runtime: Optional[Runtime]
+
+def gui(
+    viewer: "napari.Viewer", frame_count: int = ..., stream_count: int = ...
+) -> None: ...
