8db85523c9c3189909ec2a034fb708b6cf2a2423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:28 +0000

    Add package learnstdio-toml/0.0.2rc15.post1/learnstdio_toml-0.0.2rc15.post1-py3-none-any.whl

---
+++ packages/learnstdio-toml/learnstdio_toml-0.0.2rc15.post1-py3-none-any.whl/learnstdio/__init__.py
@@ -0,0 +1,8 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Learnstdio Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Learnstdio for Python package"""
+from __future__ import annotations
+
+__version__ = "0.0.2-rc15-post1"
