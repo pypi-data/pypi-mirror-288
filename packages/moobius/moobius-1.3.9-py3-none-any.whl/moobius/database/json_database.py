52f36efb77cb8d9f6da7ea9da12a88dc972bee36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 16:05:33 +0000

    Add package moobius/1.3.9/moobius-1.3.9-py3-none-any.whl

---
+++ packages/moobius/moobius-1.3.9-py3-none-any.whl/moobius/database/json_database.py
@@ -0,0 +1,110 @@
+# A simple-to-use database that dumps all it's data into nested JSON files.
+
+import json, dataclasses
+import os
+from pydoc import locate
+
+from dacite import from_dict
+from loguru import logger
+
+from moobius.utils import EnhancedJSONEncoder
+from moobius import types
+from .database_interface import DatabaseInterface
+
+DTYPE = '_type' # Used to indicate the class name.
+
+class JSONDatabase(DatabaseInterface):
+    """
+    JSONDatabase simply stores information as JSON strings in a list of files.
+    Each domain, key combination is stored as one file.
+    Dataclass objects can be seralized as wll.
+    """
+    def __init__(self, domain='', root_dir='', **kwargs):
+        """
+        Initialize a JSONDatabase object.
+
+        Parameters:
+          domain: str
+            The name of the database directory. Will be automatically added in the add_container() function in MoobiusStorage.
+          root_dir: str
+            The root directory of the all the database files.
+
+        Example:
+          Note: This should not be called directly. Users should config the database in the config file, and call MoobiusStorage to initialize the database.
+          >>> database = JSONDatabase(domain='service_1.channel_1', root_dir='data')
+        """
+
+        super().__init__()
+
+        self.path = os.path.join(root_dir, domain.replace('.', os.sep))
+        os.makedirs(self.path, exist_ok=True)
+
+    def get_value(self, key):
+        """
+        Gets the value (which is a dict) given a string-valued key.
+        Note: This "key" is different from a key to look up a CachedDict file.
+        Note: This function should not be called directly.
+
+        Raises:
+          TypeError: If the type of the value is unknown, so we can't construct the object.
+
+        Returns: is_sucessful, the_value
+        """
+        filename = os.path.join(self.path, key + '.json')
+
+        if not os.path.exists(filename):
+            return False, f'No json file found for {key}.'
+        with open(filename, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+            if data[DTYPE] == 'NoneType':
+                return True, None   # You can't use NoneType(None) to construct a NoneType object, so we have to return None directly
+            else:
+                class_name_fullpath = data[DTYPE]
+                data_type = locate(class_name_fullpath)
+
+                if not data_type: # Legacy backwards compat. Remove this if block after enough time has passed.
+                    data_type = locate(f'{types.__name__}.{class_name_fullpath}')
+                    if data_type:
+                        logger.warning('Legacy compat JSONDatabase type system. This warning should disappear on save+load of the database.')
+
+                if data_type:
+                    if dataclasses.is_dataclass(data_type):
+                        return True, from_dict(data_class=data_type, data=data[key])
+                    else:
+                        return True, data_type(data[key])
+                else:
+                    raise TypeError(f'Unknown type: {class_name_fullpath}')
+
+    def set_value(self, key, value):
+        """Updates and saves a cached dict, given a string-valued key and a dict-valued value. Returns (is_success, the key).
+           Note: This function should not be called directly."""
+        filename = os.path.join(self.path, key + '.json')
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            data = {key: value, DTYPE: type(value).__module__+'.'+type(value).__name__} # Fullpath is package.module.name.
+            json.dump(data, f, indent=4, cls=EnhancedJSONEncoder, ensure_ascii=False)
+            return True, key
+
+    def delete_key(self, key):
+        """Deletes a cached dict given a key. Returns (True, the key)
+           Note: This function should not be called directly."""
+        filename = os.path.join(self.path, key + '.json')
+        os.remove(filename)
+        return True, key
+
+    def all_keys(self):
+        """Gets all the cached dicts in the database. Returns the dicts as an iterable which internally uses yield()."""
+        def key_iterator():
+            for filename in os.listdir(self.path):
+                if filename.endswith('.json'):
+                    yield filename[:-5]
+                else:
+                    continue
+        return key_iterator()
+
+    def __str__(self):
+        the_path = os.path.realpath(self.path).replace('\\','/')
+        return f'moobius.JSONDatabase(path={the_path})'
+    def __repr__(self):
+        return self.__str__()
---
+++ packages/moobius/moobius-1.3.9-py3-none-any.whl/moobius/utils.py
@@ -0,0 +1,197 @@
+# MISC functions that don't belong anywhere else.
+import sys, os, re, json, threading, asyncio, dataclasses
+from moobius import types
+from loguru import logger
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    """A better Json Encoder which has automatic conversion of dataclasses to dicts."""
+    def default(self, o):
+        """Given an object to encode, returns the encoding as a string."""
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        else:
+            return super().default(o)
+
+    def __str__(self):
+        return f'moobius.EnhancedJSONEncoder()'
+    def __repr__(self):
+        return self.__str__()
+
+
+def summarize_html(html_str):
+    """
+    Creates a summary given an html_string.
+    Converts HTML to an easier-for-a-human format by cutting out some of the more common tags. Far from perfect.
+    Returns the summary as a string.
+    """
+    rs = [r'<div>\d+<\/div *>', r'<div class *= *"[a-zA-Z0-9]*">', r'<span class *= *"[a-zA-Z0-9]*">']
+    for tag in ['div', 'li', 'head', 'body', 'pre', 'span']:
+        rs.append(f'<{tag} *>')
+        rs.append(f'</{tag} *>')
+    for r in rs:
+        html_str = re.sub(r, "", html_str)
+    html_str = html_str.replace('\r\n','\n').replace('\t','  ')
+    while '  ' in html_str or '\n\n' in html_str or '\n ' in html_str:
+        html_str = html_str.replace('\n\n\n\n\n\n\n\n\n','\n').replace('\n\n','\n').replace('         ',' ').replace('  ',' ').replace('\n ','\n')
+    return html_str.strip()
+
+
+def make_fn_async(f):
+    """Converts functions to async functions.
+    Can be used as "await (make_fun_asycnc(f)(arg1, arg2, etc)).
+    Given a function; returns an async version of the function."""
+    _ind = [False]
+    _result = [None]
+    async def run_f(*args, **kwargs):
+        def f1():
+            _result[0] = f(*args, **kwargs)
+            _ind[0] = True
+        t=threading.Thread(target=f1)
+        t.start()
+        while not _ind[0]:
+            await asyncio.sleep(0.01)
+        return _result[0]
+    return run_f
+
+
+def maybe_make_template_files(args):
+    """
+    Makes template files if there is a need to do so, based on args and sys.argv.
+    Called by wand.run() before initializing the Moobius class if it doesn't have any templates.
+
+    Which files are created:
+      A template main.py python file which calls Wand.run:
+        Only created if the file does not exist AND "make_main main.py" (or "make_main foo.py", etc) is in the system args.
+      A sample config.py:
+        Only created if "config_path" is in args (or system args) AND the file does not exist.
+        This requires user information:
+          email: If no system arg "email my@email.com" or "username my@email.com" is specified, prompts for one with input().
+          password: If no system arg "password my_sec**t_pword", prompts for one.
+          channels: If no system arg "channels abc... def..." to specify one or more channels, prompts for one or more.
+        Note: if the user gives an empty response to input(), a nonfunctional default is used, which can be filled in later.
+
+    Unittests to run in a python prompt in an empty folder:
+      >>> # Make a main.py file:
+      >>> import sys; sys.argv = '_ make_main main.py'.split(' '); import moobius;
+      >>> # Prompt the user for credentials and put these in the service.json (NOTE: will generate an error b/c None class):
+      >>> import sys; from moobius import MoobiusWand; MoobiusWand().run(None, config_path="config/service.json")
+      >>> # Provide credentials, making a service.json with no user input (NOTE: will generate an error b/c None class):
+      >>> import sys; sys.argv = '_ email abc@123.com password IAmSecret channels abc-123 def-4561111111111111111111'.split(' '); from moobius import MoobiusWand; MoobiusWand().run(0, config_path="config/service.json")
+      >>> # Provide agent credentials. There is no need to provide a channel id (NOTE: will generate an error b/c None class).
+      >>> import sys; sys.argv = '_ email abc@123.com password IAmSecret'.split(' '); from moobius import MoobiusWand; MoobiusWand().run(0, config_path="config/agent.json", is_agent=True)
+
+    Parameters:
+      args: The list of args.
+
+    Returns:
+      None
+    """
+
+    main_py = '''
+from service import MyService as TheService # TODO: replace "from service import MyService" with the actual path to your class that inherits Moobius.
+from moobius import MoobiusWand
+if __name__ == "__main__":
+    wand = MoobiusWand()
+
+    handle = wand.run(
+        TheService,
+        log_file="logs/service.log",
+        error_log_file="logs/error.log",
+        terminal_log_level="INFO",
+        config_path="config/service.json", # Default service. Necessary to avoid hard-coding credentials.
+        db_config_path="config/db.json", # Commonly used, but not strictly necessary.
+        is_agent=False, # Make this True to run an Agent instead (agents also need thier own config_path).
+        background=True)
+'''
+
+    template_service = {
+        "http_server_uri": "https://api.moobius.net/",
+        "ws_server_uri": "wss://ws.moobius.net/",
+        "email": "<email>",
+        "password": "<password>",
+        "service_id": "", # This is filled automatically.
+        "channels": ["<Channel id 0>", "<Optional Channel id 1>", "..."],
+        "others": "include"}
+
+    template_agent = {
+        "http_server_uri": "https://api.moobius.net/",
+        "ws_server_uri": "wss://ws.moobius.net/",
+        "email": "<email>",
+        "password": "<password>"}
+
+    is_agent = args.get('is_agent')
+    template = template_agent if is_agent else template_service
+
+    # Parse system args:
+    args = args.copy()
+    for i in range(len(sys.argv)):
+        l = sys.argv[i].lower().replace('-','_')
+        if l in ['email', 'user', 'username']:
+            args['email'] = sys.argv[i+1]
+        if l in ['password', 'passphrase']:
+            args['password'] = sys.argv[i+1]
+        if l in ['config_file', 'config_path']:
+            args['config_path'] = sys.argv[i+1]
+        if l == 'channels':
+            args['channels'] = []
+            for j in range(i+1, len(sys.argv)):
+                aj = sys.argv[j]
+                if j==i+1 or ('-' in aj and set(aj).issubset('0123456789abcdef-') and len(aj)>24):
+                    args['channels'].append(aj)
+        if l in ['make_main', 'makemain']:
+            args['make_main'] = sys.argv[i+1]
+
+    if 'make_main' in args:
+        fname = os.path.realpath(args['make_main']).replace('\\','/')
+        if not fname.lower().endswith('.py'):
+            fname = fname+'.py'
+        if not os.path.exists(fname):
+            logger.info(f'Creating template main file: {fname}')
+            os.makedirs(os.path.split(fname)[0], exist_ok=True)
+            with open(fname,'w', encoding='utf-8') as f:
+                f.write(main_py)
+    if 'config_path' in args:
+        fname = os.path.realpath(args['config_path']).replace('\\','/')
+        if not fname.lower().endswith('.json'):
+            fname = fname+'.json'
+        if not os.path.exists(fname):
+            logger.info(f'Creating config file: {fname}')
+            for field in ['email', 'password'] + ([] if is_agent else ['channels']):
+                if field not in args:
+                    args[field] = input(f'Input {field} for creating a config file:').strip()
+                    if not args[field]:
+                        logger.info('No input {field} supplied, a default will be used.')
+                        args[field] = f'<{field}>'
+                    if field=='channels': # Allow inputing multible channels.
+                        args['channels'] = list(args['channels'].split(' '))
+                template[field] = args[field]
+
+            os.makedirs(os.path.split(fname)[0], exist_ok=True)
+            with open(fname,'w', encoding='utf-8') as f:
+                f.write(json.dumps(template, indent=4, ensure_ascii=False))
+
+
+def to_char_id_list(c):
+    """
+    Converts the input to a list of character_ids, designed to accept a wide range of inputs.
+    Parameters:
+      c: This can be one of many things:
+        A Character (returns it's id as one-element list).
+        A string (assumes it's an id wraps it into a one element list).
+        A list of Characters (extracts the ids).
+        A list of strings (returns a copy of the list).
+        A mixed character and string list.
+
+    Returns the list of character ids.
+    """
+    if type(c) is str or type(c) is types.Character:
+        c = [c]
+    c = [ch.character_id if type(ch) is types.Character else ch for ch in c] # Convert Character objects to IDs.
+    return c
+
+
+def set_terminal_logger_level(the_level):
+    """Sets the logger from the terminal (but preserves other files) given the level. Returns None."""
+    logger.remove() # Remove the default one.
+    logger.add(sys.stdout, level=the_level)
