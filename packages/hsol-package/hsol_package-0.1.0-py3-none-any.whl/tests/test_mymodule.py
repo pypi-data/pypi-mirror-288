6ee710434cc783909b7e64d8d83166d9ec38acc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:17 +0000

    Add package hsol-package/0.1.0/hsol_package-0.1.0-py3-none-any.whl

---
+++ packages/hsol-package/hsol_package-0.1.0-py3-none-any.whl/tests/test_mymodule.py
@@ -0,0 +1,7 @@
+import unittest
+from mypackage.mymodule import hello_world
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, World!")
+if __name__=="__main__":
+    unittest.main()
