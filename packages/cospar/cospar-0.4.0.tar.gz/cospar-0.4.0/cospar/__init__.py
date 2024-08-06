37778e1fcf9e5f9d78e03c365c9a068281ee0e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:59 +0000

    Add package cospar/0.4.0/cospar-0.4.0.tar.gz

---
+++ packages/cospar/cospar-0.4.0.tar.gz/cospar-0.4.0/cospar/__init__.py
@@ -0,0 +1,4 @@
+"""CoSpar - dynamic inference by integrating transcriptome and lineage information"""
+
+__version__ = "0.3.4"
+from . import datasets, hf, logging, pl, pp, settings, simulate, tl, tmap
