fa06aa24781c808b10cb92b726fefde7ae036214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:04:57 +0000

    Add package CTkMVC/0.0.3/ctkmvc-0.0.3.tar.gz

---
+++ packages/CTkMVC/ctkmvc-0.0.3.tar.gz/ctkmvc-0.0.3/src/ctkmvc/view.py
@@ -0,0 +1,23 @@
+from customtkinter import CTk
+from typing import TYPE_CHECKING
+from abc import ABC
+
+from .interface import ModelObserver
+from .model import ObservableModel
+
+if TYPE_CHECKING:
+    from .controller import Controller
+
+
+class View(CTk, ModelObserver):
+
+    '''Base class for views'''
+
+    def __init__(self, controller: 'Controller', model: ObservableModel):
+
+        super().__init__()
+
+        self._controller = controller
+        self._model = model
+
+        self._model.add_observer(self)
