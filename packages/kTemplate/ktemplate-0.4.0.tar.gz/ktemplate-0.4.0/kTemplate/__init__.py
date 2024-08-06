723a4ff277835d8153ae7f8eb6272e68fd12b4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:05:52 +0000

    Add package kTemplate/0.4.0/ktemplate-0.4.0.tar.gz

---
+++ packages/kTemplate/ktemplate-0.4.0.tar.gz/ktemplate-0.4.0/kTemplate/__init__.py
@@ -0,0 +1,10 @@
+"""kTempale - a minimalist python html template lib
+
+Modules exported by this package:
+
+- `main`: element generation methods.
+- `element`: pre generated common tagged elements, eg. a, em, br, div, img, span ... etc
+"""
+
+from .main import *
+from .elements import *
