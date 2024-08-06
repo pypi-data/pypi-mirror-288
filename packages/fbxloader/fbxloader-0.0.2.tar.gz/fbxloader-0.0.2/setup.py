4f50120b0e0bf334053c72a5bebd763c2cc46727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:30 +0000

    Add package fbxloader/0.0.2/fbxloader-0.0.2.tar.gz

---
+++ packages/fbxloader/fbxloader-0.0.2.tar.gz/fbxloader-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="fbxloader",
+        version="0.0.2",
+        description="utility to read fbx file",
+        long_description=open("readme.md", encoding="utf-8").read(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/ashawkey/fbxloader",
+        author="kiui",
+        author_email="ashawkey1999@gmail.com",
+        packages=find_packages(),
+        entry_points={
+            # CLI tools
+            'console_scripts': [
+                'fbxconverter = fbxloader.converter:main',
+            ],
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3 ",
+        ],
+        keywords="fbx",
+        install_requires=[
+            "numpy",
+            "scipy",
+            "trimesh",
+        ],
+    )
