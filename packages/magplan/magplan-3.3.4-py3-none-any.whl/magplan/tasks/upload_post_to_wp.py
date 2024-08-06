1ba5eb1b8fd04b5815377d1676049df282bc190c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:51 +0000

    Add package magplan/3.3.4/magplan-3.3.4-py3-none-any.whl

---
+++ packages/magplan/magplan-3.3.4-py3-none-any.whl/magplan/tasks/upload_post_to_wp.py
@@ -0,0 +1,24 @@
+import logging
+
+from celery import shared_task
+
+from magplan.models import Post
+from pymysql.err import OperationalError
+
+
+logger = logging.getLogger()
+
+
+@shared_task(
+    autoretry_for=(OperationalError,),
+    retry_kwargs={'max_retries': 5, 'countdown': 10},  # Retry up to 3 times with a 60 seconds delay between retries
+    retry_backoff=False  # Optional: exponential backoff
+)
+def upload_post_to_wp(self, post_id: int) -> None:
+    logger.info("Starting task upload_post_to_wp...")
+
+    post = Post.objects.filter(id=post_id).first()
+    if post is None:
+        return
+
+    post.upload()
