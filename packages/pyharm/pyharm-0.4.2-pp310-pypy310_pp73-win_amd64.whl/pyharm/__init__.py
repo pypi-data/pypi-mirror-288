b8ba868933c7e90bcfeaab7529155e968177a48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:06:22 +0000

    Add package pyharm/0.4.2/pyharm-0.4.2-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/pyharm/pyharm-0.4.2-pp310-pypy310_pp73-win_amd64.whl/pyharm/__init__.py
@@ -0,0 +1,40 @@
+"""
+PyHarm is a Python wrapper for CHarm.
+"""
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_7_2():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pyharm.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_7_2()
+del _delvewheel_patch_1_7_2
+# end delvewheel patch
+
+import os as _os
+from ._lib import _load_lib
+
+# Name of the shared CHarm library to load
+_libcharmname = 'libcharm'
+
+# Directory of "_libcharmname"
+_libcharmdir = _os.path.join(_os.path.dirname(__file__), '')
+
+# Load the shared CHarm library
+_libcharm = _load_lib(_libcharmdir, _libcharmname)
+
+# Prefix to be added to the CHarm function names.  Depends on the format of
+# floating point numbers used to compile CHarm (single or double precision).
+_CHARM = 'charm_'
+
+# Prefix to be added to the PyHarm functions when calling "__repr__" methods
+_pyharm = 'pyharm'
+
+# The "err" module is intentionally not imported, as users do not interact with
+# it in PyHarm.
+from . import crd, glob, integ, leg, misc, sha, shc, shs
+__all__ = ['crd', 'glob', 'integ', 'leg', 'misc', 'sha', 'shc', 'shs']
