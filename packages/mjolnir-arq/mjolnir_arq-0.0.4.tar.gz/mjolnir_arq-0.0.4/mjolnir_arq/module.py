40e70232e903af85c0a96c472e67b3ffcf2feff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 16:05:26 +0000

    Add package mjolnir-arq/0.0.4/mjolnir_arq-0.0.4.tar.gz

---
+++ packages/mjolnir-arq/mjolnir_arq-0.0.4.tar.gz/mjolnir_arq-0.0.4/mjolnir_arq/module.py
@@ -0,0 +1,12 @@
+
+from controller.mjolnir_controller import MjolnirController
+
+mjolnirController = MjolnirController()
+
+
+def main():
+    mjolnirController.menu()
+
+
+if __name__ == "__main__":
+    main()
