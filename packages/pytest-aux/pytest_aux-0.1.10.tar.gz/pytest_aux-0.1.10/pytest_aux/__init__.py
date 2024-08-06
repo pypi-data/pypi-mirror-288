988a3d83e37af6f9cadbbbc6da849f2eaa759b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:05:29 +0000

    Add package pytest-aux/0.1.10/pytest_aux-0.1.10.tar.gz

---
+++ packages/pytest-aux/pytest_aux-0.1.10.tar.gz/pytest_aux-0.1.10/pytest_aux/__init__.py
@@ -0,0 +1,94 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#     # AUX
+#     # TYPES
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .pytest_tester import (
+    # BASE
+    pytest_func_tester,
+    pytest_func_tester__no_kwargs,
+    pytest_func_tester__no_args,
+    pytest_func_tester__no_args_kwargs,
+    # AUX
+    # TYPES
+    # EXX
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .primitives import (
+    # BASE
+    GEN_COMPR,
+
+    FUNC,
+    FUNC_NONE,
+    FUNC_TRUE,
+    FUNC_FALSE,
+    FUNC_ALL,
+    FUNC_ANY,
+    FUNC_LIST_DIRECT,
+    FUNC_LIST_VALUES,
+    FUNC_DICT,
+    FUNC_EXX,
+    FUNC_GEN,
+
+    LAMBDA,
+    LAMBDA_NONE,
+    LAMBDA_TRUE,
+    LAMBDA_FALSE,
+    LAMBDA_ALL,
+    LAMBDA_ANY,
+    LAMBDA_LIST_DIRECT,
+    LAMBDA_LIST_VALUES,
+    LAMBDA_DICT,
+    LAMBDA_EXX,
+    LAMBDA_GEN,
+
+    Exx,            INST_EXX,
+
+    ClsInt,
+    ClsFloat,
+    ClsStr,
+    ClsList,
+    ClsSet,
+    ClsDict,
+
+    Cls,            INST,
+    ClsEmpty,       INST_EMPTY,
+
+    ClsInitArgsKwargs,
+    ClsInitExx,
+
+    ClsCall,        INST_CALL,
+    ClsCallNone,    INST_CALL_NONE,
+    ClsCallTrue,    INST_CALL_TRUE,
+    ClsCallFalse,   INST_CALL_FALSE,
+    ClsCallExx,     INST_CALL_EXX,
+
+    ClsBoolTrue,    INST_BOOL_TRUE,
+    ClsBoolFalse,   INST_BOOL_FALSE,
+    ClsBoolExx,     INST_BOOL_EXX,
+
+    ClsIterYield,   INST_ITER_YIELD,
+    ClsGen,         INST_GEN,
+
+    ClsEq,          INST_EQ,
+    ClsEqExx,       INST_EQ_EXX,
+
+    ClsFullTypes,   INST_FULL_TYPES,
+    # AUX
+    # TYPES
+    # EXX
+)
+
+
+# =====================================================================================================================
