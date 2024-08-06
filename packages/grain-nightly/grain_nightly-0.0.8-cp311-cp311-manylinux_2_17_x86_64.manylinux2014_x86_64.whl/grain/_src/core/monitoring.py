786676e244cd0c4f033b9c3b3e4b5e38bb8fcd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 16:05:13 +0000

    Add package grain-nightly/0.0.8/grain_nightly-0.0.8-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/grain-nightly/grain_nightly-0.0.8-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/grain/_src/core/monitoring.py
@@ -0,0 +1,45 @@
+"""Grain metrics."""
+
+import enum
+
+
+@enum.unique
+class Units(enum.Enum):
+  """Grain metric units."""
+
+  NANOSECONDS = enum.auto()
+
+
+class NoOpMetric:
+  """Grain metric no-op implementation."""
+
+  def __init__(self, *args, **kwargs):
+    del args, kwargs
+
+  def IncrementBy(self, *args, **kwargs):
+    del args, kwargs
+
+  def Increment(self, *args, **kwargs):
+    self.IncrementBy(1, *args, **kwargs)
+
+  def Set(self, *args, **kwargs):
+    del args, kwargs
+
+  def Record(self, *args, **kwargs):
+    del args, kwargs
+
+  def Get(self, *args, **kwargs):
+    del args, kwargs
+
+
+class Metadata:
+  """Grain metric no-op metadata."""
+
+  def __init__(self, *args, **kwargs):
+    del args, kwargs
+
+
+Counter = Metric = EventMetric = NoOpMetric
+
+def get_monitoring_root() -> None:
+  return None
