f7b7b27e6b3e0edf11fbca0e554a1d20813a9895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:12 +0000

    Add package sainsc/0.1.0/sainsc-0.1.0.tar.gz

---
+++ packages/sainsc/sainsc-0.1.0.tar.gz/sainsc-0.1.0/sainsc/io/_io_utils.py
@@ -0,0 +1,29 @@
+import gzip
+from pathlib import Path
+from typing import Literal
+
+import numpy as np
+from numpy.typing import NDArray
+
+from .._typealias import _PathLike
+from .._utils_rust import categorical_coordinate
+
+
+def _categorical_coordinate(
+    x: NDArray[np.int32], y: NDArray[np.int32], *, n_threads: int = 1
+) -> tuple[NDArray[np.int32], NDArray[np.int32]]:
+    assert len(x) == len(y)
+
+    return categorical_coordinate(x, y, n_threads=n_threads)
+
+
+# currently no need to support all file modes
+_File_Mode = Literal["r", "w", "rb", "wb"]
+
+
+def _open_file(file: _PathLike, mode: _File_Mode = "r"):
+    file = Path(file)
+    if file.suffix == ".gz":
+        return gzip.open(file, mode)
+    else:
+        return open(file, mode)
