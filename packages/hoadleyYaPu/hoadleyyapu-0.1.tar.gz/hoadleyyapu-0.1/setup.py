4383c96f97ac18934a74b179d022977a8b513b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:06:07 +0000

    Add package hoadleyYaPu/0.1/hoadleyyapu-0.1.tar.gz

---
+++ packages/hoadleyYaPu/hoadleyyapu-0.1.tar.gz/hoadleyyapu-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hoadleyYaPu',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    author='Yash Purohit',
+    author_email='yashpurohit1234@gmail.com',
+    description='A package for Black-Scholes option pricing and implied volatility calculation(European Options only)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/iam7pY/hoadleyYaPu',  # Replace with your actual URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
