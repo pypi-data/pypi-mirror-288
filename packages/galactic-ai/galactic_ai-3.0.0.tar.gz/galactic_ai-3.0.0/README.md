6d4f4b23a5b63cc83fc127bbc035e53604c81d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:05:24 +0000

    Add package galactic-ai/3.0.0/galactic_ai-3.0.0.tar.gz

---
+++ packages/galactic-ai/galactic_ai-3.0.0.tar.gz/galactic_ai-3.0.0/README.md
@@ -0,0 +1,3 @@
+# PACKAGE REMOVED
+
+The package previously located here has been removed due to a trademark dispute.
---
+++ packages/galactic-ai/galactic_ai-3.0.0.tar.gz/galactic_ai-3.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="galactic-ai",
+    version="3.0.0",
+    description="This package has been removed due to a trademark dispute.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="",
+    author_email="",
+    license="",
+    packages=[],
+    classifiers=[
+        "Development Status :: 7 - Inactive",
+    ],
+)
