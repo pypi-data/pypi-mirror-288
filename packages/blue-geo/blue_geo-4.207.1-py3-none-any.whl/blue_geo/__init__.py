dc0eadc7d0c7529ca4483190fda061605213f634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 16:05:32 +0000

    Add package blue-geo/4.207.1/blue_geo-4.207.1-py3-none-any.whl

---
+++ packages/blue-geo/blue_geo-4.207.1-py3-none-any.whl/blue_geo/__init__.py
@@ -0,0 +1,13 @@
+NAME = "blue_geo"
+
+ICON = "🌐"
+
+DESCRIPTION = f"{ICON} geospatial AI."
+
+VERSION = "4.207.1"
+
+REPO_NAME = "blue-geo"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/nbs/ukraine-timemap/QGIS.png?raw=true"
+)
