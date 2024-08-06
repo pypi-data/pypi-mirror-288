14d3d9c7fdb65c5e9028d8f43dd2cff045e632ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:06:14 +0000

    Add package MBQQ/0.8.5/MBQQ-0.8.5.tar.gz

---
+++ packages/MBQQ/MBQQ-0.8.5.tar.gz/MBQQ-0.8.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+setuptools.setup(
+    name='MBQQ',
+    version='0.8.5',
+    url='https://pypi.org/manage/account/token/',
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    license='',
+    author='Magpie Bridge',
+    author_email='',
+    description='',
+    install_requires=[
+        'AndTools',
+        'protobuf==4.23.4',
+        'cryptography',
+        'bs4',
+        'urllib3==1.26.16',
+        'pydantic',
+        'requests',
+        'python-box',
+        'PySocks',
+        'python-box'
+
+    ]
+
+)
