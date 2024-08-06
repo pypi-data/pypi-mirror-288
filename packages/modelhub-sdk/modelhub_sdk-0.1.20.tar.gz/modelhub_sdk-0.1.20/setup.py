6595fd9f2d307ac6c33ee6b4f2e62c794dee258c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:47 +0000

    Add package modelhub-sdk/0.1.20/modelhub_sdk-0.1.20.tar.gz

---
+++ packages/modelhub-sdk/modelhub_sdk-0.1.20.tar.gz/modelhub_sdk-0.1.20/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="modelhub-sdk",
+    version="0.1.20",
+    description="SDK for ModelHub to create and manage machine learning pipelines",
+    author="Jagveer Singh",
+    author_email="jagveer@autonomize.ai",
+    url="https://github.com/yourusername/modelhub-sdk",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "pyyaml",
+        "jinja2",
+        "kubernetes",
+        "requests",
+        "aiohttp",
+        "mlflow",
+        "azure-storage-blob",
+        "azure-identity",
+        "graphviz",
+        "IPython",
+        "pydantic",
+        "networkx",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
