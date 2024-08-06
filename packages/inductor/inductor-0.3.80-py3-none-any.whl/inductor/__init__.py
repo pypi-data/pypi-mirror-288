902906bd042c5840cc6c2f6d20985804338c8b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 16:05:31 +0000

    Add package inductor/0.3.80/inductor-0.3.80-py3-none-any.whl

---
+++ packages/inductor/inductor-0.3.80-py3-none-any.whl/inductor/__init__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Inductor, Inc.
+"""Inductor client library."""
+
+from inductor import quality
+from inductor.data_model.data_model import (
+    ChatMessage, ChatSession, HparamSpec, QualityMeasure,
+    TestCase, TestSuite, TestSuiteValueError)
+from inductor.execution.execution import hparam, log
+from inductor.execution.live_execution import logger
+
+
+__all__ = [
+    "ChatMessage",
+    "ChatSession",
+    "HparamSpec",
+    "QualityMeasure",
+    "TestCase",
+    "TestSuite",
+    "TestSuiteValueError",
+    "hparam",
+    "log",
+    "logger",
+    "quality"
+]
