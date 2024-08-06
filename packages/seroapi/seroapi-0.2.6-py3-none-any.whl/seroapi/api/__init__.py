a4dd76a61356feb8af063f02a2d691df3c177478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 18:05:13 +0000

    Add package seroapi/0.2.6/seroapi-0.2.6-py3-none-any.whl

---
+++ packages/seroapi/seroapi-0.2.6-py3-none-any.whl/seroapi/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from seroapi.api.account_management_api import AccountManagementApi
+from seroapi.api.actions_api import ActionsApi
+from seroapi.api.origins_management_and_dynamic_access_control_api import OriginsManagementAndDynamicAccessControlApi
+
