caae957bfd57dcb41baf3bd09e94c93659878cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 15:05:01 +0000

    Add package tiny-dlna/0.8.0/tiny-dlna-0.8.0.tar.gz

---
+++ packages/tiny-dlna/tiny-dlna-0.8.0.tar.gz/tiny-dlna-0.8.0/tiny_dlna/tiny_render.py
@@ -0,0 +1,252 @@
+import argparse
+import html
+import logging
+import re
+import os.path
+import subprocess
+import threading
+import time
+import xml.etree.ElementTree as ET
+
+from flask import Flask, request, Response
+from .tiny_ssdp import get_uuid, ssdp_listener
+from .tiny_xmls import *  # NOQA
+
+app = Flask(__name__)
+logger = logging.getLogger('tiny_render')
+
+
+class MPVRenderer:
+    def __init__(self):
+        self.process = None
+
+    def play_media(self, url, title=None, srt=None, dump_to=None):
+        self.stop_media()  # Stop any existing media
+        cmd = ['mpv', url]
+
+        if dump_to:
+            path_abs = os.path.abspath(dump_to)
+            cmd.append(f'--stream-record={path_abs}')
+        if title:
+            cmd.append('--title={}'.format(title))
+        if srt:
+            cmd.append('--sub-file={}'.format(srt))
+
+        logger.debug(f'running mpv: {cmd}')
+        self.process = subprocess.Popen(cmd, stderr=subprocess.DEVNULL)
+
+    def stop_media(self):
+        if self.process:
+            self.process.terminate()
+            self.process = None
+
+
+renderer = MPVRenderer()
+
+_DATA = {
+    'CURRENT_URI': '',
+    'CURRENT_SRT': '',
+    'VIDEO_TITLE': '',
+    'DUMP_TO': None,
+    'STARTED_AT': 0,
+}
+
+
+@app.route('/description.xml')
+def description():
+    friendly_name = app.config['FRIENDLY_NAME']
+    uuid_str = get_uuid()
+    xml = XML_DESC_PTN.format(friendly_name, uuid_str)
+    resp = Response(xml, mimetype="text/xml")
+    resp.headers['Server'] = 'UPnP/1.0 Werkzeug/3.0 TinyRender/0.7'
+    return resp
+
+# these 3 dlna_ routings below are only here so that some dummy app
+# could treat our "tiny render" as a proper dlna device. (Mouyu ..)
+@app.route('/dlna/AVTransport.xml')
+def dlna_avtransport():
+    resp = Response(XML_DLNA_AVT, mimetype="text/xml")
+    return resp
+
+@app.route('/dlna/RenderingControl.xml')
+def dlna_render_control():
+    resp = Response(XML_DLNA_RENDER_CTRL, mimetype="text/xml")
+    return resp
+
+@app.route('/dlna/ConnectionManager.xml')
+def dlna_conn_manager():
+    resp = Response(XML_DLNA_CONN_MANAGER, mimetype="text/xml")
+    return resp
+
+
+def to_track_time(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    remaining_seconds = seconds % 60
+    return f"{hours}:{minutes:02}:{remaining_seconds:02}"
+
+def is_play(request):
+    return b'<u:Play' in request.data
+
+def is_stop(request):
+    return b'<u:Stop' in request.data
+
+def is_setav(request):
+    return b'SetAVTransportURI' in request.data
+
+def is_gettrans(request):
+    return b'u:GetTransportInfo' in request.data
+
+def is_getpos(request):
+    return b'u:GetPositionInfo' in request.data
+
+def is_seek(request):
+    return b'u:Seek' in request.data
+
+def get_title_re(xml_data):
+    pattern = re.compile(r'<dc:title>(.*?)</dc:title>', re.DOTALL)
+    match = pattern.search(xml_data)
+    if match:
+        return match.group(1)
+
+def get_metadata(request):
+    root = ET.fromstring(request.data.strip())
+    current_uri = root.find('.//CurrentURI').text
+    metadata = root.find('.//CurrentURIMetaData').text
+    if not metadata:
+        logger.debug('no metadata')
+        return {'video': current_uri, 'title': ''}
+
+    title = ''
+    metadata = html.unescape(metadata)
+    try:
+        # HACK: replace all `&` to `&amp;`
+        metadata = metadata.replace('&', '&amp;')
+        metadata = ET.fromstring(metadata.strip())
+    except ET.ParseError:
+        # HACK: fall back to `re` to get title only (e.g. Huya)
+        logger.debug('** got xml.ParseError, fall back to re')
+        title = get_title_re(metadata)
+        return {'video': current_uri, 'title': title}
+
+    ns = {
+        'dc': 'http://purl.org/dc/elements/1.1/',
+        '': 'urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/'
+    }
+    item = metadata.find('.//item', namespaces=ns)
+    obj = item.find('.//dc:title', namespaces=ns)
+    if obj is not None:
+        title = obj.text
+
+    current_srt = ''
+    for res in metadata.findall('.//{urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/}res'):
+        if res.get('type') == 'text/subtitle':
+            current_srt = res.text.strip()
+            break
+
+    return {
+        'video': current_uri,
+        'srt': current_srt,
+        'title': title,
+    }
+
+
+@app.route('/AVTransport/control', methods=['POST'])
+def control():
+    if is_setav(request):
+        metadata = get_metadata(request)
+        current_uri = metadata['video']
+        current_srt = metadata.get('srt', '')
+        video_title = metadata.get('title', '')
+
+        logger.debug(f'Action: SetAV: {current_uri}')
+        logger.debug(f'Title: {video_title} SRT: {current_srt}')
+        _DATA['CURRENT_URI'] = current_uri
+        _DATA['CURRENT_SRT'] = current_srt
+        _DATA['VIDEO_TITLE'] = video_title
+        return Response(XML_AVSET_DONE, mimetype="text/xml")
+
+    elif is_play(request):
+        _DATA['STARTED_AT'] = time.time()
+        url = _DATA['CURRENT_URI']
+        srt = _DATA['CURRENT_SRT']
+        title = _DATA['VIDEO_TITLE']
+        dump_to = _DATA['DUMP_TO']
+        logger.debug(f'action: Play: {url}')
+        renderer.play_media(url, title, srt, dump_to)
+        return Response(XML_PLAY_DONE, mimetype="text/xml")
+
+    elif is_getpos(request):
+        # this is only a dummy impl; we need rpc to mpv process for real.
+        logger.debug('action: GetPositionInfo')
+        seconds = int(time.time() - _DATA['STARTED_AT'])
+        reltime = to_track_time(seconds)
+        return Response(XML_POSINFO.format(reltime), mimetype="text/xml")
+
+    elif is_gettrans(request):
+        logger.debug('action: GetTransportInfo')
+        return Response(XML_TRANSINFO, mimetype="text/xml")
+
+    elif is_stop(request):
+        logger.debug('stopping')
+
+        _DATA['CURRENT_URI'] = ''
+        _DATA['CURRENT_SRT'] = ''
+        _DATA['VIDEO_TITLE'] = ''
+        _DATA['STARTED_AT'] = 0
+        renderer.stop_media()
+        return Response(XML_STOP_DONE, mimetype="text/xml")
+
+    elif is_seek(request):
+        logger.debug('action:seek')
+        return Response(XML_SEEK_DONE, mimetype="text/xml")
+
+    logger.error(f'action not support: {request.data}')
+    return Response('Action Not Supported', status=500)
+
+
+class SSDPServer(threading.Thread):
+    def __init__(self, render_port):
+        super().__init__()
+        self.render_port = render_port
+
+    def run(self):
+        ssdp_listener(self.render_port)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='tiny-render')
+    parser.add_argument('--http-logs', action='store_true', help='Enable server logs')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Enable debug logs')
+    parser.add_argument('--name', type=str, default='Tiny Render', help='Change render name')
+    parser.add_argument('--dump-to', type=str, help='dump streaming to a file')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.ERROR,
+        format='[%(asctime)s][%(levelname)s] %(message)s',
+    )
+
+    if not args.http_logs:
+        logging.getLogger('werkzeug').setLevel(logging.ERROR)
+        logging.getLogger('tiny_ssdp').setLevel(logging.ERROR)
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    if args.dump_to:
+        _DATA['DUMP_TO'] = args.dump_to
+
+    port = 59876
+    ssdp = SSDPServer(port)
+    ssdp.start()
+
+    friendly_name = args.name
+    app.config['FRIENDLY_NAME'] = friendly_name
+    logging.info(f'Starting DLNA Receiver: {friendly_name}')
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/tiny-dlna/tiny-dlna-0.8.0.tar.gz/tiny-dlna-0.8.0/tiny_dlna/tiny_ssdp.py
@@ -0,0 +1,94 @@
+import datetime
+import json
+import logging
+import os
+import psutil
+import socket
+import uuid
+
+SSDP_MULTICAST_IP = '239.255.255.250'
+SSDP_PORT = 1900
+logger = logging.getLogger('tiny_ssdp')
+ST_VALUE_MEDIARENDERER = 'mediarenderer'
+ST_VALUE_AVTRANSPORT = 'avtransport'
+
+
+def get_uuid():
+    home_dir = os.path.expanduser('~')
+    app_data_dir = os.path.join(home_dir, '.config', 'tiny-dlna')
+    os.makedirs(app_data_dir, exist_ok=True)
+    config_file = os.path.join(app_data_dir, 'tiny-render.json')
+
+    if os.path.exists(config_file):
+        with open(config_file) as f:
+            configs = json.load(f)
+            if 'UUID' in configs and configs['UUID']:
+                return configs['UUID']
+
+    uuid_str = f'{uuid.uuid4()}'
+    with open(config_file, 'w') as f:
+        configs = {'UUID': uuid_str}
+        json.dump(configs, f)
+
+
+def get_host_ip():
+    ips = []
+    interfaces = psutil.net_if_addrs()
+
+    for iface_name, iface_addresses in interfaces.items():
+        for address in iface_addresses:
+            if address.family == socket.AF_INET:
+                ip = address.address
+                if ip and ip.startswith('192.168.'):
+                    ips.append(ip)
+
+    if len(ips) == 0:
+        logger.error('failed to find host IP.')
+
+    # use the smallest one, in case get like 192.168.65.1 (vm interfaces)
+    ips.sort()
+    return ips[0]
+
+
+def build_m_search_response(st, render_port):
+    now = datetime.datetime.utcnow()
+    date_str = now.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    render_ip = get_host_ip()
+    location = f'http://{render_ip}:{render_port}/description.xml'
+    uuid_str = get_uuid()
+
+    text = 'HTTP/1.1 200 OK\r\n'
+    if st == ST_VALUE_MEDIARENDERER:
+        text += 'ST: urn:schemas-upnp-org:device:MediaRenderer:1\r\n'
+        text += f'USN: uuid:{uuid_str}::urn:schemas-upnp-org:device:MediaRenderer:1\r\n'
+    else:
+        text += 'ST: urn:schemas-upnp-org:service:AVTransport:1\r\n'
+        text += f'USN: uuid:{uuid_str}::urn:schemas-upnp-org:service:AVTransport:1\r\n'
+    text += f'Location: {location}\r\n'
+    text += 'EXT: \r\n'
+    text += 'Server: Werkzeug/3.0 TinyRender/0.6\r\n'
+    text += 'Cache-Control: max-age=70\r\n'
+    text += f'Date: {date_str}\r\n\r\n'
+    return text.encode('utf-8')
+
+def get_search_target(data):
+    if b':device:MediaRenderer:1' in data:
+        return ST_VALUE_MEDIARENDERER
+    else:
+        return ST_VALUE_AVTRANSPORT
+
+def ssdp_listener(render_port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('0.0.0.0', SSDP_PORT))
+
+    # Join the SSDP multicast group
+    mreq = socket.inet_aton(SSDP_MULTICAST_IP) + socket.inet_aton('0.0.0.0')
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if b'M-SEARCH' in data and b'ssdp:discover' in data:
+            st = get_search_target(data)
+            logger.info(f'Received M-SEARCH from {addr}, sending response...')
+            sock.sendto(build_m_search_response(st, render_port), addr)
