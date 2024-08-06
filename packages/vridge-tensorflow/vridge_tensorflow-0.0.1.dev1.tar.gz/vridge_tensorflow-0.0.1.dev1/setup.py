c72ca943b213246d748d328a7c7fe76081ec950a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:05:15 +0000

    Add package vridge-tensorflow/0.0.1.dev1/vridge_tensorflow-0.0.1.dev1.tar.gz

---
+++ packages/vridge-tensorflow/vridge_tensorflow-0.0.1.dev1.tar.gz/vridge_tensorflow-0.0.1.dev1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vridge_tensorflow',
+    version='0.0.1-dev.1',
+    description='vridge 텐서플로우 패키지',
+    author='byungchan',
+    author_email='bgk@vazilcompany.com',
+    install_requires=['tensorflow==2.6.0',
+                      'keras==2.6.0',
+                      'numpy==1.19.2',
+                      ],
+    packages=find_packages(exclude=[]),
+    keywords=['vazil', 'vazilcompany', 'vridge', 'vridge_tensorflow'],
+    python_requires='>=3.7, <3.8',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.7'
+    ],
+)
