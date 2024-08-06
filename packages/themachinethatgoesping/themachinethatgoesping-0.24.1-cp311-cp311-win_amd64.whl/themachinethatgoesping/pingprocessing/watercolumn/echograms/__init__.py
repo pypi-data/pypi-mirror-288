60aa35fe9bbe73733b13844a5fb9486a6327c4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 15:05:01 +0000

    Add package themachinethatgoesping/0.24.1/themachinethatgoesping-0.24.1-cp311-cp311-win_amd64.whl

---
+++ packages/themachinethatgoesping/themachinethatgoesping-0.24.1-cp311-cp311-win_amd64.whl/themachinethatgoesping/pingprocessing/watercolumn/echograms/__init__.py
@@ -0,0 +1,11 @@
+# folders
+
+# modules
+from . import echoimagebuilders
+
+# functions
+from .echodata import EchoData
+from .echogramsection import EchogramSection
+from .echogramgroup import EchogramGroup, create_echogram_groups
+from .echogrambuilder import EchogramBuilder
+
