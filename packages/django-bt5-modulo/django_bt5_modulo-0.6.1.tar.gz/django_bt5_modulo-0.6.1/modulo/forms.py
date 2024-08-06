14dc322367e8a20dbde44ab244c4bace34129ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 18:05:40 +0000

    Add package django-bt5-modulo/0.6.1/django_bt5_modulo-0.6.1.tar.gz

---
+++ packages/django-bt5-modulo/django_bt5_modulo-0.6.1.tar.gz/django_bt5_modulo-0.6.1/modulo/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from modulo.models import TestData
+
+
+class TestDataForm(forms.ModelForm):
+    departamentos = forms.ChoiceField(choices=(("Lima", "Lima"), ("Cajamarca", "Cajamarca")),
+                                      widget=forms.RadioSelect(), required=False)
+    departamentos2 = forms.ChoiceField(choices=(("Lima", "Lima"), ("Cajamarca", "Cajamarca")), required=False)
+    departamentos3 = forms.MultipleChoiceField(choices=(("Lima", "Lima"), ("Cajamarca", "Cajamarca")), required=False)
+    search = forms.CharField(max_length=8, label='Search',widget=forms.TextInput(
+        attrs={'class': 'form-control', 'type': 'search', 'placeholder': 'Search'}), required=False)
+
+    class Meta:
+        model = TestData
+        fields = ['name', 'is_enable']
+
+    def __init__(self, *args, **kwargs):
+        super(TestDataForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = "form-control"
+            self.fields[field].widget.attrs['placeholder'] = self.fields[field].label
---
+++ packages/django-bt5-modulo/django_bt5_modulo-0.6.1.tar.gz/django_bt5_modulo-0.6.1/modulo/views.py
@@ -0,0 +1,136 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import JsonResponse
+from django.urls import reverse_lazy
+
+from django.views import View
+from django.views.generic import CreateView, TemplateView, ListView, UpdateView, DeleteView
+
+from modulo.clases.select2 import Select2JSONResponseMixin
+from modulo.clases.table_lista import TableListView
+from modulo.forms import TestDataForm
+from modulo.models import TestData
+
+
+# Create your views here.
+
+class SuccessView(View):
+
+    def get(self, request, pk):
+        return JsonResponse({
+            "success": True,
+            "mensaje": "Guardado Correctamente",
+            "pk": pk
+        })
+
+
+class HomeView(TemplateView):
+    template_name = "modulo/test/vista.html"
+
+
+class ListaView(ListView):
+    model = TestData
+    template_name = "modulo/test/lista.html"
+    paginate_by = 5
+    ordering = ['id']
+
+    def get_queryset(self):
+        query = super(ListaView, self).get_queryset()
+        search = self.request.GET.get("search")
+        pagination = self.request.GET.get("pagination")
+        if pagination:
+            self.paginate_by = int(pagination)
+        if search:
+            query = query.filter(
+                Q(name__icontains=search)
+            )
+        return query
+
+
+class TableListaView(TableListView):
+    model = TestData
+    template_name = "modulo/test/table_lista.html"
+    paginate_by = 5
+    ordering = ['id']
+    headers = ['Nombre', "Activo"]
+
+    def get_queryset(self):
+        query = super(TableListView, self).get_queryset()
+        search = self.request.GET.get("search")
+        pagination = self.request.GET.get("pagination")
+        if pagination:
+            self.paginate_by = int(pagination)
+        if search:
+            query = query.filter(
+                Q(name__icontains=search)
+            )
+        return query
+
+
+class CrearView(CreateView):
+    model = TestData
+    form_class = TestDataForm
+    template_name = "modulo/formulario/formulario.html"
+
+
+class Crear2View(CreateView):
+    model = TestData
+    form_class = TestDataForm
+    template_name = "modulo/test/form.html"
+
+
+class EditarView(UpdateView):
+    model = TestData
+    form_class = TestDataForm
+    template_name = "modulo/formulario/formulario.html"
+
+
+class EliminarView(DeleteView):
+    model = TestData
+    template_name = "modulo/formulario/eliminar.html"
+    success_url = reverse_lazy('modulo:success', kwargs={'pk': 0})
+
+
+class ComboView(Select2JSONResponseMixin, TemplateView):
+    model = TestData
+    filters = ['name']
+
+    def render_to_response(self, context, **response_kwargs):
+        return self.render_to_json_response(context, **response_kwargs)
+
+
+class VistaMaterialView(TemplateView):
+    template_name = 'modulo/test/material/vista_material.html'
+
+
+class TablaComponetsView(View):
+    def get(self, request):
+        testdata = TestData.objects.all().order_by('id')
+        page_number = request.GET.get("page")
+        paginacion = request.GET.get("paginacion", 10)
+        paginator = Paginator(testdata, paginacion)  # Show 25 contacts per page.
+        page_obj = paginator.get_page(page_number)
+        # print(page_obj.__dict__)
+        rows = []
+        for obj in page_obj:
+            obj_data = {
+                "id": obj.id,
+                "name": obj.name,
+                "is_enable": obj.is_enable,
+            }
+            rows.append(obj_data)
+        pages = {
+            "number ": page_obj.number,
+            "num_pages ": page_obj.paginator.num_pages,
+            "page_range": [page for page in page_obj.paginator.page_range],
+            "has_previous": page_obj.has_previous(),
+            "previous_page_number ": page_obj.previous_page_number() if page_obj.has_previous() else None,
+            "has_next": page_obj.has_next(),
+            "next_page_number  ": page_obj.next_page_number() if page_obj.has_next() else None,
+        }
+        data = {
+            "headers": ["Nombre", "Activo"],
+            "rows": rows,
+            "pages": pages,
+        }
+        return JsonResponse(data)
