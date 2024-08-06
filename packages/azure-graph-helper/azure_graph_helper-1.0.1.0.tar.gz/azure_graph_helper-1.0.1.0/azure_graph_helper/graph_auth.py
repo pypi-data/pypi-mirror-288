a109b35ad761fd553948567dc7f866382c738c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 18:05:16 +0000

    Add package azure-graph-helper/1.0.1.0/azure_graph_helper-1.0.1.0.tar.gz

---
+++ packages/azure-graph-helper/azure_graph_helper-1.0.1.0.tar.gz/azure_graph_helper-1.0.1.0/azure_graph_helper/graph_auth.py
@@ -0,0 +1,22 @@
+from msal import ConfidentialClientApplication
+import config
+
+
+def get_access_token(tenant_id, client_id, client_secret):
+
+    auth_url = f'{config.AUTH_BASE_URL}/{tenant_id}'
+    scopes = ['https://graph.microsoft.com/.default']
+
+    app = ConfidentialClientApplication(
+
+        client_id=client_id,
+        client_credential=client_secret,
+        authority=auth_url
+    )
+    
+    result = app.acquire_token_for_client(scopes=scopes)
+    return result.get('access_token')
+
+
+
+    
