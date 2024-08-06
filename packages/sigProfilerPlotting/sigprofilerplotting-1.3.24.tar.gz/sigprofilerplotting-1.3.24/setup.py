e948be8799ec0c5e0c74b7dadbbef5b3dcbb3d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:55 +0000

    Add package sigProfilerPlotting/1.3.24/sigprofilerplotting-1.3.24.tar.gz

---
+++ packages/sigProfilerPlotting/sigprofilerplotting-1.3.24.tar.gz/sigprofilerplotting-1.3.24/setup.py
@@ -0,0 +1,82 @@
+import os
+import shutil
+
+from setuptools import setup
+from setuptools.command.install import install
+
+# remove the dist folder first if exists
+if os.path.exists("dist"):
+    shutil.rmtree("dist")
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+VERSION = "1.3.24"
+
+
+def write_version_py(filename="sigProfilerPlotting/version.py"):
+    # Copied from numpy setup.py
+    cnt = """
+# THIS FILE IS GENERATED FROM SIGPROFILERPLOTTING SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+update = 'Upgrade v1.3.24: Make output path handling more robust with os.path.join() and add environmental variable SIGPROFILERPLOTTING_VOLUME'
+    
+    """
+    fh = open(filename, "w")
+    fh.write(
+        cnt
+        % {
+            "version": VERSION,
+        }
+    )
+    fh.close()
+
+
+write_version_py()
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="sigProfilerPlotting",
+    version=VERSION,
+    description="SigProfiler plotting tool",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexandrovlab/SigProfilerPlotting",
+    author="Erik Bergstrom",
+    author_email="ebergstr@eng.ucsd.edu",
+    license="UCSD",
+    packages=[
+        "sigProfilerPlotting",
+        "sigProfilerPlotting.reference_formats",
+        "sigProfilerPlotting.fonts",
+        "sigProfilerPlotting.controllers",
+    ],
+    install_requires=[
+        "matplotlib>=3.4.3",
+        "pandas>=1.2.4,<2.0.0",
+        "scikit-learn>=1.1.3",
+        "pillow>=10.0.0",
+    ],
+    extras_require={
+        "tests": [
+            "pytest",
+            "scikit-image>=0.21.0",
+            "numpy>=1.21.2",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "SigProfilerPlotting=sigProfilerPlotting.sigProfilerPlotting_CLI:main_function",
+        ],
+    },
+    package_data={"": ["fonts/*.ttf"]},
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ packages/sigProfilerPlotting/sigprofilerplotting-1.3.24.tar.gz/sigprofilerplotting-1.3.24/sigProfilerPlotting/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILERPLOTTING SETUP.PY
+short_version = '1.3.24'
+version = '1.3.24'
+update = 'Upgrade v1.3.24: Make output path handling more robust with os.path.join() and add environmental variable SIGPROFILERPLOTTING_VOLUME'
+    
+    
