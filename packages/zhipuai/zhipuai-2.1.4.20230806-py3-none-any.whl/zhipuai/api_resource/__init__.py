7eedc7de95d4672935ae7801be9a0b7a39d998b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:05:09 +0000

    Add package zhipuai/2.1.4.20230806/zhipuai-2.1.4.20230806-py3-none-any.whl

---
+++ packages/zhipuai/zhipuai-2.1.4.20230806-py3-none-any.whl/zhipuai/api_resource/__init__.py
@@ -0,0 +1,47 @@
+from .chat import (
+    AsyncCompletions,
+    Chat,
+    Completions,
+)
+from .images import (
+    Images
+)
+from .embeddings import (
+    Embeddings
+)
+from .files import (
+    Files,
+    FilesWithRawResponse
+)
+from .fine_tuning import (
+    FineTuning
+)
+
+from .batches import (
+    Batches
+)
+
+from .knowledge import (
+    Knowledge
+)
+from .tools import (
+    Tools
+)
+from .videos import (
+    Videos,
+)
+__all__ = [
+    'Videos',
+    'AsyncCompletions',
+    'Chat',
+    'Completions',
+    'Images',
+    'Embeddings',
+    'Files',
+    'FilesWithRawResponse',
+    'FineTuning',
+    'Batches',
+    'Knowledge',
+    'Tools',
+
+]
