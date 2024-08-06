981884a8f55d8997d487208eb6809f9885ce50ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:30 +0000

    Add package anime-parsers-ru/1.6.1/anime_parsers_ru-1.6.1.tar.gz

---
+++ packages/anime-parsers-ru/anime_parsers_ru-1.6.1.tar.gz/anime_parsers_ru-1.6.1/src/anime_parsers_ru/__init__.py
@@ -0,0 +1,6 @@
+from .parser_kodik import KodikParser
+from .parser_kodik_async import KodikParserAsync
+from .parser_aniboom import AniboomParser
+from .parser_jutsu import JutsuParser
+from .parser_shikimori import ShikimoriParser
+from .parser_shikimori_async import ShikimoriParserAsync
