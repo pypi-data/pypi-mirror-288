ff6be2d1d92e11c6384273f9be49548865f6e060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 19:05:37 +0000

    Add package django-field-logger/0.0.39/django_field_logger-0.0.39-py3-none-any.whl

---
+++ packages/django-field-logger/django_field_logger-0.0.39-py3-none-any.whl/fieldlogger/mixins.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from .models import FieldLog
+
+
+class FieldLoggerMixin(models.Model):
+    @property
+    def fieldlog_set(self):
+        return FieldLog.objects.filter(
+            instance_id=self.pk,
+            model_name=self._meta.model_name,
+            app_label=self._meta.app_label,
+        )
+
+    class Meta:
+        abstract = True
