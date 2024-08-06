4c61516b596262ee7794be733cb379034f4a4b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 14:04:52 +0000

    Add package arkitekt-next/0.7.52/arkitekt_next-0.7.52.tar.gz

---
+++ packages/arkitekt-next/arkitekt_next-0.7.52.tar.gz/arkitekt_next-0.7.52/arkitekt_next/bloks/services/name.py
@@ -0,0 +1,12 @@
+from typing import Dict, Any, Protocol
+from blok import blok, InitContext, Option
+from blok import service
+from dataclasses import dataclass
+
+
+
+@service("live.arkitekt.names")
+class NameService(Protocol):
+
+    def retrieve_name(self) -> str:
+        pass
