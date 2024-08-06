f3cfd8c14db1c81fbf5fa9446305a692e5bc77a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 15:05:04 +0000

    Add package llvmpym/0.0.4/llvmpym-0.0.4.tar.gz

---
+++ packages/llvmpym/llvmpym-0.0.4.tar.gz/llvmpym-0.0.4/src/llvm/types_priv/PymMetadataEntries.h
@@ -0,0 +1,25 @@
+#ifndef LLVMPYM_TYPES_PRIV_PYMETADATAENTRIES_H
+#define LLVMPYM_TYPES_PRIV_PYMETADATAENTRIES_H
+
+#include <llvm-c/Core.h>
+#include <memory>
+#include <unordered_map>
+#include <mutex>
+#include "PymLLVMObject.h"
+#include "utils.h"
+
+typedef LLVMValueMetadataEntry *LLVMValueMetadataEntries;
+
+class PymMetadataEntries : public PymLLVMObject<PymMetadataEntries, LLVMValueMetadataEntries> {
+public:
+  explicit PymMetadataEntries(LLVMValueMetadataEntries entries, size_t len);
+  LLVMValueMetadataEntries get() const;
+  size_t getLen() const;
+  
+private:
+  size_t len;
+  SHARED_POINTER_DEF(LLVMValueMetadataEntries, LLVMValueMetadataEntry);
+};
+
+
+#endif
