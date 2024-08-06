83cf0a332ce8bf48e7337ef9c47c5c8a2d212bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 16:05:40 +0000

    Add package nonebot-plugin-clovers/0.1.6/nonebot_plugin_clovers-0.1.6.tar.gz

---
+++ packages/nonebot-plugin-clovers/nonebot_plugin_clovers-0.1.6.tar.gz/nonebot_plugin_clovers-0.1.6/nonebot_plugin_clovers/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    clovers_priority: int = 50
+    using_adapters: set[str] = {
+        "nonebot.adapters.qq",
+        "nonebot.adapters.onebot.v11",
+        "nonebot.adapters.satori",
+    }
+
+
+class ConfigClovers(BaseModel):
+    plugins_path: str = "./clovers_library"
+    plugins_list: list = []
