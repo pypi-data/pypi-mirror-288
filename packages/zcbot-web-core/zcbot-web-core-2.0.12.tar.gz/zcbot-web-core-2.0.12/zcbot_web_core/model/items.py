94679f3d7276ea8bd96c3e2655fca064eda27d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:34 +0000

    Add package zcbot-web-core/2.0.12/zcbot-web-core-2.0.12.tar.gz

---
+++ packages/zcbot-web-core/zcbot-web-core-2.0.12.tar.gz/zcbot-web-core-2.0.12/zcbot_web_core/model/items.py
@@ -0,0 +1,73 @@
+from pydantic import BaseModel, Field
+from ..model.enums import UserStatus, UserGender
+from ..lib import time as time_lib
+
+
+class BaseData(BaseModel):
+    """
+    通用基础数据模型
+    """
+    # mongodb主键
+    _id: str = None
+    # 插入时间
+    genTime: int = Field(
+        default_factory=time_lib.current_timestamp10
+    )
+
+
+class AuthUser(BaseData):
+    """
+    用户鉴权模型
+    """
+    # 用户编号
+    id: str
+    # 租户编码
+    tenantCode: str = None
+    # 用户名
+    userName: str = None
+    # 用户姓名
+    realName: str = None
+    # 性别
+    gender: int = UserGender.MALE
+    # 电话
+    mobile: str = None
+    # 邮箱
+    email: str = None
+    # 是否超级管理员
+    superAdmin: str = None
+    # 是否超级租户
+    superTenant: int = 1
+    # 启用停用
+    isEnabled: bool = True
+    # 批次状态
+    status: str = UserStatus.NORMAL
+
+    def to_log(self):
+        return f'{self.realName}({self.id})'
+
+    def is_super_admin(self):
+        return '1' == self.superAdmin
+
+
+class AuthApp(BaseData):
+    """
+    App鉴权模型
+    """
+    # 用户编号
+    appId: str
+    # 用户名
+    appName: str = None
+    # 账号
+    accessKey: str = None
+    # 密码
+    secretKey: str = None
+    # 长效Token
+    staticToken: str = None
+    # 是否启用长效Token
+    enableStaticToken: Optional[Union[int, str]] = None
+    # 启用停用
+    isEnabled: bool = None
+    # 批次状态
+    status: str = None
+    # 租户编码
+    tenantCode: str = None
