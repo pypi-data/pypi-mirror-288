3fb8b8febe07af940025058975e6cf6b0bf1ad2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 15:05:27 +0000

    Add package dastyor/0.1.9/dastyor-0.1.9.tar.gz

---
+++ packages/dastyor/dastyor-0.1.9.tar.gz/dastyor-0.1.9/dastyor/custom_commands.py
@@ -0,0 +1,32 @@
+import os
+
+def get_sample():
+    content = """
+from dastyor.app import DastyorApp
+
+app = DastyorApp()
+
+# example for custom handler
+@app.route("/", allowed_methods=['get'])
+def home(request, response):
+    response.text = "Hey from Home page"
+
+# type your code here
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.runserver()
+
+"""        
+    filepath = "main.py"
+    if os.path.exists(filepath):
+        print(f"File '{filepath}' already exists.")
+    else:
+        with open(filepath, 'w') as manage_py_file:
+            manage_py_file.write(content)
+            print(f"Default main.py created in {os.getcwd()}")
