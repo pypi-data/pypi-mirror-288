f28f21c5d130fb9964740fd416091d1be69733c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 21:05:54 +0000

    Add package odoo-addon-dms-field/15.0.1.4.1/odoo_addon_dms_field-15.0.1.4.1-py3-none-any.whl

---
+++ packages/odoo-addon-dms-field/odoo_addon_dms_field-15.0.1.4.1-py3-none-any.whl/odoo/addons/dms_field/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright 2020 Creu Blanca
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "DMS Field",
+    "summary": """
+        Create DMS View and allow to use them inside a record""",
+    "version": "15.0.1.4.1",
+    "license": "LGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/dms",
+    "depends": ["dms"],
+    "data": [
+        "views/dms_access_group_views.xml",
+        "views/dms_directory.xml",
+        "views/dms_field_template_views.xml",
+        "views/dms_storage.xml",
+        "security/ir.model.access.csv",
+        "security/security.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "dms_field/static/src/js/**/*",
+            "dms_field/static/src/scss/**/*",
+        ],
+        "web.assets_qweb": ["dms_field/static/src/xml/**/*"],
+    },
+    "qweb": ["static/src/xml/*.xml"],
+    "demo": ["demo/partner_dms.xml"],
+}
