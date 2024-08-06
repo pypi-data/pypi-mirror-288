b861bb76b196f80c77d7347568c95ba7f6f29086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:05:22 +0000

    Add package intellibricks/0.0.1/intellibricks-0.0.1.tar.gz

---
+++ packages/intellibricks/intellibricks-0.0.1.tar.gz/intellibricks-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'todo'
+LONG_DESCRIPTION = 'todo'
+
+# Setting up
+setup(
+    name="intellibricks",
+    version=VERSION,
+    author="Arthur Brenno",
+    author_email="<arthurbreno009@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[]
+)
