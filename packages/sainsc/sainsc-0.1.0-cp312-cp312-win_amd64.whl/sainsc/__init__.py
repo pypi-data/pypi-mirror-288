5590ee27b70b6eef4ed6fd3768ca23e88c6f539d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 17:05:12 +0000

    Add package sainsc/0.1.0/sainsc-0.1.0-cp312-cp312-win_amd64.whl

---
+++ packages/sainsc/sainsc-0.1.0-cp312-cp312-win_amd64.whl/sainsc/__init__.py
@@ -0,0 +1,18 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("sainsc")
+except PackageNotFoundError:
+    __version__ = "unknown version"
+
+from ._utils_rust import GridCounts
+from .io import read_StereoSeq, read_StereoSeq_bins
+from .lazykde import LazyKDE, gaussian_kernel
+
+__all__ = [
+    "GridCounts",
+    "LazyKDE",
+    "gaussian_kernel",
+    "read_StereoSeq",
+    "read_StereoSeq_bins",
+]
