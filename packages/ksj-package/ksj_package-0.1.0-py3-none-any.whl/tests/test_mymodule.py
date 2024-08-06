d2d0b767021347b7e7264263f4be691c97d27c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 20:04:53 +0000

    Add package ksj-package/0.1.0/ksj_package-0.1.0-py3-none-any.whl

---
+++ packages/ksj-package/ksj_package-0.1.0-py3-none-any.whl/tests/test_mymodule.py
@@ -0,0 +1,8 @@
+import unittest
+from mypackage.mymodule import hello_world
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, World!")
+if __name__=="__main__":
+    unittest.main()
+    
