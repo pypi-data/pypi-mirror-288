54b18d18449873463ea62ec0754b159572eb278f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:05:15 +0000

    Add package overturetoosm/0.2.2/overturetoosm-0.2.2.tar.gz

---
+++ packages/overturetoosm/overturetoosm-0.2.2.tar.gz/overturetoosm-0.2.2/scripts/make_docs.py
@@ -0,0 +1,16 @@
+"""Generate the docs."""
+
+from pathlib import Path
+import pdoc
+import pdoc.render
+
+
+here = Path(__file__).parent
+
+pdoc.render.configure(
+    docformat="google",
+    footer_text="overturetoosm",
+    favicon="https://whubsch.github.io/overturetoosm/favicon.ico",
+    logo="https://whubsch.github.io/overturetoosm/logo.svg",
+)
+pdoc.pdoc("src/overturetoosm", output_directory=here.parent / "docs")
---
+++ packages/overturetoosm/overturetoosm-0.2.2.tar.gz/overturetoosm-0.2.2/tests/test_other.py
@@ -0,0 +1,18 @@
+"""Test miscelaneous functions in the project."""
+
+from src.overturetoosm import objects
+
+
+def test_util_source() -> None:
+    """Test the source statement function"""
+    source_1 = objects.Sources(
+        **{"property": "property1", "dataset": "dataset1", "confidence": 0.8}
+    )
+    source_2 = objects.Sources(
+        **{"property": "property2", "dataset": "dataset2", "confidence": 0.8}
+    )
+
+    assert (
+        objects.source_statement([source_1, source_2])
+        == "dataset1, dataset2 via overturetoosm"
+    )
