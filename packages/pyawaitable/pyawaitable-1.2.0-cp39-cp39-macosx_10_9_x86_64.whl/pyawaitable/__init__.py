2a91cfeccbd526833c9bac6998eda42873807647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 05:06:20 +0000

    Add package pyawaitable/1.2.0/pyawaitable-1.2.0-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/pyawaitable/pyawaitable-1.2.0-cp39-cp39-macosx_10_9_x86_64.whl/pyawaitable/__init__.py
@@ -0,0 +1,24 @@
+"""
+PyAwaitable - CPython API for asynchronous functions.
+
+Docs: https://awaitable.zintensity.dev/
+Source: https://github.com/ZeroIntensity/pyawaitable
+"""
+
+from . import abi
+from _pyawaitable import _PyAwaitableType  # type: ignore
+from typing import Type
+
+__all__ = "PyAwaitable", "include", "abi"
+__version__ = "1.2.0"
+
+PyAwaitable: Type = _PyAwaitableType
+
+
+def include() -> str:
+    """
+    Get the directory containing the `pyawaitable.h` file.
+    """
+    import os
+
+    return os.path.dirname(__file__)
