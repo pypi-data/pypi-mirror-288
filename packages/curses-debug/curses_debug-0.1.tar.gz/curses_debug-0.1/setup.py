56d69f71f3624c56910f41352d0ee8c58f3f8a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:04:35 +0000

    Add package curses-debug/0.1/curses_debug-0.1.tar.gz

---
+++ packages/curses-debug/curses_debug-0.1.tar.gz/curses_debug-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='curses_debug',
+      version='0.1',
+      description='A simple library for a beautiful output window written in curses and multiprocessing',
+      packages=['curses_debug'],
+      author_email='kirillovigor662@gmail.com',
+      zip_safe=False)
