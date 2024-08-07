803509112043f76ef744d091d3fb016c2e1bd114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:04:34 +0000

    Add package hyperSel/2.33/hypersel-2.33.tar.gz

---
+++ packages/hyperSel/hypersel-2.33.tar.gz/hypersel-2.33/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hyperSel",
+    version=2.33,
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+        "beautifulsoup4",
+        "webdriver-manager",
+        "lxml",
+        "requests",
+        "nodriver",
+        "psutil",
+    ],
+    include_package_data=True, 
+)
