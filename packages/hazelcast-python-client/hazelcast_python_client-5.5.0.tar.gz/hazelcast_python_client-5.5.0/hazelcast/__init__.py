6b8ba06188a5dc3ba70e0d2fb89452bff7fba6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:04:46 +0000

    Add package hazelcast-python-client/5.5.0/hazelcast_python_client-5.5.0.tar.gz

---
+++ packages/hazelcast-python-client/hazelcast_python_client-5.5.0.tar.gz/hazelcast_python_client-5.5.0/hazelcast/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "5.5.0"
+
+# Set the default handler to "hazelcast" loggers
+# to avoid "No handlers could be found" warnings.
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+from hazelcast.client import HazelcastClient
