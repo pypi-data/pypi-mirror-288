9e4385178076f6e3fb81de16b60d501f3a696ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:05:13 +0000

    Add package gai-cli/0.180/gai-cli-0.180.tar.gz

---
+++ packages/gai-cli/gai-cli-0.180.tar.gz/gai-cli-0.180/src/gai/cli/scripts/gai_docker_start.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+def docker_start(here):
+    try:
+        print("here:",here)
+        dest = os.path.join(here,"../../../.vscode")
+        docker_command = f"cd {dest} && docker-compose up -d --build --force-recreate"
+        subprocess.run(docker_command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
