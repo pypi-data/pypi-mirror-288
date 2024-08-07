4164bc441b7adc489487bc69861c741254d7d366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:05:26 +0000

    Add package rmfit/1.0.2/rmfit-1.0.2.tar.gz

---
+++ packages/rmfit/rmfit-1.0.2.tar.gz/rmfit-1.0.2/rmfit/__init__.py
@@ -0,0 +1,6 @@
+from .likelihood import *
+from .priors import *
+from .rmfit import *
+from .stats_help import *
+from .utils import *
+from .convective_blueshift import *
---
+++ packages/rmfit/rmfit-1.0.2.tar.gz/rmfit-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='rmfit',
+      version='1.0.2',
+      description='Fit Rossiter McLaughlin Data',
+      long_description=readme(),
+      url='https://github.com/gummiks/rmfit/',
+      author='Gudmundur Stefansson',
+      author_email='gummiks@gmail.com',
+      install_requires=['emcee','batman-package','radvel','corner','pandas','h5py','pytransit'],
+      #packages=['rmfit'],
+      packages=find_packages(),
+      license='GPLv3',
+      classifiers=['Topic :: Scientific/Engineering :: Astronomy'],
+      keywords='Astronomy',
+      include_package_data=True
+      )
