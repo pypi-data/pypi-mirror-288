ccb94b4e7736cfa0cd55dbae1246227ad7eff4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:05:17 +0000

    Add package tldraw/2.4.3/tldraw-2.4.3.tar.gz

---
+++ packages/tldraw/tldraw-2.4.3.tar.gz/tldraw-2.4.3/js/get_stroke.jsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import { useState } from "react";
+import { createRender, useModelState } from "@anywidget/react";
+
+import { Tldraw } from "@tldraw/tldraw";
+import "@tldraw/tldraw/tldraw.css";
+import "./widget.css";
+const render = createRender(() => {
+  const [length, setLength] = useModelState("length");
+  const [coord, setCoord] = useModelState("coord");
+
+  return (
+    <>
+      <div
+        style={{
+          position: "relative",
+          top: "0px",
+          fontSize: "40px",
+        }}
+      >
+        Length of currently drawn stroke: {length}
+      </div>
+      <div
+        style={{
+          position: "relative",
+          width: "500px",
+          height: "500px",
+        }}
+      >
+        <Tldraw autoFocus={false}
+          onMount={(editor) => {
+            editor.store.listen(() => {
+              if (editor.isIn("draw.drawing")) {
+                console.log("Drawing");
+
+                let ob = editor.getCurrentPageShapesSorted();
+                if (ob.length === 0) return;
+                let lastElement = ob[ob.length - 1];
+
+                if (
+                  lastElement.props.segments &&
+                  lastElement.props.segments[0].points
+                ) {
+                  setLength(lastElement.props.segments[0].points.length);
+
+                  let points = lastElement.props.segments[0].points;
+                  let transform = editor.getShapePageTransform(lastElement.id);
+
+                  // Create a new array to store transformed points
+                  let transformedPoints = [];
+
+                  // Apply the transformation to each point and add it to the transformedPoints array
+                  points.forEach((point) => {
+                    let pageSpacePoint = transform.applyToPoint(point);
+                    transformedPoints.push(pageSpacePoint);
+                  });
+
+                  console.log(transformedPoints);
+                  setCoord(transformedPoints);
+                }
+              }
+            });
+          }}
+        />
+      </div>
+    </>
+  );
+});
+export default { render };
