e30d5acd07adf263e76fc78058f274d5a43dc5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:05:03 +0000

    Add package unigen/1.4/unigen-1.4.tar.gz

---
+++ packages/unigen/unigen-1.4.tar.gz/unigen-1.4/tests/test_mp3.py
@@ -0,0 +1,15 @@
+from tests.test_unigen import IUnigenTester
+from tests.test_utils import get_test_file_path
+from unigen import AudioFactory
+from unigen.wrapper.audio_manager import IAudioManager
+
+
+class TestMp3(IUnigenTester):
+
+    @classmethod
+    def create_audio_manager(cls) -> IAudioManager:
+        return AudioFactory.buildAudioManager(get_test_file_path(cls.get_extension()))
+
+    @classmethod
+    def get_extension(cls):
+        return "mp3"
