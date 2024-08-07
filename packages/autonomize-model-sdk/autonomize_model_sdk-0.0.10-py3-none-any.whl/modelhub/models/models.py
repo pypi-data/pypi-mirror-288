f9a6c183cd352ed1bd0d74eb53708e0e0c43bd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 05:06:06 +0000

    Add package autonomize-model-sdk/0.0.10/autonomize_model_sdk-0.0.10-py3-none-any.whl

---
+++ packages/autonomize-model-sdk/autonomize_model_sdk-0.0.10-py3-none-any.whl/modelhub/models/models.py
@@ -0,0 +1,48 @@
+""" This module contains the Pydantic models for the ModelHub API. """
+from typing import List, Optional, Dict
+from pydantic import BaseModel
+
+
+class Stage(BaseModel):
+    """
+    Represents a stage in a pipeline.
+    """
+
+    name: str
+    type: str
+    params: Optional[dict] = None
+    depends_on: Optional[List[str]] = []
+    script: Optional[str] = None
+    requirements: Optional[str] = None
+    resources: Optional[Dict[str, str]] = None
+    tolerations: Optional[List[Dict[str, str]]] = None
+    node_selector: Optional[Dict[str, str]] = None
+
+
+class PipelineCreateRequest(BaseModel):
+    """
+    Represents a request to create a pipeline.
+    """
+
+    name: str
+    description: Optional[str] = None
+    experiment_id: str
+    dataset_id: str
+    image_tag: str
+    stages: List[Stage]
+
+
+class Pipeline(BaseModel):
+    """
+    Represents a pipeline.
+    """
+
+    pipeline_id: str
+    name: str
+    description: Optional[str] = None
+    experiment_id: str
+    dataset_id: str
+    image_tag: str
+    stages: List[Stage]
+    status: Optional[str] = None
+    workflows: Optional[List[dict]] = []
