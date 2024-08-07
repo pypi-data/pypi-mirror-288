393fde326d7c1a1c84f51822e89c1e479b53c8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 05:06:35 +0000

    Add package test-tunnel/0.1.1/test_tunnel-0.1.1-py3-none-any.whl

---
+++ packages/test-tunnel/test_tunnel-0.1.1-py3-none-any.whl/test_tunnel/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: Peter Pentchev <roam@ringlet.net>
+# SPDX-License-Identifier: BSD-2-Clause
+"""Top-level definitions for the tunnel testing library."""
+
+from __future__ import annotations
+
+import typing
+
+
+if typing.TYPE_CHECKING:
+    from typing import Final
+
+
+VERSION: Final = "0.1.1"
