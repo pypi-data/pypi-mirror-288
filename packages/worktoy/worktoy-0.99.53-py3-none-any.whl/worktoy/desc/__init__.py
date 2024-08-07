76618fa6843727212fc4390442708245e8eef36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:05:13 +0000

    Add package worktoy/0.99.53/worktoy-0.99.53-py3-none-any.whl

---
+++ packages/worktoy/worktoy-0.99.53-py3-none-any.whl/worktoy/desc/__init__.py
@@ -0,0 +1,18 @@
+"""The 'worktoy.desc' implements the descriptor protocol with lazy
+instantiation. """
+#  AGPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._flag import Flag
+from ._missing_value_exception import MissingValueException
+from ._core_descriptor import CoreDescriptor
+from ._abstract_descriptor import AbstractDescriptor
+from ._bag import Bag
+from ._zero_this import THIS
+from ._zero_type import TYPE
+from ._zero_box import BOX
+from ._zero_attr import ATTR
+from ._zero_default import DEFAULT
+from ._attri_box import AttriBox
+from ._field import Field
