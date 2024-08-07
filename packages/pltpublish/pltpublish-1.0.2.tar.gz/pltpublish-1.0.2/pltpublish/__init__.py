6ee4d0b1d29716d1324e12e14a8d6b11a624e6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:04:46 +0000

    Add package pltpublish/1.0.2/pltpublish-1.0.2.tar.gz

---
+++ packages/pltpublish/pltpublish-1.0.2.tar.gz/pltpublish-1.0.2/pltpublish/__init__.py
@@ -0,0 +1,11 @@
+from pltpublish.pltpublish import (
+    extract_legend_as_figure,
+    save_fig,
+    setup,
+    setup_colorblind,
+    setup_latex_fonts,
+    set_size_pixels,
+    layout_for_subplots,
+)
+
+__version__ = "1.0.2"
