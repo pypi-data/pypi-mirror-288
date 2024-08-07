685b5f5af744c146cb2ae6fbfb23034b490a932d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 05:06:18 +0000

    Add package ob-metaflow-stubs/5.0/ob-metaflow-stubs-5.0.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-5.0.tar.gz/ob-metaflow-stubs-5.0/metaflow-stubs/plugins/aws/step_functions/schedule_decorator.pyi
@@ -0,0 +1,17 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.10.2+ob(v1)                                                   #
+# Generated on 2024-08-06T17:10:53.192876                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.decorators
+
+class ScheduleDecorator(metaflow.decorators.FlowDecorator, metaclass=type):
+    def flow_init(self, flow, graph, environment, flow_datastore, metadata, logger, echo, options):
+        ...
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-5.0.tar.gz/ob-metaflow-stubs-5.0/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,14 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.12.10.2+ob(v1)                                                   #
+# Generated on 2024-08-06T17:10:53.115454                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
