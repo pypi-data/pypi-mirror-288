0b0a1d01c7569c26678a46eaa823e4439256c598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:04:29 +0000

    Add package atolibrary/0.0.5/atolibrary-0.0.5.tar.gz

---
+++ packages/atolibrary/atolibrary-0.0.5.tar.gz/atolibrary-0.0.5/atolibrary/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+from .image import * 
+from .lotto import *
+from .name import *
+
+__all__ = [
+    "show_image",
+    "get_one_number",
+    "get_name",
+]
+
+__version__ = '0.0.5'
---
+++ packages/atolibrary/atolibrary-0.0.5.tar.gz/atolibrary-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="atolibrary",
+    version="0.0.5",
+    author="ATO JEON",
+    author_email="atto.jeon@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'Pillow>=9.0.0',
+        'requests'
+    ],
+)
