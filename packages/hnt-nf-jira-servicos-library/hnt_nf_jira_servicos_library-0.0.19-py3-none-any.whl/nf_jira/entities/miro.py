62d28841916342b719f803ce0787fbf94073538c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 04:04:39 +0000

    Add package hnt-nf-jira-servicos-library/0.0.19/hnt_nf_jira_servicos_library-0.0.19-py3-none-any.whl

---
+++ packages/hnt-nf-jira-servicos-library/hnt_nf_jira_servicos_library-0.0.19-py3-none-any.whl/nf_jira/entities/miro.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from pydantic import BaseModel
+
+from .dados_basicos_miro import DadosBasicosMiro
+from .referencia_pedido import ReferenciaPedido
+from .detalhe import Detalhe
+
+class Miro(BaseModel):
+    dados_basicos: DadosBasicosMiro
+    referencia_pedido: Optional[ReferenciaPedido] = None
+    detalhe: Detalhe
