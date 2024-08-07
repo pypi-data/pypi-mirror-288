bff2cc0694d25ef33144faaab0aa6a2aa302280b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:05:19 +0000

    Add package gai-cli/0.183/gai-cli-0.183.tar.gz

---
+++ packages/gai-cli/gai-cli-0.183.tar.gz/gai-cli-0.183/src/gai/cli/scripts/gai_docker_start.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+def docker_start(here):
+    try:
+        docker_command = f"docker-compose -f scripts/docker-compose.yml up -d --force-recreate"
+        subprocess.run(docker_command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
