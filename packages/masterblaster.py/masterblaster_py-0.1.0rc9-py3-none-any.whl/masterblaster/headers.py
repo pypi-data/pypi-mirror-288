17c7e5fc388a418b2ce7acdc830e01e267a3aec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:04:52 +0000

    Add package masterblaster.py/0.1.0rc9/masterblaster_py-0.1.0rc9-py3-none-any.whl

---
+++ packages/masterblaster.py/masterblaster_py-0.1.0rc9-py3-none-any.whl/masterblaster/headers.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+import json
+from collections import abc
+from typing import Any, Iterator
+
+
+__all__ = [
+    "Header",
+]
+
+
+class Header(abc.MutableMapping):
+    """
+    Wrapper for http header-fields
+
+    :ivar fields: The header fields
+    """
+
+    def __init__(self) -> None:
+        self.fields: dict[str, Any] = {}
+
+    def add(self, name: str, value: str) -> Header:
+        """
+        Add a header field
+
+        Parameters
+        ----------
+        name : str
+            The name of the header field
+        value : str
+            The value of the header field
+
+        Returns
+        -------
+        Header
+            The header object
+        """
+        self.fields[name] = value
+        return self
+
+    def remove(self, name: str) -> Header:
+        """
+        Remove a header field
+
+        Parameters
+        ----------
+        name : str
+            The name of the header field
+
+        Returns
+        -------
+        Header
+            The header object
+        """
+        try:
+            del self.fields[name]
+        except KeyError:
+            pass
+        finally:
+            return self
+
+    def __getitem__(self, key: str) -> Any:
+        return self.fields[key]
+
+    def __getattr__(self, key: str) -> Any:
+        return self.fields[key]
+
+    def __setitem__(self, key: str, __v: Any) -> None:
+        self.fields[key] = __v
+
+    def __delitem__(self, key: str) -> None:
+        del self.fields[key]
+
+    def __len__(self) -> int:
+        return len(self.fields)
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self.fields)
+
+    def __str__(self) -> str:
+        return json.dumps(self.fields)
+
+    def __repr__(self) -> str:
+        return json.dumps(self.fields)
---
+++ packages/masterblaster.py/masterblaster_py-0.1.0rc9-py3-none-any.whl/masterblaster/member.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from dateutil import parser
+
+from .player import Player
+
+from typing import Optional
+
+__all__ = [
+    "Member",
+]
+
+
+class Member:
+    """
+    Class for organization members
+
+    :param player: The player object
+    :param email: The member email
+    :param name: The member name
+    :param playerId: The player id
+    :param role: The member role
+    :param addedAt: When the member was added
+    :param invitedAt: When the member was invited
+
+    :ivar player: The member's related player object
+    :ivar email: The member's email-address
+    :ivar name: The member's name
+    :ivar player_id: The member's player id
+    :ivar role: The member's role
+    :ivar added_at: When the member was added
+    :ivar invited_at: When the member was invited
+    """
+
+    def __init__(
+        self,
+        player: dict,
+        email: str,
+        name: str,
+        playerId: str,
+        role: int,
+        addedAt: str,
+        invitedAt: Optional[datetime],
+    ) -> None:
+        self.player: Player = Player(**player)
+        self.email: str = email
+        self.name: str = name
+        self.player_id: str = playerId
+        self.role: int = role
+        self.added_at: datetime = None if not addedAt else parser.isoparse(addedAt)
+        self.invited_at: Optional[datetime] = (
+            None if not invitedAt else parser.isoparse(invitedAt)
+        )
+
+    def __str__(self) -> str:
+        return f"{self.name}"
