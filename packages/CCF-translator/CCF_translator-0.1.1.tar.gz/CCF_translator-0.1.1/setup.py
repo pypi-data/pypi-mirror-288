d4bd7063ce961848858e62b3f136ecd15337596c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 05:05:33 +0000

    Add package CCF-translator/0.1.1/CCF_translator-0.1.1.tar.gz

---
+++ packages/CCF-translator/CCF_translator-0.1.1.tar.gz/CCF_translator-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CCF_translator",
+    version='0.1.1',
+    packages=find_packages(),
+    license='MIT',
+    description='a package to translate data between common coordinate templates'
+)
