07a97e65ff0c613816976774aa169880e27ea9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 03:04:38 +0000

    Add package graphrag-for-all/0.1.4/graphrag_for_all-0.1.4.tar.gz

---
+++ packages/graphrag-for-all/graphrag_for_all-0.1.4.tar.gz/graphrag_for_all-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# python setup.py sdist bdist_wheel
+# twine upload --skip-existing dist/*
+
+
+setup(
+    name="graphrag_for_all",
+    version="0.1.4",
+    description="Graphrag and vectorstore for all LLMs",
+    url="https://github.com/ChihchengHsieh/rag-aug",
+    author="Chihcheng Hsieh",
+    author_email="chihcheng.hsieh.82@gmail.com",
+    license="MIT",
+    # packages=["graphrag_for_all"],
+    packages=find_packages(),
+    zip_safe=False,
+)
