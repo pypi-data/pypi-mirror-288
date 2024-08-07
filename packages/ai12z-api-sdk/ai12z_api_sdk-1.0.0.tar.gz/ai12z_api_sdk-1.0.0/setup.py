cef698b13979860d2b4805c5c7419f9da7b4edcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-07 05:08:02 +0000

    Add package ai12z-api-sdk/1.0.0/ai12z_api_sdk-1.0.0.tar.gz

---
+++ packages/ai12z-api-sdk/ai12z_api_sdk-1.0.0.tar.gz/ai12z_api_sdk-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai12z_api_sdk',
+    version='1.0.0',
+    # author='your-name',
+    # author_email='your.email@example.com',
+    # description='Description of your package',
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    # url='https://github.com/yourusername/your-package-name',
+    packages=find_packages(),
+    install_requires=[
+        # 'requests',  
+    ],
+    # classifiers=[
+    #     'Programming Language :: Python :: 3',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent',
+    # ],
+)
